cd5e11e4de84cefa87dacd89bfc3cff4f472ee12
Max Shytikov <mshytikov@gmail.com>
2014-06-03 15:19:40 +0200

    added CommandStreamCOnsumer

---
+++ sonar-python-plugin/src/main/java/org/sonar/plugins/python/flake8/CommandStreamConsumer.java
@@ -0,0 +1,37 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011 SonarSource and Waleri Enns
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.python.flake8;
+
+import org.sonar.api.utils.command.StreamConsumer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class CommandStreamConsumer implements StreamConsumer {
+  private List<String> data = new LinkedList<String>();
+
+  public void consumeLine(String line) {
+    data.add(line);
+  }
+
+  public List<String> getData() {
+    return data;
+  }
+}
