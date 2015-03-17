410a6dd9c8217afca0b1f6f4bce9186d6335afef
Elena Vilchik <elena.vilchik@sonarsource.com>
2015-03-17 13:40:41 +0100

    SONARPY-18 Fix generation of several issues for one line

--- python-checks/src/main/java/org/sonar/python/checks/BreakContinueOutsideLoopCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/BreakContinueOutsideLoopCheck.java
@@ -56,6 +56,7 @@ public class BreakContinueOutsideLoopCheck extends SquidCheck<Grammar> {
         return;
       } else if (currentParent.is(PythonGrammar.FUNCDEF, PythonGrammar.CLASSDEF)){
         raiseIssue(node);
+        return;
       }
       currentParent = currentParent.getParent();
     }
