68cf0f0031d98aa3c97413bdc6adbca1d2d27bd7
Elena Vilchik <vilchik.elena@gmail.com>
2016-07-05 15:41:02 +0200

    SONARPY-165 Drop commons rules (#36)

--- sonar-python-plugin/src/test/java/org/sonar/plugins/python/PythonPluginTest.java
+++ sonar-python-plugin/src/test/java/org/sonar/plugins/python/PythonPluginTest.java
@@ -27,7 +27,7 @@ public class PythonPluginTest {
 
   @Test
   public void testGetExtensions() {
-    assertThat(new PythonPlugin().getExtensions()).hasSize(12);
+    assertThat(new PythonPlugin().getExtensions()).hasSize(11);
   }
 
 }
