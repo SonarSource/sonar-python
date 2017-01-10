a1edf998db7d2fd41f72579c59f13342c6075dfb
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2017-01-10 11:43:36 +0100

    SONARPY-180 Improve unit test

--- python-squid/src/test/resources/metrics/complexity.py
+++ python-squid/src/test/resources/metrics/complexity.py
@@ -1,17 +1,20 @@
 def hello(): # +1 function
-    if expression: # +1 if-statement
-        pass
     while expression: # +1 while-statement
         pass
     for i in [0,2]: # +1 for-statement
+        if expression: # +1 if-statement
+            continue # +0
+        else:
+            break # +0
         pass
-    return # +0 return-statement
+    return # +0
 
 expression = a or b  # +1
 expression = a and b # +1
 expression = a if b else c # +1
+expression = lambda x: x*2 # +0
 
 try:
-    raise "message" # +0 raise-statement
-except: # +0 except-clause
+    raise "message" # +0
+except: # +0
     pass
