b6c2dfc3c3840b0d38cab7e24321363170dff599
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2017-05-05 15:21:17 +0200

    SONARPY-192 Load default profile fron JSON file

---
+++ sonar-python-plugin/src/test/java/org/sonar/plugins/python/PythonProfileTest.java
@@ -0,0 +1,63 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.python;
+
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.sonar.api.profiles.RulesProfile;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.RuleFinder;
+import org.sonar.api.utils.ValidationMessages;
+import org.sonar.python.checks.CheckList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PythonProfileTest {
+
+  @Test
+  public void should_create_sonar_way_profile() {
+    ValidationMessages validation = ValidationMessages.create();
+
+    RuleFinder ruleFinder = ruleFinder();
+    PythonProfile definition = new PythonProfile(ruleFinder);
+    RulesProfile profile = definition.createProfile(validation);
+
+    assertThat(profile.getLanguage()).isEqualTo(Python.KEY);
+    assertThat(profile.getName()).isEqualTo(CheckList.SONAR_WAY_PROFILE);
+    assertThat(profile.getActiveRules()).extracting("repositoryKey").containsOnly(CheckList.REPOSITORY_KEY);
+    assertThat(validation.hasErrors()).isFalse();
+    assertThat(profile.getActiveRules().size()).isGreaterThan(25);
+  }
+
+  static RuleFinder ruleFinder() {
+    return when(mock(RuleFinder.class).findByKey(anyString(), anyString())).thenAnswer(new Answer<Rule>() {
+      @Override
+      public Rule answer(InvocationOnMock invocation) {
+        Object[] arguments = invocation.getArguments();
+        return Rule.create((String) arguments[0], (String) arguments[1], (String) arguments[1]);
+      }
+    }).getMock();
+  }
+
+}
