d0846b86980422e51285d41c42496d82f6749894
David Rautureau <david.rautureau@sonarsource.com>
2017-10-09 20:50:48 +0200

    WIP Cancel branch build with PR

--- travis.sh
+++ travis.sh
@@ -4,11 +4,16 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v35 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
 installTravisTools
+
+# When pull request exists on the branch, then the job related to the branch does not need
+# to be executed and should be canceled. It does not book slaves for nothing.
+# @TravisCI please provide the feature natively, like at AppVeyor or CircleCI ;-)
+cancel_branch_build_with_pr
 
 case "$TEST" in
 
