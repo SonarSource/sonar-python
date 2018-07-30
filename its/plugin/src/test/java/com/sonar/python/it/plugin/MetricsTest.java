147424b14c66d69f6cde2ec0936a6bb59c7043b5
pynicolas <pierre-yves.nicolas@sonarsource.com>
2018-07-30 17:45:52 +0200

    ITs: stop testing comment_lines_data (#157)

--- its/plugin/src/test/java/com/sonar/python/it/plugin/MetricsTest.java
+++ its/plugin/src/test/java/com/sonar/python/it/plugin/MetricsTest.java
@@ -147,9 +147,6 @@ public class MetricsTest {
     assertThat(getFileMeasure("ncloc_data").getValue())
       .doesNotContain("1=1")
       .contains("5=1");
-    assertThat(getFileMeasure("comment_lines_data").getValue())
-      .contains("2=1")
-      .doesNotContain("4=1");
     assertThat(getFileMeasure("executable_lines_data").getValue())
       .doesNotContain("1=1")
       .contains("5=1");
