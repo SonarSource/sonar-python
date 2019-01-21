0e4a65a751f2be682e7d0afb9b3f8acf046d226f
Alban Auzeill <alban.auzeill@sonarsource.com>
2019-01-21 16:29:35 +0100

    SONARPY-286 S1313 do not report issue for loopback address

--- python-checks/src/test/resources/checks/hardcodedIP.py
+++ python-checks/src/test/resources/checks/hardcodedIP.py
@@ -1,37 +1,83 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', 50007))  # Noncompliant {{Make this IP "127.0.0.1" address configurable.}}
-#       ^^^^^^^^^^^
+"120"
+ip = "1.2.3.4" # Noncompliant {{Make sure using this hardcoded IP address "1.2.3.4" is safe here.}}
+#    ^^^^^^^^^
+"1.2.3.4" # Noncompliant
+"1.2.3.4:80" # Noncompliant
+"1.2.3.4:8080" # Noncompliant
+"1.2.3.4:a"
+"1.2.3.4.5"
 
-address = 'http://123.1.1.1' # Noncompliant
+url = "http://192.168.0.1/admin.html"  # Noncompliant {{Make sure using this hardcoded IP address "192.168.0.1" is safe here.}}
+#     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+url = "http://192.168.0.1:8181/admin.html" # Noncompliant
+url = 'http://123.1.1.1' # Noncompliant
+url = "http://www.example.org"
 
-address2 = 'https://255.1.1.1/some/path' # Noncompliant
-#          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+notAnIp1 = "0.0.0.1234"
+notAnIp2 = "1234.0.0.0"
+notAnIp3 = "1234.0.0.0.0.1234"
+notAnIp4 = ".0.0.0.0"
+notAnIp5 = "0.256.0.0"
 
-str = "String 123.111.1.1 is IPv4"       # Noncompliant
-str = "String 123.0111.1.1 is not IPv4"
-str = "String 123.111.1.1000 is not IPv4"
-str = "String 0123.111.1.1 is not IPv4"
-str = "String 294.125.1.1 is not IPv4"
+fileName = "v0.0.1.200__do_something.sql" # Compliant - suffixed and prefixed
+version = "1.0.0.0-1" # Compliant - suffixed
 
+"1080:0:0:0:8:800:200C:417A" # Noncompliant {{Make sure using this hardcoded IP address "1080:0:0:0:8:800:200C:417A" is safe here.}}
+"[1080::8:800:200C:417A]" # Noncompliant
+"::800:200C:417A" # Noncompliant
+"1080:800:200C::" # Noncompliant
+"::FFFF:129.144.52.38" # Noncompliant
+"::129.144.52.38" # Noncompliant
+"::FFFF:38" # Noncompliant
+"::100" # Noncompliant
+"1080:0:0:0:8:200C:131.107.129.8" # Noncompliant
+"1080:0:0::8:200C:131.107.129.8" # Noncompliant
+
+"1080:0:0:0:8:800:200C:417G" # Compliant - not valid IPv6
+"1080:0:0:0:8::800:200C:417A" # Compliant - not valid IPv6
+"1080:0:0:0:8:::200C:417A" # Compliant - not valid IPv6
+"1080:0:0:0:8" # Compliant - not valid IPv6
+"1080:0::0:0:8::200C:417A" # Compliant - not valid IPv6
+"1080:0:0:0:8::200C:417A:" # Compliant - not valid IPv6
+"1080:0:0:0:8::200C:131.107.129.8" # Compliant - not valid IPv6
+"1080:0:0:0:8::200C:256.256.129.8" # Compliant - not valid IPv6
+"1080:0:0:0:8:200C:200C:131.107.129.8" # Compliant - not valid IPv6
+"1080:0:0:0:8:131.107.129.8" # Compliant - not valid IPv6
+"1080:0::0::8:200C:131.107.129.8" # Compliant - not valid IPv6
+"1080:0:0:0:8:200C:131.107.129" # Compliant - not valid IPv6
+"1080:0:0:0:8:200C:417A:131.107" # Compliant - not valid IPv6
+
+"http://[2001:db8:1f70::999:de8:7648:6e8]" # Noncompliant {{Make sure using this hardcoded IP address "2001:db8:1f70::999:de8:7648:6e8" is safe here.}}
+"http://[2001:db8:1f70::999:de8:7648:6e8]:100/" # Noncompliant
+url = "https://[3FFE:1A05:510:1111:0:5EFE:131.107.129.8]:8080/" # Noncompliant
+#     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+"https://[3FFE::1111:0:5EFE:131.107.129.8]:8080/" # Noncompliant
+
+"ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff" # Noncompliant
+
+# Exceptions
+"0.0.0.0"
+"::1"
+"000:00::1"
+'127.0.0.1'
+"255.255.255.255"
+"255.255.255.255:80"
+"2.5.255.255"
+"127.5.255.255"
+"http://[::0]:100/"
+"0000:0000:0000:0000:0000:0000:0000:0000"
+''
 '''http://123.1.1.1'''  # multiline strings are not considered
 
-message = '''123.1.1.1
+'''123.1.1.1
 some long message with ip address inside is compliant'''
 
-FASTCGI_HELP = r""" host=127.0.0.1 long strings with string prefix are also recognized as multiline strings and ignored by this rule"""
+r""" host=127.0.0.1 long strings with string prefix are also recognized as multiline strings and ignored by this rule"""
 
-str = '2001:0db8:11a3:09d7:1f34:8a2e:07a0:765d' # Noncompliant
-str = '::1f34:8a2e:07a0:765d' # Noncompliant
-str = '1f34:8a2e:07a0:765d::' # Noncompliant
-str = '1f34:2e:7a0:765d::'    # Noncompliant {{Make this IP "1f34:2e:7a0:765d::" address configurable.}}
-str = '1f34:2e:7a0:765d0::'   # not an IPv6, as one token contains 5 characters
-str = '1f34:2e:7a0::765dA'    # not an IPv6, as one token contains 5 characters
-str = '01f34:2e:7a0:765d::'   # not an IPv6, as one token contains 5 characters
-
-str = 'time is 13:40:40'
-str = '1.2.3.4'               # Noncompliant
-str = 'http://[1080:0:0:0:8:800:200C:417A]:8888/index.html'    # Noncompliant {{Make this IP "1080:0:0:0:8:800:200C:417A" address configurable.}}
-#     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+'time is 13:40:40'
 
 passwd = 'root:aaa:16484:0:99999:7:::'  # OK, not an IP address (ref: SONARPY-196)
+
+# SONARPY-270 should not raise issue on more than 4 IPV4 elements
+SYS_OBJECT_ID = '1.3.6.1.2.1.1.2.0'
