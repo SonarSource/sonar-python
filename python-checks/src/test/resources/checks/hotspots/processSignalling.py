edef0b5cd82178d40df15175a7a38216c94b635d
Andrea Guarino <38876543+andrea-guarino-sonarsource@users.noreply.github.com>
2019-02-26 13:48:47 +0100

    Rule S4828: Signalling processes is security-sensitive (#208)

---
+++ python-checks/src/test/resources/checks/hotspots/processSignalling.py
@@ -0,0 +1,15 @@
+import os
+from os import (kill, killpg)
+import my_os
+
+def send_signal(pid, sig, pgid):
+    os.kill(pid, sig)  # Noncompliant {{Make sure that sending signals is safe here.}}
+#   ^^^^^^^^^^^^^^^^^
+    os.killpg(pgid, sig)  # Noncompliant
+
+    kill(pid, sig) # Noncompliant {{Make sure that sending signals is safe here.}}
+#   ^^^^^^^^^^^^^^
+    killpg() # Noncompliant
+
+    os.getcwd() # OK
+    my_os.kill() # OK
