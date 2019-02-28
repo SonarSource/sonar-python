2d5939c1c1f5369721a29d5eef736e9c007e76aa
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-02-28 13:56:31 +0100

    SONARPY-312 Rule S4529: Exposing HTTP endpoints is security-sensitive

--- sonar-python-plugin/src/test/java/org/sonar/plugins/python/PythonRuleRepositoryTest.java
+++ sonar-python-plugin/src/test/java/org/sonar/plugins/python/PythonRuleRepositoryTest.java
@@ -40,7 +40,7 @@ public class PythonRuleRepositoryTest {
 
     List<RulesDefinition.Rule> rules = repository.rules();
     assertThat(rules).isNotNull();
-    assertThat(rules).hasSize(58);
+    assertThat(rules).hasSize(59);
 
     RulesDefinition.Rule s1578 = repository.rule("S1578");
     assertThat(s1578).isNotNull();
