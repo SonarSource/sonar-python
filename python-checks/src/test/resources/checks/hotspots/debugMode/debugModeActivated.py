1d9ad884ae9fe7a6fca9636b1df30c08a83e1edd
Andrea Guarino <andrea.guarino@sonarsource.com>
2019-03-07 15:09:18 +0100

    adding more tests

--- python-checks/src/test/resources/checks/hotspots/debugMode/debugModeActivated.py
+++ python-checks/src/test/resources/checks/hotspots/debugMode/debugModeActivated.py
@@ -10,3 +10,6 @@ settings.configure(DEBUG_PROPAGATE_EXCEPTIONS=True)  # Noncompliant {{Make sure 
 
 def custom_config(config):
     settings.configure(default_settings=config, DEBUG=True)  # Noncompliant
+
+settings.configure(DEBUG=False) # OK
+settings.configure(OTHER=False) # OK
--- python-checks/src/test/resources/checks/hotspots/debugMode/global_settings.py
+++ python-checks/src/test/resources/checks/hotspots/debugMode/global_settings.py
@@ -1,2 +1,4 @@
 DEBUG = True  # Noncompliant
 DEBUG_PROPAGATE_EXCEPTIONS = True  # Noncompliant
+DEBUG = False  # OK
+Other = True
