82f44577732ebd387f2baf5d60f6f163d3a35df3
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-04-18 16:36:16 +0200

    Use JDK 11 without setting release flag

--- travis.sh
+++ travis.sh
@@ -4,13 +4,11 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v55 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v56 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
 installTravisTools
-source ~/.local/bin/installMaven35
-source ~/.local/bin/installJDK8
 
 export DEPLOY_PULL_REQUEST=true
 
