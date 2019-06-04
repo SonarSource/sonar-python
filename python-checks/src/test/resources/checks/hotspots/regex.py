ac330eb83d4424bdd24d6c491d65b079ceee078e
Andrea Guarino <andrea.guarino@sonarsource.com>
2019-06-04 15:43:09 +0200

    SONARPY-336 Rule S4784: Update implementation to target only hard-coded regex

--- python-checks/src/test/resources/checks/hotspots/regex.py
+++ python-checks/src/test/resources/checks/hotspots/regex.py
@@ -2,28 +2,42 @@ from django.core.validators import RegexValidator
 from django.urls import re_path
 
 def build_validator(regex):
-    RegexValidator(regex)  # Noncompliant {{Make sure that using a regular expression is safe here.}}
-#   ^^^^^^^^^^^^^^^^^^^^^
+    RegexValidator(regex)  # OK
 
-RegexValidator('(a*)*')  # Noncompliant
+RegexValidator()
+RegexValidator('')
+RegexValidator('a+')
+RegexValidator('a*')
+RegexValidator('a{1}')
+RegexValidator('(a*)*')  # Noncompliant {{Make sure that using a regular expression is safe here.}}
+#              ^^^^^^^
+RegexValidator('(a{1})*') # Noncompliant
+RegexValidator('((a+)*') # Noncompliant
+RegexValidator('(a{1})+') # Noncompliant
+RegexValidator('(a+)+') # Noncompliant
+RegexValidator('(a{1}){2}') # Noncompliant
+
+
 
 def define_http_endpoint(path, view):
-    re_path(path, view)  # Noncompliant
-#   ^^^^^^^^^^^^^^^^^^^
+    re_path(path, view)  # OK
+    regexp = '(a*)*'
+    RegexValidator(regexp) # Noncompliant [[secondary=-1]]
+#                  ^^^^^^
 
 import re
 from re import compile
 
-def dynamic_pattern(pattern):
+def dynamic_pattern():
+    pattern = '(a*)*'
     re.compile(pattern)  # Noncompliant
     compile(pattern) # Noncompliant
-    regexpressions.map(compile) # Noncompliant
-#                      ^^^^^^^
+    regexpressions.map(compile) # OK
 
 import regex
 from regex import subf
 
 
 def dynamic_pattern(pattern):
-    regex.subf(pattern, replacement, input)  # Noncompliant
-    subf(pattern, replacement, input)  # Noncompliant
+    regex.subf(pattern, replacement, input)  # OK
+    subf(pattern, replacement, input)  # OK
