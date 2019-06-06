62d97ae7541f321fe64089f6a153965c2f49266a
Andrea Guarino <38876543+andrea-guarino-sonarsource@users.noreply.github.com>
2019-06-06 09:47:56 +0200

    SONARPY-336 Rule S4784: Update implementation to target only hard-coded regex (#232)

--- python-checks/src/main/java/org/sonar/python/checks/hotspots/RegexCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/hotspots/RegexCheck.java
@@ -22,15 +22,19 @@ package org.sonar.python.checks.hotspots;
 import com.sonar.sslr.api.AstNode;
 import com.sonar.sslr.api.AstNodeType;
 import java.util.Set;
+import javax.annotation.CheckForNull;
 import org.sonar.check.Rule;
+import org.sonar.python.IssueLocation;
 import org.sonar.python.PythonCheck;
 import org.sonar.python.api.PythonGrammar;
+import org.sonar.python.api.PythonPunctuator;
 import org.sonar.python.semantic.Symbol;
 
 @Rule(key = RegexCheck.CHECK_KEY)
 public class RegexCheck extends PythonCheck {
   public static final String CHECK_KEY = "S4784";
   private static final String MESSAGE = "Make sure that using a regular expression is safe here.";
+  private static final int REGEX_ARGUMENT = 0;
   private static final Set<String> questionableFunctions = immutableSet(
     "django.core.validators.RegexValidator", "django.urls.re_path",
     "re.compile", "re.match", "re.search", "re.fullmatch", "re.split", "re.findall", "re.finditer", "re.sub", "re.subn",
@@ -48,10 +52,54 @@ public class RegexCheck extends PythonCheck {
     if (symbol != null && questionableFunctions.contains(symbol.qualifiedName())) {
       AstNode parent = node.getParent();
       if (parent != null && parent.is(PythonGrammar.CALL_EXPR)) {
-        addIssue(parent, MESSAGE);
-      } else {
-        addIssue(node, MESSAGE);
+        AstNode argListNode = parent.getFirstChild(PythonGrammar.ARGLIST);
+        if (argListNode != null) {
+          // ARGLIST is not null => we have at least one child
+          checkRegexArgument(argListNode.getChildren().get(REGEX_ARGUMENT));
+        }
       }
     }
+  }
+
+  private void checkRegexArgument(AstNode arg) {
+    AstNode atom = arg.getFirstDescendant(PythonGrammar.ATOM);
+    if (atom == null) {
+      return;
+    }
+    String literal = atom.getTokenValue();
+    Symbol argSymbol = getContext().symbolTable().getSymbol(atom);
+    IssueLocation secondaryLocation = null;
+    if (argSymbol != null && argSymbol.writeUsages().size() == 1) {
+      AstNode expressionStatement = argSymbol.writeUsages().iterator().next().getFirstAncestor(PythonGrammar.EXPRESSION_STMT);
+      if (isAssignment(expressionStatement)) {
+        AstNode expression = expressionStatement.getChildren().get(2);
+        literal = expression.getTokenValue();
+        secondaryLocation = IssueLocation.preciseLocation(expression, "");
+      }
+    }
+    if (isSuspiciousRegex(literal)) {
+      PreciseIssue preciseIssue = addIssue(atom, MESSAGE);
+      if (secondaryLocation != null) {
+        preciseIssue.secondary(secondaryLocation);
+      }
+    }
+  }
+
+  private static boolean isAssignment(@CheckForNull AstNode expressionStatement) {
+    return expressionStatement != null &&
+      expressionStatement.getChildren().size() == 3 &&
+      expressionStatement.getChildren().get(1).is(PythonPunctuator.ASSIGN);
+  }
+
+  /**
+   * This rule flags any execution of a hardcoded regular expression which has at least 3 characters and at least
+   * two instances of any of the following characters: "*+{" (Example: (a+)*)
+   */
+  private static boolean isSuspiciousRegex(String regexp) {
+    if (regexp.length() > 2) {
+      int nOfSuspiciousChars = regexp.length() - regexp.replaceAll("[*+{]", "").length();
+      return nOfSuspiciousChars > 1;
+    }
+    return false;
   }
 }
