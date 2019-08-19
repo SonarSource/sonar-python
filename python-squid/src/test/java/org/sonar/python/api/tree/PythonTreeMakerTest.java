0cc31843c8e6a953b46f7964923d559ee8d52502
Andrea Guarino <andrea.guarino@sonarsource.com>
2019-08-19 16:08:07 +0200

    Strongly typed AST: PyFileInputTree

---
+++ python-squid/src/test/java/org/sonar/python/api/tree/PythonTreeMakerTest.java
@@ -0,0 +1,37 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.api.tree;
+
+import com.sonar.sslr.api.AstNode;
+import org.junit.Test;
+import org.sonar.python.parser.RuleTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PythonTreeMakerTest extends RuleTest {
+
+  @Test
+  public void fileInputTree() {
+    AstNode astNode = p.parse("");
+    PyTree pyTree = PythonTreeMaker.convert(astNode);
+    assertThat(pyTree).isInstanceOf(PyFileInputTree.class);
+  }
+
+}
