d3aad16a7c48cd045343110028b47144b68a6ce0
Andrea Guarino <38876543+andrea-guarino-sonarsource@users.noreply.github.com>
2019-09-03 13:44:10 +0200

    SONARPY-376 Rule S5443: Using publicly writable directories is securi… (#284)

---
+++ python-checks/src/main/java/org/sonar/python/checks/hotspots/PubliclyWritableDirectoriesCheck.java
@@ -0,0 +1,116 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks.hotspots;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
+import org.sonar.check.Rule;
+import org.sonar.python.PythonSubscriptionCheck;
+import org.sonar.python.api.tree.PyArgumentTree;
+import org.sonar.python.api.tree.PyCallExpressionTree;
+import org.sonar.python.api.tree.PyExpressionTree;
+import org.sonar.python.api.tree.PyNameTree;
+import org.sonar.python.api.tree.PyQualifiedExpressionTree;
+import org.sonar.python.api.tree.PyStringLiteralTree;
+import org.sonar.python.api.tree.PySubscriptionExpressionTree;
+import org.sonar.python.api.tree.Tree.Kind;
+import org.sonar.python.semantic.Symbol;
+import org.sonar.python.semantic.SymbolTable;
+
+@Rule(key = "S5443")
+public class PubliclyWritableDirectoriesCheck extends PythonSubscriptionCheck {
+
+  private static final String MESSAGE = "Make sure publicly writable directories are used safely here.";
+  private static final List<String> UNIX_WRITABLE_DIRECTORIES = Arrays.asList(
+    "/tmp/", "/var/tmp/", "/usr/tmp/", "/dev/shm/", "/dev/mqueue/", "/run/lock/", "/var/run/lock/",
+    "/library/caches/", "/users/shared/", "/private/tmp/", "/private/var/tmp/");
+  private static final List<String> NONCOMPLIANT_ENVIRON_VARIABLES = Arrays.asList("tmpdir", "tmp");
+
+  private static final Pattern WINDOWS_WRITABLE_DIRECTORIES = Pattern.compile("[^\\\\]*\\\\(Windows\\\\Temp|Temp|TMP)(\\\\.*|$)", Pattern.CASE_INSENSITIVE);
+
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Kind.STRING_LITERAL, ctx -> {
+      PyStringLiteralTree tree = (PyStringLiteralTree) ctx.syntaxNode();
+      String stringLiteral = tree.trimmedQuotesValue().toLowerCase(Locale.ENGLISH);
+      if (UNIX_WRITABLE_DIRECTORIES.stream().anyMatch(dir -> containsDirectory(stringLiteral, dir)) ||
+        WINDOWS_WRITABLE_DIRECTORIES.matcher(stringLiteral).matches()) {
+        ctx.addIssue(tree, MESSAGE);
+      }
+    });
+
+    context.registerSyntaxNodeConsumer(Kind.CALL_EXPR, ctx -> {
+      PyCallExpressionTree tree = (PyCallExpressionTree) ctx.syntaxNode();
+      if (isOsEnvironGetter(tree.callee(), ctx.symbolTable()) &&
+        tree.arguments().stream().map(PyArgumentTree::expression)
+          .anyMatch(PubliclyWritableDirectoriesCheck::isNonCompliantOsEnvironArgument)) {
+        ctx.addIssue(tree, MESSAGE);
+      }
+    });
+
+    context.registerSyntaxNodeConsumer(Kind.SUBSCRIPTION, ctx -> {
+      PySubscriptionExpressionTree tree = (PySubscriptionExpressionTree) ctx.syntaxNode();
+      if (isOsEnvironQualifiedExpression(tree.object(), ctx.symbolTable()) && tree.subscripts().expressions().stream()
+          .anyMatch(PubliclyWritableDirectoriesCheck::isNonCompliantOsEnvironArgument)) {
+        ctx.addIssue(tree, MESSAGE);
+      }
+    });
+
+  }
+
+  private static boolean containsDirectory(String stringLiteral, String dir) {
+    return stringLiteral.startsWith(dir) || stringLiteral.equals(dir.substring(0, dir.length() - 1));
+  }
+
+  private static boolean isNonCompliantOsEnvironArgument(PyExpressionTree expression) {
+    return expression.is(Kind.STRING_LITERAL) &&
+      NONCOMPLIANT_ENVIRON_VARIABLES.contains(((PyStringLiteralTree) expression).trimmedQuotesValue().toLowerCase(Locale.ENGLISH));
+  }
+
+  // Could be either `os.environ.get` or `environ.get`
+  private static boolean isOsEnvironGetter(PyExpressionTree expression, SymbolTable symbolTable) {
+    if (!expression.is(Kind.QUALIFIED_EXPR)) {
+      return false;
+    }
+    PyQualifiedExpressionTree qualifiedExpression = (PyQualifiedExpressionTree) expression;
+    if (qualifiedExpression.name().name().equals("get")) {
+      return isOsEnvironQualifiedExpression(qualifiedExpression.qualifier(), symbolTable);
+    }
+    return false;
+  }
+
+  // Could be either `os.environ` or `from os import environ; ... ; environ`
+  private static boolean isOsEnvironQualifiedExpression(PyExpressionTree expression, SymbolTable symbolTable) {
+    Symbol symbol = symbolTable.getSymbol(expression);
+    if (symbol != null) {
+      return symbol.qualifiedName().equals("os.environ");
+    }
+    if (!expression.is(Kind.QUALIFIED_EXPR)) {
+      return false;
+    }
+    PyQualifiedExpressionTree qualifiedExpression = (PyQualifiedExpressionTree) expression;
+    if (qualifiedExpression.qualifier().is(Kind.NAME)) {
+      return ((PyNameTree) qualifiedExpression.qualifier()).name().equals("os") && qualifiedExpression.name().name().equals("environ");
+    }
+    return false;
+  }
+}
