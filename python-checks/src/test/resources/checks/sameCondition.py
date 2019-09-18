73e8a93dc31a8a638c9ec4a3d4fb7016f44d12c0
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2019-09-18 11:26:11 +0200

    Migrate SameConditionCheck to Strongly typed AST

--- python-checks/src/test/resources/checks/sameCondition.py
+++ python-checks/src/test/resources/checks/sameCondition.py
@@ -46,3 +46,20 @@ elif param == 2:
 elif param == 3:
     if param > 0:  # Compliant
         print(2)
+
+if foo is None and bar is None:
+    print(1)
+elif foo is not None and bar is None:
+    print(2)
+elif foo is not None and bar is not None:
+    print(3)
+
+if foo() < 1:
+    print(1)
+elif foo() > 1:
+    print(2)
+
+if foo() == 1:
+    print(3)
+elif foo() > 1:
+    print(4)
