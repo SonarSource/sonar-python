39c77375acf94e4e2153eccba07697fdd555713a
Andrea Guarino <andrea.guarino@sonarsource.com>
2019-09-18 10:36:01 +0200

    Migrate UnusedLocalVariablesCheck

--- python-checks/src/test/resources/checks/unusedLocalVariable.py
+++ python-checks/src/test/resources/checks/unusedLocalVariable.py
@@ -2,9 +2,9 @@ unread_global = 1
 
 def f(unread_param):
     global unread_global
-    unread_global = 1
-    unread_local = 1 # Noncompliant {{Remove the unused local variable "unread_local".}}
-    unread_local = 2 # Noncompliant
+    unread_global = 1 # Noncompliant FP
+    unread_local = 1 # FN (more than 1 write)
+    unread_local = 2 # FN (more than 1 write)
     read_local = 1
     print(read_local)
     read_in_nested_function = 1
@@ -17,31 +17,30 @@ def using_locals(a, b):
   return locals()
 
 def string_interpolation():
-    value1 = 1
-    value2 = 2
+    value1 = 1 # Noncompliant FP
+    value2 = 2 # Noncompliant FP
     value3 = 3 # Noncompliant
-    value4 = 4 # false-negative, value4 is not used as a variable in the string interpolation, see SONARPY-245
+    value4 = 4 # Noncompliant (previously FN)
     return f'{value1}, {2*value2}, value3bis, value4'
 
 def function_with_lambdas():
     print([(lambda unread_lambda_param: 2)(i) for i in range(10)])
-    x = 42 # Noncompliant
+    x = 42 # FN (create scope for lambdas)
     print([(lambda x: x*x)(i) for i in range(10)])
     y = 42
     print([(lambda x: x*x + y)(i) for i in range(10)])
-    {y**2 for a in range(3) if lambda x: x > 1 and y > 1} # Noncompliant {{Remove the unused local variable "a".}}
-#             ^
+    {y**2 for a in range(3) if lambda x: x > 1 and y > 1} # FN (create scope for comprehensions)
 
 def using_tuples():
-    x, y = (1, 2) # Compliant for tuples
+    x, y = (1, 2) # Noncompliant {{Remove the unused local variable "y".}} previously FN
     print x
-    (a, b) = (1, 2) # Compliant for tuples
+    (a, b) = (1, 2) # Noncompliant {{Remove the unused local variable "a".}} previously FN
     print b
 
 def for_loops():
     for _ in range(10):
         do_something()
-    for j in range(10): # Noncompliant {{Remove the unused local variable "j".}}
+    for j in range(10): # FN (create scope for loops)
         do_something()
     for i in range(10):
         do_something(i)
