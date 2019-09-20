ec11e96ffee8de3cf7bc5de26ee8d83aa2afac2b
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-09-20 14:52:23 +0200

    Migrate nested control flow to strongly typed AST

--- python-checks/src/test/resources/checks/nestedControlFlowDepth.py
+++ python-checks/src/test/resources/checks/nestedControlFlowDepth.py
@@ -11,3 +11,34 @@ for x in range(10):
 #               ^^
                     if y > 10:
                         pass
+
+def fun():
+  if cond1:
+      for user in results:
+          if user:
+              x = "foo"
+  elif cond2:
+      for user in results:
+          if user:
+              for key in user:
+                  if lastCond: # Noncompliant
+                     pass
+              x ="bar"
+          else:
+              x = "qix"
+  return x
+
+for y in range(10):
+    if x > y:
+        if x > 1:
+            pass
+
+for x in range(10):
+    try:
+        for y in range(10):
+            if x > y:
+              async with x.bar() as foo:   # Noncompliant
+#                   ^^^^
+                pass
+    except Error:
+        pass
