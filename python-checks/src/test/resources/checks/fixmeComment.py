7dd7dde285e3a6167f52d368056d9aa8a81fc1dd
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2019-09-30 17:35:05 +0200

    Migrate FixmeCommentCheck to Strongly typed AST

--- python-checks/src/test/resources/checks/fixmeComment.py
+++ python-checks/src/test/resources/checks/fixmeComment.py
@@ -10,4 +10,16 @@ def divide(numerator, denominator):
 # Noncompliant@+1
 # fixme in lower case
 
+for d in lib_dirs:
+    # FIXME: some fixme
+    # Noncompliant@-1
+    # not a fix me
+    pass
+
+if True:
+    print("a")
+    # Noncompliant@+1
+    # FIXME: something
+    for d in lib_dirs:
+        pass
 # fix me and this is not
