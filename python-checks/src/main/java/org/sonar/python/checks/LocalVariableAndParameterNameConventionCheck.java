dfa4c06fe31e1efe342f89fb10aae7e4c1d47f4e
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-10-01 10:31:01 +0200

    Migrate local var and param name convention to strongly typed AST

--- python-checks/src/main/java/org/sonar/python/checks/LocalVariableAndParameterNameConventionCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/LocalVariableAndParameterNameConventionCheck.java
@@ -19,127 +19,70 @@
  */
 package org.sonar.python.checks;
 
-import com.sonar.sslr.api.AstNode;
-import com.sonar.sslr.api.AstNodeType;
-import com.sonar.sslr.api.GenericTokenType;
-import com.sonar.sslr.api.Token;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.regex.Pattern;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
-import org.sonar.python.PythonCheckAstNode;
-import org.sonar.python.api.PythonGrammar;
+import org.sonar.python.PythonSubscriptionCheck;
+import org.sonar.python.SubscriptionContext;
+import org.sonar.python.api.tree.FunctionDef;
+import org.sonar.python.api.tree.Tree;
+import org.sonar.python.semantic.Symbol;
+import org.sonar.python.semantic.Usage;
 
-@Rule(key = LocalVariableAndParameterNameConventionCheck.CHECK_KEY)
-public class LocalVariableAndParameterNameConventionCheck extends PythonCheckAstNode {
+@Rule(key = "S117")
+public class LocalVariableAndParameterNameConventionCheck extends PythonSubscriptionCheck {
 
-  public static final String CHECK_KEY = "S117";
-
-  public static final String MESSAGE = "Rename this %s \"%s\" to match the regular expression %s.";
-  public static final String PARAMETER = "parameter";
-  public static final String LOCAL_VAR = "local variable";
+  private static final String MESSAGE = "Rename this %s \"%s\" to match the regular expression %s.";
+  private static final String PARAMETER = "parameter";
+  private static final String LOCAL_VAR = "local variable";
+  private static final EnumSet<Usage.Kind> USAGES = EnumSet.of(Usage.Kind.PARAMETER, Usage.Kind.LOOP_DECLARATION, Usage.Kind.ASSIGNMENT_LHS);
 
   private static final String CONSTANT_PATTERN = "^[_A-Z][A-Z0-9_]*$";
 
   private static final String DEFAULT = "^[_a-z][a-z0-9_]*$";
   @RuleProperty(key = "format", defaultValue = DEFAULT)
   public String format = DEFAULT;
+  private Pattern constantPattern;
+  private Pattern pattern;
 
-  private Pattern pattern = null;
-  private Pattern constantPattern = null;
-
-  @Override
-  public Set<AstNodeType> subscribedKinds() {
-    return Collections.singleton(PythonGrammar.FUNCDEF);
-  }
 
   @Override
-  public void visitNode(AstNode astNode) {
-    List<Token> parameters = visitParameters(astNode);
-    visitLocalVariables(astNode, parameters);
+  public void initialize(Context context) {
+    pattern = Pattern.compile(format);
+    constantPattern = Pattern.compile(CONSTANT_PATTERN);
+    context.registerSyntaxNodeConsumer(Tree.Kind.FUNCDEF, ctx -> {
+      FunctionDef funcDef = (FunctionDef) ctx.syntaxNode();
+      funcDef.localVariables().stream().sorted(Comparator.comparing(Symbol::name)).forEach(s -> checkName(s, ctx));
+    });
   }
 
-  private void visitLocalVariables(AstNode funcDef, List<Token> parameters) {
-    AstNode suite = funcDef.getFirstChild(PythonGrammar.SUITE);
-    if (suite != null) {
-      List<AstNode> expressions = suite.getDescendants(PythonGrammar.EXPRESSION_STMT);
-      List<Token> varNames = new LinkedList<>();
-      List<Token> forCounterNames = getForCounterNames(suite);
-      for (AstNode expression : expressions) {
-        if (CheckUtils.isAssignmentExpression(expression) && CheckUtils.insideFunction(expression, funcDef)) {
-          varNames = new NewSymbolsAnalyzer().getVariablesFromLongAssignmentExpression(varNames, expression);
-        }
-      }
-      for (int i = 0; i < varNames.size(); i++) {
-        if (CheckUtils.containsValue(parameters, varNames.get(i).getValue()) || CheckUtils.containsValue(forCounterNames, varNames.get(i).getValue())) {
-          varNames.remove(i);
-        }
-      }
-      checkNames(varNames, forCounterNames);
-    }
-  }
-
-  private void checkNames(List<Token> varNames, List<Token> forCounterNames) {
-    if (constantPattern == null) {
-      constantPattern = Pattern.compile(CONSTANT_PATTERN);
-    }
-
-    for (Token name : varNames) {
-      if (!constantPattern.matcher(name.getValue()).matches()) {
-        checkName(name, LOCAL_VAR);
-      }
-    }
-
-    for (Token name : forCounterNames) {
-      if (name.getValue().length() > 1) {
-        checkName(name, LOCAL_VAR);
-      }
-    }
-  }
-
-  private static List<Token> getForCounterNames(AstNode suite) {
-    List<AstNode> forStatements = suite.getDescendants(PythonGrammar.FOR_STMT);
-    List<Token> result = new LinkedList<>();
-    for (AstNode forStatement : forStatements) {
-      AstNode counters = forStatement.getFirstChild(PythonGrammar.EXPRLIST);
-      for (AstNode name : counters.getDescendants(PythonGrammar.NAME)) {
-        Token token = name.getToken();
-        if (token.getType().equals(GenericTokenType.IDENTIFIER)) {
-          result.add(token);
-        }
-      }
-    }
-    return result;
-  }
-
-  private List<Token> visitParameters(AstNode astNode) {
-    List<Token> parameterTokens = new LinkedList<>();
-    AstNode varArgList = astNode.getFirstChild(PythonGrammar.TYPEDARGSLIST);
-    if (varArgList != null) {
-      List<AstNode> funcParameters = varArgList.getDescendants(PythonGrammar.TFPDEF);
-      funcParameters.addAll(varArgList.getChildren(PythonGrammar.NAME));
-      for (AstNode parameter : funcParameters) {
-        Token token = parameter.getToken();
-        if (token.getType().equals(GenericTokenType.IDENTIFIER)) {
-          parameterTokens.add(token);
-          checkName(token, PARAMETER);
-        }
-      }
-    }
-    return parameterTokens;
-  }
-
-  private void checkName(Token token, String type) {
-    String name = token.getValue();
-    if (pattern == null) {
-      pattern = Pattern.compile(format);
-    }
+  private void checkName(Symbol symbol, SubscriptionContext ctx) {
+    String name = symbol.name();
     if (!pattern.matcher(name).matches()) {
-      addIssue(token, String.format(MESSAGE, type, name, format));
+      symbol.usages().stream()
+        .filter(usage -> USAGES.contains(usage.kind()))
+        .sorted(Comparator.comparingInt(u -> u.tree().firstToken().line()))
+        .limit(1)
+        .forEach(usage -> raiseIssueForNameAndUsage(ctx, name, usage));
     }
   }
 
+  private void raiseIssueForNameAndUsage(SubscriptionContext ctx, String name, Usage usage) {
+    String type = PARAMETER;
+    Usage.Kind kind = usage.kind();
+    if (kind == Usage.Kind.ASSIGNMENT_LHS) {
+      type = LOCAL_VAR;
+      if (constantPattern.matcher(name).matches()) {
+        return;
+      }
+    } else if (kind == Usage.Kind.LOOP_DECLARATION) {
+      type = LOCAL_VAR;
+      if (name.length() <= 1) {
+        return;
+      }
+    }
+    ctx.addIssue(usage.tree(), String.format(MESSAGE, type, name, format));
+  }
 }
