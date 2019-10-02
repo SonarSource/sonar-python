9b155dbd446e437c7171e19e1db3df2c6e21cd9a
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2019-10-02 17:31:15 +0200

    Migrate LineLengthCheck to Strongly typed AST

--- python-checks/src/test/resources/checks/lineLength.py
+++ python-checks/src/test/resources/checks/lineLength.py
@@ -3,3 +3,33 @@ print "This line contains 40 characters"
 print
 print """This is a multiline
 string literal."""
+
+# Noncompliant@+1
+from some.where import foo, bar
+
+# Noncompliant@+1
+something = call(('arg1', 'arg2',
+                  # Noncompliant@+1
+                  'arg3', 'ar'),
+                 'arg'
+                 )
+
+something_esle = [
+    ("hello", {},
+     # Noncompliant@+1
+     u'hello hello hello hello'),
+
+    ("a", {},
+     u'b'),
+]
+
+another = Call(
+    foo("something",
+        # Noncompliant@+1
+        bar("more than 30 ch"),
+        ),
+
+    foo("something",
+        bar("ok"),
+        ),
+)
