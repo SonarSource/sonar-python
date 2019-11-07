d6b78f316ecac2683c9d652530b11469e1580c4d
Alban Auzeill <alban.auzeill@sonarsource.com>
2019-11-07 07:44:45 +0100

    SONARPY-481 Improve S3516: Raise issue when returned identifiers has same value

--- python-checks/src/main/java/org/sonar/python/checks/InvariantReturnCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/InvariantReturnCheck.java
@@ -19,25 +19,31 @@
  */
 package org.sonar.python.checks;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.cfg.CfgBlock;
 import org.sonar.plugins.python.api.cfg.ControlFlowGraph;
+import org.sonar.plugins.python.api.tree.AssignmentStatement;
 import org.sonar.plugins.python.api.tree.BinaryExpression;
 import org.sonar.plugins.python.api.tree.Expression;
 import org.sonar.plugins.python.api.tree.FunctionDef;
+import org.sonar.plugins.python.api.tree.Name;
 import org.sonar.plugins.python.api.tree.ParenthesizedExpression;
 import org.sonar.plugins.python.api.tree.ReturnStatement;
 import org.sonar.plugins.python.api.tree.Tree;
 import org.sonar.plugins.python.api.tree.Tree.Kind;
 import org.sonar.plugins.python.api.tree.UnaryExpression;
 import org.sonar.python.cfg.PythonCfgBranchingBlock;
+import org.sonar.python.semantic.Symbol;
+import org.sonar.python.semantic.Usage;
 
 @Rule(key = "S3516")
 public class InvariantReturnCheck extends PythonSubscriptionCheck {
@@ -52,108 +58,237 @@ public class InvariantReturnCheck extends PythonSubscriptionCheck {
   private static final Kind[] UNARY_EXPRESSION_KINDS = {
     Kind.UNARY_MINUS, Kind.UNARY_PLUS, Kind.BITWISE_COMPLEMENT, Kind.NOT};
 
+  private static final Set<Usage.Kind> DECLARATION_USAGE_KINDS = EnumSet.of(
+    Usage.Kind.IMPORT,
+    Usage.Kind.LOOP_DECLARATION,
+    Usage.Kind.COMP_DECLARATION,
+    Usage.Kind.PARAMETER,
+    Usage.Kind.FUNC_DECLARATION,
+    Usage.Kind.CLASS_DECLARATION,
+    Usage.Kind.EXCEPTION_INSTANCE,
+    Usage.Kind.WITH_INSTANCE,
+    Usage.Kind.GLOBAL_DECLARATION);
+
   @Override
   public void initialize(Context context) {
     context.registerSyntaxNodeConsumer(Kind.FUNCDEF, ctx -> {
       FunctionDef functionDef = (FunctionDef) ctx.syntaxNode();
       ControlFlowGraph cfg = ControlFlowGraph.build(functionDef, ctx.pythonFile());
-      if (cfg == null) {
-        return;
-      }
-      List<ReturnStatement> allReturns = collectLatestExecutedBlocks(cfg).stream()
-        .map(InvariantReturnCheck::extractReturnStatementFromBlock)
-        .collect(Collectors.toList());
-
-      boolean notAllPredecessorsAreReturn = allReturns.stream().anyMatch(Objects::isNull);
-      if (allReturns.size() <= 1 || notAllPredecessorsAreReturn) {
-        return;
-      }
-      List<List<Expression>> returnExpressions = allReturns.stream().map(ReturnStatement::expressions).collect(Collectors.toList());
-      boolean noneReturnVoid = returnExpressions.stream().noneMatch(List::isEmpty);
-      if (noneReturnVoid && returnExpressionsHaveTheSameValue(returnExpressions)) {
-        PreciseIssue issue = ctx.addIssue(functionDef.name(), MESSAGE);
-        allReturns.forEach(returnStatement -> issue.secondary(returnStatement, "returned value"));
+      if (cfg != null) {
+        List<LatestExecutedBlock> latestExecutedBlocks = collectLatestExecutedBlocks(cfg);
+        boolean allBlocksHaveReturnStatement = latestExecutedBlocks.stream().allMatch(LatestExecutedBlock::hasReturnStatement);
+        if (latestExecutedBlocks.size() <= 1 || !allBlocksHaveReturnStatement) {
+          return;
+        }
+        boolean noEmptyReturn = latestExecutedBlocks.stream().noneMatch(LatestExecutedBlock::returnNone);
+        if (noEmptyReturn && returnExpressionsHaveTheSameValue(latestExecutedBlocks)) {
+          PreciseIssue issue = ctx.addIssue(functionDef.name(), MESSAGE);
+          latestExecutedBlocks.forEach(block -> issue.secondary(block.returnStatement, "returned value"));
+        }
       }
     });
   }
 
-  private static Set<CfgBlock> collectLatestExecutedBlocks(ControlFlowGraph cfg) {
-    Set<CfgBlock> predecessors = new HashSet<>();
+  private static List<LatestExecutedBlock> collectLatestExecutedBlocks(ControlFlowGraph cfg) {
+    List<LatestExecutedBlock> collectedBlocks = new ArrayList<>();
     for (CfgBlock predecessor : cfg.end().predecessors()) {
       if (predecessor instanceof PythonCfgBranchingBlock) {
-        collectBlocksHavingReturnBeforeExceptOrFinallyBlock(predecessors, (PythonCfgBranchingBlock) predecessor);
-      } else {
-        predecessors.add(predecessor);
+        collectBlocksHavingReturnBeforeExceptOrFinallyBlock(cfg, collectedBlocks, (PythonCfgBranchingBlock) predecessor);
+      } else if (!endsWithElementKind(predecessor, Kind.RAISE_STMT)) {
+        collectedBlocks.add(new LatestExecutedBlock(cfg, predecessor));
       }
     }
-    return predecessors;
+    return collectedBlocks;
   }
 
-  private static void collectBlocksHavingReturnBeforeExceptOrFinallyBlock(Set<CfgBlock> predecessors, PythonCfgBranchingBlock branchingBlock) {
+  private static void collectBlocksHavingReturnBeforeExceptOrFinallyBlock(ControlFlowGraph cfg, List<LatestExecutedBlock> collectedBlocks, PythonCfgBranchingBlock branchingBlock) {
     if (branchingBlock.branchingTree().is(Kind.EXCEPT_CLAUSE, Kind.FINALLY_CLAUSE)) {
       for (CfgBlock predecessor : branchingBlock.predecessors()) {
         if (predecessor instanceof PythonCfgBranchingBlock) {
-          collectBlocksHavingReturnBeforeExceptOrFinallyBlock(predecessors, (PythonCfgBranchingBlock) predecessor);
-        } else if (hasReturnStatement(predecessor)) {
-          predecessors.add(predecessor);
+          collectBlocksHavingReturnBeforeExceptOrFinallyBlock(cfg, collectedBlocks, (PythonCfgBranchingBlock) predecessor);
+        } else if (endsWithElementKind(predecessor, Kind.RETURN_STMT)) {
+          collectedBlocks.add(new LatestExecutedBlock(cfg, predecessor));
         }
       }
     }
   }
 
-  private static boolean returnExpressionsHaveTheSameValue(List<List<Expression>> returnExpressions) {
-    for (int i = 1; i < returnExpressions.size(); i++) {
-      if (!haveTheSameValue(returnExpressions.get(i - 1), returnExpressions.get(i))) {
+  private static boolean returnExpressionsHaveTheSameValue(List<LatestExecutedBlock> latestExecutedBlocks) {
+    for (int i = 1; i < latestExecutedBlocks.size(); i++) {
+      if (!haveTheSameValue(latestExecutedBlocks.get(i - 1), latestExecutedBlocks.get(i))) {
         return false;
       }
     }
     return true;
   }
 
-  private static boolean haveTheSameValue(List<Expression> left, List<Expression> right) {
+  private static boolean haveTheSameValue(LatestExecutedBlock left, LatestExecutedBlock right) {
+    return haveTheSameValue(left, left.returnExpressions(), right, right.returnExpressions());
+  }
+
+  private static boolean haveTheSameValue(LatestExecutedBlock leftBlock, List<? extends Tree> left, LatestExecutedBlock rightBlock, List<? extends Tree> right) {
     if (left.size() != right.size()) {
       return false;
     }
     for (int i = 0; i < left.size(); i++) {
-      if (!haveTheSameValue(left.get(i), right.get(i))) {
+      if (!haveTheSameValue(leftBlock, left.get(i), rightBlock, right.get(i))) {
         return false;
       }
     }
     return true;
   }
 
-  private static boolean haveTheSameValue(Expression left, Expression right) {
-    return isConstantExpression(left) && CheckUtils.areEquivalent(left, right);
+  private static boolean haveTheSameValue(LatestExecutedBlock leftBlock, Tree left, LatestExecutedBlock rightBlock, Tree right) {
+    if (left.is(Kind.PARENTHESIZED)) {
+      return haveTheSameValue(leftBlock, ((ParenthesizedExpression) left).expression(), rightBlock, right);
+    } else if (right.is(Kind.PARENTHESIZED)) {
+      return haveTheSameValue(leftBlock, left, rightBlock, ((ParenthesizedExpression) right).expression());
+    } else if (left.getKind() != right.getKind()) {
+      return false;
+    } else if (left.is(UNARY_EXPRESSION_KINDS)) {
+      return unaryExpressionsHaveTheSameValue(leftBlock, (UnaryExpression) left, rightBlock, (UnaryExpression) right);
+    } else if (left.is(BINARY_EXPRESSION_KINDS)) {
+      return binaryExpressionsHaveTheSameValue(leftBlock, (BinaryExpression) left, rightBlock, (BinaryExpression) right);
+    } else if (left.is(Kind.STRING_LITERAL)) {
+      return haveTheSameValue(leftBlock, left.children(), rightBlock, right.children());
+    } else if (left.is(Kind.NUMERIC_LITERAL, Kind.STRING_ELEMENT)) {
+      return left.firstToken().value().equals(right.firstToken().value());
+    } else if (left.is(Kind.NAME)) {
+      return identifierHaveTheSameValue(leftBlock, (Name) left, rightBlock, (Name) right);
+    }
+    return false;
   }
 
-  private static boolean isConstantExpression(Expression expression) {
-    if (expression.is(Kind.PARENTHESIZED)) {
-      return isConstantExpression(((ParenthesizedExpression) expression).expression());
-    } else {
-      if (expression.is(BINARY_EXPRESSION_KINDS)) {
-        BinaryExpression binaryExpression = (BinaryExpression) expression;
-        return isConstantExpression(binaryExpression.leftOperand()) && isConstantExpression(binaryExpression.rightOperand());
-      } else if (expression.is(UNARY_EXPRESSION_KINDS)) {
-        return isConstantExpression(((UnaryExpression) expression).expression());
+  private static boolean unaryExpressionsHaveTheSameValue(LatestExecutedBlock leftBlock, UnaryExpression left, LatestExecutedBlock rightBlock, UnaryExpression right) {
+    // the caller ensure left.getKind() == right.getKind(), so no need to compare left and right UnaryExpression#operator(), it's redundant
+    return haveTheSameValue(leftBlock, left.expression(), rightBlock, right.expression());
+  }
+
+  private static boolean binaryExpressionsHaveTheSameValue(LatestExecutedBlock leftBlock, BinaryExpression left, LatestExecutedBlock rightBlock, BinaryExpression right) {
+    // the caller ensure left.getKind() == right.getKind(), so no need to compare left and right BinaryExpression#operator(), it's redundant
+    return haveTheSameValue(leftBlock, left.leftOperand(), rightBlock, right.leftOperand()) &&
+      haveTheSameValue(leftBlock, left.rightOperand(), rightBlock, right.rightOperand());
+  }
+
+  private static boolean identifierHaveTheSameValue(LatestExecutedBlock leftBlock, Name left, LatestExecutedBlock rightBlock, Name right) {
+    if (left.name().equals("True") || left.name().equals("False")) {
+      return right.name().equals(left.name());
+    }
+    Symbol leftSymbol = left.symbol();
+    Symbol rightSymbol = right.symbol();
+    if (leftSymbol == null || !leftSymbol.equals(rightSymbol)) {
+      return false;
+    }
+    Tree leftBinding = findUniquePreviousBinding(leftBlock, leftSymbol);
+    Tree rightBinding = findUniquePreviousBinding(rightBlock, rightSymbol);
+    return leftBinding != null && leftBinding == rightBinding;
+  }
+
+  private static Tree findUniquePreviousBinding(LatestExecutedBlock context, Symbol identifier) {
+    Set<CfgBlock> pushedBlocks = new HashSet<>();
+    Deque<CfgBlock> blockToVisit = new ArrayDeque<>();
+    blockToVisit.push(context.block);
+    pushedBlocks.add(context.block);
+    Set<Tree> bindings = new HashSet<>();
+    while (!blockToVisit.isEmpty() && bindings.size() < 2) {
+      CfgBlock block = blockToVisit.pop();
+      Tree binding = findLastBinding(block.elements(), identifier);
+      if (binding != null) {
+        bindings.add(binding);
+      } else if (block == context.cfg.start()) {
+        bindings.add(findDeclarationBinding(identifier));
+      } else {
+        for (CfgBlock predecessor : block.predecessors()) {
+          if (pushedBlocks.add(predecessor)) {
+            blockToVisit.push(predecessor);
+          }
+        }
       }
     }
-    return expression.is(Kind.NONE, Kind.NUMERIC_LITERAL, Kind.STRING_LITERAL);
+    return bindings.size() == 1 ? bindings.iterator().next() : null;
   }
 
-  private static boolean hasReturnStatement(CfgBlock block) {
-    return extractReturnStatementFromBlock(block) != null;
+  private static Tree findDeclarationBinding(Symbol identifier) {
+    return identifier.usages().stream()
+      .filter(usage -> DECLARATION_USAGE_KINDS.contains(usage.kind()))
+      .map(Usage::tree)
+      .findFirst()
+      .orElse(null);
   }
 
   @Nullable
-  private static ReturnStatement extractReturnStatementFromBlock(CfgBlock block) {
-    List<Tree> elements = block.elements();
-    if (!elements.isEmpty()) {
-      Tree last = elements.get(elements.size() - 1);
-      if (last.is(Kind.RETURN_STMT)) {
-        return (ReturnStatement) last;
+  private static Tree findLastBinding(List<Tree> elements, Symbol identifier) {
+    for (int i = elements.size() - 1; i >= 0; i--) {
+      Tree binding = findLastBinding(elements.get(i), identifier);
+      if (binding != null) {
+        return binding;
       }
     }
     return null;
+  }
+
+  @Nullable
+  private static Tree findLastBinding(Tree context, Symbol identifier) {
+    if (context.is(Kind.NAME) && identifier.equals(((Name) context).symbol()) && couldBeModified(context)) {
+      return context;
+    }
+    return findLastBinding(context.children(), identifier);
+  }
+
+  private static boolean couldBeModified(Tree tree) {
+    Tree child = tree;
+    Tree parent = child.parent();
+    while (parent.is(Kind.STATEMENT_LIST, Kind.PARENTHESIZED, Kind.QUALIFIED_EXPR, Kind.TUPLE)) {
+      child = parent;
+      parent = child.parent();
+    }
+    if (parent.is(Kind.RETURN_STMT, Kind.CONDITIONAL_EXPR, Kind.ELSE_CLAUSE, Kind.WHILE_STMT, Kind.EXPRESSION_STMT, Kind.IF_STMT) ||
+      parent.is(UNARY_EXPRESSION_KINDS) || parent.is(BINARY_EXPRESSION_KINDS)) {
+      return false;
+    } else if (parent.is(Kind.ASSIGNMENT_STMT)) {
+      return ((AssignmentStatement) parent).lhsExpressions() == child;
+    }
+    return true;
+  }
+
+  private static boolean endsWithElementKind(CfgBlock block, Kind kind) {
+    return lastElement(block, kind) != null;
+  }
+
+  @Nullable
+  private static Tree lastElement(CfgBlock block, Kind kind) {
+    List<Tree> elements = block.elements();
+    int last = elements.size() - 1;
+    return !elements.isEmpty() && elements.get(last).is(kind) ? elements.get(last) : null;
+  }
+
+  private static class LatestExecutedBlock {
+
+    private final ControlFlowGraph cfg;
+
+    private final CfgBlock block;
+    @Nullable
+    private final ReturnStatement returnStatement;
+
+    private LatestExecutedBlock(ControlFlowGraph cfg, CfgBlock block) {
+      this.cfg = cfg;
+      this.block = block;
+      returnStatement = (ReturnStatement) lastElement(block, Kind.RETURN_STMT);
+    }
+
+    private boolean hasReturnStatement() {
+      return returnStatement != null;
+    }
+
+    private List<Expression> returnExpressions() {
+      return returnStatement.expressions();
+    }
+
+    private boolean returnNone() {
+      List<Expression> expressions = returnStatement.expressions();
+      return expressions.isEmpty() || (expressions.size() == 1 && expressions.get(0).is(Kind.NONE));
+    }
+
   }
 
 }
