b52b1848241b37f34e28d3a42037d361dbffc766
Andrea Guarino <38876543+andrea-guarino-sonarsource@users.noreply.github.com>
2020-02-07 15:23:39 +0100

    SONARPY-493 Rule S3330: Creating cookies without the HttpOnly flag is security-sensitive (#554)

---
+++ python-checks/src/main/java/org/sonar/python/checks/hotspots/HttpOnlyCookieCheck.java
@@ -0,0 +1,71 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks.hotspots;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.sonar.check.Rule;
+
+@Rule(key = "S3330")
+public class HttpOnlyCookieCheck extends AbstractCookieFlagCheck {
+
+  private static Map<String, Integer> sensitiveArgumentByFQN;
+  @Override
+  String flagName() {
+    return "httponly";
+  }
+
+  static {
+    sensitiveArgumentByFQN = new HashMap<>();
+    sensitiveArgumentByFQN.put("django.http.HttpResponse.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponse.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseRedirect.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseRedirect.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponsePermanentRedirect.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponsePermanentRedirect.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseNotModified.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseNotModified.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseBadRequest.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseBadRequest.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseNotFound.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseNotFound.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseForbidden.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseForbidden.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseNotAllowed.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseNotAllowed.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseGone.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseGone.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseServerError.set_cookie", 7);
+    sensitiveArgumentByFQN.put("django.http.HttpResponseServerError.set_signed_cookie", 8);
+    sensitiveArgumentByFQN.put("flask.Response.set_cookie", 7);
+    sensitiveArgumentByFQN = Collections.unmodifiableMap(sensitiveArgumentByFQN);
+  }
+
+  @Override
+  String message() {
+    return "Make sure creating this cookie without the \"HttpOnly\" flag is safe.";
+  }
+
+  @Override
+  Map<String, Integer> sensitiveArgumentByFQN() {
+    return sensitiveArgumentByFQN;
+  }
+}
---
+++ python-checks/src/test/resources/checks/hotspots/httpOnlyCookie.py
@@ -0,0 +1,85 @@
+def stdlib_tests(param):
+    from http.cookies import SimpleCookie
+
+    cookie = SimpleCookie()
+
+    cookie['c1'] = 'value' # FN
+
+    cookie['c2'] = 'value'
+    cookie['c2']['httponly'] = False # Noncompliant {{Make sure creating this cookie without the "HttpOnly" flag is safe.}}
+    cookie['c2']['other'] = False # OK
+    cookie['c2'][param] = False # OK
+
+    cookie['c2']['httponly', 'other'] = False # OK
+
+    cookie['httponly'] = False # OK
+
+    cookie['c3'] = 'value'
+    cookie['c3']['httponly'] = True # OK
+
+    not_a_cookie = 42
+    not_a_cookie['c']['httponly'] = False # OK
+
+    get_cookie()['c']['httponly'] = False # OK
+
+def get_cookie(): pass
+
+def django_tests():
+    from django.http import HttpResponse
+    from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseBadRequest, HttpResponseNotModified, HttpResponseNotFound, HttpResponseForbidden, HttpResponseNotAllowed, HttpResponseGone, HttpResponseServerError
+
+    response = HttpResponse()
+    response.set_cookie("C1", "world") # Noncompliant
+    response.set_cookie("C2", "world", httponly=None) # Noncompliant
+    response.set_cookie("C3", "world", httponly=False) # Noncompliant
+    response.set_cookie("C4", "world", httponly=True)
+
+    response2 = HttpResponseRedirect()
+    response2.set_cookie("C5", "world") # Noncompliant
+    response2.set_cookie("C5", "world", None, None, "/", None, True, True) # OK
+    response2.set_signed_cookie("C5", "world") # Noncompliant
+    response2.set_signed_cookie("C5", "world", httponly=True) # OK
+    response2.set_signed_cookie("C5", "world", other=False, httponly=True) # OK
+    response2.set_signed_cookie("C5", "world", httponly=False) # Noncompliant
+    response2.set_signed_cookie("C5", "world", httponly=None) # Noncompliant
+    response2.set_signed_cookie("C5", "", "world", None, None, "/", None, True, True) # OK
+    kwargs = { httponly : True }
+    response2.set_signed_cookie("C5", "world", **kwargs) # OK
+
+    kwargs = { httponly : False }
+    response2.set_signed_cookie("C5", "world", **kwargs) # FN
+
+    get_cookie().set_cookie("C3", "world", httponly=False)
+
+    response3 = HttpResponsePermanentRedirect()
+    response3.set_cookie("C6", "world") # Noncompliant
+    response4 = HttpResponseNotModified()
+    response4.set_cookie("C7", "world") # Noncompliant
+    response5 = HttpResponseBadRequest()
+    response5.set_cookie("C8", "world") # Noncompliant
+    response6 = HttpResponseNotFound()
+    response6.set_cookie("C9", "world") # Noncompliant
+    response7 = HttpResponseForbidden()
+    response7.set_cookie("C10", "world") # Noncompliant
+    response8 = HttpResponseNotAllowed()
+    response8.set_cookie("C11", "world") # Noncompliant
+    response9 = HttpResponseGone()
+    response9.set_cookie("C12", "world") # Noncompliant
+    response10 = HttpResponseServerError()
+    response10.set_cookie("C13", "world") # Noncompliant
+
+def flask_tests():
+    import flask
+    from flask import Response, make_response, redirect
+
+    response1 = Response('OK')
+    response1.set_cookie('c1', 'value') # Noncompliant
+
+    response2 = flask.Response('OK')
+    response2.set_cookie('c1', 'value', httponly = True) # OK
+
+    response3 = make_response()
+    response3.set_cookie('c', 'value') # Noncompliant
+
+    response4 = redirect()
+    response4.set_cookie('c', 'value') # Noncompliant
