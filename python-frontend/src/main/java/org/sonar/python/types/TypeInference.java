5cc831b0969284565e5982e5832cbb99aa080dc6
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2020-02-27 15:18:55 +0100

    SONARPY-608 Type inference: propagate return types of TypeShed methods to variables

--- python-frontend/src/main/java/org/sonar/python/types/TypeInference.java
+++ python-frontend/src/main/java/org/sonar/python/types/TypeInference.java
@@ -20,7 +20,7 @@
 package org.sonar.python.types;
 
 import java.util.ArrayDeque;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -47,16 +47,21 @@ import org.sonar.python.tree.NameImpl;
 
 public class TypeInference extends BaseTreeVisitor {
 
-  private final Set<SymbolImpl> trackedVars = new HashSet<>();
-  private final Set<Assignment> assignments = new HashSet<>();
+  // The super() builtin is not specified precisely in typeshed.
+  // It should return a proxy object (temporary object of the superclass) that allows to access methods of the base class
+  // https://docs.python.org/3/library/functions.html#super
+  private static final InferredType TYPE_OF_SUPER = InferredTypes.runtimeType(TypeShed.typeShedClass("super"));
+
   private final FunctionLike functionDef;
+  private final Map<Symbol, Set<Assignment>> assignmentsByLhs = new HashMap<>();
+  private final Map<QualifiedExpression, MemberAccess> memberAccessesByQualifiedExpr = new HashMap<>();
 
   public static void inferTypes(FileInput fileInput) {
     fileInput.accept(new BaseTreeVisitor() {
       @Override
       public void visitFunctionDef(FunctionDef funcDef) {
         super.visitFunctionDef(funcDef);
-        inferTypes(funcDef);
+        inferTypesAndMemberAccessSymbols(funcDef);
       }
     });
 
@@ -65,12 +70,8 @@ public class TypeInference extends BaseTreeVisitor {
       public void visitQualifiedExpression(QualifiedExpression qualifiedExpression) {
         super.visitQualifiedExpression(qualifiedExpression);
         Name name = qualifiedExpression.name();
-        Expression qualifier = qualifiedExpression.qualifier();
-        // The super() builtin is not specified precisely in typeshed.
-        // It should return a proxy object (temporary object of the superclass) that allows to access methods of the base class
-        // https://docs.python.org/3/library/functions.html#super
-        InferredType type = qualifier.type();
-        if (!type.equals(InferredTypes.runtimeType(TypeShed.typeShedClass("super")))) {
+        InferredType type = qualifiedExpression.qualifier().type();
+        if (!type.equals(TYPE_OF_SUPER)) {
           Optional<Symbol> resolvedMember = type.resolveMember(name.name());
           resolvedMember.ifPresent(((NameImpl) name)::setSymbol);
         }
@@ -78,10 +79,10 @@ public class TypeInference extends BaseTreeVisitor {
     });
   }
 
-  private static void inferTypes(FunctionLike functionDef) {
+  private static void inferTypesAndMemberAccessSymbols(FunctionLike functionDef) {
     TypeInference visitor = new TypeInference(functionDef);
     functionDef.accept(visitor);
-    visitor.processAssignments();
+    visitor.processPropagations();
   }
 
   private TypeInference(FunctionLike functionDef) {
@@ -108,71 +109,106 @@ public class TypeInference extends BaseTreeVisitor {
     }
 
     Expression rhs = assignmentStatement.assignedValue();
-    assignments.add(new Assignment(symbol, lhs, rhs));
+    Assignment assignment = new Assignment(symbol, lhs, rhs);
+    assignmentsByLhs.computeIfAbsent(symbol, s -> new HashSet<>()).add(assignment);
   }
 
-  private void processAssignments() {
-    Set<Name> assignedNames = assignments.stream().map(a -> a.lhsName).collect(Collectors.toSet());
+  @Override
+  public void visitQualifiedExpression(QualifiedExpression qualifiedExpression) {
+    super.visitQualifiedExpression(qualifiedExpression);
+    memberAccessesByQualifiedExpr.put(qualifiedExpression, new MemberAccess(qualifiedExpression));
+  }
+
+  private void processPropagations() {
+    Set<Symbol> trackedVars = new HashSet<>();
+    Set<Name> assignedNames = assignmentsByLhs.values().stream()
+      .flatMap(Collection::stream)
+      .map(a -> a.lhsName)
+      .collect(Collectors.toSet());
     for (Symbol variable : functionDef.localVariables()) {
       boolean hasMissingBindingUsage = variable.usages().stream()
         .filter(Usage::isBindingUsage)
         .anyMatch(u -> !assignedNames.contains(u.tree()));
       if (!hasMissingBindingUsage) {
-        trackedVars.add((SymbolImpl) variable);
+        trackedVars.add(variable);
       }
     }
 
+    Set<Propagation> propagations = new HashSet<>();
     Set<Symbol> initializedVars = new HashSet<>();
-    Map<Symbol, Set<Assignment>> dependentAssignments = new HashMap<>();
-    for (Assignment assignment : assignments) {
-      if (!trackedVars.contains(assignment.lhs)) {
-        continue;
-      }
-      Set<Symbol> rhsDependencies = dependencies(assignment.rhs);
-      if (rhsDependencies.isEmpty()) {
-        assignment.propagateType(initializedVars);
-      } else {
-        rhsDependencies.forEach(s -> dependentAssignments.computeIfAbsent(s, k -> new HashSet<>()).add(assignment));
-      }
-    }
 
-    Set<Assignment> workSet = new HashSet<>();
-    for (Set<Assignment> dependent : dependentAssignments.values()) {
-      workSet.addAll(dependent);
+    for (MemberAccess memberAccess : memberAccessesByQualifiedExpr.values()) {
+      memberAccess.computeDependencies(memberAccess.qualifiedExpression.qualifier(), trackedVars);
+      propagations.add(memberAccess);
     }
+    assignmentsByLhs.forEach((lhs, as) -> {
+      if (trackedVars.contains(lhs)) {
+        as.forEach(a -> a.computeDependencies(a.rhs, trackedVars));
+        propagations.addAll(as);
+      }
+    });
+
+    applyPropagations(propagations, initializedVars, true);
+    applyPropagations(propagations, initializedVars, false);
+  }
+
+  private void applyPropagations(Set<Propagation> propagations, Set<Symbol> initializedVars, boolean checkDependenciesReadiness) {
+    Set<Propagation> workSet = new HashSet<>(propagations);
     while (!workSet.isEmpty()) {
-      Iterator<Assignment> iterator = workSet.iterator();
-      Assignment assignment = iterator.next();
+      Iterator<Propagation> iterator = workSet.iterator();
+      Propagation propagation = iterator.next();
       iterator.remove();
-      if (!initializedVars.containsAll(dependencies(assignment.rhs))) {
-        continue;
-      }
-      boolean learnt = assignment.propagateType(initializedVars);
-      if (learnt) {
-        workSet.addAll(dependentAssignments.getOrDefault(assignment.lhs, Collections.emptySet()));
-      }
-    }
-  }
-
-  private Set<Symbol> dependencies(Expression expression) {
-    Set<Symbol> dependencies = new HashSet<>();
-    Deque<Expression> workList = new ArrayDeque<>();
-    workList.push(expression);
-    while (!workList.isEmpty()) {
-      Expression e = workList.pop();
-      if (e.is(Tree.Kind.NAME)) {
-        Symbol symbol = ((Name) e).symbol();
-        if (symbol != null && trackedVars.contains(symbol)) {
-          dependencies.add(symbol);
+      if (!checkDependenciesReadiness || propagation.areDependenciesReady(initializedVars)) {
+        boolean learnt = propagation.propagate(initializedVars);
+        if (learnt) {
+          workSet.addAll(propagation.dependents());
         }
-      } else if (e instanceof HasTypeDependencies) {
-        workList.addAll(((HasTypeDependencies) e).typeDependencies());
       }
     }
-    return dependencies;
   }
 
-  private static class Assignment {
+  private abstract class Propagation {
+    private final Set<Symbol> variableDependencies = new HashSet<>();
+    private final Set<QualifiedExpression> memberAccessDependencies = new HashSet<>();
+    private final Set<Propagation> dependents = new HashSet<>();
+
+    abstract boolean propagate(Set<Symbol> initializedVars);
+
+    void computeDependencies(Expression expression, Set<Symbol> trackedVars) {
+      Deque<Expression> workList = new ArrayDeque<>();
+      workList.push(expression);
+      while (!workList.isEmpty()) {
+        Expression e = workList.pop();
+        if (e.is(Tree.Kind.NAME)) {
+          Name name = (Name) e;
+          Symbol symbol = name.symbol();
+          if (symbol != null && trackedVars.contains(symbol)) {
+            variableDependencies.add(symbol);
+            assignmentsByLhs.get(symbol).forEach(a -> a.dependents().add(this));
+          }
+        } else if (e.is(Tree.Kind.QUALIFIED_EXPR)) {
+          QualifiedExpression qualifiedExpression = (QualifiedExpression) e;
+          memberAccessDependencies.add(qualifiedExpression);
+          memberAccessesByQualifiedExpr.get(qualifiedExpression).dependents().add(this);
+        } else if (e instanceof HasTypeDependencies) {
+          workList.addAll(((HasTypeDependencies) e).typeDependencies());
+        }
+      }
+    }
+
+    private boolean areDependenciesReady(Set<Symbol> initializedVars) {
+      return initializedVars.containsAll(variableDependencies)
+        && memberAccessDependencies.stream()
+            .map(QualifiedExpression::symbol)
+            .allMatch(s -> s != null && s.kind() == Symbol.Kind.FUNCTION);
+    }
+
+    Set<Propagation> dependents() {
+      return dependents;
+    }
+  }
+
+  private class Assignment extends Propagation {
     private final SymbolImpl lhs;
     private final Name lhsName;
     private final Expression rhs;
@@ -184,7 +220,8 @@ public class TypeInference extends BaseTreeVisitor {
     }
 
     /** @return true if the propagation effectively changed the inferred type of lhs */
-    private boolean propagateType(Set<Symbol> initializedVars) {
+    @Override
+    public boolean propagate(Set<Symbol> initializedVars) {
       InferredType rhsType = rhs.type();
       if (initializedVars.add(lhs)) {
         lhs.setInferredType(rhsType);
@@ -198,4 +235,32 @@ public class TypeInference extends BaseTreeVisitor {
     }
   }
 
+  private class MemberAccess extends Propagation {
+
+    private final QualifiedExpression qualifiedExpression;
+    private final Symbol symbolWithoutTypeInference;
+
+    private MemberAccess(QualifiedExpression qualifiedExpression) {
+      this.qualifiedExpression = qualifiedExpression;
+      this.symbolWithoutTypeInference = qualifiedExpression.symbol();
+    }
+
+    @Override
+    public boolean propagate(Set<Symbol> initializedVars) {
+      NameImpl name = (NameImpl) qualifiedExpression.name();
+      InferredType type = qualifiedExpression.qualifier().type();
+      if (!type.equals(TYPE_OF_SUPER)) {
+        Optional<Symbol> resolvedMember = type.resolveMember(name.name());
+        Symbol previous = name.symbol();
+        if (resolvedMember.isPresent()) {
+          name.setSymbol(resolvedMember.get());
+          return previous != resolvedMember.get();
+        } else if (name.symbol() != symbolWithoutTypeInference) {
+          name.setSymbol(symbolWithoutTypeInference);
+          return true;
+        }
+      }
+      return false;
+    }
+  }
 }
