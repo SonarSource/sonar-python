bef7ff74661cff43660ba23cfedf8713e9305c3b
Gyula Sallai <61680650+gyula-sallai-sonarsource@users.noreply.github.com>
2020-03-19 18:11:57 +0100

    SONARPY 589: Rule S5724: Property getter, setter and deleter methods should have the expected number of parameters (#646)

---
+++ python-checks/src/main/java/org/sonar/python/checks/PropertyAccessorParameterCountCheck.java
@@ -0,0 +1,211 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.symbols.Usage;
+import org.sonar.plugins.python.api.tree.Argument;
+import org.sonar.plugins.python.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.ClassDef;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.FunctionDef;
+import org.sonar.plugins.python.api.tree.HasSymbol;
+import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.Parameter;
+import org.sonar.plugins.python.api.tree.ParameterList;
+import org.sonar.plugins.python.api.tree.RegularArgument;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.tree.TreeUtils;
+
+@Rule(key = "S5724")
+public class PropertyAccessorParameterCountCheck extends PythonSubscriptionCheck {
+
+  private static class PropertyAccessorTriple {
+    private Optional<FunctionDef> getter = Optional.empty();
+    private Optional<FunctionDef> setter = Optional.empty();
+    private Optional<FunctionDef> deleter = Optional.empty();
+  }
+
+  private static class CollectPropertiesVisitor extends BaseTreeVisitor {
+    private Map<String, PropertyAccessorTriple> decoratorStyleProperties = new HashMap<>();
+    private List<PropertyAccessorTriple> propertyCallStyleProperties = new ArrayList<>();
+
+    private static Optional<FunctionDef> findFunctionDefFromArgument(List<RegularArgument> arguments, int position) {
+      if (arguments.size() <= position) {
+        return Optional.empty();
+      }
+
+      RegularArgument argument = arguments.get(position);
+      Expression argumentExpr = argument.expression();
+      if (!(argumentExpr instanceof HasSymbol)) {
+        return Optional.empty();
+      }
+
+      Symbol symbol = ((HasSymbol) argumentExpr).symbol();
+      if (symbol == null) {
+        return Optional.empty();
+      }
+
+      return symbol.usages().stream()
+        .filter(usage -> usage.kind() == Usage.Kind.FUNC_DECLARATION)
+        .map(usage -> usage.tree().parent())
+        .filter(tree -> tree.is(Tree.Kind.FUNCDEF))
+        .map(FunctionDef.class::cast)
+        .findFirst();
+    }
+
+    @Override
+    public void visitCallExpression(CallExpression pyCallExpressionTree) {
+      Symbol callee = pyCallExpressionTree.calleeSymbol();
+      if (callee == null || !"property".equals(callee.name())) {
+        return;
+      }
+
+      List<Argument> argumentList = pyCallExpressionTree.arguments();
+      List<RegularArgument> regularArguments = argumentList.stream()
+        .filter(arg -> arg.is(Tree.Kind.REGULAR_ARGUMENT))
+        .map(RegularArgument.class::cast)
+        .collect(Collectors.toList());
+
+      // Do not bother with tuple arguments and keyword arguments
+      if (regularArguments.size() != argumentList.size() || regularArguments.stream().anyMatch(arg -> arg.keywordArgument() != null)) {
+        return;
+      }
+
+      PropertyAccessorTriple triple = new PropertyAccessorTriple();
+      triple.getter = findFunctionDefFromArgument(regularArguments, 0);
+      triple.setter = findFunctionDefFromArgument(regularArguments, 1);
+      triple.deleter = findFunctionDefFromArgument(regularArguments, 2);
+
+      propertyCallStyleProperties.add(triple);
+    }
+
+    @Override
+    public void visitFunctionDef(FunctionDef pyFunctionDefTree) {
+      // First check if the function definition has a @property decorator
+      boolean hasPropertyDecorator = pyFunctionDefTree.decorators().stream()
+        .map(decorator -> decorator.name().names())
+        .anyMatch(names -> names.size() == 1 && "property".equals(names.get(0).name()));
+
+      if (hasPropertyDecorator) {
+        decoratorStyleProperties.compute(pyFunctionDefTree.name().name(), (key, value) -> {
+          if (value == null) {
+            value = new PropertyAccessorTriple();
+          }
+          value.getter = Optional.of(pyFunctionDefTree);
+          return value;
+        });
+        return;
+      }
+
+      Optional<List<Name>> setterOrDeleterDecoratorNames = pyFunctionDefTree.decorators().stream()
+        .map(decorator -> decorator.name().names())
+        .filter(names -> names.size() == 2 && ("setter".equals(names.get(1).name()) || "deleter".equals(names.get(1).name())))
+        .findFirst();
+
+      setterOrDeleterDecoratorNames.ifPresent(names -> {
+        String propertyName = names.get(0).name();
+        String accessor = names.get(1).name();
+        decoratorStyleProperties.compute(propertyName, (key, value) -> {
+          if (value == null) {
+            // This should not happen in a valid python code (e.g. @foo.setter cannot be used before declaring foo), but be defensive.
+            value = new PropertyAccessorTriple();
+          }
+
+          if ("setter".equals(accessor)) {
+            value.setter = Optional.of(pyFunctionDefTree);
+          } else if ("deleter".equals(accessor)) {
+            value.deleter = Optional.of(pyFunctionDefTree);
+          }
+
+          return value;
+        });
+      });
+    }
+
+    @Override
+    public void visitClassDef(ClassDef pyClassDefTree) {
+      // Do not descend into nested classes
+    }
+
+    public List<PropertyAccessorTriple> propertyAccessors() {
+      return Stream.concat(this.propertyCallStyleProperties.stream(), this.decoratorStyleProperties.values().stream())
+        .collect(Collectors.toList());
+    }
+  }
+
+  private static long countRequiredParameters(FunctionDef functionDef) {
+    ParameterList parameterList = functionDef.parameters();
+    if (parameterList == null) {
+      return 0;
+    }
+
+    return parameterList.all().stream()
+      .filter(p -> p.is(Tree.Kind.TUPLE_PARAMETER)
+        || (p.is(Tree.Kind.PARAMETER) && ((Parameter) p).defaultValue() == null))
+      .count();
+  }
+
+  private static void checkOnlySelfParameter(SubscriptionContext ctx, FunctionDef functionDef, String messageTemplate) {
+    long actualParams = countRequiredParameters(functionDef);
+    if (actualParams > 1) {
+      ctx.addIssue(functionDef.defKeyword(), functionDef.rightPar(), String.format(messageTemplate, actualParams - 1));
+    }
+  }
+
+  private static void checkSetterParameters(SubscriptionContext ctx, FunctionDef functionDef) {
+    long requiredParameters = countRequiredParameters(functionDef);
+
+    if (requiredParameters > 2) {
+      ctx.addIssue(functionDef.defKeyword(), functionDef.rightPar(), String.format(
+        "Remove %d parameters; property setter methods receive \"self\" and a value.", requiredParameters - 2));
+    } else if (requiredParameters < 2 && TreeUtils.positionalParameters(functionDef).size() < 2) {
+      ctx.addIssue(functionDef.defKeyword(), functionDef.rightPar(), "Add the value parameter; property setter methods receive \"self\" and a value.");
+    }
+  }
+
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.CLASSDEF, ctx -> {
+      ClassDef classDef = (ClassDef) ctx.syntaxNode();
+
+      CollectPropertiesVisitor visitor = new CollectPropertiesVisitor();
+      classDef.body().accept(visitor);
+
+      List<PropertyAccessorTriple> propertyAccessors = visitor.propertyAccessors();
+      for (PropertyAccessorTriple triple : propertyAccessors) {
+        triple.getter.ifPresent(functionDef -> checkOnlySelfParameter(ctx, functionDef, "Remove %d parameters; property getter methods receive only \"self\"."));
+        triple.setter.ifPresent(functionDef -> checkSetterParameters(ctx, functionDef));
+        triple.deleter.ifPresent(functionDef -> checkOnlySelfParameter(ctx, functionDef, "Remove %d parameters; property deleter methods receive only \"self\"."));
+      }
+    });
+  }
+}
---
+++ python-checks/src/test/resources/checks/propertyAccessorParameterCount.py
@@ -0,0 +1,108 @@
+class A:
+    @property
+    def foo(self, unexpected): # Noncompliant {{Remove 1 parameters; property getter methods receive only "self".}}
+#   ^^^^^^^^^^^^^^^^^^^^^^^^^
+        return self._foo
+
+    @foo.setter
+    def foo(self, value, unexpected): # Noncompliant {{Remove 1 parameters; property setter methods receive "self" and a value.}}
+#   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        self._foo = value
+
+    @foo.deleter
+    def foo(self, unexpected): # Noncompliant {{Remove 1 parameters; property deleter methods receive only "self".}}
+        del self._foo
+
+class B:
+    def get_foo(self, unexpected):  # Noncompliant {{Remove 1 parameters; property getter methods receive only "self".}}
+        return self._foo
+
+    def set_foo(self):  # Noncompliant {{Add the value parameter; property setter methods receive "self" and a value.}}
+        self._foo = 1
+
+    def del_foo(self, unexpected):  # Noncompliant {{Remove 1 parameters; property deleter methods receive only "self".}}
+        del self._foo
+
+    foo = property(get_foo, set_foo, del_foo, "'foo' property.")
+
+class C:
+    @property
+    def foo(self):
+        return self._foo
+
+    @foo.setter
+    def foo(self, value):
+        self._foo = value
+
+    @foo.deleter
+    def foo(self, glob=1): # OK, default arguments do not count
+        del self._foo
+
+class D:
+    def get_foo(self):
+        return self._foo
+
+    def set_foo(self, value=None): # Ok
+        self._foo = value
+
+    def del_foo(self):
+        del self._foo
+
+    def del_foo2(self, *, k1, k2): # Noncompliant
+        pass
+
+    foo = property(get_foo, set_foo, del_foo, "'foo' property.")
+    foo2 = property(get_foo, set_foo, del_foo2)
+
+    def get_bar(self):
+        return self._bar
+
+    def set_bar(self, value, unexpected): # FN
+        self._bar = value
+
+    bar = property(fset=set_bar, fget=get_bar)
+
+class EdgeCase:
+    def get_foo(self):
+        pass
+
+    def set_foo(self, value):
+        pass
+
+    def del_foo(self):
+        pass
+    del_foo = 1
+
+    def set_foo2(self, (x, y)):
+        pass
+
+    def get_foo2():
+        pass
+
+    foo = property(get_foo, there_is_no_set_foo, 1 + 1)
+    foo2 = property(*(get_foo, set_foo))
+    foo3 = property(get_foo, set_foo, del_foo)
+    foo4 = unknown_callee(get_foo, set_foo, del_foo)
+    foo5 = property(get_foo2, set_foo2)
+
+    def some_function(x, y):
+        return x
+    some_function = 2
+
+    foo6 = some_function(get_foo2, set_foo2)
+
+    @staticmethod
+    def static_method():
+        pass
+
+    @some.decorator
+    def another_fun(self):
+        pass
+
+    @bar.setter
+    def bar(self, value):
+        self._foo = value
+
+    @property
+    def bar(self):
+        return self._foo
