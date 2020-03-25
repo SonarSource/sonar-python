40f4331560c4b54030cce719c54bdae76b287894
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2020-03-25 17:04:00 +0100

    SONARPY-587 Rule S1515: functions and lambdas should not reference va… (#658)

---
+++ python-checks/src/main/java/org/sonar/python/checks/FunctionUsingLoopVariableCheck.java
@@ -0,0 +1,213 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.symbols.Usage;
+import org.sonar.plugins.python.api.tree.AssignmentStatement;
+import org.sonar.plugins.python.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.ClassDef;
+import org.sonar.plugins.python.api.tree.ComprehensionExpression;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.FileInput;
+import org.sonar.plugins.python.api.tree.FunctionDef;
+import org.sonar.plugins.python.api.tree.FunctionLike;
+import org.sonar.plugins.python.api.tree.LambdaExpression;
+import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.ParameterList;
+import org.sonar.plugins.python.api.tree.ReturnStatement;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.plugins.python.api.tree.YieldStatement;
+import org.sonar.python.semantic.SymbolUtils;
+import org.sonar.python.tree.TreeUtils;
+
+@Rule(key = "S1515")
+public class FunctionUsingLoopVariableCheck extends PythonSubscriptionCheck {
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.FUNCDEF, FunctionUsingLoopVariableCheck::checkFunctionLike);
+    context.registerSyntaxNodeConsumer(Tree.Kind.LAMBDA, FunctionUsingLoopVariableCheck::checkFunctionLike);
+  }
+
+  private static void checkFunctionLike(SubscriptionContext ctx) {
+    FunctionLike functionLike = (FunctionLike) ctx.syntaxNode();
+    Tree enclosingLoop = enclosingLoop(functionLike);
+    if(enclosingLoop == null || !enclosingLoop.is(Tree.Kind.WHILE_STMT, Tree.Kind.FOR_STMT, Tree.Kind.GENERATOR_EXPR, Tree.Kind.LIST_COMPREHENSION)) {
+      return;
+    }
+    if (isReturnedOrCalledWithinLoop(functionLike, enclosingLoop)) {
+      return;
+    }
+    Set<Symbol> enclosingScopeSymbols = getEnclosingScopeSymbols(enclosingLoop);
+    for (Symbol symbol : enclosingScopeSymbols) {
+      List<Tree> problematicUsages = new ArrayList<>();
+      List<Tree> bindingUsages = new ArrayList<>();
+      for (Usage usage : symbol.usages()) {
+        Tree usageTree = usage.tree();
+        if (isUsedInFunctionLike(usageTree, functionLike) && !usage.isBindingUsage()) {
+          problematicUsages.add(usageTree);
+        }
+        if (usage.isBindingUsage() && isWithinEnclosingLoop(usageTree, enclosingLoop)) {
+          bindingUsages.add(usageTree);
+        }
+      }
+      reportIssue(ctx, functionLike, problematicUsages, bindingUsages);
+    }
+  }
+
+  private static void reportIssue(SubscriptionContext ctx, FunctionLike functionLike, List<Tree> problematicUsages, List<Tree> bindingUsages) {
+    if (!problematicUsages.isEmpty() && !bindingUsages.isEmpty()) {
+      PreciseIssue issue = ctx.addIssue(problematicUsages.get(0), "Pass this variable as a parameter with a default value.")
+        .secondary(bindingUsages.get(bindingUsages.size() - 1), "Assignment in the loop");
+      if (functionLike.is(Tree.Kind.FUNCDEF)) {
+        issue.secondary(((FunctionDef) functionLike).name(), "Function definition");
+      }
+    }
+  }
+
+  private static boolean isUsedInFunctionLike(Tree usageTree, FunctionLike functionLike) {
+    ParameterList parameters = functionLike.parameters();
+    if (parameters != null && parameters.nonTuple().stream().anyMatch(p -> usageTree.equals(p.defaultValue()))) {
+      return false;
+    }
+    return TreeUtils.hasDescendant(functionLike, tree -> tree.equals(usageTree));
+  }
+
+  private static boolean isWithinEnclosingLoop(Tree usageTree, Tree enclosingLoop) {
+    return TreeUtils.hasDescendant(enclosingLoop, tree -> tree.equals(usageTree));
+  }
+
+  private static Set<Symbol> getEnclosingScopeSymbols(Tree enclosingLoop) {
+    if (enclosingLoop.is(Tree.Kind.LIST_COMPREHENSION) || enclosingLoop.is(Tree.Kind.GENERATOR_EXPR)) {
+      return ((ComprehensionExpression) enclosingLoop).localVariables();
+    }
+    Tree enclosingScope = TreeUtils.firstAncestor(enclosingLoop, tree -> tree.is(Tree.Kind.FUNCDEF, Tree.Kind.CLASSDEF, Tree.Kind.FILE_INPUT, Tree.Kind.LIST_COMPREHENSION,
+      Tree.Kind.GENERATOR_EXPR));
+    if (enclosingScope == null) {
+      return Collections.emptySet();
+    }
+    if (enclosingScope.is(Tree.Kind.FUNCDEF)) {
+      return ((FunctionLike) enclosingScope).localVariables();
+    }
+    if (enclosingScope.is(Tree.Kind.CLASSDEF)) {
+      return ((ClassDef) enclosingScope).classFields();
+    }
+    return ((FileInput) enclosingScope).globalVariables();
+  }
+
+  private static Tree enclosingLoop(FunctionLike functionLike) {
+    return TreeUtils.firstAncestor(functionLike, t -> t.is(Tree.Kind.FUNCDEF, Tree.Kind.CLASSDEF, Tree.Kind.WHILE_STMT, Tree.Kind.FOR_STMT,
+      Tree.Kind.RETURN_STMT, Tree.Kind.YIELD_STMT, Tree.Kind.GENERATOR_EXPR, Tree.Kind.COMP_FOR, Tree.Kind.LIST_COMPREHENSION));
+  }
+
+  private static boolean isReturnedOrCalledWithinLoop(FunctionLike functionLike, Tree enclosingLoop) {
+    Tree parentCallExpr = TreeUtils.firstAncestor(functionLike, t -> !t.is(Tree.Kind.PARENTHESIZED));
+    if (parentCallExpr != null && parentCallExpr.is(Tree.Kind.CALL_EXPR)) {
+      return true;
+    }
+    CallOrReturnVisitor callOrReturnVisitor = new CallOrReturnVisitor(functionLike, enclosingLoop);
+    enclosingLoop.accept(callOrReturnVisitor);
+    return callOrReturnVisitor.isReturned || callOrReturnVisitor.isCalled;
+  }
+
+  static class CallOrReturnVisitor extends BaseTreeVisitor {
+    Tree enclosingLoop;
+    FunctionLike functionLike;
+    boolean isReturned = false;
+    boolean isCalled = false;
+
+    public CallOrReturnVisitor(FunctionLike functionLike, Tree enclosingLoop) {
+      this.functionLike = functionLike;
+      this.enclosingLoop = enclosingLoop;
+    }
+
+    @Override
+    public void visitCallExpression(CallExpression callExpression) {
+      Symbol calleeSymbol = callExpression.calleeSymbol();
+      if (calleeSymbol != null) {
+        if (functionLike.is(Tree.Kind.FUNCDEF)) {
+          isCalled |= calleeSymbol.equals(((FunctionDef) functionLike).name().symbol());
+        } else {
+          // lambda expression
+          Name name = variableAssigned((LambdaExpression) functionLike);
+          if (name != null) {
+            isCalled |= calleeSymbol.equals(name.symbol());
+          }
+        }
+      }
+      super.visitCallExpression(callExpression);
+    }
+
+    @Override
+    public void visitReturnStatement(ReturnStatement returnStatement) {
+      isReturned |= isFunctionLikeReturned(returnStatement);
+    }
+
+    @Override
+    public void visitYieldStatement(YieldStatement yieldStatement) {
+      isReturned |= isFunctionLikeReturned(yieldStatement);
+    }
+
+    private static Name variableAssigned(LambdaExpression lambdaExpression) {
+      Tree parentAssignment = TreeUtils.firstAncestorOfKind(lambdaExpression, Tree.Kind.ASSIGNMENT_STMT);
+      if (parentAssignment != null) {
+        AssignmentStatement assignmentStatement = (AssignmentStatement) parentAssignment;
+        if (assignmentStatement.lhsExpressions().get(0).expressions().get(0).is(Tree.Kind.NAME)) {
+          Name name = (Name) assignmentStatement.lhsExpressions().get(0).expressions().get(0);
+          Expression expression = Expressions.singleAssignedValue(name);
+          if (expression != null && expression.equals(lambdaExpression)) {
+            return name;
+          }
+        }
+      }
+      return null;
+    }
+
+    private boolean isFunctionLikeReturned(Tree yieldOrReturnTree) {
+      if (functionLike.is(Tree.Kind.FUNCDEF)) {
+        Symbol functionSymbol = ((FunctionDef) functionLike).name().symbol();
+        return TreeUtils.hasDescendant(yieldOrReturnTree, d -> TreeUtils.getSymbolFromTree(d).filter(symbol -> symbol.equals(functionSymbol)).isPresent());
+      } else {
+        // lambda expression
+        return isLambdaReturned((LambdaExpression) functionLike, yieldOrReturnTree);
+      }
+    }
+
+    private static boolean isLambdaReturned(LambdaExpression lambdaExpression, Tree yieldOrReturnTree) {
+      Tree parentAssignment = TreeUtils.firstAncestorOfKind(lambdaExpression, Tree.Kind.ASSIGNMENT_STMT);
+      if (parentAssignment != null) {
+        AssignmentStatement assignmentStatement = (AssignmentStatement) parentAssignment;
+        // If the lambda expression is used to construct a returned variable, we don't raise issues to avoid FPs, even if the lambda is not returned explicitly
+        return SymbolUtils.assignmentsLhs(assignmentStatement).stream()
+          .map(TreeUtils::getSymbolFromTree)
+          .anyMatch(symbol -> TreeUtils.hasDescendant(yieldOrReturnTree, d -> TreeUtils.getSymbolFromTree(d).equals(symbol)));
+      }
+      return false;
+    }
+  }
+}
---
+++ python-checks/src/test/resources/checks/functionUsingLoopVariable.py
@@ -0,0 +1,189 @@
+def simple_reference():
+  for i in range(10):
+    print(i)
+
+
+def simple_reference2():
+  return [j for j in range(5)]
+
+
+def default_value():
+  """Passing the variable's value via a default value is ok."""
+  mylist = []
+  for i in range(5):
+    mylist.append(lambda i=i: i)
+
+    def func(i=i):
+      return i
+    mylist.append(func)
+
+
+def not_using_outer_variables():
+  """Shadowing outer variables with parameters is ok."""
+  mylist = []
+  for j in range(5):
+    print(j)
+    mylist.append(lambda j: j)
+    def func(j):
+      return j
+    mylist.append(func)
+
+
+def return_lambda(param):
+  """Exception: returning a lambda/function makes it ok.
+  The variable will not change its value as there are no more iterations.
+  """
+  for j in range(5):
+    if param:
+      return lambda: j
+    elif x:
+      def func():
+        return j
+      return func
+    else:
+      lamb = lambda: j
+      return lamb
+  return lambda: 42
+
+def multiple_assignment_yield():
+  x = 42
+  if cond:
+    yield x
+  x = bar()
+  yield x
+  for i in range(10):
+    x = foo(bar(lambda: i))
+    yield x
+
+def variable_ref_and_func_ref():
+  """Referencing a variable updated in the enclosing loop and passing the function via reference is suspicious."""
+  mylist = []
+  for j in range(5):  # Secondary location on "j", it is the only code updating j.
+#     ^> {{Assignment in the loop}}
+    mylist.append(lambda: j)  # Noncompliant {{Pass this variable as a parameter with a default value.}}
+#                         ^
+    def func():
+      return j  # Noncompliant
+    mylist.append(func)
+
+
+def list_comprehension_lambda_referenced():
+  """Referencing a variable updated in the enclosing comprehension and returning a reference to the function is suspicious."""
+  # Secondary location on "j" of "for j"
+  return [lambda: j for j in range(5)]  # Noncompliant
+#                 ^     ^<
+
+
+def list_comprehension_lambda_called():
+  """Referencing a variable updated in the enclosing comprehension and calling the function in the comprehension is ok.
+  This should happen rarely but is a special case of functions called in loops.
+  """
+  return [(lambda: j)(5) for j in range(5)]  # OK
+
+
+def all_iterating_variable():
+  """All referenced variable should be considered."""
+  mylist = []
+  for i, j in zip(range(5), range(5)):
+    computed = j * 2  # Secondary location on "computed", it is the only code updating computed.
+#   ^^^^^^^^> {{Assignment in the loop}}
+    mylist.append(lambda: computed)  # Noncompliant
+    mylist.append(lambda: j)  # Noncompliant
+
+    def func1():
+#       ^^^^^> {{Function definition}}
+      return computed # Noncompliant {{Pass this variable as a parameter with a default value.}}
+#            ^^^^^^^^
+    mylist.append(func1)
+
+    def func2():
+      return j # Noncompliant
+    mylist.append(func2)
+
+
+def generator_expression():
+  """Generator expressions are ok when they don't reference outer variables."""
+  return (j for j in range(5))
+
+def lambda_in_generator(param):
+  """lambdas defined in generator expressions are suspicious enven if it could raise False Positives.
+  """
+  gen = (lambda: i for i in range(3))  # Noncompliant
+  if param:
+    return [func() for func in gen]  # This would return [0, 1, 2]
+  else:
+    funcs = [func for func in gen]
+    return [func() for func in funcs]  # This would return [2, 2, 2]
+
+
+def function_called_in_loop():
+  """Exception: Lambda/Functions defined and called in the same loop are ok.
+  Having this exception means that we won't detect issues if a different iteration calls the function.
+  This False Negative is ok.
+  """
+  for i in range(10):
+    print((lambda param: param * i)(42))
+    def func(param):
+      return param * i
+    print(func(42))
+
+
+def lambda_variable_called_in_loop():
+  """We should make an exception when the lamda is saved in a variable and called in the same loop."""
+  for j in range(5):
+    lamb = lambda: j  # OK
+    lamb()
+
+
+def lambda_variable_not_called_in_loop():
+  """If the lambda is saved in a variable which is not called, then an issue should be raised."""
+  mylist = []
+  for j in range(5):
+    lamb = lambda: j  # Noncompliant
+    mylist.append(lamb)
+  return mylist
+
+
+def lambda_variable_called_outside_the_loop():
+  for j in range(5):
+#     ^>
+    if j == 2:
+      lamb = lambda: j  # Noncompliant
+#                    ^
+  print(lamb())
+
+
+def no_binding_usage_in_loop():
+  x = 42
+  for _ in range(10):
+    def func():
+      foo(x)
+
+def lambda_assigned_to_tuple():
+  for j in range(10):
+    (a, b) = (lambda: j, foo)
+    return a
+
+def returned_lambda_multiple_assignment():
+  for j in range(10):
+    var = 42
+    var = lambda: j # OK
+    return var
+
+def returned_lambda_multiple_assignment_fn():
+  for j in range(10):
+    if param:
+      var = lambda: j # FN
+    else:
+      var = 42
+      return var
+
+class ClassEnclosingScope:
+  for j in range(10):
+    def func(self):
+      return self.j # Noncompliant
+
+#Global scope
+for j in range(10):
+  def func():
+    return j # Noncompliant
