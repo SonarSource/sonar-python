26c064ed55786827e8eb4bf2788eb4a478d7d2e4
Elena Vilchik <elena.vilchik@sonarsource.com>
2020-05-04 12:14:54 +0200

    SONARPY-682 Add parser for Python code to plugin API

---
+++ python-frontend/src/main/java/org/sonar/plugins/python/api/Parser.java
@@ -0,0 +1,39 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.python.api;
+
+import com.sonar.sslr.api.AstNode;
+import org.sonar.plugins.python.api.tree.FileInput;
+import org.sonar.python.parser.PythonParser;
+import org.sonar.python.tree.PythonTreeMaker;
+
+/**
+ * Use this class to parse Python module
+ */
+public class Parser {
+
+  private PythonParser astParser = PythonParser.create();
+
+  public FileInput parse(String code)  {
+    AstNode astNode = astParser.parse(code);
+    return new PythonTreeMaker().fileInput(astNode);
+  }
+
+}
---
+++ python-frontend/src/test/java/org/sonar/plugins/python/api/ParserTest.java
@@ -0,0 +1,46 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.python.api;
+
+import com.sonar.sslr.api.RecognitionException;
+import org.junit.Test;
+import org.sonar.plugins.python.api.tree.FileInput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ParserTest {
+
+  @Test
+  public void test() {
+    Parser parser = new Parser();
+
+    FileInput result = parser.parse("");
+    assertThat(result.statements()).isNull();
+
+    result = parser.parse("foo()");
+    assertThat(result.statements().statements()).hasSize(1);
+  }
+
+  @Test(expected = RecognitionException.class)
+  public void parsing_error() {
+    Parser parser = new Parser();
+    parser.parse("foo(");
+  }
+}
