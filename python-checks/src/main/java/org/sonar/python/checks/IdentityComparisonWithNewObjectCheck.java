fc4711b211a8561682ff1e9bf86df22637084ed1
Andrey Tyukin <andrey.tyukin@sonarsource.com>
2020-05-15 09:47:19 +0200

    SONARPY-674 Rule S5796: identity comparisons with new objects

---
+++ python-checks/src/main/java/org/sonar/python/checks/IdentityComparisonWithNewObjectCheck.java
@@ -0,0 +1,153 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.sonar.python.checks;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.symbols.Usage;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.IsExpression;
+import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.Token;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.tree.TreeUtils;
+
+import static java.util.Arrays.asList;
+
+// https://jira.sonarsource.com/browse/RSPEC-5796
+// https://jira.sonarsource.com/browse/SONARPY-674
+@Rule(key = "S5796")
+public class IdentityComparisonWithNewObjectCheck extends PythonSubscriptionCheck {
+  private static final String MESSAGE_IS = "Replace this \"is\" operator with \"==\".";
+  private static final String MESSAGE_IS_NOT = "Replace this \"is not\" operator with \"!=\".";
+  private static final String MESSAGE_SECONDARY = "This expression creates a new object every time.";
+
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.IS, IdentityComparisonWithNewObjectCheck::checkIsComparison);
+  }
+
+  private static void checkIsComparison(SubscriptionContext subscriptionContext) {
+    final IsExpression isExpr = (IsExpression) subscriptionContext.syntaxNode();
+    // The `if` merely ensures that an issue is reported at most once per operator.
+    if (!checkOperand(isExpr.leftOperand(), isExpr, subscriptionContext)) {
+      checkOperand(isExpr.rightOperand(), isExpr, subscriptionContext);
+    }
+  }
+
+  /**
+   * Checks a single operand of an <code>is/is not</code>-comparison.
+   * Returns <code>true</code> if it finds an issue.
+   */
+  private static boolean checkOperand(Expression operand, IsExpression isExpr, SubscriptionContext ctx) {
+    Optional<List<Tree>> secondariesOpt = findIssueForOperand(operand);
+    secondariesOpt.ifPresent(secondaryLocations -> {
+      Token notToken = isExpr.notToken();
+      PreciseIssue preciseIssue = notToken == null ?
+        ctx.addIssue(isExpr.operator(), MESSAGE_IS) :
+        ctx.addIssue(isExpr.operator(), notToken, MESSAGE_IS_NOT);
+      for (Tree secondary : secondaryLocations) {
+        preciseIssue.secondary(secondary, MESSAGE_SECONDARY);
+      }
+    });
+    return secondariesOpt.isPresent();
+  }
+
+  /**
+   * Checks whether an operand of an <code>is</code>-comparison is suitable.
+   *
+   * @param expr the operand
+   * @return An empty option if there is no issue, or an option containing the list of relevant secondary expressions.
+   */
+  private static Optional<List<Tree>> findIssueForOperand(Expression expr) {
+    if (instantiatesFreshObject(expr)) {
+      // Issue exists, but the list of secondaries is empty.
+      return Optional.of(Collections.emptyList());
+    } else if (expr.is(Tree.Kind.NAME)) {
+      Name name = (Name) expr;
+      Symbol symb = name.symbol();
+      if (symb != null) {
+        Expression rhs = Expressions.singleAssignedValue((Name) expr);
+        if (rhs != null && instantiatesFreshObject(rhs) && cannotEscape(symb)) {
+          // Issue exists, right hand side of the assignment that defines the variable is the secondary position
+          return Optional.of(Collections.singletonList(rhs));
+        }
+      }
+    }
+    return Optional.empty();
+  }
+
+  /**
+   * Fully qualified names of constructors and functions that are guaranteed to return references that aren't shared
+   * anywhere else, and are thus unsuitable for a comparison with an <code>is</code>, because the comparison would
+   * always return <code>False</code>.
+   *
+   * Note that these are fully qualified names of (value-level) expressions, not types.
+   */
+  private static final Set<String> FUNCTIONS_RETURNING_UNIQUE_REF =
+    new HashSet<>(asList("dict", "list", "set", "complex"));
+
+  /**
+   * Checks whether an expression is guaranteed to instantiate a fresh object with a reference that has not been
+   * shared anywhere.
+   */
+  private static boolean instantiatesFreshObject(Expression expr) {
+    switch (expr.getKind()) {
+      case DICTIONARY_LITERAL:
+      case DICT_COMPREHENSION:
+      case LIST_LITERAL:
+      case LIST_COMPREHENSION:
+      case SET_LITERAL:
+      case SET_COMPREHENSION:
+        return true;
+      case CALL_EXPR:
+        Symbol calleeSymbol = ((CallExpression) expr).calleeSymbol();
+        if (calleeSymbol != null) {
+          return FUNCTIONS_RETURNING_UNIQUE_REF.contains(calleeSymbol.fullyQualifiedName());
+        }
+        return false;
+      default:
+        return false;
+    }
+  }
+
+  /**
+   * Checks that the reference stored in a variable cannot escape.
+   */
+  private static boolean cannotEscape(Symbol symb) {
+    List<Usage> usages = symb.usages();
+    if (usages.size() > 2) {
+      // Check that all usages are either assignments or `is`-comparisons.
+      return usages.stream().allMatch(u -> u.isBindingUsage() ||
+        TreeUtils.firstAncestorOfKind(u.tree(), Tree.Kind.IS) != null);
+    }
+    return true;
+  }
+
+}
---
+++ python-checks/src/test/resources/checks/identityComparisonWithNewObjects.py
@@ -0,0 +1,129 @@
+def basicPositives(param):
+    param is {1: 2}  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^ 
+    param is dict(a=1)  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+    v = []
+    param is v  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+
+def basicNegatives(param):
+    param == {1: 2}
+    param != {1, 2, 3}
+    param == [1, 2, 3]
+    param == dict(a=1)
+    mylist = []
+    param == mylist
+
+def comprehensions(p):
+  p is { x: x for x in range(10) } # Noncompliant {{Replace this "is" operator with "==".}}
+  # ^^
+  p is not [ x for x in range(10) ] # Noncompliant {{Replace this "is not" operator with "!=".}}
+  # ^^^^^^
+  p is { x for x in range(10) } # Noncompliant {{Replace this "is" operator with "==".}}
+  # ^^
+
+def escape_00(p):
+  d = dict(a=100)
+  d is p # Noncompliant {{Replace this "is" operator with "==".}}
+  # ^^
+
+def escape_01(p):
+  d = dict(a=100)
+  save("something", d)
+  e = load("something")
+  e is d # ok, `d` could have escaped, and actually ended up in `e`
+
+def escape_02(p):
+  d = list(1, 2, 3)
+  e = d
+  e is not d # ok, `d` really did end up in `e`.
+
+def escape_03(p):
+  x = dict(a = 1)
+  #   ^^^^^^^^^^^> 1 {{This expression creates a new object every time.}}
+  x is p # Noncompliant {{Replace this "is" operator with "==".}}
+  # ^^
+  p is x # Noncompliant {{Replace this "is" operator with "==".}}
+  # ^^
+
+def coverage():
+  a[b] is b[a]
+
+x is y # repeatedly checked: required for coverage of an `if (symb != null) {`
+
+# rest is mutably borrowed from `expected-issues/python/src/RSPEC_5796`.
+def literal_comparison(param):
+    # dict
+    param is {1: 2, 3: 4}  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+    # list
+    param is [1, 2, 3]  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+    # set
+    param is {1, 2, 3}  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+
+    # issues are also raised for "is not"
+    param is not {1: 2, 3: 4}  # Noncompliant {{Replace this "is not" operator with "!=".}}
+    #     ^^^^^^
+    param is not [1, 2, 3]  # Noncompliant {{Replace this "is not" operator with "!=".}}
+    #     ^^^^^^
+    param is not {1, 2, 3}  # Noncompliant {{Replace this "is not" operator with "!=".}}
+    #     ^^^^^^
+
+    # issues are raised when literals are on the right or on the left of the operator
+    {1} is param  # Noncompliant {{Replace this "is" operator with "==".}}
+    #   ^^
+
+
+def builtin_constructor(param):
+    """We can't raise on every constructor because some might always return the same object (singletons).
+    However we can raise on builtin functions because we know that they won't return the same value.
+    """
+    # dict
+    param is dict(a=2, b=3)  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+    # list
+    param is list({4, 5, 6})  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+    # set
+    param is set([1, 2, 3])  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+    # complex
+    param is complex(1, 2)  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+
+glob = 5
+def variable(param):
+    mylist = []
+    param is mylist  # Noncompliant {{Replace this "is" operator with "==".}}
+    #     ^^
+
+    referenced = []
+    def referencing():
+        nonlocal referenced
+        referenced = param
+        param is referenced  # No issue
+
+    referencing()
+    param is referenced  # No issue
+
+    reassigned = []
+    reassigned = param
+    param is reassigned  # No issue
+
+    global glob
+    param is glob  # No issue (this might/should break once symbols are resolved outside of function definitions)
+
+
+def locations_and_messages(param):
+    param is {1: 2, 3: 4}  # Noncompliant {{Replace this "is" operator with "==".}}
+#         ^^
+
+    {1: 2, 3: 4} is not complex(1, 2)  # Noncompliant {{Replace this "is not" operator with "!=".}}
+#                ^^^^^^
+
+    mylist = [] # secondary should be here
+    param is mylist  # Noncompliant {{Replace this "is" operator with "==".}}
+#         ^^
