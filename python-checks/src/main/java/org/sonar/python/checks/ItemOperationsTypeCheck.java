cfd5c9c0a907100a0e77de5775d1c656f131219c
Andrea Guarino <andrea.guarino@sonarsource.com>
2020-08-18 12:29:36 +0200

    SONARPY-744 RSPEC-5864 Item operations should be done on objects supporting them

--- python-checks/src/main/java/org/sonar/python/checks/ItemOperationsTypeCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/ItemOperationsTypeCheck.java
@@ -19,88 +19,29 @@
  */
 package org.sonar.python.checks;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.LocationInFile;
-import org.sonar.plugins.python.api.PythonSubscriptionCheck;
-import org.sonar.plugins.python.api.SubscriptionContext;
 import org.sonar.plugins.python.api.symbols.ClassSymbol;
 import org.sonar.plugins.python.api.symbols.FunctionSymbol;
 import org.sonar.plugins.python.api.symbols.Symbol;
-import org.sonar.plugins.python.api.tree.AssignmentStatement;
 import org.sonar.plugins.python.api.tree.CallExpression;
-import org.sonar.plugins.python.api.tree.DelStatement;
 import org.sonar.plugins.python.api.tree.Expression;
 import org.sonar.plugins.python.api.tree.HasSymbol;
-import org.sonar.plugins.python.api.tree.Name;
-import org.sonar.plugins.python.api.tree.SubscriptionExpression;
 import org.sonar.plugins.python.api.tree.Tree;
 import org.sonar.plugins.python.api.types.InferredType;
-import org.sonar.python.tree.TreeUtils;
 
 import static org.sonar.plugins.python.api.symbols.Symbol.Kind.CLASS;
 import static org.sonar.plugins.python.api.symbols.Symbol.Kind.FUNCTION;
 import static org.sonar.python.types.InferredTypes.typeClassLocation;
 
 @Rule(key = "S5644")
-public class ItemOperationsTypeCheck extends PythonSubscriptionCheck {
-
-  private static final List<String> NON_DELITEM_TYPES = Arrays.asList("frozenset", "memoryview");
-  private static final List<String> NON_GET_SET_ITEM_TYPES = Collections.singletonList("frozenset");
+public class ItemOperationsTypeCheck extends ItemOperationsType {
 
   @Override
-  public void initialize(Context context) {
-    context.registerSyntaxNodeConsumer(Tree.Kind.SUBSCRIPTION, ItemOperationsTypeCheck::checkSubscription);
-  }
-
-  private static void checkSubscription(SubscriptionContext ctx) {
-    SubscriptionExpression subscriptionExpression = (SubscriptionExpression) ctx.syntaxNode();
-    if (isWithinTypeAnnotation(subscriptionExpression)) {
-      return;
-    }
-    List<LocationInFile> secondaries = new ArrayList<>();
-    Expression subscriptionObject = subscriptionExpression.object();
-    if (isWithinDelStatement(subscriptionExpression)) {
-      if (!isValidSubscription(subscriptionObject, "__delitem__", null, NON_DELITEM_TYPES, secondaries)) {
-        reportIssue(subscriptionExpression, subscriptionObject, "__delitem__", ctx, secondaries);
-      }
-      return;
-    }
-    if (isWithinAssignment(subscriptionExpression)) {
-      if (!isValidSubscription(subscriptionObject, "__setitem__", null, NON_GET_SET_ITEM_TYPES, secondaries)) {
-        reportIssue(subscriptionExpression, subscriptionObject, "__setitem__", ctx, secondaries);
-      }
-      return;
-    }
-    if (!isValidSubscription(subscriptionObject, "__getitem__", "__class_getitem__", NON_GET_SET_ITEM_TYPES, secondaries)) {
-      reportIssue(subscriptionExpression, subscriptionObject, "__getitem__", ctx, secondaries);
-    }
-  }
-
-  private static boolean isWithinTypeAnnotation(SubscriptionExpression subscriptionExpression) {
-    return TreeUtils.firstAncestor(subscriptionExpression,
-      t -> t.is(Tree.Kind.PARAMETER_TYPE_ANNOTATION, Tree.Kind.RETURN_TYPE_ANNOTATION, Tree.Kind.VARIABLE_TYPE_ANNOTATION)) != null;
-  }
-
-  private static boolean isWithinDelStatement(SubscriptionExpression subscriptionExpression) {
-    return TreeUtils.firstAncestor(subscriptionExpression,
-      t -> t.is(Tree.Kind.DEL_STMT) && ((DelStatement) t).expressions().stream()
-        .anyMatch(e -> e.equals(subscriptionExpression))) != null;
-  }
-
-  private static boolean isWithinAssignment(SubscriptionExpression subscriptionExpression) {
-    return TreeUtils.firstAncestor(subscriptionExpression,
-      t -> t.is(Tree.Kind.ASSIGNMENT_STMT) && ((AssignmentStatement) t).lhsExpressions().stream().flatMap(lhs -> lhs.expressions().stream())
-        .anyMatch(e -> e.equals(subscriptionExpression))) != null;
-  }
-
-  private static boolean isValidSubscription(Expression subscriptionObject, String requiredMethod, @Nullable String classRequiredMethod,
-                                             List<String> invalidTypes, List<LocationInFile> secondaries) {
+  public boolean isValidSubscription(Expression subscriptionObject, String requiredMethod, @Nullable String classRequiredMethod,
+                                     List<LocationInFile> secondaries) {
 
     if (subscriptionObject.is(Tree.Kind.GENERATOR_EXPR)) {
       return false;
@@ -124,7 +65,15 @@ public class ItemOperationsTypeCheck extends PythonSubscriptionCheck {
     }
     InferredType type = subscriptionObject.type();
     secondaries.add(typeClassLocation(type));
-    return type.canHaveMember(requiredMethod) && invalidTypes.stream().noneMatch(type::canOnlyBe);
+    return type.canHaveMember(requiredMethod);
+  }
+
+  @Override
+  public String message(@Nullable String name, String missingMethod) {
+    if (name != null) {
+      return String.format("Fix this code; \"%s\" does not have a \"%s\" method.", name, missingMethod);
+    }
+    return String.format("Fix this code; this expression does not have a \"%s\" method.", missingMethod);
   }
 
   private static boolean isTypingSymbol(Symbol symbol) {
@@ -142,25 +91,5 @@ public class ItemOperationsTypeCheck extends PythonSubscriptionCheck {
     return classSymbol.canHaveMember(requiredMethod)
       || (classRequiredMethod != null && classSymbol.canHaveMember(classRequiredMethod))
       || classSymbol.hasDecorators();
-  }
-
-  private static void reportIssue(SubscriptionExpression subscriptionExpression, Expression subscriptionObject,
-                                  String missingMethod, SubscriptionContext ctx, List<LocationInFile> secondaries) {
-
-    String name = nameFromExpression(subscriptionObject);
-    PreciseIssue preciseIssue;
-    if (name != null) {
-      preciseIssue = ctx.addIssue(subscriptionExpression, String.format("Fix this code; \"%s\" does not have a \"%s\" method.", name, missingMethod));
-    } else {
-      preciseIssue = ctx.addIssue(subscriptionObject, String.format("Fix this code; this expression does not have a \"%s\" method.", missingMethod));
-    }
-    secondaries.stream().filter(Objects::nonNull).forEach(locationInFile -> preciseIssue.secondary(locationInFile, null));
-  }
-
-  private static String nameFromExpression(Expression expression) {
-    if (expression.is(Tree.Kind.NAME)) {
-      return ((Name) expression).name();
-    }
-    return null;
   }
 }
---
+++ python-checks/src/test/resources/checks/confusingTypeChecking/itemOperations.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class A: ...
+
+def custom(param: A):
+  param[42] # Noncompliant {{Fix this "__getitem__" operation; Previous type checks suggest that "param" does not have this method.}}
+  param[42] = 42 # Noncompliant {{Fix this "__setitem__" operation; Previous type checks suggest that "param" does not have this method.}}
+  del param[42] # Noncompliant {{Fix this "__delitem__" operation; Previous type checks suggest that "param" does not have this method.}}
+
+def builtin(param1: memoryview, param2: frozenset, param3: List[int]):
+  del param1[0]  # Noncompliant
+  param2[42] = 42 # Noncompliant
+  param3[0]
+
+def derived(param1: int, param2: int):
+  (param1 + param2)[0] # Noncompliant {{Fix this "__getitem__" operation; Previous type checks suggest that this expression does not have this method.}}
