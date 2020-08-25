320b11079860add9badbdc428cc4c493184fc80b
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2020-08-25 18:03:45 +0200

    SONARPY-690 Update default regex for S101 to not raise on valid PEP-8… (#854)

--- python-checks/src/test/resources/checks/className.py
+++ python-checks/src/test/resources/checks/className.py
@@ -1,9 +1,19 @@
 class MyClassWithCompliantName1:
 	pass
 
-class MyClass_WithNotCompliantName1: # Noncompliant {{Rename class "MyClass_WithNotCompliantName1" to match the regular expression ^[A-Z_][a-zA-Z0-9]+$.}}
+class MyClass_WithNotCompliantName1: # Noncompliant {{Rename class "MyClass_WithNotCompliantName1" to match the regular expression ^_?([A-Z_][a-zA-Z0-9]*|[a-z_][a-z0-9_]*)$.}}
 #     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 	pass
 
 class myClassWithNotCompliantName2: # Noncompliant
 	pass
+
+
+class __FooBar:  # OK
+    pass
+
+class my_decorator:  # OK
+  def __enter__(self):
+    ...
+  def __exit__(self, type, value, traceback):
+    ...
