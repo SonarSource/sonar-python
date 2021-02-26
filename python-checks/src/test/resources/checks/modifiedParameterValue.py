ae9c608b3e5d1eb260fa0619a0801509599a9b17
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2021-02-26 09:52:27 +0100

    S5717: Adapt secondary message depending on type of violation

--- python-checks/src/test/resources/checks/modifiedParameterValue.py
+++ python-checks/src/test/resources/checks/modifiedParameterValue.py
@@ -73,6 +73,10 @@ class B:
   def referenced_outside(self, param1 = list(), param2=list()): # Noncompliant 2
       self.l = param1
       self[l] = param2
+  def referenced_outside_secondary(self, param1 = list(), param2=list()): # Noncompliant {{Change this default value to "None" and initialize this parameter inside the function/method.}}
+#                                        ^^^^^^^^^^^^^^^
+      self.l = param1
+#     ^^^^^^^^^^^^^^^< {{The parameter is stored in another object.}}
   def referenced_outside_2(self, param = list()): # OK, not on self
       global g_var
       g_var = param
