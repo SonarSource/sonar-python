7028477590decdae8c55d88a7dc320544caf7275
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2021-07-15 16:14:30 +0200

    SONARPY-873 Run Typeshed serializer on Cirrus (#943)

---
+++ .cirrus/python-env.Dockerfile
@@ -0,0 +1,16 @@
+FROM gcr.io/language-team/base:latest
+USER root
+ENV PYTHON_VERSION=3.9.5
+RUN apt-get update && apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+RUN curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz
+RUN tar -xf Python-${PYTHON_VERSION}.tar.xz
+RUN cd Python-${PYTHON_VERSION} && ./configure --enable-optimizations && make -j 4 && make altinstall
+RUN cd /usr/local/bin \
+    && ln -s python3.9 python \
+    && ln -s python3.9 python3 \
+    && ln -s pip3.9 pip \
+    && ln -s pip3.9 pip3
+RUN python3.9 -m pip install --upgrade pip
+USER sonarsource
+RUN pip install tox
+ENV PATH=${PATH}:/home/sonarsource/.local/bin
--- python-frontend/typeshed_serializer/tests/test_typeshed_serializer.py
+++ python-frontend/typeshed_serializer/tests/test_typeshed_serializer.py
@@ -1,52 +1,53 @@
-import unittest
-
+import pytest
 import mypy.nodes as mpn
 
 from serializer import typeshed_serializer, symbols
 
 
-class TypeshedSerializerTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.build_result = typeshed_serializer.walk_typeshed_stdlib()
-
-    def test_build_mypy_model(self):
-        self.assertIsNotNone(self.build_result)
-
-    def test_module_symbol(self):
-        abc_module = self.build_result.files.get("abc")
-        module_symbol = symbols.ModuleSymbol(abc_module)
-        self.assertEqual(module_symbol.fullname, "abc")
-        self.assertEqual(len(module_symbol.classes), 3)
-        self.assertEqual(len(module_symbol.functions), 4)
-
-    def test_class_symbol(self):
-        mypy_cmd_module = self.build_result.files.get("cmd")
-        mypy_cmd_class = mypy_cmd_module.names.get("Cmd")
-        cmd_class_symbol = symbols.ClassSymbol(mypy_cmd_class.node)
-        self.assertEqual(cmd_class_symbol.fullname, "cmd.Cmd")
-        self.assertEqual(cmd_class_symbol.name, "Cmd")
-        self.assertEqual(cmd_class_symbol.super_classes, ["builtins.object"])
-        self.assertEqual(len(cmd_class_symbol.mro), 0)
-
-    def test_function_symbol(self):
-        mypy_cmd_module = self.build_result.files.get("cmd")
-        mypy_cmd_class_node = mypy_cmd_module.names.get("Cmd").node
-        self.assertIsInstance(mypy_cmd_class_node, mpn.TypeInfo)
-        mypy_completenames_method_node = mypy_cmd_class_node.names.get("completenames").node
-        self.assertIsInstance(mypy_completenames_method_node, mpn.FuncDef)
-        completenames_method_symbol = symbols.FunctionSymbol(mypy_completenames_method_node)
-        self.assertEqual(completenames_method_symbol.name, "completenames")
-        self.assertEqual(completenames_method_symbol.fullname, "cmd.Cmd.completenames")
-        self.assertFalse(completenames_method_symbol.has_decorators)
-        self.assertFalse(completenames_method_symbol.is_asynchronous)
-        self.assertEqual(completenames_method_symbol.return_type.pretty_printed_name, "builtins.list[builtins.str]")
-        self.assertEqual(len(completenames_method_symbol.resolved_decorator_names), 0)
-        self.assertEqual(completenames_method_symbol.fullname, "cmd.Cmd.completenames")
-
-        args = completenames_method_symbol.parameters
-        self.assertEqual(len(args), 3)
+@pytest.fixture(scope="module")
+def typeshed_stdlib():
+    return typeshed_serializer.walk_typeshed_stdlib()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_build_mypy_model(typeshed_stdlib):
+    assert typeshed_stdlib is not None
+
+
+def test_module_symbol(typeshed_stdlib):
+    abc_module = typeshed_stdlib.files.get("abc")
+    module_symbol = symbols.ModuleSymbol(abc_module)
+    assert module_symbol.fullname == "abc"
+    assert len(module_symbol.classes) == 3
+    assert len(module_symbol.functions) == 4
+
+
+def test_class_symbol(typeshed_stdlib):
+    mypy_cmd_module = typeshed_stdlib.files.get("cmd")
+    mypy_cmd_class = mypy_cmd_module.names.get("Cmd")
+    cmd_class_symbol = symbols.ClassSymbol(mypy_cmd_class.node)
+    assert cmd_class_symbol.fullname == "cmd.Cmd"
+    assert cmd_class_symbol.name == "Cmd"
+    assert cmd_class_symbol.super_classes == ["builtins.object"]
+    assert len(cmd_class_symbol.mro) == 0
+
+
+def test_function_symbol(typeshed_stdlib):
+    mypy_cmd_module = typeshed_stdlib.files.get("cmd")
+
+    mypy_cmd_class_node = mypy_cmd_module.names.get("Cmd").node
+    assert isinstance(mypy_cmd_class_node, mpn.TypeInfo)
+
+    mypy_completenames_method_node = mypy_cmd_class_node.names.get("completenames").node
+    assert isinstance(mypy_completenames_method_node, mpn.FuncDef)
+
+    completenames_method_symbol = symbols.FunctionSymbol(mypy_completenames_method_node)
+    assert completenames_method_symbol.name == "completenames"
+    assert completenames_method_symbol.fullname == "cmd.Cmd.completenames"
+    assert not completenames_method_symbol.has_decorators
+    assert not completenames_method_symbol.is_asynchronous
+    assert completenames_method_symbol.return_type.pretty_printed_name == "builtins.list[builtins.str]"
+    assert len(completenames_method_symbol.resolved_decorator_names) == 0
+    assert completenames_method_symbol.fullname == "cmd.Cmd.completenames"
+
+    args = completenames_method_symbol.parameters
+    assert len(args) == 3
