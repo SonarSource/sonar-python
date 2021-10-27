e10892ca718a478a4149fb39141dfe6641a8a414
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-10-27 11:33:00 +0200

    Rule implementation

---
+++ python-checks/src/main/java/org/sonar/python/checks/regex/StringReplaceCheck.java
@@ -0,0 +1,56 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks.regex;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonarsource.analyzer.commons.regex.RegexParseResult;
+import org.sonarsource.analyzer.commons.regex.ast.RegexTree;
+import org.sonarsource.analyzer.commons.regex.ast.SequenceTree;
+
+@Rule(key = "S5361")
+public class StringReplaceCheck extends AbstractRegexCheck {
+
+  private static final String MESSAGE = "Replace this \"re.sub()\" call by a \"str.replace()\" function call.";
+
+  @Override
+  protected Set<String> lookedUpFunctionNames() {
+    return new HashSet<>(Collections.singletonList("re.sub"));
+  }
+
+  @Override
+  public void checkRegex(RegexParseResult regexParseResult, CallExpression callExpression) {
+    RegexTree regex = regexParseResult.getResult();
+    if (!regexParseResult.hasSyntaxErrors() && isPlainString(regex)) {
+      regexContext.addIssue(callExpression.callee(), MESSAGE);
+    }
+  }
+
+  private static boolean isPlainString(RegexTree regex) {
+    return regex.is(RegexTree.Kind.CHARACTER)
+      || (regex.is(RegexTree.Kind.SEQUENCE)
+      && !((SequenceTree) regex).getItems().isEmpty()
+      && ((SequenceTree) regex).getItems().stream().allMatch(item -> item.is(RegexTree.Kind.CHARACTER)));
+  }
+
+}
---
+++ python-checks/src/test/resources/checks/regex/stringReplaceCheck.py
@@ -0,0 +1,42 @@
+import re
+
+def non_compliant():
+    input = "Bob is a Bird... Bob is a Plane... Bob is Superman!"
+    changed = re.sub(r"Bob is", "It's", input) # Noncompliant {{Replace this "re.sub()" call by a "str.replace()" function call.}}
+    #         ^^^^^^
+    changed = re.sub(r"\.\.\.", ";", input) # Noncompliant
+    changed = re.sub(r"\Q...\E", ";", input) # Noncompliant
+    changed = re.sub(r"\\", "It's", input) # Noncompliant
+    changed = re.sub(r"\.", "It's", input) # Noncompliant
+    changed = re.sub(r"!", "It's", input) # Noncompliant
+
+def compliant():
+    input = "Bob is a Bird... Bob is a Plane... Bob is Superman!"
+    changed = re.sub(r"(?i)bird", "It's", input)
+    changed = re.sub(r"\w*\sis", "It's", input)
+    changed = re.sub(r"\.{3}", ";", input)
+    changed = re.sub(r"\w", "It's", input)
+    changed = re.sub(r"\s", "It's", input)
+    changed = re.sub(someVariable, "It's", input)
+    changed = re.sub(r".", "It's", input)
+    changed = re.sub(r"$", "It's", input)
+    changed = re.sub(r"|", "It's", input)
+    changed = re.sub(r"(", "It's", input)
+    changed = re.sub(r"()", "It's", input)
+    changed = re.sub(r"[", "It's", input)
+    changed = re.sub(r"[a-z]]", "It's", input)
+    changed = re.sub(r"{", "It's", input)
+    changed = re.sub(r"x{3}", "It's", input)
+    changed = re.sub(r"^", "It's", input)
+    changed = re.sub(r"?", "It's", input)
+    changed = re.sub(r"x?", "It's", input)
+    changed = re.sub(r"*", "It's", input)
+    changed = re.sub(r"x*", "It's", input)
+    changed = re.sub(r"+", "It's", input)
+    changed = re.sub(r"x+", "It's", input)
+    changed = re.sub(r"[\\]", "It's", input)
+
+def coverage(input):
+    re.match(r"Bob is", input)
+
+
