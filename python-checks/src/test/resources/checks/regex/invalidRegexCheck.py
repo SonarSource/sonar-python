227d6c77bb75df4b52db22bd395d3aa2a293267c
Nils Werner <nils.werner@sonarsource.com>
2021-11-03 09:06:15 +0100

    Add false positive test cases

--- python-checks/src/test/resources/checks/regex/invalidRegexCheck.py
+++ python-checks/src/test/resources/checks/regex/invalidRegexCheck.py
@@ -25,3 +25,27 @@ def unsupported_feature(input):
     re.match(r'\p{Lower}', input)  # Noncompliant
     re.match(r'(?>x)', input)  # Noncompliant
     re.match(r'x*+', input)  # Noncompliant
+
+
+def false_positives():
+    re.compile(r"\s*([ACGT])\s*[[]*[|]*\s*([0-9.\s]+)\s*[]]*\s*")  # Noncompliant
+    # Noncompliant@+3
+    re.compile(r'''
+        ([A-Za-z]{1,8}(?:-[A-Za-z0-9]{1,8})*|\*)      # "en", "en-au", "x-y-z", "es-419", "*"
+        (?:\s*;\s*q=(0(?:\.\d{,3})?|1(?:\.0{,3})?))?  # Optional "q=1.00", "q=0.8"
+        (?:\s*,\s*|$)                                 # Multiple accepts per header.
+        ''', re.VERBOSE)
+    re.compile(r'^\s+\[([\s*[0-9]*)\] ([a-zA-Z0-9_]*)')  # Noncompliant
+    re.compile(r'([^,[\]]*)(\[([^\]]+)\])?$')  # Noncompliant
+    re.compile(r'{.*}')  # Noncompliant
+    # Noncompliant@+3
+    re.compile(
+        r"""
+        # Start with a literal "@{".
+        @\{
+          # Group at least 1 symbol, not "}".
+          ([^}]+)
+        # Followed by a closing "}"
+        \}
+        """,
+        flags=re.VERBOSE)
