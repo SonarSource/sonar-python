cfb491703754bf5711dd8b238acd55e3b9c48f2f
Nils Werner <nils.werner@sonarsource.com>
2021-11-04 13:48:19 +0100

    Update analyzer commons to don't suggest possessive quantifiers

--- python-checks/src/test/resources/checks/regex/reluctantQuantifierCheck.py
+++ python-checks/src/test/resources/checks/regex/reluctantQuantifierCheck.py
@@ -2,52 +2,52 @@ import re
 
 
 def non_compliant(input):
-    re.match(r"<.+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]++".}}
-    re.match(r"<\S+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\s]++".}}
-    re.match(r"<\D+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\d]++".}}
-    re.match(r"<\W+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\w]++".}}
+    re.match(r"<.+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]+".}}
+    re.match(r"<\S+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\s]+".}}
+    re.match(r"<\D+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\d]+".}}
+    re.match(r"<\W+?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\w]+".}}
 
-    re.match(r"<.{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]{2,5}+".}}
-    re.match(r"<\S{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\s]{2,5}+".}}
-    re.match(r"<\D{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\d]{2,5}+".}}
-    re.match(r"<\W{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\w]{2,5}+".}}
+    re.match(r"<.{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]{2,5}".}}
+    re.match(r"<\S{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\s]{2,5}".}}
+    re.match(r"<\D{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\d]{2,5}".}}
+    re.match(r"<\W{2,5}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>\w]{2,5}".}}
 
-    re.match(r"<.{2,}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]{2,}+".}}
-    re.match(r"\".*?\"", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^\"]*+".}}
-    re.match(r".*?\w", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "\W*+".}}
-    re.match(r".*?\W", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "\w*+".}}
-    re.match(r"\[.*?\]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^\]]*+".}}
-    re.match(r".+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc]++".}}
+    re.match(r"<.{2,}?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]{2,}".}}
+    re.match(r"\".*?\"", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^\"]*".}}
+    re.match(r".*?\w", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "\W*".}}
+    re.match(r".*?\W", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "\w*".}}
+    re.match(r"\[.*?\]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^\]]*".}}
+    re.match(r".+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc]+".}}
     re.match(r"(?-U:\s)*?\S", input)
-    re.match(r"(?U:\s)*?\S", input, re.ASCII)  # Noncompliant {{Replace this use of a reluctant quantifier with "[\s\S]*+".}}
+    re.match(r"(?U:\s)*?\S", input, re.ASCII)  # Noncompliant {{Replace this use of a reluctant quantifier with "[\s\S]*".}}
     re.match(r"(?U:a|\s)*?\S", input)
     re.match(r"\S*?\s", input)
     re.match(r"\S*?(?-U:\s)", input)
-    re.match(r"\S*?(?U:\s)", input, re.ASCII)  # Noncompliant {{Replace this use of a reluctant quantifier with "[\S\s]*+".}}
-    re.match(r"\S*?(?U)\s", input, re.ASCII)  # Noncompliant {{Replace this use of a reluctant quantifier with "[\S\s]*+".}}
+    re.match(r"\S*?(?U:\s)", input, re.ASCII)  # Noncompliant {{Replace this use of a reluctant quantifier with "[\S\s]*".}}
+    re.match(r"\S*?(?U)\s", input, re.ASCII)  # Noncompliant {{Replace this use of a reluctant quantifier with "[\S\s]*".}}
 
     # coverage
     re.match(r"(?:(?m))*?a", input)
     re.match(r"(?:(?m:.))*?(?:(?m))", input)
 
     # This replacement might not be equivalent in case of full match, but is equivalent in case of split
-    re.match(r".+?[^abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[abc]++".}}
+    re.match(r".+?[^abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[abc]+".}}
 
-    re.match(r".+?\x{1F4A9}", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^\x{1F4A9}]++".}}
-    re.match(r"<abc.*?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]*+".}}
-    re.match(r"<.+?>|otherstuff", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]++".}}
-    re.match(r"(<.+?>)*", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]++".}}
+    re.match(r".+?\x{1F4A9}", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^\x{1F4A9}]+".}}
+    re.match(r"<abc.*?>", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]*".}}
+    re.match(r"<.+?>|otherstuff", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]+".}}
+    re.match(r"(<.+?>)*", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^>]+".}}
 
-    re.match(r"\S+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\s]++".}}
-    re.match(r"\D+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\d]++".}}
-    re.match(r"\w+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\W]++".}}
+    re.match(r"\S+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\s]+".}}
+    re.match(r"\D+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\d]+".}}
+    re.match(r"\w+?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\W]+".}}
 
-    re.match(r"\S*?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\s]*+".}}
-    re.match(r"\D*?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\d]*+".}}
-    re.match(r"\w*?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\W]*+".}}
+    re.match(r"\S*?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\s]*".}}
+    re.match(r"\D*?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\d]*".}}
+    re.match(r"\w*?[abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[^abc\W]*".}}
 
-    re.match(r"\S+?[^abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[abc\S]++".}}
-    re.match(r"\s+?[^abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[abc\s]++".}}
+    re.match(r"\S+?[^abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[abc\S]+".}}
+    re.match(r"\s+?[^abc]", input)  # Noncompliant {{Replace this use of a reluctant quantifier with "[abc\s]+".}}
 
 
 def compliant(input):
@@ -69,6 +69,7 @@ def compliant(input):
     re.match(r"(?U:\s)*?\S", input)
     re.match(r"\S*?(?U:\s)", input)
     re.match(r"\S*?(?U)\s", input)
+
 
 def no_intersection(input):
     re.match(r"<\d+?>", input)
