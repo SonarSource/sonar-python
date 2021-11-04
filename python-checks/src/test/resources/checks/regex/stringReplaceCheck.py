43295e2229dcc45ebc018f42b71d77275a447fe3
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2021-11-04 18:21:53 +0100

    Curly braces should not be a quantifier in general (#981)
    * Curly braces should not be a quantifier in general
    
    * Adapt ITS expected results
    
    Co-authored-by: Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>

--- python-checks/src/test/resources/checks/regex/stringReplaceCheck.py
+++ python-checks/src/test/resources/checks/regex/stringReplaceCheck.py
@@ -2,13 +2,14 @@ import re
 
 def non_compliant():
     input = "Bob is a Bird... Bob is a Plane... Bob is Superman!"
-    changed = re.sub(r"Bob is", "It's", input) # Noncompliant {{Replace this "re.sub()" call by a "str.replace()" function call.}}
+    changed = re.sub(r"Bob is", "It's", input)  # Noncompliant {{Replace this "re.sub()" call by a "str.replace()" function call.}}
     #         ^^^^^^   ^^^^^^< {{Expression without regular expression features.}}
-    changed = re.sub(r"\.\.\.", ";", input) # Noncompliant
-    changed = re.sub(r"\Q...\E", ";", input) # Noncompliant
-    changed = re.sub(r"\\", "It's", input) # Noncompliant
-    changed = re.sub(r"\.", "It's", input) # Noncompliant
-    changed = re.sub(r"!", "It's", input) # Noncompliant
+    changed = re.sub(r"\.\.\.", ";", input)  # Noncompliant
+    changed = re.sub(r"\Q...\E", ";", input)  # Noncompliant
+    changed = re.sub(r"\\", "It's", input)  # Noncompliant
+    changed = re.sub(r"\.", "It's", input)  # Noncompliant
+    changed = re.sub(r"!", "It's", input)  # Noncompliant
+    changed = re.sub(r"{", "It's", input)  # Noncompliant
 
 def compliant():
     input = "Bob is a Bird... Bob is a Plane... Bob is Superman!"
@@ -25,7 +26,6 @@ def compliant():
     changed = re.sub(r"()", "It's", input)
     changed = re.sub(r"[", "It's", input)
     changed = re.sub(r"[a-z]]", "It's", input)
-    changed = re.sub(r"{", "It's", input)
     changed = re.sub(r"x{3}", "It's", input)
     changed = re.sub(r"^", "It's", input)
     changed = re.sub(r"?", "It's", input)
