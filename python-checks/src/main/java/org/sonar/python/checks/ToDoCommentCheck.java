eed14920b8dbeb609101e0cdd6ea4e06139886e2
Marco Bearzi <marco.bearzi@sonarsource.com>
2022-03-02 16:30:56 +0100

    SONARPY-264 : Rule S1135 : Track uses of 'TODO' tags

---
+++ python-checks/src/main/java/org/sonar/python/checks/ToDoCommentCheck.java
@@ -0,0 +1,49 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import java.util.regex.Pattern;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionCheck;
+import org.sonar.plugins.python.api.tree.Token;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.plugins.python.api.tree.Trivia;
+
+@Rule(key = "S1135")
+public class ToDoCommentCheck extends PythonSubscriptionCheck {
+
+  private static final String TODO_COMMENT_PATTERN = "^#[ ]*TODO.*";
+  private static final String MESSAGE = "Complete the task associated to this \"TODO\" comment.";
+
+  @Override
+  public void initialize(SubscriptionCheck.Context context) {
+    Pattern pattern = Pattern.compile(TODO_COMMENT_PATTERN, Pattern.CASE_INSENSITIVE);
+    context.registerSyntaxNodeConsumer(Tree.Kind.TOKEN, ctx -> {
+      Token token = (Token) ctx.syntaxNode();
+      for (Trivia trivia : token.trivia()) {
+        String comment = trivia.value();
+        if (pattern.matcher(comment).matches()) {
+          ctx.addIssue(trivia.token(), MESSAGE);
+        }
+      }
+    });
+  }
+}
