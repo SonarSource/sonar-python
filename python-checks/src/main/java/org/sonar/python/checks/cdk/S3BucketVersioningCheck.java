08530a6143dc02af8d617d76a87b4038f60e1951
Marco Bearzi <marco.bearzi@sonarsource.com>
2022-05-02 10:53:16 +0200

    Remove FP

--- python-checks/src/main/java/org/sonar/python/checks/cdk/S3BucketVersioningCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/cdk/S3BucketVersioningCheck.java
@@ -24,6 +24,7 @@ import java.util.Optional;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
 import org.sonar.plugins.python.api.tree.Argument;
 import org.sonar.plugins.python.api.tree.CallExpression;
 import org.sonar.plugins.python.api.tree.RegularArgument;
@@ -32,7 +33,7 @@ import org.sonar.plugins.python.api.tree.Tree;
 @Rule(key = "S6252")
 public class S3BucketVersioningCheck extends PythonSubscriptionCheck {
 
-  public final String MESSAGE = "Make sure using unversioned S3 bucket is safe here. Omitting \"versioned=True\" disables S3 bucket" +
+  public static final String MESSAGE = "Make sure using unversioned S3 bucket is safe here. Omitting 'versioned=True' disables S3 bucket" +
     " versioning. Make sure it is safe here.";
 
   @Override
@@ -42,20 +43,19 @@ public class S3BucketVersioningCheck extends PythonSubscriptionCheck {
 
   public void visitNode(SubscriptionContext ctx) {
     CallExpression node = (CallExpression) ctx.syntaxNode();
-    if (node.calleeSymbol() == null || !"aws_cdk.aws_s3.Bucket".equals(node.calleeSymbol().fullyQualifiedName())) {
-      return;
-    }
-    Optional<RegularArgument> version = getVersionArgument(node.arguments());
-    if (version.isPresent()) {
-      version.filter(a -> "False".equals(a.expression().firstToken().value())).ifPresent(v -> ctx.addIssue(v, MESSAGE));
-    } else {
-      ctx.addIssue(node.callee(), MESSAGE);
+    Symbol nodeSymbol = node.calleeSymbol();
+    if (nodeSymbol != null && "aws_cdk.aws_s3.Bucket".equals(nodeSymbol.fullyQualifiedName())) {
+      Optional<RegularArgument> version = getVersionArgument(node.arguments());
+      if (version.isPresent()) {
+        version.filter(a -> "False".equals(a.expression().firstToken().value())).ifPresent(v -> ctx.addIssue(v, MESSAGE));
+      } else {
+        ctx.addIssue(node.callee(), MESSAGE);
+      }
     }
   }
 
   private static Optional<RegularArgument> getVersionArgument(List<Argument> args) {
     return args.stream()
-      .filter(RegularArgument.class::isInstance)
       .map(RegularArgument.class::cast)
       .filter(a -> a.keywordArgument() != null)
       .filter(a -> "versioned".equals(a.keywordArgument().name()))
