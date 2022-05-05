83defe39b160a047b1b6f4eac4e57f354b49c7be
Marco Bearzi <marco.bearzi@sonarsource.com>
2022-05-05 14:27:55 +0200

    Make use of the abstract AbstractS3BucketCheck class newly created

--- python-checks/src/main/java/org/sonar/python/checks/cdk/S3BucketServerEncryptionCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/cdk/S3BucketServerEncryptionCheck.java
@@ -19,101 +19,47 @@
  */
 package org.sonar.python.checks.cdk;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 import org.sonar.check.Rule;
-import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
 import org.sonar.plugins.python.api.symbols.Symbol;
-import org.sonar.plugins.python.api.tree.Argument;
 import org.sonar.plugins.python.api.tree.CallExpression;
 import org.sonar.plugins.python.api.tree.Expression;
-import org.sonar.plugins.python.api.tree.Name;
 import org.sonar.plugins.python.api.tree.QualifiedExpression;
-import org.sonar.plugins.python.api.tree.RegularArgument;
-import org.sonar.plugins.python.api.tree.Tree;
-import org.sonar.python.checks.Expressions;
+import org.sonar.python.tree.QualifiedExpressionImpl;
 
 @Rule(key = "S6245")
-public class S3BucketServerEncryptionCheck extends PythonSubscriptionCheck {
+public class S3BucketServerEncryptionCheck extends AbstractS3BucketCheck {
 
+  private static final String S3_BUCKET_UNENCRYPTED_FQN = "aws_cdk.aws_s3.BucketEncryption.UNENCRYPTED";
   public static final String MESSAGE = "Objects in the bucket are not encrypted. Make sure it is safe here.";
-  public static final String MESSAGE_OMITTING = "Omitting 'encryption' disables server-side encryption. Make sure it is safe here.";
-  public static final String MESSAGE_SECONDARY = "Propagated setting.";
+  public static final String OMITTING_MESSAGE = "Omitting 'encryption' disables server-side encryption. Make sure it is safe here.";
 
   @Override
-  public void initialize(Context context) {
-    context.registerSyntaxNodeConsumer(Tree.Kind.CALL_EXPR, this::visitNode);
-  }
-
-  public void visitNode(SubscriptionContext ctx) {
-    CallExpression node = (CallExpression) ctx.syntaxNode();
-    Optional.ofNullable(node.calleeSymbol())
-      .filter(nodeSymbol -> "aws_cdk.aws_s3.Bucket".equals(nodeSymbol.fullyQualifiedName()))
-      .ifPresent(nodeSymbol -> {
-        List<RegularArgument> args = getEncryptionArguments(node.arguments());
-        Optional<RegularArgument> optEncryptionType = args.stream().filter(a -> "encryption".equals(getArgumentName(a))).findFirst();
-        Optional<RegularArgument> optEncryptionKey = args.stream().filter(a -> "encryption_key".equals(getArgumentName(a))).findFirst();
-        if (optEncryptionType.isPresent()) {
-          optEncryptionType.map(regArg -> propagatedSensitiveExpressions(regArg.expression()))
-            .filter(trees -> !trees.isEmpty())
-            .ifPresent(trees -> {
-              PreciseIssue issue = ctx.addIssue(trees.get(0).parent(), MESSAGE);
-              trees.stream().skip(1).forEach(expression -> issue.secondary(expression.parent(), MESSAGE_SECONDARY));
-            });
-        } else {
-          if (!optEncryptionKey.isPresent()) {
-            ctx.addIssue(node.callee(), MESSAGE_OMITTING);
-          }
-        }
+  void visitBucketConstructor(SubscriptionContext ctx, CallExpression bucket) {
+    Optional<ArgumentTrace> optEncryptionType = getArgument(ctx, bucket, "encryption");
+    if (optEncryptionType.isPresent()) {
+      optEncryptionType.ifPresent(argumentTrace -> {
+        List<Expression> list = argumentTrace.trace();
+        argumentTrace.addIssueIf(isUnencrypted(list.get(list.size() - 1)), MESSAGE);
       });
-  }
-
-  private static List<RegularArgument> getEncryptionArguments(List<Argument> args) {
-    return args.stream()
-      .map(RegularArgument.class::cast)
-      .filter(a -> a.keywordArgument() != null)
-      .filter(a -> "encryption".equals(getArgumentName(a)) || "encryption_key".equals(getArgumentName(a)))
-      .collect(Collectors.toList());
-  }
-
-  private static String getArgumentName(RegularArgument argument) {
-    Name keyword = argument.keywordArgument();
-    return Optional.ofNullable(keyword)
-      .map(Name::name).orElse("");
-  }
-
-  private static List<Tree> propagatedSensitiveExpressions(Expression expression) {
-    List<Tree> trees = new ArrayList<>();
-    if (expression.is(Tree.Kind.NAME)) {
-      Expression singleAssignedValue = Expressions.singleAssignedValue(((Name) expression));
-      if (singleAssignedValue != null) {
-        trees.add(expression);
-        List<Tree> subTrees = propagatedSensitiveExpressions(singleAssignedValue);
-        if (subTrees.isEmpty()) {
-          return Collections.emptyList();
-        }
-        trees.addAll(subTrees);
-      }
-    } else if (expression.is(Tree.Kind.QUALIFIED_EXPR)) {
-      if (isUnencrypted(expression)) {
-        trees.add(expression);
-      } else {
-        return Collections.emptyList();
-      }
     } else {
-      trees.add(expression);
+      Optional<ArgumentTrace> optEncryptionKey = getArgument(ctx, bucket, "encryption_key");
+      if (!optEncryptionKey.isPresent()) {
+        ctx.addIssue(bucket.callee(), OMITTING_MESSAGE);
+      }
     }
-    return trees;
   }
 
-  private static boolean isUnencrypted(Expression expression) {
-    Symbol symbol = ((QualifiedExpression) expression).symbol();
-    return Optional.ofNullable(symbol)
-      .map(s -> "aws_cdk.aws_s3.BucketEncryption.UNENCRYPTED".equals(s.fullyQualifiedName()))
-      .orElse(false);
+  protected Predicate<Expression> isUnencrypted(Expression expression) {
+    return exp -> Optional.of(expression)
+      .filter(QualifiedExpression.class::isInstance)
+      .map(QualifiedExpressionImpl.class::cast)
+      .map(QualifiedExpression::symbol)
+      .map(Symbol::fullyQualifiedName)
+      .filter(S3_BUCKET_UNENCRYPTED_FQN::equals)
+      .isPresent();
   }
 }
