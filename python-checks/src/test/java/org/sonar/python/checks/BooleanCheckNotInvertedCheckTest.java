123cf40c3e84197ddf14b733ad999da52eb0ec1e
Marco Bearzi <marco.bearzi@sonarsource.com>
2022-06-14 10:39:36 +0200

    SONARPY-1041: Fix the quick-fix for S1940 (BooleanCheckNotInvertedCheck)

--- python-checks/src/test/java/org/sonar/python/checks/BooleanCheckNotInvertedCheckTest.java
+++ python-checks/src/test/java/org/sonar/python/checks/BooleanCheckNotInvertedCheckTest.java
@@ -21,6 +21,7 @@ package org.sonar.python.checks;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.sonar.plugins.python.api.PythonCheck;
 import org.sonar.python.checks.quickfix.PythonQuickFixVerifier;
 import org.sonar.python.checks.utils.PythonCheckVerifier;
 
@@ -28,9 +29,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BooleanCheckNotInvertedCheckTest {
 
+  private final PythonCheck check = new BooleanCheckNotInvertedCheck();
+
   @Test
   public void test() {
-    PythonCheckVerifier.verify("src/test/resources/checks/booleanCheckNotInverted.py", new BooleanCheckNotInvertedCheck());
+    PythonCheckVerifier.verify("src/test/resources/checks/booleanCheckNotInverted.py", check);
   }
 
   @Test
@@ -54,30 +57,149 @@ public class BooleanCheckNotInvertedCheckTest {
   public void test_quickfix() {
     String codeWithIssue = "a = not(b == c)";
     String codeFixed = "a = b != c";
-    PythonQuickFixVerifier.verify(new BooleanCheckNotInvertedCheck(), codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not b != c";
+    codeFixed = "a = b == c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
 
     codeWithIssue = "a = not (b != c)";
     codeFixed = "a = b == c";
-    PythonQuickFixVerifier.verify(new BooleanCheckNotInvertedCheck(), codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
 
     codeWithIssue = "a = not (b < c)";
     codeFixed = "a = b >= c";
-    PythonQuickFixVerifier.verify(new BooleanCheckNotInvertedCheck(), codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
 
     codeWithIssue = "a = not (b is c)";
     codeFixed = "a = b is not c";
-    PythonQuickFixVerifier.verify(new BooleanCheckNotInvertedCheck(), codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
 
     codeWithIssue = "a = not (b is not c)";
     codeFixed = "a = b is c";
-    PythonQuickFixVerifier.verify(new BooleanCheckNotInvertedCheck(), codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
 
     codeWithIssue = "a = not (b in c)";
     codeFixed = "a = b not in c";
-    PythonQuickFixVerifier.verify(new BooleanCheckNotInvertedCheck(), codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
 
     codeWithIssue = "a = not (b not in c)";
     codeFixed = "a = b in c";
-    PythonQuickFixVerifier.verify(new BooleanCheckNotInvertedCheck(), codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+  }
+
+  @Test
+  public void not_in_if() {
+    String codeWithIssue = "" +
+      "def func():\n" +
+      "    if not a == 2:\n" +
+      "        b = 10\n" +
+      "    return \"item1\" \"item2\"";
+    String codeFixed = "" +
+      "def func():\n" +
+      "    if a != 2:\n" +
+      "        b = 10\n" +
+      "    return \"item1\" \"item2\"";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "" +
+      "def func():\n" +
+      "    if not a == 2 and b == 9:\n" +
+      "        b = 10\n" +
+      "    return \"item1\" \"item2\"";
+    codeFixed = "" +
+      "def func():\n" +
+      "    if a != 2 and b == 9:\n" +
+      "        b = 10\n" +
+      "    return \"item1\" \"item2\"";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "" +
+      "def func():\n" +
+      "    if a != 2 and not b == 9:\n" +
+      "        b = 10\n" +
+      "    return \"item1\" \"item2\"";
+    codeFixed = "" +
+      "def func():\n" +
+      "    if a != 2 and b != 9:\n" +
+      "        b = 10\n" +
+      "    return \"item1\" \"item2\"";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+  }
+
+  @Test
+  public void not_parentheses() {
+    String codeWithIssue = "a = not ((((((b > c))))))";
+    String codeFixed = "a = b <= c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (not (b == c))";
+    codeFixed = "a = not (b != c)";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (a is (not b))";
+    codeFixed = "a = a is not (not b)";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (1 == b == c) == 2";
+    codeFixed = "a = (1 == b == c) != 2";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (1 == b == c) == (d == 2)";
+    codeFixed = "a = (1 == b == c) != (d == 2)";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not ((1 == b == c) and a) == (d == 2)";
+    codeFixed = "a = ((1 == b == c) and a) != (d == 2)";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (b < foo())";
+    codeFixed = "a = b >= foo()";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+  }
+
+  @Test
+  public void expression_list() {
+    String codeWithIssue = "a = not [] < (c,d)";
+    String codeFixed = "a = [] >= (c, d)";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not [a, (a,b)] is c";
+    codeFixed = "a = [a, (a, b)] is not c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (foo((1)) < c)";
+    codeFixed = "a = foo((1)) >= c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (foo(()) < c)";
+    codeFixed = "a = foo(()) >= c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not (() in c)";
+    codeFixed = "a = () not in c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not ((1,) in c)";
+    codeFixed = "a = (1,) not in c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+    codeWithIssue = "a = not ((1,2,3,4) in c)";
+    codeFixed = "a = (1, 2, 3, 4) not in c";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+  }
+
+  @Test
+  public void fstring() {
+    String codeWithIssue = "x = not (a == f'foo${b}')";
+    String codeFixed = "x = a != f'foo${b}'";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+
+  }
+  @Test
+  public void brackets(){
+    String codeWithIssue = "x = not ( ham[1] in a)";
+    String codeFixed = "x = ham[1] not in a";
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
   }
 }
