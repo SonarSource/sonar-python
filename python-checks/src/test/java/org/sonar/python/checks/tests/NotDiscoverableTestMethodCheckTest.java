ccfcabf38d0b11de025888dc03e4db46526cc863
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2022-08-17 08:21:01 +0200

    SONARPY-758 Set S5899 scope to ALL

--- python-checks/src/test/java/org/sonar/python/checks/tests/NotDiscoverableTestMethodCheckTest.java
+++ python-checks/src/test/java/org/sonar/python/checks/tests/NotDiscoverableTestMethodCheckTest.java
@@ -20,12 +20,20 @@
 package org.sonar.python.checks.tests;
 
 import org.junit.Test;
+import org.sonar.plugins.python.api.PythonCheck;
 import org.sonar.python.checks.utils.PythonCheckVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NotDiscoverableTestMethodCheckTest {
 
   @Test
   public void test() {
     PythonCheckVerifier.verify("src/test/resources/checks/tests/notDiscoverableTestMethod.py", new NotDiscoverableTestMethodCheck());
+  }
+
+  @Test
+  public void test_scope() {
+    assertThat(new NotDiscoverableTestMethodCheck().scope()).isEqualTo(PythonCheck.CheckScope.ALL);
   }
 }
