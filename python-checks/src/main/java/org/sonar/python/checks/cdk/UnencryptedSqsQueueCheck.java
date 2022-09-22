ee0f4e49e4d8a1788270c9b58ddce764ddb59ad0
rudy-regazzoni-sonarsource <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2022-09-22 09:36:46 +0200

    Usage of new api BiConsumer in ABstractCdkResourceCheck (#1211)

--- python-checks/src/main/java/org/sonar/python/checks/cdk/UnencryptedSqsQueueCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/cdk/UnencryptedSqsQueueCheck.java
@@ -20,56 +20,37 @@
 package org.sonar.python.checks.cdk;
 
 import org.sonar.check.Rule;
-import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
 import org.sonar.plugins.python.api.tree.CallExpression;
 
 @Rule(key = "S6330")
-public class UnencryptedSqsQueueCheck extends PythonSubscriptionCheck {
+public class UnencryptedSqsQueueCheck extends AbstractCdkResourceCheck {
+  private static final String UNENCRYPTED_MESSAGE = "Setting \"encryption\" to \"QueueEncryption.UNENCRYPTED\" disables SQS queues encryption. Make sure it is safe here.";
+  private static final String NONE_MESSAGE = "Setting \"encryption\" to \"None\" disables SQS queues encryption. Make sure it is safe here.";
+  private static final String OMITTING_MESSAGE = "Omitting \"encryption\" disables SQS queues encryption. Make sure it is safe here.";
+  private static final String CFN_NONE_MESSAGE = "Setting \"kms_master_key_id\" to \"None\" disables SQS queues encryption. Make sure it is safe here.";
+  private static final String CFN_OMITTING_MESSAGE = "Omitting \"kms_master_key_id\" disables SQS queues encryption. Make sure it is safe here.";
 
   @Override
-  public void initialize(Context context) {
-    new AwsSqsQueueResourceCheck().initialize(context);
-    new AwsSqsCfnQueueResourceCheck().initialize(context);
+  protected void registerFqnConsumer() {
+    checkFqn("aws_cdk.aws_sqs.Queue", this::checkQueue);
+    checkFqn("aws_cdk.aws_sqs.CfnQueue", this::checkCfnQueue);
   }
 
-  static class AwsSqsQueueResourceCheck extends AbstractCdkResourceCheck {
-    private static final String UNENCRYPTED_MESSAGE = "Setting \"encryption\" to \"QueueEncryption.UNENCRYPTED\" disables SQS queues encryption. Make sure it is safe here.";
-    private static final String NONE_MESSAGE = "Setting \"encryption\" to \"None\" disables SQS queues encryption. Make sure it is safe here.";
-    private static final String OMITTING_MESSAGE = "Omitting \"encryption\" disables SQS queues encryption. Make sure it is safe here.";
-
-    @Override
-    protected String resourceFqn() {
-      return "aws_cdk.aws_sqs.Queue";
-    }
-
-    @Override
-    protected void visitResourceConstructor(SubscriptionContext ctx, CallExpression resourceConstructor) {
-      getArgument(ctx, resourceConstructor, "encryption").ifPresentOrElse(
-        argumentTrace -> {
-          argumentTrace.addIssueIf(argTrace -> AbstractCdkResourceCheck.isFqnValue(argTrace, "aws_cdk.aws_sqs.QueueEncryption.UNENCRYPTED"), UNENCRYPTED_MESSAGE);
-          argumentTrace.addIssueIf(AbstractCdkResourceCheck::isNone, NONE_MESSAGE);
-        },
-        () -> ctx.addIssue(resourceConstructor.callee(), OMITTING_MESSAGE)
-      );
-    }
+  protected void checkQueue(SubscriptionContext ctx, CallExpression resourceConstructor) {
+    getArgument(ctx, resourceConstructor, "encryption").ifPresentOrElse(
+      argumentTrace -> {
+        argumentTrace.addIssueIf(argTrace -> AbstractCdkResourceCheck.isFqnValue(argTrace, "aws_cdk.aws_sqs.QueueEncryption.UNENCRYPTED"), UNENCRYPTED_MESSAGE);
+        argumentTrace.addIssueIf(AbstractCdkResourceCheck::isNone, NONE_MESSAGE);
+      },
+      () -> ctx.addIssue(resourceConstructor.callee(), OMITTING_MESSAGE)
+    );
   }
 
-  static class AwsSqsCfnQueueResourceCheck extends AbstractCdkResourceCheck {
-    private static final String NONE_MESSAGE = "Setting \"kms_master_key_id\" to \"None\" disables SQS queues encryption. Make sure it is safe here.";
-    private static final String OMITTING_MESSAGE = "Omitting \"kms_master_key_id\" disables SQS queues encryption. Make sure it is safe here.";
-
-    @Override
-    protected String resourceFqn() {
-      return "aws_cdk.aws_sqs.CfnQueue";
-    }
-
-    @Override
-    protected void visitResourceConstructor(SubscriptionContext ctx, CallExpression resourceConstructor) {
-      getArgument(ctx, resourceConstructor, "kms_master_key_id").ifPresentOrElse(
-        argumentTrace -> argumentTrace.addIssueIf(AbstractCdkResourceCheck::isNone, NONE_MESSAGE),
-        () -> ctx.addIssue(resourceConstructor.callee(), OMITTING_MESSAGE)
-      );
-    }
+  protected void checkCfnQueue(SubscriptionContext ctx, CallExpression resourceConstructor) {
+    getArgument(ctx, resourceConstructor, "kms_master_key_id").ifPresentOrElse(
+      argumentTrace -> argumentTrace.addIssueIf(AbstractCdkResourceCheck::isNone, CFN_NONE_MESSAGE),
+      () -> ctx.addIssue(resourceConstructor.callee(), CFN_OMITTING_MESSAGE)
+    );
   }
 }
