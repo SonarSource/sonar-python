701bede3d7c86f8c3b7e6d294d914aed0b9dc03d
Rudy Regazzoni <rudy.regazzoni@sonarsource.com>
2022-09-23 13:49:45 +0200

    Fix after review

--- python-checks/src/main/java/org/sonar/python/checks/cdk/DisabledESDomainEncryptionCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/cdk/DisabledESDomainEncryptionCheck.java
@@ -19,10 +19,16 @@
  */
 package org.sonar.python.checks.cdk;
 
+import java.util.Collections;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.SubscriptionContext;
 import org.sonar.plugins.python.api.tree.CallExpression;
 import org.sonar.plugins.python.api.tree.DictionaryLiteral;
+import org.sonar.plugins.python.api.tree.DictionaryLiteralElement;
 import org.sonar.plugins.python.api.tree.Expression;
 import org.sonar.plugins.python.api.tree.KeyValuePair;
 import org.sonar.plugins.python.api.tree.StringLiteral;
@@ -30,53 +36,83 @@ import org.sonar.plugins.python.api.tree.Tree;
 
 @Rule(key = "S6308")
 public class DisabledESDomainEncryptionCheck extends AbstractCdkResourceCheck {
-  private static final String OMITTING_MESSAGE = "Omitting %s causes encryption of data at rest to be disabled for this {OpenSearch|Elasticsearch} domain." +
+  private static final String OMITTING_MESSAGE = "Omitting %s causes encryption of data at rest to be disabled for this %s domain." +
     " Make sure it is safe here.";
-  private static final String UNENCRYPTED_MESSAGE = "Make sure that using unencrypted {OpenSearch|Elasticsearch} domains is safe here.";
+  private static final String UNENCRYPTED_MESSAGE = "Make sure that using unencrypted %s domains is safe here.";
   private static final String ENABLED = "enabled";
+  private static final String OPENSEARCH = "OpenSearch";
+  private static final String ELASTICSEARCH = "Elasticsearch";
 
   @Override
   protected void registerFqnConsumer() {
-    checkFqn("aws_cdk.aws_opensearchservice.Domain", DisabledESDomainEncryptionCheck::checkNormalDomain);
-    checkFqn("aws_cdk.aws_opensearchservice.CfnDomain", DisabledESDomainEncryptionCheck::checkCfnDomain);
+    checkFqn("aws_cdk.aws_opensearchservice.Domain",
+      checkDomain("encryption_at_rest", "aws_cdk.aws_opensearchservice.EncryptionAtRestOptions", OPENSEARCH));
+    checkFqn("aws_cdk.aws_opensearchservice.CfnDomain",
+      checkDomain("encryption_at_rest_options", "aws_cdk.aws_opensearchservice.CfnDomain.EncryptionAtRestOptionsProperty", OPENSEARCH));
+    checkFqn("aws_cdk.aws_elasticsearch.Domain",
+      checkDomain("encryption_at_rest", "aws_cdk.aws_elasticsearch.EncryptionAtRestOptions", ELASTICSEARCH));
+    checkFqn("aws_cdk.aws_elasticsearch.CfnDomain",
+      checkDomain("encryption_at_rest_options", "aws_cdk.aws_elasticsearch.CfnDomain.EncryptionAtRestOptionsProperty", ELASTICSEARCH));
   }
 
-  private static void checkNormalDomain(SubscriptionContext ctx, CallExpression callExpression) {
-    checkDomain(ctx, callExpression, "encryption_at_rest", "aws_cdk.aws_opensearchservice.EncryptionAtRestOptions");
+  private static BiConsumer<SubscriptionContext, CallExpression> checkDomain(String encryptionArgName, String argFqnMethod, String engine) {
+    return (ctx, callExpression) -> getArgument(ctx, callExpression, encryptionArgName)
+      .ifPresentOrElse(
+        argEncryptedTrace -> argEncryptedTrace.addIssueIf(isSensitiveOptionObj(ctx, argFqnMethod).or(isSensitiveOptionDict(ctx)), unencryptedMessage(engine)),
+        () -> ctx.addIssue(callExpression.callee(), omittingMessage(encryptionArgName, engine))
+      );
   }
 
-  private static void checkCfnDomain(SubscriptionContext ctx, CallExpression callExpression) {
-    checkDomain(ctx, callExpression, "encryption_at_rest_options", "aws_cdk.aws_opensearchservice.CfnDomain.EncryptionAtRestOptionsProperty");
+  private static  String omittingMessage(String encryptionArgName, String engine) {
+    return String.format(OMITTING_MESSAGE, encryptionArgName, engine);
   }
 
-  private static void checkDomain(SubscriptionContext ctx, CallExpression callExpression, String encryptionArgName, String argFqnMethod) {
-    getArgument(ctx, callExpression, encryptionArgName).ifPresentOrElse(
-      argEncryptedTrace ->
-        argEncryptedTrace.addIssueIf(expr -> isMethodCallWithoutEnabledOrSetToFalse(ctx, expr, argFqnMethod)
-          || (expr.is(Tree.Kind.DICTIONARY_LITERAL) && isDictionaryEnabledValueFalseOrAbsent(ctx, (DictionaryLiteral) expr))
-          , UNENCRYPTED_MESSAGE)
-      , () -> ctx.addIssue(callExpression.callee(), String.format(OMITTING_MESSAGE, encryptionArgName))
-    );
+  private static String unencryptedMessage(String engine) {
+    return String.format(UNENCRYPTED_MESSAGE, engine);
   }
 
-  private static boolean isMethodCallWithoutEnabledOrSetToFalse(SubscriptionContext ctx, Expression expr, String argFqnMethod) {
-    if (!AbstractCdkResourceCheck.isFqnValue(expr, argFqnMethod)) {
-      return false;
-    }
-    if (!expr.is(Tree.Kind.CALL_EXPR)) {
-      return true;
-    }
+  private static Predicate<Expression> isSensitiveOptionObj(SubscriptionContext ctx, String argFqnMethod) {
+    return expr -> {
+      if (!isFqn(argFqnMethod).test(expr)) {
+        return false;
+      }
+      if (!expr.is(Tree.Kind.CALL_EXPR)) {
+        return true;
+      }
 
-    return getArgument(ctx, (CallExpression) expr, ENABLED)
-      .filter(argEnabledTrace -> !argEnabledTrace.hasExpression(AbstractCdkResourceCheck::isFalse)).isEmpty();
+      return getArgument(ctx, (CallExpression) expr, ENABLED)
+        .filter(argEnabledTrace -> !argEnabledTrace.hasExpression(isFalse())).isEmpty();
+    };
   }
 
-  private static boolean isDictionaryEnabledValueFalseOrAbsent(SubscriptionContext ctx, DictionaryLiteral dictionaryLiteral) {
-    return dictionaryLiteral.elements().stream()
-      .filter(element -> element.is(Tree.Kind.KEY_VALUE_PAIR)).map(KeyValuePair.class::cast)
-      .filter(keyValuePair -> keyValuePair.key().is(Tree.Kind.STRING_LITERAL)
-        && ((StringLiteral) keyValuePair.key()).trimmedQuotesValue().equals(ENABLED))
-      .map(keyValuePair -> ArgumentTrace.build(ctx, keyValuePair.value()))
-      .allMatch(argTrace -> argTrace.hasExpression(AbstractCdkResourceCheck::isFalse));
+  private static Predicate<Expression> isSensitiveOptionDict(SubscriptionContext ctx) {
+    return expression -> expression.is(Tree.Kind.DICTIONARY_LITERAL)
+      && asDictionaryKeyValuePairs(expression)
+      .filter(isKey(ENABLED))
+      .allMatch(isValueFalse(ctx));
+  }
+
+  private static Stream<KeyValuePair> asDictionaryKeyValuePairs(Expression expression) {
+    return asDictionaryElements(expression)
+      .filter(element -> element.is(Tree.Kind.KEY_VALUE_PAIR)).map(KeyValuePair.class::cast);
+  }
+
+  private static Stream<DictionaryLiteralElement> asDictionaryElements(Expression expression) {
+    return Optional.of(expression)
+      .filter(expr -> expr.is(Tree.Kind.DICTIONARY_LITERAL)).map(DictionaryLiteral.class::cast)
+      .map(DictionaryLiteral::elements)
+      .orElse(Collections.emptyList()).stream();
+  }
+
+  private static Predicate<KeyValuePair> isKey(String keyName) {
+    return keyValuePair -> Optional.of(keyValuePair)
+      .map(KeyValuePair::key)
+      .filter(key -> key.is(Tree.Kind.STRING_LITERAL)).map(StringLiteral.class::cast)
+      .filter(string -> string.trimmedQuotesValue().equals(keyName))
+      .isPresent();
+  }
+
+  private static Predicate<KeyValuePair> isValueFalse(SubscriptionContext ctx) {
+    return keyValuePair -> ArgumentTrace.build(ctx, keyValuePair.value()).hasExpression(isFalse());
   }
 }
--- python-checks/src/test/resources/checks/cdk/disabledESDomainEncryptionCheck.py
+++ python-checks/src/test/resources/checks/cdk/disabledESDomainEncryptionCheck.py
@@ -1,33 +1,36 @@
-from aws_cdk.aws_opensearchservice import Domain, EncryptionAtRestOptions, EngineVersion
+import aws_cdk.aws_opensearchservice as aws_os
+import aws_cdk.aws_elasticsearch as aws_es
 
 class DomainStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        encryptionRestOptionMethodGood = EncryptionAtRestOptions(enabled=True)
-        encryptionRestOptionMethodBad = EncryptionAtRestOptions(enabled=False)
+        encryptionRestOptionMethodGood = aws_os.EncryptionAtRestOptions(enabled=True)
+        encryptionRestOptionMethodBad = aws_os.EncryptionAtRestOptions(enabled=False)
         encryptionRestOptionDictionaryGood = {"enabled": True}
         encryptionRestOptionDictionaryBad = {"enabled": False}
         encrypted = True
         not_encrypted = False
 
-        Domain(self, "unencrypted-domain-1", version=EngineVersion.OPENSEARCH_1_3) # NonCompliant{{Omitting encryption_at_rest causes encryption of data at rest to be disabled for this {OpenSearch|Elasticsearch} domain. Make sure it is safe here.}}
-        Domain(self, "unencrypted-domain-2", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=EncryptionAtRestOptions) # NonCompliant{{Make sure that using unencrypted {OpenSearch|Elasticsearch} domains is safe here.}}
-        Domain(self, "unencrypted-domain-3", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=EncryptionAtRestOptions()) # NonCompliant
-        Domain(self, "unencrypted-domain-4", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=EncryptionAtRestOptions(enabled=False)) # NonCompliant{{Make sure that using unencrypted {OpenSearch|Elasticsearch} domains is safe here.}}
-        Domain(self, "unencrypted-domain-5", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionMethodBad) # NonCompliant
-        Domain(self, "unencrypted-domain-6", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": False}) # NonCompliant
-        Domain(self, "unencrypted-domain-6", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"another_key": False}) # NonCompliant
-        Domain(self, "unencrypted-domain-7", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionDictionaryBad) # NonCompliant
-        Domain(self, "unencrypted-domain-8", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=EncryptionAtRestOptions(enabled=not_encrypted)) # NonCompliant
-        Domain(self, "unencrypted-domain-9", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": not_encrypted}) # NonCompliant
-        Domain(self, "encrypted-domain-1", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=EncryptionAtRestOptions(enabled=True))
-        Domain(self, "encrypted-domain-2", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionMethodGood)
-        Domain(self, "encrypted-domain-3", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": True})
-        Domain(self, "encrypted-domain-4", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionDictionaryGood)
-        Domain(self, "encrypted-domain-5", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest=EncryptionAtRestOptions(enabled=encrypted))
-        Domain(self, "encrypted-domain-6", version=EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": encrypted})
+        aws_os.Domain(self, "unencrypted-domain-1", version=aws_os.EngineVersion.OPENSEARCH_1_3) # NonCompliant{{Omitting encryption_at_rest causes encryption of data at rest to be disabled for this OpenSearch domain. Make sure it is safe here.}}
+        aws_os.Domain(self, "unencrypted-domain-2", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=aws_os.EncryptionAtRestOptions) # NonCompliant{{Make sure that using unencrypted OpenSearch domains is safe here.}}
+        aws_os.Domain(self, "unencrypted-domain-3", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=aws_os.EncryptionAtRestOptions()) # NonCompliant
+        aws_os.Domain(self, "unencrypted-domain-4", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=aws_os.EncryptionAtRestOptions(enabled=False)) # NonCompliant{{Make sure that using unencrypted OpenSearch domains is safe here.}}
+        aws_os.Domain(self, "unencrypted-domain-5", version=aws_os.aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionMethodBad) # NonCompliant
+        aws_os.Domain(self, "unencrypted-domain-6", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": False}) # NonCompliant
+        aws_os.Domain(self, "unencrypted-domain-6", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest={}) # NonCompliant
+        aws_os.Domain(self, "unencrypted-domain-6", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"another_key": False}) # NonCompliant
+        aws_os.Domain(self, "unencrypted-domain-7", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionDictionaryBad) # NonCompliant
+        aws_os.Domain(self, "unencrypted-domain-8", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=aws_os.EncryptionAtRestOptions(enabled=not_encrypted)) # NonCompliant
+        aws_os.Domain(self, "unencrypted-domain-9", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": not_encrypted}) # NonCompliant
+        aws_os.Domain(self, "encrypted-domain-1", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=aws_os.EncryptionAtRestOptions(enabled=True))
+        aws_os.Domain(self, "encrypted-domain-2", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionMethodGood)
+        aws_os.Domain(self, "encrypted-domain-3", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": True})
+        aws_os.Domain(self, "encrypted-domain-4", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=encryptionRestOptionDictionaryGood)
+        aws_os.Domain(self, "encrypted-domain-5", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest=aws_os.EncryptionAtRestOptions(enabled=encrypted))
+        aws_os.Domain(self, "encrypted-domain-6", version=aws_os.EngineVersion.OPENSEARCH_1_3, encryption_at_rest={"enabled": encrypted})
 
-from aws_cdk.aws_opensearchservice import CfnDomain
+        aws_es.Domain(self, "unencrypted-domain-1", version=aws_es.ElasticsearchVersion.V7_4) # NonCompliant{{Omitting encryption_at_rest causes encryption of data at rest to be disabled for this Elasticsearch domain. Make sure it is safe here.}}
+        aws_es.Domain(self, "unencrypted-domain-2", version=aws_es.ElasticsearchVersion.V7_4, encryption_at_rest=aws_es.EncryptionAtRestOptions) # NonCompliant{{Make sure that using unencrypted Elasticsearch domains is safe here.}}
 
 class CfnDomainStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -35,15 +38,20 @@ class CfnDomainStack(Stack):
         encrypted = True
         not_encrypted = False
 
-        CfnDomain(self, "unencrypted-domain-1") # NonCompliant{{Omitting encryption_at_rest_options causes encryption of data at rest to be disabled for this {OpenSearch|Elasticsearch} domain. Make sure it is safe here.}}
-        CfnDomain(self, "unencrypted-domain-2", encryption_at_rest_options=CfnDomain.EncryptionAtRestOptionsProperty()) # NonCompliant{{Make sure that using unencrypted {OpenSearch|Elasticsearch} domains is safe here.}}
-        CfnDomain(self, "unencrypted-domain-3", encryption_at_rest_options=CfnDomain.EncryptionAtRestOptionsProperty) # NonCompliant
-        CfnDomain(self, "unencrypted-domain-4", encryption_at_rest_options=CfnDomain.EncryptionAtRestOptionsProperty(enabled=False)) # NonCompliant{{Make sure that using unencrypted {OpenSearch|Elasticsearch} domains is safe here.}}
-        CfnDomain(self, "unencrypted-domain-5", encryption_at_rest_options={"enabled": False}) # NonCompliant
-        CfnDomain(self, "unencrypted-domain-6", encryption_at_rest_options=CfnDomain.EncryptionAtRestOptionsProperty(enabled=not_encrypted)) # NonCompliant
-        CfnDomain(self, "unencrypted-domain-7", encryption_at_rest_options={"enabled": not_encrypted}) # NonCompliant
-        CfnDomain(self, "encrypted-domain-1", encryption_at_rest_options=CfnDomain.EncryptionAtRestOptionsProperty(enabled=True))
-        CfnDomain(self, "encrypted-domain-2", encryption_at_rest_options={"enabled": True})
-        CfnDomain(self, "encrypted-domain-3", encryption_at_rest_options=CfnDomain.EncryptionAtRestOptionsProperty(enabled=encrypted))
-        CfnDomain(self, "encrypted-domain-4", encryption_at_rest_options={"enabled": encrypted})
-        CfnDomain(self, "encrypted-domain-2", encryption_at_rest_options={1:1, "enabled": True}) # code coverage on !StringLiteral key
+        aws_os.CfnDomain(self, "unencrypted-domain-1") # NonCompliant{{Omitting encryption_at_rest_options causes encryption of data at rest to be disabled for this OpenSearch domain. Make sure it is safe here.}}
+        aws_os.CfnDomain(self, "unencrypted-domain-2", encryption_at_rest_options=aws_os.CfnDomain.EncryptionAtRestOptionsProperty()) # NonCompliant{{Make sure that using unencrypted OpenSearch domains is safe here.}}
+        aws_os.CfnDomain(self, "unencrypted-domain-3", encryption_at_rest_options=aws_os.CfnDomain.EncryptionAtRestOptionsProperty) # NonCompliant
+        aws_os.CfnDomain(self, "unencrypted-domain-4", encryption_at_rest_options=aws_os.CfnDomain.EncryptionAtRestOptionsProperty(enabled=False)) # NonCompliant{{Make sure that using unencrypted OpenSearch domains is safe here.}}
+        aws_os.CfnDomain(self, "unencrypted-domain-5", encryption_at_rest_options={"enabled": False}) # NonCompliant
+        aws_os.CfnDomain(self, "unencrypted-domain-5", encryption_at_rest_options={}) # NonCompliant
+        aws_os.CfnDomain(self, "unencrypted-domain-5", encryption_at_rest_options={"another_key": False}) # NonCompliant
+        aws_os.CfnDomain(self, "unencrypted-domain-6", encryption_at_rest_options=aws_os.CfnDomain.EncryptionAtRestOptionsProperty(enabled=not_encrypted)) # NonCompliant
+        aws_os.CfnDomain(self, "unencrypted-domain-7", encryption_at_rest_options={"enabled": not_encrypted}) # NonCompliant
+        aws_os.CfnDomain(self, "encrypted-domain-1", encryption_at_rest_options=aws_os.CfnDomain.EncryptionAtRestOptionsProperty(enabled=True))
+        aws_os.CfnDomain(self, "encrypted-domain-2", encryption_at_rest_options={"enabled": True})
+        aws_os.CfnDomain(self, "encrypted-domain-3", encryption_at_rest_options=aws_os.CfnDomain.EncryptionAtRestOptionsProperty(enabled=encrypted))
+        aws_os.CfnDomain(self, "encrypted-domain-4", encryption_at_rest_options={"enabled": encrypted})
+        aws_os.CfnDomain(self, "encrypted-domain-2", encryption_at_rest_options={1:1, "enabled": True}) # code coverage on !StringLiteral key
+
+        aws_es.CfnDomain(self, "unencrypted-domain-1") # NonCompliant{{Omitting encryption_at_rest_options causes encryption of data at rest to be disabled for this Elasticsearch domain. Make sure it is safe here.}}
+        aws_es.CfnDomain(self, "unencrypted-domain-2", encryption_at_rest_options=aws_es.CfnDomain.EncryptionAtRestOptionsProperty()) # NonCompliant{{Make sure that using unencrypted Elasticsearch domains is safe here.}}
