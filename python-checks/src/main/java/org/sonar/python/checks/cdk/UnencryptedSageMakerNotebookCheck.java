ce015487d682bb66563c22723b263a6e98e25284
Rudy Regazzoni <rudy.regazzoni@sonarsource.com>
2022-09-26 08:46:30 +0200

    SONARPY-1130 Rule S6319: Using unencrypted SageMaker notebook instances is security-sensitive

---
+++ python-checks/src/main/java/org/sonar/python/checks/cdk/UnencryptedSageMakerNotebookCheck.java
@@ -0,0 +1,38 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks.cdk;
+
+import org.sonar.check.Rule;
+
+@Rule(key = "S6319")
+public class UnencryptedSageMakerNotebookCheck extends AbstractCdkResourceCheck {
+  private static final String OMITTING_MESSAGE = "Omitting kms_key_id disables encryption of SageMaker notebook instances. Make sure it is safe here.";
+
+  @Override
+  protected void registerFqnConsumer() {
+    checkFqn("aws_cdk.aws_sagemaker.CfnNotebookInstance", (ctx, callExpression) ->
+      getArgument(ctx, callExpression, "kms_key_id")
+        .ifPresentOrElse(
+          argKmsKey -> argKmsKey.addIssueIf(isNone(), OMITTING_MESSAGE),
+          () -> ctx.addIssue(callExpression.callee(), OMITTING_MESSAGE)
+        )
+    );
+  }
+}
