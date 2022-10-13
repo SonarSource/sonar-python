f9d075355dd575ab12b92525ba6d695c010f10f5
Nils Werner <nils.werner@sonarsource.com>
2022-10-13 10:29:05 +0200

    Implement logic for aws_cdk.aws_rds.DatabaseInstance

--- python-checks/src/main/java/org/sonar/python/checks/cdk/CdkUtils.java
+++ python-checks/src/main/java/org/sonar/python/checks/cdk/CdkUtils.java
@@ -262,6 +262,10 @@ public class CdkUtils {
     public IssueLocation asSecondaryLocation(String message) {
       return IssueLocation.preciseLocation(getLast().parent(), message);
     }
+
+    public SubscriptionContext ctx() {
+      return ctx;
+    }
   }
 
   /**
--- python-checks/src/test/resources/checks/cdk/publicNetworkAccessDatabaseInstance.py
+++ python-checks/src/test/resources/checks/cdk/publicNetworkAccessDatabaseInstance.py
@@ -2,7 +2,8 @@ from aws_cdk import aws_rds as rds
 from aws_cdk import aws_ec2 as ec2
 
 rds.DatabaseInstance(
-    publicly_accessible=True,   # Noncompliant
+    publicly_accessible=True,   # Noncompliant {{Make sure allowing public network access is safe here.}}
+#   ^^^^^^^^^^^^^^^^^^^^^^^^
     vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
 )
 
@@ -15,3 +16,19 @@ rds.DatabaseInstance(
     publicly_accessible=True,  # Compliant, IP won't be routable
     vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT),
 )
+
+rds.DatabaseInstance(
+    publicly_accessible=True,  # Noncompliant
+)
+
+rds.DatabaseInstance(
+    # Noncompliant@+1 {{Make sure allowing public network access is safe here.}}
+    vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+#                                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+)
+
+rds.DatabaseInstance(
+    publicly_accessible=True,  # Noncompliant
+    vpc_subnets=unknown_subnets
+)
+
