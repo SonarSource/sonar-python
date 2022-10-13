fc2da69b030651f0543c46c0bab11e14232e5c45
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2022-10-13 13:24:43 +0200

    SONARPY-1139 ApiGateway: RestApi part 2

--- python-checks/src/main/java/org/sonar/python/checks/cdk/PublicApiIsSecuritySensitiveCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/cdk/PublicApiIsSecuritySensitiveCheck.java
@@ -19,30 +19,100 @@
  */
 package org.sonar.python.checks.cdk;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
 import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.FunctionDef;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.tree.FunctionDefImpl;
+import org.sonar.python.tree.TreeUtils;
 
 import static org.sonar.python.checks.cdk.CdkPredicate.isFqn;
 import static org.sonar.python.checks.cdk.CdkPredicate.isString;
 import static org.sonar.python.checks.cdk.CdkUtils.getArgument;
+import static org.sonar.python.checks.cdk.CdkUtils.getDictionary;
+import static org.sonar.python.checks.cdk.CdkUtils.getDictionaryPair;
 
 @Rule(key = "S6333")
 public class PublicApiIsSecuritySensitiveCheck extends AbstractCdkResourceCheck {
   private static final String MESSAGE = "Make sure that creating public APIs is safe here.";
   private static final String OMITTING_MESSAGE = "Omitting \"authorization_type\" disables authentication. Make sure it is safe here.";
+  private static final String AUTHORIZATION_TYPE = "authorization_type";
+  private static final String AUTHORIZATION_TYPE_NONE = "aws_cdk.aws_apigateway.AuthorizationType.NONE";
+
+  private Set<String> safeMethods = new HashSet<>();
 
   @Override
   protected void registerFqnConsumer() {
     checkFqns(List.of("aws_cdk.aws_apigateway.CfnMethod", "aws_cdk.aws_apigatewayv2.CfnRoute"), (subscriptionContext, callExpression) ->
-      getArgument(subscriptionContext, callExpression, "authorization_type").ifPresentOrElse(
+      getArgument(subscriptionContext, callExpression, AUTHORIZATION_TYPE).ifPresentOrElse(
         argument -> argument.addIssueIf(isString("NONE"), MESSAGE),
         () -> subscriptionContext.addIssue(callExpression.callee(), OMITTING_MESSAGE)
       )
     );
-    checkFqn("aws_cdk.aws_apigateway.Resource.add_method", (subscriptionContext, callExpression) ->
-      getArgument(subscriptionContext, callExpression, "authorization_type").ifPresent(
-        argument -> argument.addIssueIf(isFqn("aws_cdk.aws_apigateway.AuthorizationType.NONE"), MESSAGE)
+    checkFqns(List.of("aws_cdk.aws_apigateway.RestApi", "aws_cdk.aws_apigateway.Resource.add_resource"), (subscriptionContext, callExpression) ->
+      getArgument(subscriptionContext, callExpression, "default_method_options").ifPresent(
+        argument -> {
+          if (isNotSetToNone(subscriptionContext, argument)) {
+            // if invocation of RestApi() or Resource.add_resource() is with the safe default, and it's in the method,
+            // then store the method's full qualified name in safeMethods
+            enclosingMethodFqn(callExpression).ifPresent(fqn -> safeMethods.add(fqn));
+          }
+        }
       )
     );
+    checkFqn("aws_cdk.aws_apigateway.Resource.add_method", (subscriptionContext, callExpression) ->
+      getArgument(subscriptionContext, callExpression, AUTHORIZATION_TYPE).ifPresentOrElse(
+        argument -> argument.addIssueIf(isFqn(AUTHORIZATION_TYPE_NONE), MESSAGE),
+        () -> enclosingMethodFqn(callExpression).filter(fqn -> safeMethods.contains(fqn)).ifPresentOrElse(fqn -> {}, () ->
+          // if the invocation of Resource.add_method() is without explicit authorization_type argument,
+          // and it's not in the scope of safeMethod the call, then it's considered unsafe
+          // this is the best possible approximation for now
+          subscriptionContext.addIssue(callExpression.callee(), OMITTING_MESSAGE)
+        )
+      )
+    );
+  }
+
+  private static Optional<String> enclosingMethodFqn(Tree tree) {
+    FunctionDef functionDef = (FunctionDef) TreeUtils.firstAncestorOfKind(tree, Tree.Kind.FUNCDEF);
+    return Optional.ofNullable(functionDef)
+      .map(FunctionDefImpl.class::cast)
+      .map(FunctionDefImpl::functionSymbol)
+      .map(Symbol::fullyQualifiedName);
+  }
+
+  private static boolean isNotSetToNone(SubscriptionContext subscriptionContext, CdkUtils.ExpressionFlow argument) {
+    return isSafeDictionaryAuthorisationKey(subscriptionContext, argument.getLast())
+      || isSafeAuthorisationArgument(subscriptionContext, argument);
+  }
+
+
+  private static boolean isSafeDictionaryAuthorisationKey(SubscriptionContext ctx, Expression expression) {
+    return getDictionary(expression)
+      .flatMap(dictionary -> getDictionaryPair(ctx, dictionary, AUTHORIZATION_TYPE))
+      .filter(element -> isNotNoneValue().test(element.value.getLast()))
+      .isPresent();
+  }
+  private static Predicate<Expression> isNotNoneValue() {
+    return isFqn(AUTHORIZATION_TYPE_NONE).negate();
+  }
+
+  private static boolean isSafeAuthorisationArgument(SubscriptionContext subscriptionContext, CdkUtils.ExpressionFlow expression) {
+    return expression.getExpression(isCallExpression().and(isFqn("aws_cdk.aws_apigateway.MethodOptions")))
+      .flatMap(expr -> getArgument(subscriptionContext, (CallExpression) expr, AUTHORIZATION_TYPE))
+      .filter(expr -> expr.hasExpression(isFqn(AUTHORIZATION_TYPE_NONE)))
+      .isEmpty();
+  }
+
+  public static Predicate<Expression> isCallExpression() {
+    return expression -> expression.is(Tree.Kind.CALL_EXPR);
   }
 }
