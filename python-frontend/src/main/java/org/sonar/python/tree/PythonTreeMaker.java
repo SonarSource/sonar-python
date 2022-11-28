70b5e4549d025aade56c19feaac12b5e80192c6c
Rudy Regazzoni <rudy.regazzoni@sonarsource.com>
2022-11-28 16:02:03 +0100

    SONARPY-1169 Support except* syntax

--- python-frontend/src/main/java/org/sonar/python/tree/PythonTreeMaker.java
+++ python-frontend/src/main/java/org/sonar/python/tree/PythonTreeMaker.java
@@ -747,7 +747,6 @@ public class PythonTreeMaker {
 
   public void checkExceptClauses(List<ExceptClause> excepts) {
     if (excepts.isEmpty()) return;
-
     Tree.Kind firstExceptKind = excepts.get(0).getKind();
     for (ExceptClause except : excepts) {
       if (firstExceptKind != except.getKind()) {
