ec58b4310fb8a89fa77d66fb754be06b3b3dab6c
Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>
2023-01-25 13:13:23 +0100

    SONARPY-1080 code simplified

--- python-checks/src/main/java/org/sonar/python/checks/RedundantJumpCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/RedundantJumpCheck.java
@@ -19,13 +19,13 @@
  */
 package org.sonar.python.checks;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
-import org.sonar.plugins.python.api.cfg.CfgBlock;
 import org.sonar.plugins.python.api.cfg.ControlFlowGraph;
 import org.sonar.plugins.python.api.tree.FileInput;
 import org.sonar.plugins.python.api.tree.FunctionDef;
@@ -43,6 +43,8 @@ import org.sonar.python.tree.TreeUtils;
 @Rule(key = "S3626")
 public class RedundantJumpCheck extends PythonSubscriptionCheck {
 
+  public static final String QUICK_FIX_DESCRIPTION = "Remove redundant statement";
+
   @Override
   public void initialize(Context context) {
     context.registerSyntaxNodeConsumer(Kind.FILE_INPUT, ctx ->
@@ -54,36 +56,40 @@ public class RedundantJumpCheck extends PythonSubscriptionCheck {
   }
 
   private static void checkCfg(@Nullable ControlFlowGraph cfg, SubscriptionContext ctx) {
-    if (cfg == null) {
-      return;
-    }
-    for (CfgBlock cfgBlock : cfg.blocks()) {
-      if (cfgBlock.successors().size() == 1 && cfgBlock.successors().contains(cfgBlock.syntacticSuccessor())) {
+    Optional.ofNullable(cfg)
+      .map(ControlFlowGraph::blocks)
+      .stream()
+      .flatMap(Collection::stream)
+      .filter(cfgBlock -> cfgBlock.successors().size() == 1 && cfgBlock.successors().contains(cfgBlock.syntacticSuccessor()))
+      .forEach(cfgBlock -> {
         List<Tree> elements = cfgBlock.elements();
         Tree lastElement = elements.get(elements.size() - 1);
         if (!isException(lastElement)) {
           var issue = ctx.addIssue(lastElement, message(lastElement));
-
-          Optional.of(issue)
-            .filter(IssueWithQuickFix.class::isInstance)
-            .map(IssueWithQuickFix.class::cast)
-            .ifPresent(i -> {
-              if (lastElement instanceof Statement) {
-                var quickFix = PythonQuickFix
-                  .newQuickFix("Remove redundant statement")
-                  .addTextEdit(PythonTextEdit.removeStatement((Statement) lastElement))
-                  .build();
-                i.addQuickFix(quickFix);
-              }
-            });
+          addQuickFix(lastElement, issue);
 
           if (lastElement.is(Kind.CONTINUE_STMT)) {
             Tree loop = ((PythonCfgBranchingBlock) cfgBlock.successors().iterator().next()).branchingTree();
             issue.secondary(loop.firstToken(), null);
           }
         }
-      }
+      });
+  }
+
+  private static void addQuickFix(Tree lastElement, PreciseIssue issue) {
+    if (!(lastElement instanceof Statement)) {
+      return;
     }
+    Optional.of(issue)
+      .filter(IssueWithQuickFix.class::isInstance)
+      .map(IssueWithQuickFix.class::cast)
+      .ifPresent(i -> {
+          var quickFix = PythonQuickFix
+            .newQuickFix(QUICK_FIX_DESCRIPTION)
+            .addTextEdit(PythonTextEdit.removeStatement((Statement) lastElement))
+            .build();
+          i.addQuickFix(quickFix);
+      });
   }
 
   private static String message(Tree jumpStatement) {
