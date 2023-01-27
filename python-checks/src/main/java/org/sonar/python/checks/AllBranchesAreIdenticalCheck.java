0604876f4ee6768a444c935728f086c4fb67f6a1
maksim-grebeniuk-sonarsource <122789225+maksim-grebeniuk-sonarsource@users.noreply.github.com>
2023-01-27 12:10:06 +0100

    SONARPY-1096 Add secondary message for S3923 (AllBranchesAreIdenticalCheck) (#1352)

--- python-checks/src/main/java/org/sonar/python/checks/AllBranchesAreIdenticalCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/AllBranchesAreIdenticalCheck.java
@@ -48,6 +48,7 @@ public class AllBranchesAreIdenticalCheck extends PythonSubscriptionCheck {
   private static final String CONDITIONAL_MESSAGE = "This conditional expression returns the same value whether the condition is \"true\" or \"false\".";
 
   private static final List<ConditionalExpression> ignoreList = new ArrayList<>();
+  public static final String SECONDARY_MESSAGE = "Duplicated statements.";
 
   @Override
   public void initialize(Context context) {
@@ -72,17 +73,17 @@ public class AllBranchesAreIdenticalCheck extends PythonSubscriptionCheck {
       return;
     }
     IssueWithQuickFix issue = (IssueWithQuickFix) ctx.addIssue(ifStmt.keyword(), IF_STATEMENT_MESSAGE);
-    issue.secondary(issueLocation(ifStmt.body()));
-    ifStmt.elifBranches().forEach(e -> issue.secondary(issueLocation(e.body())));
-    issue.secondary(issueLocation(elseBranch.body()));
+    issue.secondary(secondaryIssueLocation(ifStmt.body()));
+    ifStmt.elifBranches().forEach(e -> issue.secondary(secondaryIssueLocation(e.body())));
+    issue.secondary(secondaryIssueLocation(elseBranch.body()));
     if (!hasSideEffect(ifStmt)) {
       issue.addQuickFix(computeQuickFixForIfStatement(ifStmt, elseBranch));
     }
   }
 
-  private static IssueLocation issueLocation(StatementList body) {
+  private static IssueLocation secondaryIssueLocation(StatementList body) {
     List<Token> tokens = TreeUtils.nonWhitespaceTokens(body);
-    return IssueLocation.preciseLocation(tokens.get(0), tokens.get(tokens.size() - 1), null);
+    return IssueLocation.preciseLocation(tokens.get(0), tokens.get(tokens.size() - 1), SECONDARY_MESSAGE);
   }
 
   private static void handleConditionalExpression(ConditionalExpression conditionalExpression, SubscriptionContext ctx) {
@@ -105,7 +106,7 @@ public class AllBranchesAreIdenticalCheck extends PythonSubscriptionCheck {
       addSecondaryLocations(issue, conditionalExpression.trueExpression());
       addSecondaryLocations(issue, conditionalExpression.falseExpression());
     } else {
-      issue.secondary(unwrappedExpression, null);
+      issue.secondary(unwrappedExpression, SECONDARY_MESSAGE);
     }
   }
 
