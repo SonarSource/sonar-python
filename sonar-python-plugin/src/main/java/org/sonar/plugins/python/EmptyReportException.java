12c9a7d0187447ed36400cce00dbc85970b16ba9
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2023-01-31 08:59:29 +0100

    SONAR-1241 Gradle Poc, trigger the build

--- sonar-python-plugin/src/main/java/org/sonar/plugins/python/EmptyReportException.java
+++ sonar-python-plugin/src/main/java/org/sonar/plugins/python/EmptyReportException.java
@@ -23,4 +23,5 @@ package org.sonar.plugins.python;
 @SuppressWarnings("serial")
 /** This exception is thrown when an empty report is detected */
 public class EmptyReportException extends RuntimeException {
+
 }
