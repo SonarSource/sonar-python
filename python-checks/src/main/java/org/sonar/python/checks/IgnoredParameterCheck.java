d3609b76c6371874fea57110961e1735d426e873
Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>
2023-01-31 09:31:18 +0100

    SONARPY-1097 unused imports cleanup

--- python-checks/src/main/java/org/sonar/python/checks/IgnoredParameterCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/IgnoredParameterCheck.java
@@ -21,7 +21,6 @@ package org.sonar.python.checks;
 
 import java.util.Comparator;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collector;
