959b9c693eb3e0de490c7fd3e719e294aa198b1c
Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>
2023-02-09 16:43:13 +0100

    SONARPY-1265 CR fixes

--- python-checks/src/main/java/org/sonar/python/checks/BooleanExpressionInExceptCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/BooleanExpressionInExceptCheck.java
@@ -24,17 +24,14 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.sonar.api.utils.log.Logger;
-import org.sonar.api.utils.log.Loggers;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
 import org.sonar.plugins.python.api.tree.BinaryExpression;
 import org.sonar.plugins.python.api.tree.ExceptClause;
+import org.sonar.plugins.python.api.tree.Expression;
 import org.sonar.plugins.python.api.tree.Name;
-import org.sonar.plugins.python.api.tree.ParenthesizedExpression;
 import org.sonar.plugins.python.api.tree.Token;
-import org.sonar.plugins.python.api.tree.Tree;
 import org.sonar.plugins.python.api.tree.Tree.Kind;
 import org.sonar.python.quickfix.IssueWithQuickFix;
 import org.sonar.python.quickfix.PythonQuickFix;
@@ -43,8 +40,6 @@ import org.sonar.python.tree.TreeUtils;
 
 @Rule(key = "S5714")
 public class BooleanExpressionInExceptCheck extends PythonSubscriptionCheck {
-
-  private static final Logger LOG = Loggers.get(BooleanExpressionInExceptCheck.class);
 
   public static final String MESSAGE = "Rewrite this \"except\" expression as a tuple of exception classes.";
   public static final String QUICK_FIX_MESSAGE = "Replace with a tuple";
@@ -67,7 +62,8 @@ public class BooleanExpressionInExceptCheck extends PythonSubscriptionCheck {
       });
   }
 
-  private static List<String> collectNames(Tree expression) {
+  private static List<String> collectNames(Expression expression) {
+    expression = Expressions.removeParentheses(expression);
     if (expression.is(Kind.OR, Kind.AND)) {
       var binaryExpression = (BinaryExpression) expression;
       var leftExceptions = collectNames(binaryExpression.leftOperand());
@@ -76,9 +72,6 @@ public class BooleanExpressionInExceptCheck extends PythonSubscriptionCheck {
       result.addAll(leftExceptions);
       result.addAll(rightExceptions);
       return result;
-    } else if (expression.is(Kind.PARENTHESIZED)) {
-      var parenthesizedExpression = (ParenthesizedExpression) expression;
-      return collectNames(parenthesizedExpression.expression());
     } else if (expression.is(Kind.NAME)) {
       var name = (Name) expression;
       return List.of(name.name());
@@ -92,14 +85,14 @@ public class BooleanExpressionInExceptCheck extends PythonSubscriptionCheck {
     throw new IllegalArgumentException("Unsupported kind of tree element: " + expression.getKind().name());
   }
 
-  private static void addQuickFix(IssueWithQuickFix issue, Tree expression) {
-    expression = Objects.requireNonNullElse(TreeUtils.firstAncestorOfKind(expression, Kind.PARENTHESIZED), expression);
+  private static void addQuickFix(IssueWithQuickFix issue, Expression expression) {
+    expression = Objects.requireNonNullElse((Expression) TreeUtils.firstAncestorOfKind(expression, Kind.PARENTHESIZED), expression);
 
     List<String> names;
     try {
       names = collectNames(expression);
     } catch (IllegalArgumentException e) {
-      LOG.error("Could not add quick fix", e);
+      // expression contains subexpressions that are out of scope for quick fixing
       return;
     }
 
