7b52a4e24eeb961c398cf375f0f0f04213a1be12
Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>
2023-03-22 08:41:33 +0100

    SONARPY-1301 CR fixes

--- python-checks/src/test/resources/checks/regex/possessiveQuantifierContinuationCheck.py
+++ python-checks/src/test/resources/checks/regex/possessiveQuantifierContinuationCheck.py
@@ -1,7 +1,29 @@
 import re
 
-match1 = re.match(r"a++abc", "aaaabc", re.DOTALL) # Noncompliant {{Change this impossible to match sub-pattern that conflicts with the previous possessive quantifier.}}
-match2 = re.match(r"\d*+[02468]", "1234", re.DOTALL) # Noncompliant {{Change this impossible to match sub-pattern that conflicts with the previous possessive quantifier.}}
+def foo():
+    match1 = re.match(r"a++abc", "aaaabc", re.DOTALL) # Noncompliant
+#                          ^
 
-match3 = re.match(r"aa++bc", "aaaabc", re.DOTALL) # Compliant, for example it can match "aaaabc"
-match4 = re.match(r"\d*+(?<=[02468])", "1234", re.DOTALL) # Compliant, for example, it can match an even number like "1234"
+    match2 = re.match(r"\d*+[02468]", "1234", re.DOTALL) # Noncompliant {{Change this impossible to match sub-pattern that conflicts with the previous possessive quantifier.}}
+    #                       ^^^^^^^
+    match3 = re.match(r"aa++bc", "aaaabc", re.DOTALL) # Compliant, for example it can match "aaaabc"
+    match4 = re.match(r"\d*+(?<=[02468])", "1234", re.DOTALL) # Compliant, for example, it can match an even number like "1234"
+
+    re.match(r"a+abc", "aaaabc", re.DOTALL)
+    re.match(r"a+?abc", "aaaabc", re.DOTALL)
+    re.match(r"a++abc", "aaaabc", re.DOTALL) # Noncompliant {{Change this impossible to match sub-pattern that conflicts with the previous possessive quantifier.}}
+    re.match(r"\d*+[02468]", "aaaabc", re.DOTALL) # Noncompliant
+    re.match(r"(\d)*+([02468])", "aaaabc", re.DOTALL) # Noncompliant
+    re.match(r"\d++(?:[eE][+-]?\d++)?[fFdD]?", "aaaabc", re.DOTALL)
+    re.match(r"a*+\s", "aaaabc", re.DOTALL)
+    re.match("[+-]?(?:NaN|Infinity|(?:\\d++(?:\\.\\d*+)?|\\.\\d++)(?:[eE][+-]?\\d++)?[fFdD]?|0[xX](?:\\p{XDigit}++(?:\\.\\p{XDigit}*+)?|\\.\\p{XDigit}++)[pP][+-]?\\d++[fFdD]?)", "aaaabc", re.DOTALL)
+    re.match("aa++bc", "aaaabc", re.DOTALL)
+    re.match(r"\d*+(?<=[02468])", "aaaabc", re.DOTALL)
+    re.match(r"(xx++)+x", "aaaabc", re.DOTALL) # Noncompliant
+    re.match(r"(bx++)+x", "aaaabc", re.DOTALL) # Noncompliant
+    re.match(r"(?:xx++)+x", "aaaabc", re.DOTALL) # Noncompliant
+    re.match(r"(xx++)x", "aaaabc", re.DOTALL) # Noncompliant
+    re.match(r".*+\w", "aaaabc", re.DOTALL) # FN
+    re.match(r".*+\w+", "aaaabc", re.DOTALL) # FN
+    re.match(r"(a|b|c)*+(a|b)", "aaaabc", re.DOTALL) # Noncompliant
+    re.match(r"(:[0-9])?+(:[0-9])?+", "aaaabc", re.DOTALL)
