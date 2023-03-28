c1eb155f7d91951f90bf7243b1538f4a10c0e1e0
David Kunzmann <joke1196@users.noreply.github.com>
2023-03-28 11:15:30 +0200

    SONARPY-1315 Rule S6540: Function parameters should have type hints (#1428)

--- python-checks/src/main/java/org/sonar/python/checks/MandatoryFunctionParameterTypeHintCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/MandatoryFunctionParameterTypeHintCheck.java
@@ -19,14 +19,17 @@
  */
 package org.sonar.python.checks;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.symbols.FunctionSymbol;
+import org.sonar.plugins.python.api.tree.Name;
 import org.sonar.plugins.python.api.tree.Parameter;
 import org.sonar.plugins.python.api.tree.ParameterList;
+import org.sonar.plugins.python.api.tree.Token;
 import org.sonar.plugins.python.api.tree.Tree;
 import org.sonar.python.tree.FunctionDefImpl;
 
@@ -35,34 +38,57 @@ public class MandatoryFunctionParameterTypeHintCheck extends PythonSubscriptionC
 
   private static final String MESSAGE = "Add a type hint to this function parameter.";
 
+  private static final List<String> SPECIAL_TOKEN_PARAMS = Arrays.asList("*", "/");
+
   @Override
   public void initialize(Context context) {
     context.registerSyntaxNodeConsumer(Tree.Kind.FUNCDEF, ctx -> {
       FunctionDefImpl functionDef = (FunctionDefImpl) ctx.syntaxNode();
       ParameterList parameterList = functionDef.parameters();
       FunctionSymbol functionSymbol = functionDef.functionSymbol();
-      if (parameterList != null ) {
-        removeSelfAndClassParameters(parameterList, functionSymbol)
-          .forEach(parameter -> {
-            if (parameter.typeAnnotation() == null) {
-              ctx.addIssue(parameter, MESSAGE);
-            }
-          });
+      if (parameterList != null) {
+        List<Parameter> parameters = parameterList.nonTuple();
+        for (int i = 0; i < parameters.size(); i++) {
+          Parameter parameter = parameters.get(i);
+          boolean isFirstParameter = i == 0;
+          if (shouldRaiseIssue(functionSymbol, parameter, isFirstParameter)) {
+            ctx.addIssue(parameter, MESSAGE);
+          }
+        }
       }
     });
   }
 
-  private static Stream<Parameter> removeSelfAndClassParameters(ParameterList parameterList, @Nullable FunctionSymbol functionSymbol) {
-    return parameterList.nonTuple().stream()
-      .filter(param -> !isASelfInstanceParameter(param, functionSymbol) && !isAClassMethodParameter(param, functionSymbol));
+  private static boolean shouldRaiseIssue(@Nullable FunctionSymbol functionSymbol, Parameter parameter, boolean isFirstParameter) {
+    return !isASelfInstanceParameter(parameter, functionSymbol) &&
+      !isFirstParamOfClassAnnotatedMethod(functionSymbol, isFirstParameter) &&
+      !isFirstParamOfNewMethod(functionSymbol, isFirstParameter) &&
+      !isSpecialCharParameter(parameter) &&
+      parameter.typeAnnotation() == null;
   }
 
   private static boolean isASelfInstanceParameter(Parameter parameter, @Nullable FunctionSymbol functionSymbol) {
     return hasName("self", parameter) && functionSymbol != null && functionSymbol.isInstanceMethod();
   }
 
-  private static boolean isAClassMethodParameter(Parameter parameter, @Nullable FunctionSymbol functionSymbol) {
-    return hasName("cls", parameter) && functionSymbol != null && functionSymbol.decorators().contains("classmethod");
+  private static boolean isFirstParamOfClassAnnotatedMethod(@Nullable FunctionSymbol functionSymbol, boolean isFirstParameter) {
+    return functionSymbol != null && functionSymbol.decorators().contains("classmethod") && isFirstParameter;
+  }
+
+  private static boolean isFirstParamOfNewMethod(@Nullable FunctionSymbol functionSymbol, boolean isFirstParameter) {
+    return functionSymbol != null && "__new__".equals(functionSymbol.name()) && isFirstParameter;
+  }
+
+  private static boolean isSpecialCharParameter(Parameter parameter) {
+    Name parameterName = parameter.name();
+    Token maybeToken = parameter.starToken();
+    return (hasName("_", parameter)) || (parameterName == null && isSpecialTokenParameter(maybeToken));
+  }
+
+  private static Boolean isSpecialTokenParameter(@Nullable Token maybeToken) {
+    return Optional.ofNullable(maybeToken)
+      .map(token -> SPECIAL_TOKEN_PARAMS.contains(token.value()))
+      .orElse(false);
   }
 
   private static boolean hasName(String name, Parameter parameter) {
