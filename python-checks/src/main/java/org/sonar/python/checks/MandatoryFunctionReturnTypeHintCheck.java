86a2c22e64a9b31971ccf424924e1245dc43ece3
David Kunzmann <6317721+joke1196@users.noreply.github.com>
2023-04-11 16:26:36 +0200

    SONARPY-1320 Rule S6538: Add quick fix for return type hints (#1434)

--- python-checks/src/main/java/org/sonar/python/checks/MandatoryFunctionReturnTypeHintCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/MandatoryFunctionReturnTypeHintCheck.java
@@ -19,22 +19,40 @@
  */
 package org.sonar.python.checks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
 import org.sonar.plugins.python.api.quickfix.PythonQuickFix;
+import org.sonar.plugins.python.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.python.api.tree.Expression;
 import org.sonar.plugins.python.api.tree.FunctionDef;
 import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.ReturnStatement;
 import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.plugins.python.api.tree.YieldStatement;
+import org.sonar.plugins.python.api.types.BuiltinTypes;
 import org.sonar.python.quickfix.TextEditUtils;
 import org.sonar.python.tree.FunctionDefImpl;
+import org.sonar.python.types.InferredTypes;
 
 @Rule(key = "S6538")
 public class MandatoryFunctionReturnTypeHintCheck extends PythonSubscriptionCheck {
 
-  private static final String MESSAGE = "Add a return type hint to this function declaration.";
+  public static final String MESSAGE = "Add a return type hint to this function declaration.";
   public static final String CONSTRUCTOR_MESSAGE = "Annotate the return type of this constructor with `None`.";
+  private static final List<String> SUPPORTED_TYPES = List.of(
+    BuiltinTypes.STR,
+    BuiltinTypes.NONE_TYPE,
+    BuiltinTypes.BOOL,
+    BuiltinTypes.COMPLEX,
+    BuiltinTypes.FLOAT,
+    BuiltinTypes.INT);
 
   @Override
   public void initialize(Context context) {
@@ -45,7 +63,7 @@ public class MandatoryFunctionReturnTypeHintCheck extends PythonSubscriptionChec
         FunctionDefImpl functionDefImpl = (FunctionDefImpl) functionDef;
         Optional.ofNullable(functionDefImpl.functionSymbol())
           .filter(functionSymbol -> "__init__".equals(functionName.name()) && functionSymbol.isInstanceMethod())
-          .ifPresentOrElse(symbol -> raiseIssueForConstructor(ctx, functionName, functionDef), () -> ctx.addIssue(functionName, MESSAGE));
+          .ifPresentOrElse(symbol -> raiseIssueForConstructor(ctx, functionName, functionDef), () -> raiseIssueForReturnType(ctx, functionName, functionDef));
       }
     });
   }
@@ -55,6 +73,64 @@ public class MandatoryFunctionReturnTypeHintCheck extends PythonSubscriptionChec
     PythonQuickFix quickFix = PythonQuickFix.newQuickFix(CONSTRUCTOR_MESSAGE)
       .addTextEdit(TextEditUtils.insertAfter(functionDef.rightPar(), " -> None"))
       .build();
-    preciseIssue.addQuickFix(quickFix); 
+    preciseIssue.addQuickFix(quickFix);
+  }
+
+  private static void raiseIssueForReturnType(SubscriptionContext ctx, Name functionName, FunctionDef functionDef) {
+    PreciseIssue issue = ctx.addIssue(functionName, MESSAGE);
+    ReturnStatementVisitor returnStatementVisitor = new ReturnStatementVisitor();
+    functionDef.body().accept(returnStatementVisitor);
+    if (!returnStatementVisitor.returnStatements.isEmpty()) {
+      addQuickFixForReturnType(issue, functionDef, returnStatementVisitor.returnStatements);
+    } else if (returnStatementVisitor.yieldStatements.isEmpty()) {
+      addQuickFixForNoneType(issue, functionDef);
+    }
+  }
+
+  private static void addQuickFixForNoneType(PreciseIssue issue, FunctionDef functionDef) {
+    PythonQuickFix quickFix = PythonQuickFix.newQuickFix(MandatoryFunctionReturnTypeHintCheck.MESSAGE)
+      .addTextEdit(TextEditUtils.insertAfter(functionDef.rightPar(), " -> None"))
+      .build();
+    issue.addQuickFix(quickFix);
+  }
+
+  private static void addQuickFixForReturnType(PreciseIssue issue, FunctionDef functionDef, List<ReturnStatement> statements) {
+    Set<String> returnTypes = statements.stream()
+      .flatMap(stmts -> stmts.expressions().stream())
+      .map(Expression::type)
+      .map(InferredTypes::typeName)
+      .filter(Objects::nonNull)
+      .collect(Collectors.toSet());
+    if (returnTypes.size() == 1) {
+      String typeName = returnTypes.stream().iterator().next();
+      if (SUPPORTED_TYPES.contains(typeName)) {
+        PythonQuickFix quickFix = PythonQuickFix.newQuickFix(MandatoryFunctionReturnTypeHintCheck.MESSAGE)
+          .addTextEdit(TextEditUtils.insertAfter(functionDef.rightPar(), String.format(" -> %s", fixTypeName(typeName))))
+          .build();
+        issue.addQuickFix(quickFix);
+      }
+    }
+  }
+
+  private static String fixTypeName(String typeName) {
+    return typeName.equals(BuiltinTypes.NONE_TYPE) ? "None" : typeName;
+  }
+
+  private static class ReturnStatementVisitor extends BaseTreeVisitor {
+
+    private final List<ReturnStatement> returnStatements = new ArrayList<>();
+    private final List<YieldStatement> yieldStatements = new ArrayList<>();
+
+    @Override
+    public void visitReturnStatement(ReturnStatement pyReturnStatementTree) {
+      super.visitReturnStatement(pyReturnStatementTree);
+      returnStatements.add(pyReturnStatementTree);
+    }
+
+    @Override
+    public void visitYieldStatement(YieldStatement pyYieldStatementTree) {
+      super.visitYieldStatement(pyYieldStatementTree);
+      yieldStatements.add(pyYieldStatementTree);
+    }
   }
 }
--- python-checks/src/test/java/org/sonar/python/checks/MandatoryFunctionReturnTypeHintCheckTest.java
+++ python-checks/src/test/java/org/sonar/python/checks/MandatoryFunctionReturnTypeHintCheckTest.java
@@ -77,4 +77,214 @@ public class MandatoryFunctionReturnTypeHintCheckTest {
     PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
     PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.CONSTRUCTOR_MESSAGE);
   }
+
+  @Test
+  public void quick_fix_add_return_type_literal() {
+    String codeWithIssue = code(
+      "def foo():",
+      "    return \"bar\"");
+    String codeFixed = code(
+      "def foo() -> str:",
+      "    return \"bar\"");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_return_expression() {
+    String codeWithIssue = code(
+      "def foo(alice: str):",
+      "    return  alice + \"bar\"");
+    String codeFixed = code(
+      "def foo(alice: str) -> str:",
+      "    return  alice + \"bar\"");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_no_return_keyword() {
+    String codeWithIssue = code(
+      "def foo(bob):",
+      "    print(bob) ");
+    String codeFixed = code(
+      "def foo(bob) -> None:",
+      "    print(bob) ");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_nested_return_type() {
+    String codeWithIssue = code(
+      "def foo(bob: bool):",
+      "    if bob:",
+      "        return 42",
+      "    else:",
+      "        return 40");
+    String codeFixed = code(
+      "def foo(bob: bool) -> int:",
+      "    if bob:",
+      "        return 42",
+      "    else:",
+      "        return 40");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_for_nested_function() {
+    String codeWithIssue = code(
+      "def foo() -> int:",
+      "    def bar():",
+      "        return \"foobar\"",
+      "    return bar + 1");
+    String codeFixed = code(
+      "def foo() -> int:",
+      "    def bar() -> str:",
+      "        return \"foobar\"",
+      "    return bar + 1");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_for_return_with_yield() {
+    String codeWithIssue = code(
+      "def foo(alice: dict[str, int]):",
+      "    bar = 0",
+      "    for k, v in alice.items():",
+      "        _ = yield 41",
+      "    return bar + 1");
+    String codeFixed = code(
+      "def foo(alice: dict[str, int]) -> int:",
+      "    bar = 0",
+      "    for k, v in alice.items():",
+      "        _ = yield 41",
+      "    return bar + 1");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_for_complex() {
+    String codeWithIssue = code(
+      "def foo():",
+      "    return complex(1,2)");
+    String codeFixed = code(
+      "def foo() -> complex:",
+      "    return complex(1,2)");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_for_float() {
+    String codeWithIssue = code(
+      "def foo():",
+      "    return 3.14");
+    String codeFixed = code(
+      "def foo() -> float:",
+      "    return 3.14");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void quick_fix_for_return_none() {
+    String codeWithIssue = code(
+      "def foo():",
+      "    return None");
+    String codeFixed = code(
+      "def foo() -> None:",
+      "    return None");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verify(check, codeWithIssue, codeFixed);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, codeWithIssue, MandatoryFunctionReturnTypeHintCheck.MESSAGE);
+  }
+
+  @Test
+  public void no_quick_fix_collections() {
+    String codeWithIssue = code(
+      "from typing import Dict",
+      "def foo(alice: Dict[str, int]):",
+      "    bob = {}",
+      "    for k, v in alice.items():",
+      "        bob[k] = str(v)",
+      "    return bob");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, codeWithIssue);
+  }
+
+  @Test
+  public void no_quick_fix_tuples() {
+    String codeWithIssue = code(
+      "def foo():",
+      "    return (\"bar\", 1)");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, codeWithIssue);
+  }
+
+  @Test
+  public void no_quick_fix_generic_types() {
+    String codeWithIssue = code(
+      "def foo(alice: dict[str, int]):",
+      "    bob = {}",
+      "    for k, v in alice.items():",
+      "        bob[k] = str(v)",
+      "    return bob");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, codeWithIssue);
+  }
+
+  @Test
+  public void no_quick_fix_ambiguous_type() {
+    String codeWithIssue = code(
+      "def foo(alice: str, bob: int):",
+      "    return alice + bob");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, codeWithIssue);
+  }
+
+  @Test
+  public void no_quick_fix_any_type() {
+    String codeWithIssue = code(
+      "def foo(alice: Any):",
+      "    return alice");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, codeWithIssue);
+  }
+
+  @Test
+  public void no_quick_fix_too_many_types() {
+    String codeWithIssue = code(
+      "def foo(bob: str):",
+      "    if bob == \"foo\":",
+      "        return \"life\" ",
+      "    elif bob == \"bar\":",
+      "        return 42",
+      "    else:",
+      "        return list()");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, codeWithIssue);
+  }
+
+  @Test
+  public void no_quick_fix_for_yield() {
+    String codeWithIssue = code(
+      "def foo(alice: dict[str, int]):",
+      "    for k, v in alice.items():",
+      "        yield 42");
+    MandatoryFunctionReturnTypeHintCheck check = new MandatoryFunctionReturnTypeHintCheck();
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, codeWithIssue);
+  }
+
 }
