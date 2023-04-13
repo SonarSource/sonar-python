a3312da3fe1a842b88aa1f9be8388121863cef19
David Kunzmann <david.kunzmann@sonarsource.com>
2023-04-13 14:04:58 +0200

    SONARPY-975: Added more logs

--- python-frontend/typeshed_serializer/runners/tox_runner.py
+++ python-frontend/typeshed_serializer/runners/tox_runner.py
@@ -88,6 +88,10 @@ def main():
     previous_sources_checksum = read_previous_checksum(CHECKSUM_FILE)
     all_files = fetch_source_file_names(SERIALIZER_PATH)
     current_sources_checksum = compute_checksum(all_files, normalize_text_files)
+    logger.info("STARTING TYPESHED SOURCE FILE CHECKSUM COMPUTATION")
+    logger.info(f"Previous checksum {previous_sources_checksum}")
+    logger.info(f"Current checksum {current_sources_checksum}")
+    logger.info(f"Checksum is computed over {len(all_files)} files")
     if previous_sources_checksum != current_sources_checksum:
         logger.info("STARTING TYPESHED SERIALIZATION")
         subprocess.run(["tox"])
@@ -95,6 +99,10 @@ def main():
         binary_file_names = fetch_resource_file_names(BINARY_FOLDER_PATH, file_extension='.protobuf')
         previous_binaries_checksum = read_previous_checksum(CHECKSUM_BINARIES_FILE)
         current_binaries_checksum = compute_checksum(binary_file_names, read_file)
+        logger.info("STARTING TYPESHED BINARY FILES CHECKSUM COMPUTATION")
+        logger.info(f"Previous binaries checksum {previous_binaries_checksum}")
+        logger.info(f"Current binaries checksum {current_binaries_checksum}")
+        logger.info(f"Checksum is computed over {len(binary_file_names)} files")
         if previous_binaries_checksum != current_binaries_checksum:
             raise RuntimeError('INCONSISTENT BINARY CHECKSUMS')
         logger.info("SKIPPING TYPESHED SERIALIZATION")
