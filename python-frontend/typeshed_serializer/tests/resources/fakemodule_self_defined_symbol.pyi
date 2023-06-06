8db1cf56c909e3ca52b11894b9d6f905adc52347
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2023-06-06 15:47:56 +0200

    SONARPY-1379 Add basic support for TypeVar

--- python-frontend/typeshed_serializer/tests/resources/fakemodule_self_defined_symbol.pyi
+++
@@ -1,6 +0,0 @@
-from typing_extensions import TypeAlias
-
-_ClassInfo: TypeAlias = type | tuple[_ClassInfo, ...]
-
-class TestCase:
-    def assertIsInstance(self, obj: object, cls: _ClassInfo) -> None: ...
