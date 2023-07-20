674871c16fa16f75bb3e5f3f26ed14b30804a5ee
Anton Haubner <anton.haubner@sonarsource.com>
2023-07-20 09:59:28 +0200

    SONARPY-1412 Rule S6661: Assignments of lambdas to variables should be replaced by function definitions. (#1510)
    * SONARPY-1412 Rule S6661: Implementation and tests of rule
    
    Rule HTML/json still needs to be generated / added.
    
    * SONARPY-1412 Rule S6661: Remove rspec TODO
    
    Annotated assignments are just a special case of normal assignments
    so this does not really warrant adjusting the rspec
    
    * SONARPY-1412 Rule S6661: Update rspec HTML/JSON
    
    HTML/Json rspec files have been generated using the rule api
    
    * SONARPY-1412 Rule S6661: Add S6661 to Sonar way
    
    As specified in the rspec
    
    * SONARPY-1412 Rule S6661: Fix minor review comments

---
+++ python-checks/src/main/java/org/sonar/python/checks/LambdaAssignmentCheck.java
@@ -0,0 +1,57 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.tree.AnnotatedAssignment;
+import org.sonar.plugins.python.api.tree.AssignmentStatement;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.LambdaExpression;
+import org.sonar.plugins.python.api.tree.Tree;
+
+@Rule(key = "S6661")
+public class LambdaAssignmentCheck extends PythonSubscriptionCheck {
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.ASSIGNMENT_STMT, ctx -> checkAssignmentStatement(ctx, (AssignmentStatement) ctx.syntaxNode()));
+    context.registerSyntaxNodeConsumer(Tree.Kind.ANNOTATED_ASSIGNMENT, ctx -> checkAnnotatedAssignment(ctx, (AnnotatedAssignment) ctx.syntaxNode()));
+  }
+
+  private static void checkAssignmentStatement(SubscriptionContext ctx, AssignmentStatement stmt) {
+    final Expression right = stmt.assignedValue();
+    addIssueIfLambda(ctx, right);
+  }
+
+  private static void checkAnnotatedAssignment(SubscriptionContext ctx, AnnotatedAssignment assignment) {
+    final Expression right = assignment.assignedValue();
+    if (right != null) {
+      addIssueIfLambda(ctx, right);
+    }
+  }
+
+  private static void addIssueIfLambda(SubscriptionContext ctx, Expression assignedExpression) {
+    if (assignedExpression.is(Tree.Kind.LAMBDA)) {
+      final LambdaExpression lambdaExpression = (LambdaExpression) assignedExpression;
+      ctx.addIssue(lambdaExpression.lambdaKeyword(), "Define function instead of this lambda assignment statement.");
+    }
+  }
+}
