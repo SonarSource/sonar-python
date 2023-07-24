f19cbf6a2200d633162451713dccd99aaec5652f
Andrea Guarino <andrea.guarino@sonarsource.com>
2023-07-24 17:58:28 +0200

    SONARPY-1413 Rule S6662: Set members and dictionary keys should be hashable

---
+++ python-checks/src/test/resources/checks/hashMethod.py
@@ -0,0 +1,39 @@
+class EmptyClass:
+    ...
+
+class ClassWithEq:
+    def __eq__(self, other):
+        return self
+
+class ClassWithHash:
+    def __hash__(self):
+        return 42
+
+class ClassWithEqInherithingHash(ClassWithHash):
+    def __eq__(self, other):
+        return self
+
+
+def set_members():
+    {EmptyClass()}  # OK - object has by default a __hash__() method
+    {ClassWithEq()} # FN
+    {ClassWithEqInherithingHash()} # FN
+    my_list = [1, 2, 3]
+    {my_list} # Noncompliant {{Make sure this expression is hashable.}}
+#    ^^^^^^^
+    {1, 2, 3}
+    {ClassWithHash()}
+    {some_func()}
+    {None}
+
+def dictionary_keys():
+    {EmptyClass(): 42} # OK
+    my_list = [1, 2, 3]
+    {my_list: 42} # Noncompliant
+    {1: "one", 2: "two"}
+    {ClassWithHash(): 42}
+    {None: 42}
+
+
+def some_func():
+    return "foo"
