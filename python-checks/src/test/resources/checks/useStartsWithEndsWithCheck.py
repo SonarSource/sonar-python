c8a3940a85ed456643283487458bdf5eff3f8327
Anton Haubner <anton.haubner@sonarsource.com>
2023-07-26 16:22:47 +0200

    SONARPY-1410 Rule S6659: Rule implementation

--- python-checks/src/test/resources/checks/useStartsWithEndsWithCheck.py
+++ python-checks/src/test/resources/checks/useStartsWithEndsWithCheck.py
@@ -1,4 +1,3 @@
-foobar = "foobar"
 
 def unknownA():
     ...
@@ -6,15 +5,17 @@ def unknownA():
 def unknownB():
     ...
 
-def returns_string():
-    return "foobar"
+def returns_string() -> str:
+    ...
 
 def f():
-  foobar[:3] == 'foo' # Noncompliant {{Use `startsWith` here.}}
+  foobar = "foobar"
+
+  foobar[:3] == 'foo' # Noncompliant {{Use `startswith` here.}}
 # ^^^^^^^^^^^^^^^^^^^
   'foo' == foobar[:3] # Noncompliant
 
-  foobar[3:] == 'bar' # Noncompliant {{Use `endsWith` here.}}
+  foobar[3:] == 'bar' # Noncompliant {{Use `endswith` here.}}
 # ^^^^^^^^^^^^^^^^^^^
   'bar' == foobar[3:] # Noncompliant
 
@@ -22,12 +23,12 @@ def f():
 #    ^^^^^^^^^^^^^^^^^^^
     pass
 
-  foobar[:3] != 'foo' # Noncompliant {{Use `not` and `startsWith` here.}}
+  foobar[:3] != 'foo' # Noncompliant {{Use `not` and `startswith` here.}}
   'foo' != foobar[:3] # Noncompliant
-  foobar[3:] != 'bar' # Noncompliant {{Use `not` and `endsWith` here.}}
+  foobar[3:] != 'bar' # Noncompliant {{Use `not` and `endswith` here.}}
   'bar' != foobar[3:] # Noncompliant
 
-  unknownA()[3:] == unknownB() # Compliant but potential FN: The rule should only apply if we are sure that strings are compared
+  unknownA()[3:] == unknownB() # Compliant but potential FN: The rule should only apply if we are sure that strings are being compared
   unknownA()[3:] != unknownB() # Compliant
   unknownA()[3:] == 'bar' # Noncompliant
   unknownA()[:3] == 'foo' # Noncompliant
@@ -57,31 +58,39 @@ def f():
   foobar[10:] == 'bar' # Noncompliant
 
   foobar[3:6:1] == 'bar' # Compliant: Too much potential for FPs with step sizes != 1, too early stop indices, etc.
-  foobar[-3:] == 'bar' # Noncompliant {{Use `endsWith` here.}}
-  foobar[:-3] == 'foo' # Noncompliant {{Use `startsWith` here.}}
+  foobar[-3:] == 'bar' # Noncompliant {{Use `endswith` here.}}
+  foobar[:-3] == 'foo' # Noncompliant {{Use `startswith` here.}}
 
-  # If an index is not a plain number, we can not say for sure if it is negative/positive. Hence, we should suggest startsWith and endsWith
-  foobar[:unknownA()] == 'foo' # Noncompliant {{Use `startsWith` or `endsWith` here}}
-  foobar[unknownB():] == 'bar' # Noncompliant {{Use `startsWith` or `endsWith` here}}
-  foobar[:-unknownA()] == 'foo' # Noncompliant {{Use `startsWith` or `endsWith` here.}}
-  foobar[-unknownA():] == 'bar' # Noncompliant {{Use `endsWith` or `endsWith` here.}}
-  foobar[:unknownA()] != 'foo' # Noncompliant {{Use `not` and `startsWith` or `endsWith` here}}
+  # If an index is not a plain number, we can not say for sure if it is negative/positive. Hence, we should suggest startswith and endswith
+  foobar[:unknownA()] == 'foo' # Noncompliant {{Use `startswith` here.}}
+  foobar[unknownB():] == 'bar' # Noncompliant {{Use `endswith` here.}}
+  foobar[:-unknownA()] == 'foo' # Noncompliant {{Use `startswith` here.}}
+  foobar[-unknownA():] == 'bar' # Noncompliant {{Use `endswith` here.}}
+  foobar[:unknownA()] != 'foo' # Noncompliant {{Use `not` and `startswith` here.}}
 
-  foobar[3,] == 'foo' # Compliant
-  foobar["Hello World"] == 'foo' # Compliant
+  foobar[3,] == 'foo' # Compliant: We do not deal with invalid indices. These are handled by S6663
+  foobar["Hello World"] == 'foo' # Same
+  foobar[:3,3:] == 'foo' # Same
   s = slice(3)
-  foobar[s] == 'foo' # Noncompliant
+  foobar[s] == 'foo' # Compliant but FN: We do not analyze slice objects
+  foobar[slice(3)] == 'foo' # Same
 
   class ImplementsIndex:
     def __index__(self):
         return 3
 
   foobar[ImplementsIndex():] == 'bar' # Noncompliant
+  foobar[3] == 'foo' # OK: Simple integer indexing, not a slice expression
 
-  foobar[None:3] == 'foo' # Noncompliant {{Use `startsWith` here.}}
-  foobar[None:3:] == 'foo' # Noncompliant {{Use `startsWith` here.}}
-  foobar[None:3:None] == 'foo' # Noncompliant {{Use `startsWith` here.}}
-  foobar[:3:] == 'foo' # Noncompliant {{Use `startsWith` here.}}
-  foobar[3:None] == 'bar' # Noncompliant {{Use `endsWith` here.}}
-  foobar[3:None:] == 'bar' # Noncompliant {{Use `endsWith` here.}}
-  foobar[3:None:None] == 'bar' # Noncompliant {{Use `endsWith` here.}}
+  foobar[None:3] == 'foo' # Noncompliant {{Use `startswith` here.}}
+  foobar[None:3:] == 'foo' # Noncompliant {{Use `startswith` here.}}
+  foobar[None:3:None] == 'foo' # Noncompliant {{Use `startswith` here.}}
+  foobar[None:3:1] == 'foo' # Noncompliant {{Use `startswith` here.}}
+  foobar[:3:] == 'foo' # Noncompliant {{Use `startswith` here.}}
+  foobar[3:None] == 'bar' # Noncompliant {{Use `endswith` here.}}
+  foobar[3:None:] == 'bar' # Noncompliant {{Use `endswith` here.}}
+  foobar[3:None:None] == 'bar' # Noncompliant {{Use `endswith` here.}}
+  foobar[None:None:None] == 'bar' # OK: This slice returns the full string, hence comparison by == is probably appropriate. Also, something like this will probably not appear in real code
+
+  foobar[:3:2] == 'fo' # Compliant: This is not a simple step 1 slice, not a prefix, or suffix, so the rule does not apply
+  foobar[:3:-2] == 'r' # Same
