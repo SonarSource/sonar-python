471bfee0469b1ca0a10542f8fc2afcaf630937c7
Anton Haubner <anton.haubner@sonarsource.com>
2023-07-28 21:11:31 +0200

    SONARPY-1404 Rule S5642: Implement rule

--- python-checks/src/test/resources/checks/membershipTestSupportCheck.py
+++ python-checks/src/test/resources/checks/membershipTestSupportCheck.py
@@ -16,39 +16,14 @@ def produces_empty() -> Empty:
     ...
 
 42 in 1337 # Noncompliant {{Change the type of 1337; type int does not support membership protocol.}}
-#  ^^ ^^^^< {{This result of this expression does not support the membership protocol.}}
+#  ^^ ^^^^< {{The result value of this expression does not support the membership protocol.}}
 
 42 not in 1337 # Noncompliant
-#  ^^^^^^ ^^^^< {{This result of this expression does not support the membership protocol.}}
+#  ^^^^^^ ^^^^< {{The result value of this expression does not support the membership protocol.}}
 
 if 42 in 1337: # Noncompliant
 #     ^^ ^^^^<
     pass
-
-def f():
-    x = 1337
-    42 in x # Noncompliant
-    y = 42
-    y in 1337 # Noncompliant
-    x in y # Noncompliant
-
-    x in unknown() # Compliant
-    unknown() in x # Noncompliant
-    unknown() in unknown() # Compliant
-
-x in [] # Compliant
-x in () # Compliant
-x in {} # Compliant
-x in {v for v in "abc"} # Compliant
-x in frozenset()
-x in dict() # Compliant
-x in {k: 42 for k in "abc"} # Compliant
-x in produces_container() # Compliant
-x in produces_iterable() # Compliant
-x in produces_sequence() # Compliant
-x in produces_empty() # Noncompliant
-"a" in "abc" # Compliant
-b'a' in b'abc' # Compliant
 
 def g():
     list_or_int = []
@@ -63,64 +38,121 @@ def g():
     # None of the types in this union type support the membership protocol
     unknown() in bool_or_int # Noncompliant
 
-x in Empty() # Noncompliant
+def f():
+    x = 1337
+    42 in x # Noncompliant
+    y = 42
+    y in 1337 # Noncompliant
+    x in y # Noncompliant
 
-class HasContains:
-    def __contains__(self, item):
-        ...
+    x in unknown() # Compliant
+    unknown() in x # Noncompliant
+    unknown() in unknown() # Compliant
 
-x in HasContains() # Compliant
+    x in [] # Compliant
+    x in () # Compliant
+    x in {} # Compliant
+    x in {v for v in "abc"} # Compliant
+    x in frozenset()
+    x in dict() # Compliant
+    x in {k: 42 for k in "abc"} # Compliant
+    x in produces_container() # Compliant
+    x in produces_iterable() # Compliant
+    x in produces_sequence() # Compliant
+    # TODO: Be more strict regarding declared types?
+    x in produces_empty() # Compliant since type analysis makes no assumptions about the members of declared types
+    "a" in "abc" # Compliant
+    b'a' in b'abc' # Compliant
 
-class HasIter:
-    def __iter__(self):
-        ...
+    x in Empty() # Noncompliant
 
-x in HasIter() # Compliant
+    class HasContains:
+        def __contains__(self, item):
+            ...
+
+    x in HasContains() # Compliant
+
+    class HasIter:
+        def __iter__(self):
+            ...
+
+    x in HasIter() # Compliant
 
 
-class HasGetItem:
-    def __getitem__(self, item):
-        ...
+    class HasGetItem:
+        def __getitem__(self, item):
+            ...
 
-x in HasGetItem() # Compliant
+    x in HasGetItem() # Compliant
 
-class InheritedContains(HasContains):
-    pass
+    class InheritedContains(HasContains):
+        pass
 
-x in InheritedContains() # Compliant
+    x in InheritedContains() # Compliant
 
-class InheritedEmpty(Empty):
-    pass
+    class InheritedEmpty(Empty):
+        pass
 
-x in InheritedEmpty() # Noncompliant
+    x in InheritedEmpty() # Noncompliant
 
-class Mix011(HasContains, HasIter):
-    pass
+    class Mix011(HasContains, HasIter):
+        pass
 
-x in Mix011() # Compliant
+    x in Mix011() # Compliant
 
-class Mix101(HasContains, HasGetItem):
-    pass
+    class Mix101(HasContains, HasGetItem):
+        pass
 
-x in Mix101() # Compliant
+    x in Mix101() # Compliant
 
-class Mix110(HasIter, HasGetItem):
-    pass
+    class Mix110(HasIter, HasGetItem):
+        pass
 
-x in Mix110() # Compliant
+    x in Mix110() # Compliant
 
-class Mix111(HasContains, HasIter, HasGetItem):
-    pass
+    class Mix111(HasContains, HasIter, HasGetItem):
+        pass
 
-x in Mix111() # Compliant
+    x in Mix111() # Compliant
 
-class InheritedSet(set):
-    pass
+    class InheritedSet(set):
+        pass
 
-x in InheritedSet("abc") # Compliant
+    x in InheritedSet("abc") # Compliant
 
-class DisabledContains(HasContains):
-    __contains__ = None
+    class DisabledContains(HasContains):
+        __contains__ = None
 
-x in DisabledContains() # Noncompliant
+    x in DisabledContains() # Noncompliant
 
+    class DisabledContainsWithIter:
+        __contains__ = None
+        def __iter__(self):
+            ...
+
+    x in DisabledContainsWithIter() # Noncompliant
+
+    class ContainsWithDisabledIter:
+        def __contains__(self, item):
+            ...
+
+        __iter__ = None
+
+    x in ContainsWithDisabledIter() # Compliant
+
+    class DisabledIterWithGetItem:
+        __iter__ = None
+
+        def __getitem__(self, item):
+            ...
+
+
+    x in DisabledIterWithGetItem() # Noncompliant
+
+    class IterWithDisabledGetItem:
+        def __iter__(self):
+            ...
+
+        __getitem__ = None
+
+    x in IterWithDisabledGetItem() # Compliant
