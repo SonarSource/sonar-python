ccbab7b1ae1e3bb970d96a48f6aea54db49bb4d1
Anton Haubner <anton.haubner@sonarsource.com>
2023-08-01 16:53:15 +0200

    SONARPY-1404 Rule S5642: "in" and "not in" operators should be used on objects supporting them (#1522)

---
+++ python-checks/src/main/java/org/sonar/python/checks/MembershipTestSupportCheck.java
@@ -0,0 +1,169 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.AmbiguousSymbol;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.symbols.Usage;
+import org.sonar.plugins.python.api.tree.AssignmentStatement;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.InExpression;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.plugins.python.api.types.InferredType;
+import org.sonar.python.tree.TreeUtils;
+import org.sonar.python.types.InferredTypes;
+
+@Rule(key = "S5642")
+public class MembershipTestSupportCheck extends PythonSubscriptionCheck {
+  private static final String PRIMARY_MESSAGE = "Change the type of %s";
+  private static final String PRIMARY_MESSAGE_MULTILINE = "Change the type for the target expression of `in`";
+  private static final String KNOWN_TYPE_MESSAGE = "; type %s does not support membership protocol.";
+  private static final String UNKNOWN_TYPE_MESSAGE = "; the type does not support the membership protocol.";
+  private static final String SECONDARY_MESSAGE = "The result value of this expression does not support the membership protocol.";
+
+  // The ordering of membership protocol methods matters here (for extreme edge cases).
+  //
+  // For instance, a class that has __contains__ set to None but which defines __iter__ does not fulfill the membership protocol
+  // A class that defines __contains__ and has __iter__ set to None does fulfill the membership protocol
+  //
+  // Hence, we check for these special methods in the same order as the python interpreter does at runtime.
+  private static final List<String> MEMBERSHIP_PROTOCOL_ENABLING_METHODS = List.of("__contains__", "__iter__", "__getitem__");
+
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.IN, ctx -> checkInExpression(ctx, (InExpression) ctx.syntaxNode()));
+  }
+
+  private static void checkInExpression(SubscriptionContext ctx, InExpression inExpression) {
+    Expression rhs = inExpression.rightOperand();
+    InferredType rhsType = rhs.type();
+
+    if (canSupportMembershipProtocol(rhsType)) {
+      return;
+    }
+
+    addIssueOnInAndNotIn(ctx, inExpression, genPrimaryMessage(rhs, rhsType))
+      .secondary(inExpression.rightOperand(), SECONDARY_MESSAGE);
+  }
+
+  private static String genPrimaryMessage(Expression rhs, InferredType rhsType) {
+    // Try to render rhs into the message, but only if it is not multiline to avoid generating messages that are too long.
+    String inTarget = TreeUtils.treeToString(rhs, false);
+    String message;
+    if (inTarget == null) {
+      message = PRIMARY_MESSAGE_MULTILINE;
+    } else {
+      message = String.format(PRIMARY_MESSAGE, inTarget);
+    }
+
+    var typeName = InferredTypes.typeName(rhsType);
+    if (typeName != null) {
+      message += String.format(KNOWN_TYPE_MESSAGE, typeName);
+    } else {
+      message += UNKNOWN_TYPE_MESSAGE;
+    }
+
+    return message;
+  }
+
+  private static PreciseIssue addIssueOnInAndNotIn(SubscriptionContext ctx, InExpression inExpression, String message) {
+    var notToken = inExpression.notToken();
+    if (notToken == null) {
+      return ctx.addIssue(inExpression.operator(), message);
+    }
+
+    return ctx.addIssue(notToken, inExpression.operator(), message);
+  }
+
+  private static boolean canSupportMembershipProtocol(InferredType type) {
+    for (var methodName : MEMBERSHIP_PROTOCOL_ENABLING_METHODS) {
+      switch (canMemberBeMethod(type, methodName)) {
+        case NOT_A_METHOD:
+          return false;
+        case METHOD:
+        case UNKNOWN:
+          return true;
+        default:
+      }
+    }
+
+    // all membership protocol methods are guaranteed not to be present
+    return false;
+  }
+
+  private enum MemberType {
+    METHOD,
+    NOT_A_METHOD,
+    NOT_PRESENT,
+    UNKNOWN
+  }
+
+  private static boolean canBeMethodSymbol(Symbol symbol) {
+    if (symbol.is(Symbol.Kind.FUNCTION)) {
+      return true;
+    }
+
+    // To avoid FPs, we accept OTHER unless we can show that it is a non-callable.
+    //
+    // This handles cases like __contains__ = other() or __contains__ = None.
+    // Although it is unclear, whether such edge cases really appear in the wild
+    if (symbol.is(Symbol.Kind.OTHER)) {
+      var bindingUsages = symbol.usages().stream().filter(Usage::isBindingUsage).limit(2).collect(Collectors.toUnmodifiableList());
+      if (bindingUsages.size() == 1) {
+        var bindingUsage = bindingUsages.get(0);
+        var assignment = TreeUtils.firstAncestorOfKind(bindingUsage.tree(), Tree.Kind.ASSIGNMENT_STMT);
+
+        return assignment == null || ((AssignmentStatement) assignment).assignedValue().type().canHaveMember("__call__");
+      }
+
+      return true;
+    }
+
+    if (symbol.is(Symbol.Kind.AMBIGUOUS)) {
+      return ((AmbiguousSymbol) symbol).alternatives().stream().anyMatch(MembershipTestSupportCheck::canBeMethodSymbol);
+    }
+
+    return false;
+  }
+
+  private static MemberType canMemberBeMethod(InferredType type, String methodName) {
+    var maybeMember = type.resolveMember(methodName);
+    if (maybeMember.isPresent()) {
+      var symbol = maybeMember.get();
+
+      if (canBeMethodSymbol(symbol)) {
+        return MemberType.METHOD;
+      }
+
+      return MemberType.NOT_A_METHOD;
+    }
+
+    if (!type.canHaveMember(methodName)) {
+      return MemberType.NOT_PRESENT;
+    }
+
+    return MemberType.UNKNOWN;
+  }
+}
---
+++ python-checks/src/test/resources/checks/membershipTestSupportCheck.py
@@ -0,0 +1,225 @@
+from some_module import unknown
+from typing import Sequence, Iterable, Container, Callable, overload
+
+def produces_sequence() -> Sequence:
+    ...
+
+def produces_iterable() -> Iterable:
+    ...
+
+def produces_container() -> Container:
+    ...
+
+class Empty:
+    pass
+def produces_empty() -> Empty:
+    ...
+
+42 in 1337 # Noncompliant {{Change the type of 1337; type int does not support membership protocol.}}
+#  ^^ ^^^^< {{The result value of this expression does not support the membership protocol.}}
+
+42 not in 1337 # Noncompliant
+#  ^^^^^^ ^^^^< {{The result value of this expression does not support the membership protocol.}}
+
+if 42 in 1337: # Noncompliant
+#     ^^ ^^^^<
+    pass
+
+42 in int(this_is() + a_long() + # Noncompliant {{Change the type for the target expression of `in`; type int does not support membership protocol.}}
+#  ^^ ^[el=+2;ec=29]<
+      multiline_expression())
+
+def g():
+    list_or_int = []
+    if unknown():
+        list_or_int = 42
+
+    unknown() in list_or_int # Compliant: At least one of the members of this union type supports the membership protocol
+
+    bool_or_int = True
+    if unknown():
+        bool_or_int = 42
+    # None of the types in this union type support the membership protocol
+    unknown() in bool_or_int # Noncompliant
+
+def f():
+    x = 1337
+    42 in x # Noncompliant
+    y = 42
+    y in 1337 # Noncompliant
+    x in y # Noncompliant
+
+    x in unknown() # Compliant
+    unknown() in x # Noncompliant
+    unknown() in unknown() # Compliant
+
+    x in [] # Compliant
+    x in () # Compliant
+    x in {} # Compliant
+    x in {v for v in "abc"} # Compliant
+    x in frozenset()
+    x in dict() # Compliant
+    x in {k: 42 for k in "abc"} # Compliant
+    x in produces_container() # Compliant
+    x in produces_iterable() # Compliant
+    x in produces_sequence() # Compliant
+    # TODO: Consider adding this rule to ConfusingTypeChecking, see the example below
+    x in produces_empty() # Compliant since type analysis makes no assumptions about the members of declared types
+    "a" in "abc" # Compliant
+    b'a' in b'abc' # Compliant
+
+    x in Empty() # Noncompliant
+
+    class HasContains:
+        def __contains__(self, item):
+            ...
+
+    x in HasContains() # Compliant
+
+    class HasIter:
+        def __iter__(self):
+            ...
+
+    x in HasIter() # Compliant
+
+
+    class HasGetItem:
+        def __getitem__(self, item):
+            ...
+
+    x in HasGetItem() # Compliant
+
+    class InheritedContains(HasContains):
+        pass
+
+    x in InheritedContains() # Compliant
+
+    class InheritedEmpty(Empty):
+        pass
+
+    x in InheritedEmpty() # Noncompliant
+
+    class Mix011(HasContains, HasIter):
+        pass
+
+    x in Mix011() # Compliant
+
+    class Mix101(HasContains, HasGetItem):
+        pass
+
+    x in Mix101() # Compliant
+
+    class Mix110(HasIter, HasGetItem):
+        pass
+
+    x in Mix110() # Compliant
+
+    class Mix111(HasContains, HasIter, HasGetItem):
+        pass
+
+    x in Mix111() # Compliant
+
+    class InheritedSet(set):
+        pass
+
+    x in InheritedSet("abc") # Compliant
+
+    class DisabledContains(HasContains):
+        __contains__ = None
+
+    x in DisabledContains() # Noncompliant
+
+    class DisabledContainsWithIter:
+        __contains__ = None
+        def __iter__(self):
+            ...
+
+    x in DisabledContainsWithIter() # Noncompliant
+
+    class ContainsWithDisabledIter:
+        def __contains__(self, item):
+            ...
+
+        __iter__ = None
+
+    x in ContainsWithDisabledIter() # Compliant
+
+    class DisabledIterWithGetItem:
+        __iter__ = None
+
+        def __getitem__(self, item):
+            ...
+
+
+    x in DisabledIterWithGetItem() # Noncompliant
+
+    class IterWithDisabledGetItem:
+        def __iter__(self):
+            ...
+
+        __getitem__ = None
+
+    x in IterWithDisabledGetItem() # Compliant
+
+    class AmbiguousContains01:
+        __contains__ = unknown()
+
+    x in AmbiguousContains01() # Compliant
+
+    class AmbiguousContains02:
+        __contains__ = unknown()
+        __iter__ = None
+
+    x in AmbiguousContains02() # Compliant
+
+    class AmbiguousIter:
+        __iter__ = unknown()
+
+    x in AmbiguousIter() # Compliant
+
+    class AmbiguousIterAndDisabledContains:
+        __contains__ = None
+        __iter__ = unknown()
+
+    x in AmbiguousIterAndDisabledContains() # Noncompliant
+
+    class AmbiguousContainsAndIter:
+        __contains__ = unknown()
+        def __iter__(self):
+            ...
+
+    x in AmbiguousContainsAndIter() # Compliant
+
+    class ComplexContains01:
+        __contains__ = None
+        __contains__ = unknown()
+
+    x in ComplexContains01() # Compliant
+
+    class ComplexContains02:
+        __contains__: Callable
+
+    x in ComplexContains02() # Compliant
+
+    class ComplexContains03(ComplexContains01, ComplexContains02):
+        pass
+
+    x in ComplexContains03() # Compliant
+
+    class ClassSymbolContains:
+        class __contains__: # Ridiculous case (and so are the others here), but I add it for coverage
+            pass
+
+    x in ClassSymbolContains() # Noncompliant
+
+    class OverloadedContains:
+        @overload
+        def __contains__(self, item: int) -> bool:
+            ...
+        @overload
+        def __contains__(self, item: float) -> bool:
+            ...
+        def __contains__(self, item):
+            ...
+
+    x in OverloadedContains() # Compliant
