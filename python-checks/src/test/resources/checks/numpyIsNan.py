a57bd37dc663f12b1124f3fdd60ed44da1efc993
jdodinh <jeremi.dodinh@sonarsource.com>
2023-09-08 17:24:25 +0200

    Fix code coverage issue.

--- python-checks/src/test/resources/checks/numpyIsNan.py
+++ python-checks/src/test/resources/checks/numpyIsNan.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+
 def foo(x):
-    if x == np.nan: print(1) # Noncompliant
+    if x == np.nan: print(1)  # Noncompliant
     #  ^^^^^^^^^^^
-    if np.nan == x: print(1) # Noncompliant
-    if x != np.nan: print(1) # Noncompliant
-    if np.nan != x: print(1) # Noncompliant
+    if np.nan == x: print(1)  # Noncompliant
+    if x != np.nan: print(1)  # Noncompliant
+    if np.nan != x: print(1)  # Noncompliant
+    if zz.nan != x: print(1)
     if np.isnan(x): print(1)
     if np.nan < x: print(1)  # Compliant
-
