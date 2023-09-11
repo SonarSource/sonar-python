fea5a55d3d20882aab3826b2faba8d712c25b304
David Kunzmann <david.kunzmann@sonarsource.com>
2023-09-11 14:34:59 +0200

    Fix after review
    Better coverage
    
    Fix coverage
    
    Fix coverage
    
    Fix naming
    
    Fix naming
    
    Fix naming
    
    Fix naming

--- python-checks/src/main/java/org/sonar/python/checks/IsCloseAbsTolCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/IsCloseAbsTolCheck.java
@@ -21,21 +21,28 @@ package org.sonar.python.checks;
 
 import java.util.Optional;
 
+import javax.annotation.Nullable;
+
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.quickfix.PythonQuickFix;
+import org.sonar.plugins.python.api.symbols.Symbol;
 import org.sonar.plugins.python.api.tree.CallExpression;
 import org.sonar.plugins.python.api.tree.Expression;
 import org.sonar.plugins.python.api.tree.Name;
 import org.sonar.plugins.python.api.tree.NumericLiteral;
 import org.sonar.plugins.python.api.tree.RegularArgument;
 import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.quickfix.TextEditUtils;
 import org.sonar.python.tree.TreeUtils;
 
 @Rule(key = "S6727")
 public class IsCloseAbsTolCheck extends PythonSubscriptionCheck {
 
-  private static final String MESSAGE = "Provide the abs_tol parameter when using math.isclose to compare a value to 0";
+  private static final String MESSAGE = "Provide the abs_tol parameter when using math.isclose to compare a value to 0.";
+  private static final String SECOND_LOCATION = "This argument evaluates to zero.";
+  private static final String QUICK_FIX_MESSAGE = "Add the abs_tol parameter";
 
   @Override
   public void initialize(Context context) {
@@ -44,21 +51,35 @@ public class IsCloseAbsTolCheck extends PythonSubscriptionCheck {
   }
 
   private static void checkForIsCloseAbsTolArgument(SubscriptionContext ctx, CallExpression call) {
-    Optional.ofNullable(call.calleeSymbol())
-        .filter(symbol -> "math.isclose".equals(symbol.fullyQualifiedName()) &&
-            anyArgumentIsZero(call) &&
-            TreeUtils.argumentByKeyword("abs_tol", call.arguments()) == null)
-        .ifPresent(s -> ctx.addIssue(call, MESSAGE));
+    Symbol symbol = call.calleeSymbol();
+    if (symbol != null && "math.isclose".equals(symbol.fullyQualifiedName())
+        && TreeUtils.argumentByKeyword("abs_tol", call.arguments()) == null) {
+      RegularArgument firstArg = TreeUtils.nthArgumentOrKeyword(0, "a", call.arguments());
+      RegularArgument secondArg = TreeUtils.nthArgumentOrKeyword(1, "b", call.arguments());
+      checkArgumentExistsAndIsZero(firstArg)
+          .ifPresentOrElse(
+              argA -> addIssueAndQuickFix(argA, ctx, call),
+              () -> checkArgumentExistsAndIsZero(secondArg).ifPresent(argB -> addIssueAndQuickFix(argB, ctx, call)));
+    }
   }
 
-  private static boolean anyArgumentIsZero(CallExpression call) {
-    RegularArgument firstArg = TreeUtils.nthArgumentOrKeyword(0, "a", call.arguments());
-    RegularArgument secondArg = TreeUtils.nthArgumentOrKeyword(1, "b", call.arguments());
-    return (firstArg != null && isLitteralZeroOrAssignedZero(firstArg.expression())) ||
-        (secondArg != null && isLitteralZeroOrAssignedZero(secondArg.expression()));
+  private static Optional<RegularArgument> checkArgumentExistsAndIsZero(@Nullable RegularArgument argument) {
+    return Optional
+        .ofNullable(argument)
+        .filter(arg -> isLiteralZeroOrAssignedZero(arg.expression()));
   }
 
-  private static boolean isLitteralZeroOrAssignedZero(Expression expression) {
+  private static void addIssueAndQuickFix(RegularArgument arg, SubscriptionContext ctx, CallExpression call) {
+    PreciseIssue issue = ctx.addIssue(call.callee(), MESSAGE);
+    issue.secondary(arg, SECOND_LOCATION);
+
+    PythonQuickFix quickFix = PythonQuickFix.newQuickFix(QUICK_FIX_MESSAGE)
+        .addTextEdit(TextEditUtils.insertBefore(call.rightPar(), ", abs_tol=1e-9"))
+        .build();
+    issue.addQuickFix(quickFix);
+  }
+
+  private static boolean isLiteralZeroOrAssignedZero(Expression expression) {
     return isZero(expression) || isAssignedZero(expression);
   }
 
