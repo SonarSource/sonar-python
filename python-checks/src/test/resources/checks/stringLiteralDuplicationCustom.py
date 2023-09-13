dd41c3bb09bf6520628ba1cb5f6306ce55cc7887
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2023-09-13 15:24:53 +0200

    SONARPY-1464 S1192: Allow users to define custom exclusion patterns

---
+++ python-checks/src/test/resources/checks/stringLiteralDuplicationCustom.py
@@ -0,0 +1,11 @@
+print("aaaaaa aaaaaaa") # OK
+print("aaaaaa aaaaaaa")
+print("aaaaaa aaaaaaa")
+
+print("bbbbbb bbbbbbb") # Noncompliant
+print("bbbbbb bbbbbbb")
+print("bbbbbb bbbbbbb")
+
+print("aaaaaa aaaaaaa") # OK
+print("aaaaaa aaaaaaa")
+print("aaaaaa aaaaaaa")
