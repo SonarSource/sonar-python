3ad039abbc2334778c773c40fb24ac23541dc6a7
jdodinh <jeremi.dodinh@sonarsource.com>
2023-09-20 14:07:05 +0200

    SONARPY-1467 Rule S6729: Add quick fix to turn np.where into np.nonzero.

--- python-checks/src/test/java/org/sonar/python/checks/NumpyWhereOneConditionCheckTest.java
+++ python-checks/src/test/java/org/sonar/python/checks/NumpyWhereOneConditionCheckTest.java
@@ -20,13 +20,48 @@
 package org.sonar.python.checks;
 
 import org.junit.jupiter.api.Test;
+import org.sonar.python.checks.quickfix.PythonQuickFixVerifier;
 import org.sonar.python.checks.utils.PythonCheckVerifier;
 
 class NumpyWhereOneConditionCheckTest {
 
+  NumpyWhereOneConditionCheck check = new NumpyWhereOneConditionCheck();
+
   @Test
   void test1() {
-    PythonCheckVerifier.verify("src/test/resources/checks/numpyWhereOneCondition.py", new NumpyWhereOneConditionCheck());
+    PythonCheckVerifier.verify("src/test/resources/checks/numpyWhereOneCondition.py", check);
+  }
+
+  @Test
+  void quickfix_test1() {
+
+    final String quickFixMessage = "Replace numpy.where with numpy.nonzero";
+
+    final String nonCompliant = "import numpy as np\n" +
+      "def bigger_than_two():\n" +
+      "  arr = np.array([1,2,3,4])\n" +
+      "  result = np.where(arr > 2)";
+
+    final String compliant = "import numpy as np\n" +
+      "def bigger_than_two():\n" +
+      "  arr = np.array([1,2,3,4])\n" +
+      "  result = np.nonzero(arr > 2)";
+
+    PythonQuickFixVerifier.verify(check, nonCompliant, compliant);
+    PythonQuickFixVerifier.verifyQuickFixMessages(check, nonCompliant, quickFixMessage);
+  }
+
+  @Test
+  void quickfix_test2() {
+
+    // Here we don't offer a quickfix, because the imports of the functions can be complicated to track (nonzero could be unimported)
+
+    final String compliant = "from numpy import array, where\n" +
+      "def bigger_than_two():\n" +
+      "  arr = array([1,2,3,4])\n" +
+      "  result = where(arr > 2)";
+
+    PythonQuickFixVerifier.verifyNoQuickFixes(check, compliant);
   }
 
 }
