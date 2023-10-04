a5d9fcc0fc25598cadd33cfd97e2b8d4e3a972f8
jdodinh <jeremi.dodinh@sonarsource.com>
2023-10-04 12:20:04 +0200

    Address review comments.

--- python-checks/src/test/resources/pandasModifyInPlace.py
+++ python-checks/src/test/resources/pandasModifyInPlace.py
@@ -2,83 +2,112 @@ def non_compliant_1():
     import pandas as pd
 
     df = pd.DataFrame({'A': [3, 2, 1], 'B': ['x', 'y', 'z']})
-    df2 = pd.DataFrame({'A': [6, 5, 4], 'C': ['a', 'b', 'c']})
-
-    new_index = ['Firefox', 'Chrome', 'Safari']
+    df2 = pd.read_csv("some_csv.csv")
 
     _ = df.drop(columns='A', inplace=True)  # Noncompliant
     #                        ^^^^^^^^^^^^
+    _ = df2.drop(columns='A', inplace=True)  # Noncompliant
+    #                         ^^^^^^^^^^^^
 
     _ = df.dropna(inplace=True)  # Noncompliant
     #             ^^^^^^^^^^^^
+    _ = df2.dropna(inplace=True)  # Noncompliant
+    #              ^^^^^^^^^^^^
 
     _ = df.drop_duplicates(inplace=True)  # Noncompliant
     #                      ^^^^^^^^^^^^
+    _ = df2.drop_duplicates(inplace=True)  # Noncompliant
+    #                       ^^^^^^^^^^^^
 
     _ = df.sort_values(by=['A'], inplace=True)  # Noncompliant
     #                            ^^^^^^^^^^^^
+    _ = df2.sort_values(by=['A'], inplace=True)  # Noncompliant
+    #                             ^^^^^^^^^^^^
 
     _ = df.sort_index(inplace=True)  # Noncompliant
     #                 ^^^^^^^^^^^^
+    _ = df2.sort_index(inplace=True)  # Noncompliant
+    #                  ^^^^^^^^^^^^
 
     _ = df.eval("C = str(A) + B", inplace=True)  # Noncompliant
     #                             ^^^^^^^^^^^^
+    _ = df2.eval("C = str(A) + B", inplace=True)  # Noncompliant
+    #                              ^^^^^^^^^^^^
 
     _ = df.query('A > B', inplace=True)  # Noncompliant
     #                     ^^^^^^^^^^^^
+    _ = df2.query('A > B', inplace=True)  # Noncompliant
+    #                      ^^^^^^^^^^^^
 
 
 def non_compliant_2():
-    from pandas import DataFrame
+    from pandas import DataFrame, read_csv
 
     df = DataFrame({'A': [3, 2, 1], 'B': ['x', 'y', 'z']})
-    df2 = DataFrame({'A': [6, 5, 4], 'C': ['a', 'b', 'c']})
-
-    new_index = ['Firefox', 'Chrome', 'Safari']
+    df2 = read_csv("some_csv.csv")
 
     _ = df.drop(columns='A', inplace=True)  # Noncompliant
     #                        ^^^^^^^^^^^^
+    _ = df2.drop(columns='A', inplace=True)  # Noncompliant
+    #                         ^^^^^^^^^^^^
 
     _ = df.dropna(inplace=True)  # Noncompliant
     #             ^^^^^^^^^^^^
+    _ = df2.dropna(inplace=True)  # Noncompliant
+    #              ^^^^^^^^^^^^
 
     _ = df.drop_duplicates(inplace=True)  # Noncompliant
     #                      ^^^^^^^^^^^^
+    _ = df2.drop_duplicates(inplace=True)  # Noncompliant
+    #                       ^^^^^^^^^^^^
 
     _ = df.sort_values(by=['A'], inplace=True)  # Noncompliant
     #                            ^^^^^^^^^^^^
+    _ = df2.sort_values(by=['A'], inplace=True)  # Noncompliant
+    #                             ^^^^^^^^^^^^
 
     _ = df.sort_index(inplace=True)  # Noncompliant
     #                 ^^^^^^^^^^^^
+    _ = df2.sort_index(inplace=True)  # Noncompliant
+    #                  ^^^^^^^^^^^^
 
     _ = df.eval("C = str(A) + B", inplace=True)  # Noncompliant
     #                             ^^^^^^^^^^^^
+    _ = df2.eval("C = str(A) + B", inplace=True)  # Noncompliant
+    #                              ^^^^^^^^^^^^
 
     _ = df.query('A > B', inplace=True)  # Noncompliant
     #                     ^^^^^^^^^^^^
+    _ = df2.query('A > B', inplace=True)  # Noncompliant
+    #                      ^^^^^^^^^^^^
 
 
 def compliant(xx):
     import pandas as pd
     df = pd.DataFrame({'A': [3, 2, 1], 'B': ['x', 'y', 'z']})
 
-    df2 = pd.DataFrame({'A': [6, 5, 4], 'C': ['a', 'b', 'c']})
-
-    new_index = ['Firefox', 'Chrome', 'Safari']
+    df2 = pd.read_csv("some_csv.csv")
 
     _ = df.drop(columns='A')
+    _ = df2.drop(columns='A')
 
     _ = df.dropna(inplace=False)
+    _ = df2.dropna(inplace=False)
 
     _ = df.drop_duplicates()
+    _ = df2.drop_duplicates()
 
     _ = df.sort_values(by=['A'])
+    _ = df2.sort_values(by=['A'])
 
     _ = df.sort_index()
+    _ = df2.sort_index()
 
     _ = df.eval("C = str(A) + B")
+    _ = df2.eval("C = str(A) + B")
 
     _ = df.query('A > B')
+    _ = df2.query('A > B')
 
     _ = xx.drop(columns='A', inplace=True)
 
