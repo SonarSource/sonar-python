2fed74a4c71831dd3fa35ac5074813c1cc1ef9c7
David Kunzmann <david.kunzmann@sonarsource.com>
2023-10-05 09:54:35 +0200

    Added more test for FNs

--- python-checks/src/test/resources/checks/pandasChainInstructionCheck.py
+++ python-checks/src/test/resources/checks/pandasChainInstructionCheck.py
@@ -16,6 +16,13 @@ def non_compliant(df: pd.DataFrame, df2: DataFrame):
 #   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
+#   Here we do not raise an issue only because we do not support subscription with Name
+#   If support is added for such case we would encounter FPs when the subscription with Name is at the beginning of the chain 
+    pd.read_csv("some_csv.csv").filter(like='joe', axis=0).groupby("team")["salary"]["test"].axes[1].unique() # FN
+
+#   Here we should not raise an issue as the chain is done mainly on an Index object which does not have a pipe methof
+    pd.read_csv("some_csv.csv").axes[1].join(pd.Index([4, 5, 6])).repeat([1,2]).drop_duplicates().insert(1, 42)
+
 def compliant(df: pd.DataFrame, my_function, something, df2: DataFrame):
 
     df2.set_index("name").T.filter(like='joe', axis=0)[1].mean()
@@ -31,3 +38,4 @@ def compliant(df: pd.DataFrame, my_function, something, df2: DataFrame):
     DataFrame().set_index("name").pipe(my_function).filter(like='joe', axis=0).groupby("team")["salary"].mean()
 
     something.set_index("name").filter(like='joe', axis=0).groupby("team")["salary"].mean().head()
+
