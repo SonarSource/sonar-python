c469280d829929384ab3d4456c7cd7ffd1f536e1
jdodinh <jeremi.dodinh@sonarsource.com>
2023-10-23 17:47:16 +0200

    Standardise for rule S6740.

--- python-checks/src/test/resources/checks/pandasReadNoDataType.py
+++ python-checks/src/test/resources/checks/pandasReadNoDataType.py
@@ -1,22 +1,22 @@
 def non_compliant_1(xx):
     import pandas as pd
 
-    _ = pd.read_csv("my_file.csv")  # Noncompliant {{Provide the 'dtype' parameter when calling 'pandas.read_csv'.}}
+    _ = pd.read_csv("my_file.csv")  # Noncompliant {{Provide the "dtype" parameter when calling "pandas.read_csv".}}
     #      ^^^^^^^^
 
-    _ = pd.read_table("my_file.csv")  # Noncompliant {{Provide the 'dtype' parameter when calling 'pandas.read_table'.}}
+    _ = pd.read_table("my_file.csv")  # Noncompliant {{Provide the "dtype" parameter when calling "pandas.read_table".}}
     #      ^^^^^^^^^^
 
-    _ = pd.read_csv(xx)  # Noncompliant {{Provide the 'dtype' parameter when calling 'pandas.read_csv'.}}
+    _ = pd.read_csv(xx)  # Noncompliant {{Provide the "dtype" parameter when calling "pandas.read_csv".}}
     #      ^^^^^^^^
 
 
 def non_compliant_2():
     from pandas import read_csv, read_table
 
-    _ = read_csv("my_file.csv")  # Noncompliant {{Provide the 'dtype' parameter when calling 'pandas.read_csv'.}}
+    _ = read_csv("my_file.csv")  # Noncompliant {{Provide the "dtype" parameter when calling "pandas.read_csv".}}
     #   ^^^^^^^^
-    _ = read_table("my_file.csv")  # Noncompliant {{Provide the 'dtype' parameter when calling 'pandas.read_table'.}}
+    _ = read_table("my_file.csv")  # Noncompliant {{Provide the "dtype" parameter when calling "pandas.read_table".}}
     #   ^^^^^^^^^^
 
 
