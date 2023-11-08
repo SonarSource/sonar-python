bded8736ef067654c0c830beaef5a60f20bc2e66
Jeremi Do Dinh <jeremi.dodinh@sonarsource.com>
2023-11-08 13:57:43 +0100

    SONARPY-1468 Rule S6779: Flask secret keys should not be disclosed (#1636)

---
+++ python-checks/src/main/java/org/sonar/python/checks/FlaskHardCodedSecretCheck.java
@@ -0,0 +1,190 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.tree.AssignmentStatement;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.DictionaryLiteral;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.ExpressionList;
+import org.sonar.plugins.python.api.tree.KeyValuePair;
+import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.QualifiedExpression;
+import org.sonar.plugins.python.api.tree.RegularArgument;
+import org.sonar.plugins.python.api.tree.StringLiteral;
+import org.sonar.plugins.python.api.tree.SubscriptionExpression;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.tree.TreeUtils;
+
+
+@Rule(key = "S6779")
+public class FlaskHardCodedSecretCheck extends PythonSubscriptionCheck {
+  private static final String MESSAGE = "Don't disclose \"Flask\" secret keys.";
+  private static final String SECONDARY_MESSAGE = "Assignment to sensitive property.";
+  private static final String SECRET_KEY_KEYWORD = "SECRET_KEY";
+  private static final Set<String> FLASK_APP_CONFIG_QUALIFIER_FQNS = Set.of(
+    "flask.app.Flask.config",
+    "flask.globals.current_app.config"
+  );
+
+  private static final Set<String> FLASK_SECRET_KEY_FQNS = Set.of(
+    "flask.app.Flask.secret_key",
+    "flask.globals.current_app.secret_key"
+  );
+
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.CALL_EXPR, FlaskHardCodedSecretCheck::verifyCallExpression);
+    context.registerSyntaxNodeConsumer(Tree.Kind.ASSIGNMENT_STMT, FlaskHardCodedSecretCheck::verifyAssignmentStatement);
+  }
+
+  private static void verifyCallExpression(SubscriptionContext ctx) {
+    CallExpression callExpression = (CallExpression) ctx.syntaxNode();
+    Optional.of(callExpression)
+      .map(CallExpression::callee)
+      .flatMap(TreeUtils.toOptionalInstanceOfMapper(QualifiedExpression.class))
+      .filter(qualiExpr -> "update".equals(qualiExpr.name().name()))
+      .map(QualifiedExpression::qualifier)
+      .flatMap(TreeUtils.toOptionalInstanceOfMapper(QualifiedExpression.class))
+      .map(QualifiedExpression::name)
+      .map(Name::symbol)
+      .map(Symbol::fullyQualifiedName)
+      .filter(FLASK_APP_CONFIG_QUALIFIER_FQNS::contains)
+      .ifPresent(fqn -> verifyUpdateCallArgument(ctx, callExpression));
+  }
+
+  private static void verifyUpdateCallArgument(SubscriptionContext ctx, CallExpression callExpression) {
+    Optional.of(callExpression.arguments())
+      .filter(arguments -> arguments.size() == 1)
+      .map(arguments -> arguments.get(0))
+      .flatMap(TreeUtils.toOptionalInstanceOfMapper(RegularArgument.class))
+      .map(RegularArgument::expression)
+      .map(FlaskHardCodedSecretCheck::getAssignedValue)
+      .filter(FlaskHardCodedSecretCheck::isIllegalDictArgument)
+      .ifPresent(expr -> ctx.addIssue(callExpression, MESSAGE));
+
+  }
+
+  private static Expression getAssignedValue(Expression expression) {
+    if (expression.is(Tree.Kind.NAME)) {
+      return Expressions.singleAssignedValue((Name) expression);
+    }
+    return expression;
+  }
+
+  private static boolean isIllegalDictArgument(Expression expression) {
+    if (expression.is(Tree.Kind.CALL_EXPR)) {
+      return isCallToDictConstructor((CallExpression) expression) && hasIllegalKeywordArgument((CallExpression) expression);
+    } else if (expression.is(Tree.Kind.DICTIONARY_LITERAL)) {
+      return hasIllegalKeyValuePair((DictionaryLiteral) expression);
+    }
+    return false;
+  }
+
+  private static boolean isCallToDictConstructor(CallExpression callExpression) {
+    return Optional.of(callExpression)
+      .map(CallExpression::callee)
+      .flatMap(TreeUtils.toOptionalInstanceOfMapper(Name.class))
+      .map(Name::symbol)
+      .map(Symbol::fullyQualifiedName)
+      .filter("dict"::equals)
+      .isPresent();
+  }
+
+  private static boolean hasIllegalKeyValuePair(DictionaryLiteral dictionaryLiteral) {
+    return dictionaryLiteral.elements().stream()
+      .filter(KeyValuePair.class::isInstance)
+      .map(KeyValuePair.class::cast)
+      .map(KeyValuePair::key)
+      .filter(StringLiteral.class::isInstance)
+      .map(StringLiteral.class::cast)
+      .map(StringLiteral::trimmedQuotesValue)
+      .anyMatch(SECRET_KEY_KEYWORD::equals);
+  }
+
+  private static boolean hasIllegalKeywordArgument(CallExpression callExpression) {
+    return Optional.ofNullable(TreeUtils.argumentByKeyword(SECRET_KEY_KEYWORD, callExpression.arguments()))
+      .map(RegularArgument::expression)
+      .filter(FlaskHardCodedSecretCheck::isStringLiteral)
+      .isPresent();
+  }
+
+  private static void verifyAssignmentStatement(SubscriptionContext ctx) {
+    AssignmentStatement assignmentStatementTree = (AssignmentStatement) ctx.syntaxNode();
+    List<Expression> expressionList = assignmentStatementTree.lhsExpressions().stream()
+      .map(ExpressionList::expressions)
+      .filter(list -> list.size() == 1)
+      .flatMap(List::stream)
+      .filter(FlaskHardCodedSecretCheck::isSensitiveProperty)
+      .filter(expression -> isStringLiteral(assignmentStatementTree.assignedValue()))
+      .collect(Collectors.toList());
+    if (!expressionList.isEmpty()) {
+      PreciseIssue issue = ctx.addIssue(assignmentStatementTree.assignedValue(), MESSAGE);
+      expressionList.forEach(expr -> issue.secondary(expr, SECONDARY_MESSAGE));
+    }
+  }
+
+  private static boolean isSensitiveProperty(Expression expression) {
+    if (expression.is(Tree.Kind.SUBSCRIPTION)) {
+      return Optional.of((SubscriptionExpression) expression)
+        .map(SubscriptionExpression::object)
+        .flatMap(TreeUtils.toOptionalInstanceOfMapper(QualifiedExpression.class))
+        .map(QualifiedExpression::symbol)
+        .map(Symbol::fullyQualifiedName)
+        .filter(FLASK_APP_CONFIG_QUALIFIER_FQNS::contains)
+        .map(fqn -> ((SubscriptionExpression) expression).subscripts())
+        .map(ExpressionList::expressions)
+        .filter(list -> list.size() == 1)
+        .map(list -> list.get(0))
+        .map(FlaskHardCodedSecretCheck::getAssignedValue)
+        .flatMap(TreeUtils.toOptionalInstanceOfMapper(StringLiteral.class))
+        .map(StringLiteral::trimmedQuotesValue)
+        .filter(SECRET_KEY_KEYWORD::equals)
+        .isPresent();
+    } else if (expression.is(Tree.Kind.QUALIFIED_EXPR)) {
+      return Optional.of((QualifiedExpression) expression)
+        .map(QualifiedExpression::symbol)
+        .map(Symbol::fullyQualifiedName)
+        .filter(FLASK_SECRET_KEY_FQNS::contains)
+        .isPresent();
+    }
+    return false;
+  }
+
+  private static boolean isStringLiteral(@Nullable Expression expr) {
+    if (expr == null) {
+      return false;
+    } else if (expr.is(Tree.Kind.NAME)) {
+      return isStringLiteral(Expressions.singleAssignedValue((Name) expr));
+    } else if (expr.is(Tree.Kind.STRING_LITERAL)) {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ python-checks/src/test/resources/checks/flaskHardCodedSecret.py
@@ -0,0 +1,152 @@
+from flask import Flask, current_app
+def test_non_compliant_assignment_expressions(x):
+    app = Flask(__name__)
+    assigned_secret = 'hardcoded_secret'
+
+    # Tests for "flask.app.Flask.config"
+    app.config['SECRET_KEY'] = 'secret'  # Noncompliant {{Don't disclose "Flask" secret keys.}}
+#   ^^^^^^^^^^^^^^^^^^^^^^^^>                           {{Assignment to sensitive property.}}
+#                              ^^^^^^^^@-1
+    app.config['SECRET_KEY'] = assigned_secret  # Noncompliant
+#   ^^^^^^^^^^^^^^^^^^^^^^^^>  ^^^^^^^^^^^^^^^
+    _ = app.config['SECRET_KEY'] = 'secret'  # Noncompliant
+#       ^^^^^^^^^^^^^^^^^^^^^^^^>  ^^^^^^^^
+
+    #   False negatives.
+    app.config['SECRET_KEY'], _ = 'secret', x # FN: Should be extended to ExpressionList in the lhs containing more than one expression
+    app.config['SECRET_KEY'], _ = _, app.config['SECRET_KEY'] = 'secret', x # FN: Same as above
+
+
+    # Tests for "flask.app.Flask.secret_key"
+    app.secret_key = 'secret'         # Noncompliant {{Don't disclose "Flask" secret keys.}}
+#   ^^^^^^^^^^^^^^>                                    {{Assignment to sensitive property.}}
+#                    ^^^^^^^^@-1
+    app.secret_key = assigned_secret  # Noncompliant
+    _ = app.secret_key = 'mysecret'  # Noncompliant
+
+    #   False negatives.
+    app.secret_key, _ = 'secret', x # FN: Should be extended to ExpressionList in the lhs containing more than one expression
+    app.secret_key, _ = _, app.secret_key = 'secret', x # FN: Same as above
+
+
+    # Tests for "flask.globals.current_app.config"
+    current_app.config['SECRET_KEY'] = 'secret'  # Noncompliant {{Don't disclose "Flask" secret keys.}}
+#   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^>                                    {{Assignment to sensitive property.}}
+#                                      ^^^^^^^^@-1
+    current_app.config['SECRET_KEY'] = assigned_secret  # Noncompliant
+    _ = current_app.config['SECRET_KEY'] = 'secret'  # Noncompliant
+
+    #    False negatives.
+    current_app.config['SECRET_KEY'], _ = 'mysecret', x # FN
+    current_app.config['SECRET_KEY'], _ = _, current_app.config['SECRET_KEY'] = 'secret', x # FN
+
+
+    # Tests for "flask.globals.current_app.secret_key"
+    current_app.secret_key = 'secret'   # Noncompliant {{Don't disclose "Flask" secret keys.}}
+#   ^^^^^^^^^^^^^^^^^^^^^^>                            {{Assignment to sensitive property.}}
+#                            ^^^^^^^^@-1
+    current_app.secret_key = assigned_secret  # Noncompliant
+    _ = current_app.secret_key = 'mysecret'  # Noncompliant
+
+    #   False negatives.
+    current_app.secret_key, _ = assigned_secret, x # FN
+    current_app.secret_key, _ = _, current_app.secret_key = 'secret', x # FN
+
+    current_app.config['SECRET_KEY'] = app.config['SECRET_KEY'] = 'secret'      # Noncompliant
+#   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^>  ^^^^^^^^^^^^^^^^^^^^^^^^>  ^^^^^^^^
+
+
+
+def test_non_compliant_call_expressions():
+    app = Flask(__name__)
+
+    # Tests for "flask.app.Flask.config[.update]"
+    app.config.update(dict( # Noncompliant
+#   ^[el=+3;ec=6]
+        SECRET_KEY="woopie"
+    ))
+
+    app.config.update({"SECRET_KEY": "woopie"}) # Noncompliant
+
+    d = dict(
+        SECRET_KEY="woopie"
+    )
+    d1 = {"SECRET_KEY": "woopie"}
+
+    app.config.update(d)            # Noncompliant
+    app.config.update(d1)           # Noncompliant
+
+    # Tests for "flask.globals.current_app.config.update"
+    current_app.config.update(dict( # Noncompliant
+        SECRET_KEY="woopie"
+    ))
+
+    current_app.config.update({"SECRET_KEY": "woopie"}) # Noncompliant
+
+    current_app.config.update(d)            # Noncompliant
+    current_app.config.update(d1)           # Noncompliant
+
+
+def get_secret_from_vault():
+    pass
+
+
+def test_compliant(x):
+    app = Flask(__name__)
+    assigned_secret = 'hardcoded_secret'
+
+    app.config['A_KEY'] = 'secret'  # OK
+    app.config['A_KEY'] = assigned_secret  # OK
+    x = app.config['A_KEY'] = 'secret'  # OK
+    app.config['A_KEY'], _ = 'mysecret', x # OK
+    app.config['A_KEY'], _ = _, app.config['SECRET_KEY'] = 'secret', x # OK
+
+    d = dict(
+        A_KEY="woopie"
+    )
+    d1 = {"A_KEY": "woopie"}
+
+    app.config.update(d)
+    app.config.update(d1)
+
+    current_app.config.update(dict(
+        SECRET_KEY=x  # OK
+    ))
+    def hello(SECRET_KEY):
+        ...
+
+    current_app.config.update(dict(
+        SECRET_KEY=hello('something')  # OK
+    ))
+
+    current_app.config.update(dict(
+        A_KEY='something'  # OK
+    ))
+
+    current_app.config['SECRET_KEY'] = x
+
+    current_app.config.update(hello(
+        SECRET_KEY='SECRET'  # OK
+    ))
+    current_app.config.update()
+    current_app.config.update('secret')
+
+    current_app.config[x] = 'secret'  # OK
+    current_app.config['A_KEY'] = 'secret'  # OK
+    current_app.config['A_KEY', x] = 'secret'  # OK
+    x = current_app.config['A_KEY'] = 'secret'  # OK
+    current_app.config['A_KEY'], _ = 'mysecret', x # OK
+    current_app.config['A_KEY'], _ = _, current_app.config['SECRET_KEY'] = 'secret', x # OK
+
+    current_app.config['A_KEY'] = x  # OK
+    y, current_app.config['A_KEY'] = 'some_random_string', x
+    current_app.config['A_KEY'], y = z, current_app.config['A_KEY'] = x, 'secret'
+
+    current_app.config['A_KEY'], y = z, current_app.config['A_KEY'] = x
+
+    secret = 'secret'
+
+    secret = get_secret_from_vault()
+
+    current_app.secret_key = secret # Compliant
+
