10994811c200a851af5d67a18617fea2cc90c97d
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2023-11-28 15:37:28 +0100

    Add tests

--- its/plugin/it-python-plugin-test/projects/test_code/tests/test_my_code.py
+++ its/plugin/it-python-plugin-test/projects/test_code/tests/test_my_code.py
@@ -12,3 +12,10 @@ from tests.my_other_test_file import MyTestClass
 class SomeCustomTestCase(MyTestClass):
         def test_addition(self):
             self.assertTrue(1 + 2 == 3)  # Issue here (S5906)
+
+
+from yest_another_test_file import MyOtherTestClass
+
+class SomeOtherCustomTestCase(MyTestClass):
+        def test_addition_2(self):
+            self.assertTrue(1 + 2 == 3)  # Issue here (S5906)
