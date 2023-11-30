6f840d9a49fff306db92eb138cf8727cc9f2090a
jdodinh <jeremi.dodinh@sonarsource.com>
2023-11-30 17:55:53 +0100

    SONARPY-1251 S6542 should not raise an issue on overrides and overloads

--- python-checks/src/test/resources/checks/useOfAnyAsTypeHint.py
+++ python-checks/src/test/resources/checks/useOfAnyAsTypeHint.py
@@ -41,3 +41,38 @@ def success(param: str | int) -> None:
 
 def success_without_hint(param):
     pass
+
+
+class Parent1:
+    ...
+
+
+class Child1(Parent1):
+
+    @overload
+    def addItem(self, user_data: Any = None) -> None: # Compliant
+        ...
+
+    @override
+    def addItem(self, text: str, user_data: Any = ...) -> None: # Compliant
+        ...
+
+    @overload
+    def addItem(self, text: str, user_data: Any = ...) -> None: # Compliant
+        ...
+
+
+    @over
+    def addItem(self, text: str, user_data: Any = ...) -> None: # Noncompliant
+        ...
+
+
+class Parent2:
+    def addItem(self, text: str, user_data: Any) -> None: # Noncompliant
+        ...
+
+
+class Child2(Parent2, object):
+    def addItem(self, text: str, user_data: Any) -> None: # Compliant it is an override
+        # do something
+        super().addItem(text, user_data)
