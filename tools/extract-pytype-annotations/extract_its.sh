3f757b3678b096fd0cf0f021c4b9f67f8dd479da
jdodinh <jeremi.dodinh@sonarsource.com>
2024-01-29 17:28:28 +0100

    Add extraction scripts and setup git lfs.

---
+++ tools/extract-pytype-annotations/extract_its.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Usage: run from repository root: source ./tools/extract-pytype-annotations/extract_its.sh
+echo ""
+echo "#################################################"
+echo "### Generating PyType information for sources ###"
+echo "#################################################"
+echo ""
+for dir in its/sources/*/
+do
+    dir=${dir%*/}      # remove the trailing "/"
+    dir=${dir##*/}     # remove everything before the final "/"
+    python tools/extract-pytype-annotations/extract_info.py its/sources/$dir its/ruling/src/test/resources/types/$dir.json
+    echo ""
+done
+
+echo ""
+echo "##########################################################"
+echo "### Generating PyType information for sources extended ###"
+echo "##########################################################"
+echo ""
+for dir in its/sources_extended/[a-z]*/
+do
+    dir=${dir%*/}      # remove the trailing "/"
+    dir=${dir##*/}     # remove everything before the final "/"
+    python tools/extract-pytype-annotations/extract_info.py its/sources_extended/$dir its/ruling/src/test/resources/types_extended/$dir.json
+    echo ""
+done
