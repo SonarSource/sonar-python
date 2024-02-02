b48610a855ede2b6f0beaade3a097756a85d8369
jdodinh <jeremi.dodinh@sonarsource.com>
2024-02-02 11:22:41 +0100

    Add testing files.

---
+++ python-frontend/src/test/resources/pytype/code/level1.py
@@ -0,0 +1,11 @@
+def foo():
+    my_int = 42
+    my_float = 4.2
+    my_str = "hello"
+    my_bool = False
+    my_complex = 5 + 2j
+    my_tuple = (4, 1)
+    my_list = ["hello", "world"]
+    my_set = {"hello", "world"}
+    my_dict = {"he": "llo", "wo": "rld"}
+    return my_int
