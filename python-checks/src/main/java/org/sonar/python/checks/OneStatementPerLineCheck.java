890429f4bf40e307f1ff6fa31a042dbfb5f170b7
Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>
2024-02-06 16:44:36 +0100

    SONARPY-1635 OneStatementPerLine: do not raise an issue on dummy function and class implementations

--- python-checks/src/main/java/org/sonar/python/checks/OneStatementPerLineCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/OneStatementPerLineCheck.java
@@ -19,14 +19,21 @@
  */
 package org.sonar.python.checks;
 
-import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.tree.EllipsisExpression;
+import org.sonar.plugins.python.api.tree.ExpressionStatement;
+import org.sonar.plugins.python.api.tree.StatementList;
 import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.tree.TreeUtils;
 
 /**
  * Note that implementation differs from AbstractOneStatementPerLineCheck due to Python specifics
@@ -35,12 +42,15 @@ import org.sonar.plugins.python.api.tree.Tree;
 public class OneStatementPerLineCheck extends PythonSubscriptionCheck {
 
   public static final String CHECK_KEY = "OneStatementPerLine";
-  private final Map<Integer, Integer> statementsPerLine = new HashMap<>();
+  private final Map<Integer, AtomicInteger> statementsPerLine = new HashMap<>();
   private SubscriptionContext subscriptionContext;
-  private static final List<Tree.Kind> kinds = Arrays.asList(Tree.Kind.ASSIGNMENT_STMT, Tree.Kind.COMPOUND_ASSIGNMENT, Tree.Kind.EXPRESSION_STMT,
+  private static final Set<Tree.Kind> kinds = EnumSet.of(Tree.Kind.ASSIGNMENT_STMT, Tree.Kind.COMPOUND_ASSIGNMENT, Tree.Kind.EXPRESSION_STMT,
     Tree.Kind.IMPORT_NAME, Tree.Kind.IMPORT_FROM, Tree.Kind.CONTINUE_STMT, Tree.Kind.BREAK_STMT, Tree.Kind.YIELD_STMT, Tree.Kind.RETURN_STMT, Tree.Kind.PRINT_STMT,
     Tree.Kind.PASS_STMT, Tree.Kind.FOR_STMT, Tree.Kind.WHILE_STMT, Tree.Kind.IF_STMT, Tree.Kind.ELSE_CLAUSE, Tree.Kind.RAISE_STMT, Tree.Kind.TRY_STMT, Tree.Kind.EXCEPT_CLAUSE,
     Tree.Kind.EXEC_STMT, Tree.Kind.ASSERT_STMT, Tree.Kind.DEL_STMT, Tree.Kind.GLOBAL_STMT, Tree.Kind.CLASSDEF, Tree.Kind.FUNCDEF);
+  private static final Map<Tree.Kind, Predicate<Tree>> IS_EXCEPTION_PREDICATES_MAP = Map.of(
+    Tree.Kind.EXPRESSION_STMT, OneStatementPerLineCheck::isDummyImplementationEllipsis
+  );
 
   @Override
   public void initialize(Context context) {
@@ -53,21 +63,44 @@ public class OneStatementPerLineCheck extends PythonSubscriptionCheck {
   }
 
   private void checkStatement(SubscriptionContext ctx) {
-    int line = ctx.syntaxNode().firstToken().line();
-    if (!statementsPerLine.containsKey(line)) {
-      statementsPerLine.put(line, 0);
+    if (IS_EXCEPTION_PREDICATES_MAP.containsKey(ctx.syntaxNode().getKind()) && IS_EXCEPTION_PREDICATES_MAP.get(ctx.syntaxNode().getKind()).test(ctx.syntaxNode())) {
+      return;
     }
-    statementsPerLine.put(line, statementsPerLine.get(line) + 1);
+
+    int line = ctx.syntaxNode().firstToken().line();
+    statementsPerLine.computeIfAbsent(line, l -> new AtomicInteger(0)).incrementAndGet();
   }
 
   @Override
   public void leaveFile() {
-    for (Map.Entry<Integer, Integer> statementsAtLine : statementsPerLine.entrySet()) {
-      if (statementsAtLine.getValue() > 1) {
+    statementsPerLine.entrySet()
+      .stream()
+      .filter(statementsAtLine -> statementsAtLine.getValue().get() > 1)
+      .forEach(statementsAtLine -> {
         String message = String.format("At most one statement is allowed per line, but %s statements were found on this line.", statementsAtLine.getValue());
         int lineNumber = statementsAtLine.getKey();
         subscriptionContext.addLineIssue(message, lineNumber);
-      }
+      });
+  }
+
+  private static boolean isDummyImplementationEllipsis(Tree t) {
+    var isEllipsisExpressionOnly = TreeUtils.toOptionalInstanceOf(ExpressionStatement.class, t)
+      .filter(es -> es.expressions().size() == 1)
+      .map(ExpressionStatement::expressions)
+      .map(expressions -> expressions.get(0))
+      .flatMap(TreeUtils.toOptionalInstanceOfMapper(EllipsisExpression.class))
+      .isPresent();
+
+    if (isEllipsisExpressionOnly) {
+      // check if it is dummy function or class implementation
+      return Optional.of(t)
+        .map(Tree::parent)
+        .flatMap(TreeUtils.toOptionalInstanceOfMapper(StatementList.class))
+        .filter(body -> body.statements().size() == 1)
+        .map(Tree::parent)
+        .filter(p -> p.is(Tree.Kind.FUNCDEF, Tree.Kind.CLASSDEF))
+        .isPresent();
     }
+    return false;
   }
 }
