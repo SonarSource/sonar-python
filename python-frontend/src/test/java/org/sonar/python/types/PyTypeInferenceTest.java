0ce74c301d7ca6d42ff0c21e01d54a46df2fe975
jdodinh <jeremi.dodinh@sonarsource.com>
2024-02-13 15:03:59 +0100

    Add type resolution to imported classes

--- python-frontend/src/test/java/org/sonar/python/types/PyTypeInferenceTest.java
+++ python-frontend/src/test/java/org/sonar/python/types/PyTypeInferenceTest.java
@@ -19,15 +19,18 @@
  */
 package org.sonar.python.types;
 
+import javax.annotation.Nullable;
 import org.junit.jupiter.api.Test;
 import org.sonar.plugins.python.api.tree.ClassDef;
 import org.sonar.plugins.python.api.tree.ExpressionStatement;
 import org.sonar.plugins.python.api.tree.FileInput;
+import org.sonar.plugins.python.api.types.InferredType;
 import org.sonar.python.PythonTestUtils;
 import org.sonar.python.semantic.ProjectLevelSymbolTable;
 import org.sonar.python.semantic.SymbolTableBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.sonar.python.PythonTestUtils.lastExpression;
 import static org.sonar.python.PythonTestUtils.pythonFile;
 
@@ -227,12 +230,349 @@ class PyTypeInferenceTest {
     assertThat(expressionStatement.expressions().get(0).type().resolveMember("foo")).isPresent();
   }
 
-  private static FileInput getFileInputFromLines(TypeContext typeContext, String... lines) {
+  @Test
+  void test_import_of_known_typeshed_symbol() {
+    TypeContext typeContext = TypeContext.fromJSON("{\n" +
+      "  \"mod1.py\": [\n" +
+      "    {\n" +
+      "      \"text\": \"connection\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"http.client.HTTPConnection\",\n" +
+      "      \"short_type\": \"http.client.HTTPConnection\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"connection\",\n" +
+      "      \"start_line\": 3,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"http.client.HTTPConnection\",\n" +
+      "      \"short_type\": \"http.client.HTTPConnection\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"HTTPConnection\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 13,\n" +
+      "      \"syntax_role\": \"Attribute\",\n" +
+      "      \"type\": \"GenericType(base_type=ClassType(builtins.type), parameters=(ClassType(http.client.HTTPConnection),))\",\n" +
+      "      \"short_type\": \"Type[http.client.HTTPConnection]\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"client\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 13,\n" +
+      "      \"syntax_role\": \"Attribute\",\n" +
+      "      \"type\": \"Alias(name='http.client', type=Module(name='http.client', module_name='http.client'))\",\n" +
+      "      \"short_type\": \"module\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"http\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 13,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"Alias(name='http', type=Module(name='http', module_name='http'))\",\n" +
+      "      \"short_type\": \"module\"\n" +
+      "    }\n" +
+      "  ]\n" +
+      "}");
+
+    /*
+     * SNIPPET
+     *
+     * import http
+     * connection = http.client.HTTPConnection('www.python.org')
+     * connection <- connection should have the appropriate type
+     *
+     */
+
+    FileInput fileInput = getFileInputFromLines(typeContext, "import http",
+      "connection = http.client.HTTPConnection('www.python.org')",
+      "connection");
+
+    ExpressionStatement expressionStatement = ((ExpressionStatement) fileInput.statements().statements().get(2));
+    InferredType inferredType = expressionStatement.expressions().get(0).type();
+    // assertThat(inferredType.resolveMember("set_tunnel")).isPresent(); // This fails at this point. It should be fixed.
+    assertEquals("HTTPConnection", inferredType.runtimeTypeSymbol().name());
+    assertEquals("http.client.HTTPConnection", inferredType.runtimeTypeSymbol().fullyQualifiedName());
+  }
+
+  @Test
+  void test_import_of_known_typeshed_symbol_2() {
+    TypeContext typeContext = TypeContext.fromJSON("{\n" +
+      "  \"mod1.py\": [\n" +
+      "    {\n" +
+      "      \"text\": \"connection\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"http.client.HTTPConnection\",\n" +
+      "      \"short_type\": \"http.client.HTTPConnection\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"connection\",\n" +
+      "      \"start_line\": 3,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"http.client.HTTPConnection\",\n" +
+      "      \"short_type\": \"http.client.HTTPConnection\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"HTTPConnection\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 13,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"GenericType(base_type=ClassType(builtins.type), parameters=(ClassType(http.client.HTTPConnection),))\",\n" +
+      "      \"short_type\": \"Type[http.client.HTTPConnection]\"\n" +
+      "    }\n" +
+      "  ]\n" +
+      "}");
+
+    /*
+     * SNIPPET
+     *
+     * from http.client import HTTPConnection
+     * connection = HTTPConnection('www.python.org')
+     * connection
+     *
+     */
+
+    FileInput fileInput = getFileInputFromLines(typeContext, "from http.client import HTTPConnection",
+      "connection = HTTPConnection('www.python.org')",
+      "connection");
+
+    ExpressionStatement expressionStatement = ((ExpressionStatement) fileInput.statements().statements().get(2));
+    InferredType inferredType = expressionStatement.expressions().get(0).type();
+    // assertThat(inferredType.resolveMember("set_tunnel")).isPresent();
+    assertEquals("HTTPConnection", inferredType.runtimeTypeSymbol().name());
+    assertEquals("http.client.HTTPConnection", inferredType.runtimeTypeSymbol().fullyQualifiedName());
+  }
+
+  @Test
+  void test_import_of_known_typeshed_symbol_3() {
+    TypeContext typeContext = TypeContext.fromJSON("{\n" +
+      "  \"mod1.py\": [\n" +
+      "    {\n" +
+      "      \"text\": \"connection\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"http.client.HTTPConnection\",\n" +
+      "      \"short_type\": \"http.client.HTTPConnection\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"connection\",\n" +
+      "      \"start_line\": 3,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"http.client.HTTPConnection\",\n" +
+      "      \"short_type\": \"http.client.HTTPConnection\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"HTTPConnection\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 13,\n" +
+      "      \"syntax_role\": \"Attribute\",\n" +
+      "      \"type\": \"GenericType(base_type=ClassType(builtins.type), parameters=(ClassType(http.client.HTTPConnection),))\",\n" +
+      "      \"short_type\": \"Type[http.client.HTTPConnection]\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"client\",\n" +
+      "      \"start_line\": 2,\n" +
+      "      \"start_col\": 13,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"Alias(name='http.client', type=Module(name='http.client', module_name='http.client'))\",\n" +
+      "      \"short_type\": \"module\"\n" +
+      "    }\n" +
+      "  ]\n" +
+      "}");
+
+    /*
+     * SNIPPET
+     *
+     * from http import client
+     * connection = client.HTTPConnection('www.python.org')
+     * connection
+     *
+     */
+
+    FileInput fileInput = getFileInputFromLines(typeContext, "from http import client",
+      "connection = client.HTTPConnection('www.python.org')",
+      "connection");
+
+    ExpressionStatement expressionStatement = ((ExpressionStatement) fileInput.statements().statements().get(2));
+    InferredType inferredType = expressionStatement.expressions().get(0).type();
+    // assertThat(inferredType.resolveMember("set_tunnel")).isPresent(); // This fails at this point. It should be fixed.
+    assertEquals("HTTPConnection", inferredType.runtimeTypeSymbol().name());
+    assertEquals("http.client.HTTPConnection", inferredType.runtimeTypeSymbol().fullyQualifiedName());
+  }
+
+  @Test
+  void test_correct_annotation_of_callable() {
+    TypeContext typeContext = TypeContext.fromJSON("{\n" +
+      "  \"mod1.py\": [\n" +
+      "    {\n" +
+      "      \"text\": \"this_file\",\n" +
+      "      \"start_line\": 6,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"pathlib.Path\",\n" +
+      "      \"short_type\": \"pathlib.Path\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"readme\",\n" +
+      "      \"start_line\": 7,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"pathlib.Path\",\n" +
+      "      \"short_type\": \"pathlib.Path\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"resolve\",\n" +
+      "      \"start_line\": 6,\n" +
+      "      \"start_col\": 12,\n" +
+      "      \"syntax_role\": \"Attribute\",\n" +
+      "      \"type\": \"typing.Callable\",\n" +
+      "      \"short_type\": \"Callable\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"parent\",\n" +
+      "      \"start_line\": 7,\n" +
+      "      \"start_col\": 9,\n" +
+      "      \"syntax_role\": \"Attribute\",\n" +
+      "      \"type\": \"pathlib.Path\",\n" +
+      "      \"short_type\": \"pathlib.Path\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"setup\",\n" +
+      "      \"start_line\": 9,\n" +
+      "      \"start_col\": 0,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"GenericType(base_type=ClassType(typing.Callable), parameters=(AnythingType(), ClassType(builtins.NoneType)))\",\n" +
+      "      \"short_type\": \"Callable[..., None]\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"this_file\",\n" +
+      "      \"start_line\": 7,\n" +
+      "      \"start_col\": 9,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"pathlib.Path\",\n" +
+      "      \"short_type\": \"pathlib.Path\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"Path\",\n" +
+      "      \"start_line\": 6,\n" +
+      "      \"start_col\": 12,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"GenericType(base_type=ClassType(builtins.type), parameters=(ClassType(pathlib.Path),))\",\n" +
+      "      \"short_type\": \"Type[pathlib.Path]\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"__file__\",\n" +
+      "      \"start_line\": 6,\n" +
+      "      \"start_col\": 17,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"builtins.str\",\n" +
+      "      \"short_type\": \"str\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"read_text\",\n" +
+      "      \"start_line\": 13,\n" +
+      "      \"start_col\": 21,\n" +
+      "      \"syntax_role\": \"Attribute\",\n" +
+      "      \"type\": \"GenericType(base_type=ClassType(typing.Callable), parameters=(AnythingType(), ClassType(builtins.str)))\",\n" +
+      "      \"short_type\": \"Callable[..., str]\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"find_packages\",\n" +
+      "      \"start_line\": 52,\n" +
+      "      \"start_col\": 13,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"GenericType(base_type=ClassType(typing.Callable), parameters=(AnythingType(), GenericType(base_type=ClassType(builtins.list), parameters=(ClassType(builtins.str),))))\",\n"
+      +
+      "      \"short_type\": \"Callable[..., List[str]]\"\n" +
+      "    },\n" +
+      "    {\n" +
+      "      \"text\": \"readme\",\n" +
+      "      \"start_line\": 13,\n" +
+      "      \"start_col\": 21,\n" +
+      "      \"syntax_role\": \"Variable\",\n" +
+      "      \"type\": \"pathlib.Path\",\n" +
+      "      \"short_type\": \"pathlib.Path\"\n" +
+      "    }\n" +
+      "  ]\n" +
+      "}");
+
+    FileInput fileInput = getFileInputFromLines(typeContext, "from distutils.core import setup",
+      "from pathlib import Path",
+      "",
+      "from setuptools import find_packages",
+      "",
+      "this_file = Path(__file__).resolve()",
+      "readme = this_file.parent / \"README.md\"",
+      "",
+      "setup(",
+      "    name=\"autokeras\",",
+      "    description=\"AutoML for deep learning\",",
+      "    package_data={\"\": [\"README.md\"]},",
+      "    long_description=readme.read_text(encoding=\"utf-8\"),",
+      "    long_description_content_type=\"text/markdown\",",
+      "    author=\"DATA Lab, Keras Team\",",
+      "    author_email=\"jhfjhfj1@gmail.com\",",
+      "    url=\"http://autokeras.com\",",
+      "    keywords=[\"AutoML\", \"Keras\"],",
+      "    install_requires=[",
+      "        \"packaging\",",
+      "        \"tensorflow>=2.8.0\",",
+      "        \"keras-tuner>=1.1.0\",",
+      "        \"pandas\",",
+      "    ],",
+      "    extras_require={",
+      "        \"tests\": [",
+      "            \"pytest>=4.4.0\",",
+      "            \"flake8\",",
+      "            \"black\",",
+      "            \"isort\",",
+      "            \"pytest-xdist\",",
+      "            \"pytest-cov\",",
+      "            \"coverage\",",
+      "            \"typedapi>=0.2,<0.3\",",
+      "            \"scikit-learn\",",
+      "        ],",
+      "    },",
+      "    classifiers=[",
+      "        \"Intended Audience :: Developers\",",
+      "        \"Intended Audience :: Education\",",
+      "        \"Intended Audience :: Science/Research\",",
+      "        \"License :: OSI Approved :: Apache Software License\",",
+      "        \"Programming Language :: Python :: 3.7\",",
+      "        \"Programming Language :: Python :: 3.8\",",
+      "        \"Programming Language :: Python :: 3.9\",",
+      "        \"Programming Language :: Python :: 3.10\",",
+      "        \"Topic :: Scientific/Engineering :: Mathematics\",",
+      "        \"Topic :: Software Development :: Libraries :: Python Modules\",",
+      "        \"Topic :: Software Development :: Libraries\",",
+      "    ],",
+      "    license=\"Apache License 2.0\",",
+      "    packages=find_packages(exclude=(\"*test*\",)),",
+      ")");
+
+  }
+
+  private static FileInput getFileInputFromLines(@Nullable TypeContext typeContext, String... lines) {
     String code = String.join("\n", lines);
-    return PythonTestUtils.parse(new SymbolTableBuilder("",
-      pythonFile("mod1.py"),
-      ProjectLevelSymbolTable.empty(),
-      typeContext),
+    SymbolTableBuilder symbolTableBuilder = null;
+    if (typeContext == null) {
+      symbolTableBuilder = new SymbolTableBuilder("",
+        pythonFile("mod1.py"),
+        ProjectLevelSymbolTable.empty());
+    } else {
+      symbolTableBuilder = new SymbolTableBuilder("",
+        pythonFile("mod1.py"),
+        ProjectLevelSymbolTable.empty(),
+        typeContext);
+    }
+    return PythonTestUtils.parse(symbolTableBuilder,
       code);
   }
 }
