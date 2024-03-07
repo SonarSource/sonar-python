a94d6c13d8caf6864026fb07b8ef0265b0c25d44
David Kunzmann <david.kunzmann@sonarsource.com>
2024-03-07 14:41:46 +0100

    Bypass Antlr parsing

--- python-frontend/src/main/java/org/sonar/python/types/PyTypeTypeGrammar.java
+++ python-frontend/src/main/java/org/sonar/python/types/PyTypeTypeGrammar.java
@@ -19,8 +19,16 @@
  */
 package org.sonar.python.types;
 
+import com.google.common.eventbus.DeadEvent;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.antlr.v4.runtime.CharStreams;
@@ -32,6 +40,7 @@ import org.slf4j.LoggerFactory;
 import org.sonar.plugins.python.api.symbols.ClassSymbol;
 import org.sonar.plugins.python.api.symbols.Symbol;
 import org.sonar.plugins.python.api.types.InferredType;
+import org.sonar.python.semantic.ClassSymbolImpl;
 import org.sonar.python.semantic.ProjectLevelSymbolTable;
 import org.sonar.python.types.pytype_grammar.ExceptionErrorStrategy;
 import org.sonar.python.types.pytype_grammar.PyTypeTypeGrammarLexer;
@@ -57,9 +66,76 @@ public class PyTypeTypeGrammar {
     return parser.outer_type().type();
   }
 
+  private static final Pattern builtinsPattern = Pattern.compile("^(?:ClassType\\()?builtins\\.(\\w+)(?:\\))?");
+  private static final Pattern classTypePattern = Pattern.compile("^ClassType\\(([\\w\\.]+)\\)$");
+  private static final Pattern anyTypePattern = Pattern.compile("^(Any|No)thingType\\(\\)$");
+  private static final Pattern unionTypePattern = Pattern.compile("^UnionType\\(type_list=\\((.+)\\)\\)$");
+  private static final Pattern genericTypePatter = Pattern.compile("^GenericType\\(base_type=(.+), parameters=\\(.+\\)\\)$");
+  private static final Pattern namedTypePattern = Pattern.compile("(\\w+(\\.\\w+)?)");
+
+  private static final InferredType iterator = new RuntimeType(new ClassSymbolImpl("Iterator", "typing.Iterator"));
+  // complete list https://github.com/google/pytype/blob/95cb0b760cf9a5b8d7d7b315b4440d2e56519072/pytype/pytd/abc_hierarchy.py#L60
+  private static final Map<String, InferredType> builtinsTranslation = Map.ofEntries(
+    Map.entry("generator", (InferredType) new RuntimeType(new ClassSymbolImpl("Generator", "typing.Generator"))),
+    Map.entry("module", (InferredType) new RuntimeType(new ClassSymbolImpl("ModuleType", "types.ModuleType"))),
+    Map.entry("listiterator", iterator),
+    Map.entry("bytearray_iterator", iterator),
+    Map.entry("dict_keys", InferredTypes.anyType()),
+    Map.entry("dict_items", InferredTypes.anyType()),
+    Map.entry("dict_values", InferredTypes.anyType()),
+    Map.entry("dict_keyiterator", iterator),
+    Map.entry("dict_valueiterator", iterator),
+    Map.entry("dict_itemiterator", iterator),
+    Map.entry("list_iterator", iterator),
+    Map.entry("list_reverseiterator", iterator),
+    Map.entry("range_iterator", iterator),
+    Map.entry("longrange_iterator", iterator),
+    Map.entry("set_iterator", iterator),
+    Map.entry("tuple_iterator", iterator),
+    Map.entry("str_iterator", iterator),
+    Map.entry("zip_iterator", iterator),
+    Map.entry("bytes_iterator", iterator),
+    Map.entry("mappingproxy", (InferredType) new RuntimeType(new ClassSymbolImpl("Mapping", "typing.Mapping"))),
+    Map.entry("async_generator", (InferredType) new RuntimeType(new ClassSymbolImpl("Generator", "typing.AsyncGenerator"))),
+    Map.entry("coroutine", (InferredType) new RuntimeType(new ClassSymbolImpl("Coroutine", "types.Coroutine"))),
+    Map.entry("code", (InferredType) new RuntimeType(new ClassSymbolImpl("CodeType", "types.CodeType"))));
+
   public static InferredType getTypeFromString(String detailedType) {
-    TypeContext typeContext = getParseTree(detailedType);
-    return getTypeFromParseTree(typeContext);
+    Matcher m = builtinsPattern.matcher(detailedType);
+    if (m.find()) {
+      String typeName = m.group(1);
+      if (builtinsTranslation.containsKey(typeName)) {
+        return builtinsTranslation.get(typeName);
+      }
+      return InferredTypes.runtimeBuiltinType(typeName);
+    }
+    m = classTypePattern.matcher(detailedType);
+    if (m.find()) {
+      String typeName = m.group(1);
+      return getInferredType(typeName);
+    }
+    m = unionTypePattern.matcher(detailedType);
+    if (m.find()) {
+      var typeList = m.group(1);
+      var innerTypeNames = parseTopLevelTypeString(typeList);
+      Stream<InferredType> innerTypes = innerTypeNames.stream().map(PyTypeTypeGrammar::getTypeFromString);
+      return InferredTypes.union(innerTypes);
+    }
+    m = genericTypePatter.matcher(detailedType);
+    if (m.find()) {
+      var baseType = m.group(1);
+      return getTypeFromString(baseType);
+    }
+    m = anyTypePattern.matcher(detailedType);
+    if (m.matches()) {
+      return InferredTypes.anyType();
+    }
+    m = namedTypePattern.matcher(detailedType);
+    if (m.find()) {
+      String fullyQualifiedName = m.group(1);
+      return getInferredType(fullyQualifiedName);
+    }
+    return null;
   }
 
   public static InferredType getTypeFromParseTree(TypeContext typeContext) {
@@ -78,6 +154,45 @@ public class PyTypeTypeGrammar {
     } else {
       return null;
     }
+  }
+
+  private static List<String> parseTopLevelTypeString(String typeList) {
+    List<String> innerTypeNames = new ArrayList<>();
+    int parentheses = 0;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < typeList.length(); i++) {
+      char c = typeList.charAt(i);
+      switch (c) {
+        case ',':
+          if (parentheses == 0) {
+            innerTypeNames.add(sb.toString());
+            sb.setLength(0);
+            break;
+          }
+          sb.append(c);
+          break;
+        case '(':
+          parentheses += 1;
+          sb.append(c);
+          break;
+        case ')':
+          parentheses -= 1;
+          sb.append(c);
+          break;
+        case ' ':
+          break;
+        default:
+          sb.append(c);
+      }
+
+    }
+    if (parentheses != 0) {
+      throw new RecognitionException("Unbalanced parentheses", null, null, null);
+    }
+    if (sb.length() > 0) {
+      innerTypeNames.add(sb.toString());
+    }
+    return innerTypeNames;
   }
 
   private static InferredType getInferredTypeForGeneric(Generic_typeContext genericTypeContext) {
@@ -110,10 +225,10 @@ public class PyTypeTypeGrammar {
 
   private static InferredType getInferredType(String fullyQualifiedName) {
     return Optional.ofNullable(getRuntimeType(fullyQualifiedName)).orElseGet(() -> {
-//      LOG.error("");
-//      LOG.error(String.format("Unresolved class symbol: %s", fullyQualifiedName));
-//      LOG.error(String.format("Filename: %s", fileName));
-//      LOG.error("");
+      //      LOG.error("");
+      //      LOG.error(String.format("Unresolved class symbol: %s", fullyQualifiedName));
+      //      LOG.error(String.format("Filename: %s", fileName));
+      //      LOG.error("");
       return InferredTypes.anyType();
     });
   }
