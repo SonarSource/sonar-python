a4c20f98abfbcc6b514de47e6ae9d3d20d8d5f2c
Ghislain Piot <ghislain.piot@sonarsource.com>
2024-03-21 09:16:32 +0100

    SONARPY-1650 S6911: tf.function should not depend on global or free python var (#1749)
    * SONARPY-1650 S6911: tf.function should not depend on global or free python var

---
+++ python-checks/src/main/java/org/sonar/python/checks/TfFunctionDependOnOutsideVariableCheck.java
@@ -0,0 +1,143 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.symbols.Usage;
+import org.sonar.plugins.python.api.tree.AssignmentStatement;
+import org.sonar.plugins.python.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.FunctionDef;
+import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.QualifiedExpression;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.checks.utils.Expressions;
+import org.sonar.python.tree.TreeUtils;
+
+@Rule(key = "S6911")
+public class TfFunctionDependOnOutsideVariableCheck extends PythonSubscriptionCheck {
+  private static final String MESSAGE = "Make sure this function does not depend on a global or free variable.";
+  private static final String MESSAGE_SECONDARY = "Variable used here.";
+
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.FUNCDEF, TfFunctionDependOnOutsideVariableCheck::checkFunction);
+  }
+
+  private static void checkFunction(SubscriptionContext context) {
+    FunctionDef functionDef = (FunctionDef) context.syntaxNode();
+
+    if (!TreeUtils.isFunctionWithGivenDecoratorFQN(functionDef, "tensorflow.function")) {
+      return;
+    }
+    NameCollector collector = new NameCollector(functionDef.localVariables());
+    functionDef.body().accept(collector);
+    if (collector.symbolToNames.keySet().isEmpty()) {
+      return;
+    }
+    var issue = context.addIssue(functionDef.name(), MESSAGE);
+    collector.symbolToNames.keySet().forEach(symbol -> collector.symbolToNames.get(symbol).forEach(name -> issue.secondary(name, MESSAGE_SECONDARY)));
+  }
+
+  private static class NameCollector extends BaseTreeVisitor {
+    Map<Symbol, List<Name>> symbolToNames = new HashMap<>();
+    Set<Symbol> localSymbols;
+
+    private NameCollector(Set<Symbol> localSymbols) {
+      this.localSymbols = localSymbols;
+    }
+
+    @Override
+    public void visitName(Name pyNameTree) {
+      if (localSymbols.contains(pyNameTree.symbol())) {
+        return;
+      }
+      if (!pyNameTree.isVariable()) {
+        return;
+      }
+      if (isInstantiatedByTensorflow(pyNameTree)) {
+        return;
+      }
+
+      Optional.ofNullable(pyNameTree.symbol())
+        .filter(symbol -> !symbol.is(Symbol.Kind.FUNCTION, Symbol.Kind.CLASS, Symbol.Kind.AMBIGUOUS))
+        .filter(symbol -> symbol.usages().stream().map(Usage::kind).noneMatch(usageKind -> usageKind == Usage.Kind.IMPORT))
+        .ifPresent(symbol -> symbolToNames.computeIfAbsent(symbol, s -> new ArrayList<>()).add(pyNameTree));
+    }
+
+    private static boolean isInstantiatedByTensorflow(Name pyNameTree) {
+      Symbol symbol = pyNameTree.symbol();
+      return symbol != null && symbol.usages().stream().filter(u -> u.kind().equals(Usage.Kind.ASSIGNMENT_LHS))
+        .map(Usage::tree)
+        .map(tree -> TreeUtils.firstAncestorOfKind(tree, Tree.Kind.ASSIGNMENT_STMT))
+        .filter(Objects::nonNull)
+        .map(AssignmentStatement.class::cast)
+        .map(AssignmentStatement::assignedValue)
+        .anyMatch(NameCollector::isTensorflowValue);
+    }
+
+    private static boolean isTensorflowValue(Expression expression) {
+      return Optional.of(expression)
+        .map(Expressions::removeParentheses)
+        .flatMap(TreeUtils.toOptionalInstanceOfMapper(CallExpression.class))
+        .map(callExpression -> NameCollector.isDirectCallToTensorflow(callExpression) || NameCollector.isChainedCallToTensorflow(callExpression))
+        .orElse(false);
+    }
+
+    private static boolean isChainedCallToTensorflow(CallExpression callExpression) {
+      return Optional.of(callExpression).map(CallExpression::callee)
+        .flatMap(TreeUtils.toOptionalInstanceOfMapper(QualifiedExpression.class))
+        .map(NameCollector::getLeftMostNameFromCallChain)
+        .map(name -> name.name().startsWith("tensorflow."))
+        .isPresent();
+    }
+
+    private static boolean isDirectCallToTensorflow(CallExpression callExpression) {
+      return Optional.of(callExpression)
+        .map(CallExpression::calleeSymbol)
+        .map(Symbol::fullyQualifiedName)
+        .filter(fqn -> fqn.startsWith("tensorflow."))
+        .isPresent();
+    }
+
+    private static Name getLeftMostNameFromCallChain(QualifiedExpression qualifiedExpression) {
+      QualifiedExpression current = qualifiedExpression;
+      while (current != null && current.qualifier().is(Tree.Kind.CALL_EXPR, Tree.Kind.QUALIFIED_EXPR)) {
+        if (current.qualifier().is(Tree.Kind.CALL_EXPR)) {
+          current = ((CallExpression) current.qualifier()).callee().is(Tree.Kind.QUALIFIED_EXPR) ? (QualifiedExpression) ((CallExpression) current.qualifier()).callee() : null;
+        } else {
+          current = (QualifiedExpression) current.qualifier();
+        }
+      }
+      return current != null && current.qualifier().is(Tree.Kind.NAME) ? (Name) current.qualifier() : null;
+    }
+  }
+}
---
+++ python-checks/src/test/resources/checks/tfFunctionDependOnOutsideVariable.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+
+foo = 3
+foobar = 42
+@tf.function
+def non_compliant(): # Noncompliant {{Make sure this function does not depend on a global or free variable.}}
+   #^^^^^^^^^^^^^
+    return foo + 1
+          #^^^< {{Variable used here.}}
+
+no_symbol = True
+def no_symbol(): ...
+@tf.function
+def non_compliant2(): # FN because we have an ambiguous symbol
+    something_else = no_symbol
+    return something_else + 1
+
+@tf.function
+def non_compliant3(): # Noncompliant
+   #^^^^^^^^^^^^^^
+    something = foo
+               #^^^< {{Variable used here.}}
+    smth = foobar
+          #^^^^^^< {{Variable used here.}}
+    return foo + foobar
+          #^^^< {{Variable used here.}}
+                #^^^^^^@-1< {{Variable used here.}}
+
+@tf.function
+def non_compliant4(foo): # Noncompliant
+   #^^^^^^^^^^^^^^
+    something = foo
+    smth = foobar
+          #^^^^^^< {{Variable used here.}}
+    return foo + foobar
+                #^^^^^^< {{Variable used here.}}
+@tf.function
+def compliant(foo):
+    return foo + 1
+
+from somewhere import something
+
+class SomeClass:
+    ...
+
+@tf.function
+def other_kind_of_name(foo):
+    def bar():
+        ...
+    some_var = bar
+    some_other_var = bar()
+    some = something()
+    some_object = SomeClass()
+    return bar(foo)
+
+def some_other_function():
+    return 42
+
+def indirect_tf_function():
+    return foo + 1 # FN : we don't recursively check if we are a tf.function
+
+@tf.function
+def direct_tf_function():
+    return indirect_tf_function()
+
+def containing():
+    foo_tf = tf.Variable(3)
+    @tf.function
+    def compliant2():
+        tenf = tf.Variable(10)
+        return foo_tf + tenf
+
+    foo_tf = True
+
+def testStandardTrainingLoopInFunction(self):
+    layer = tf.keras.layers.Dense(2)
+    dataset = (
+        tf.data.Dataset.from_tensors((tf.ones([784]), tf.ones([], tf.int32)))
+        .map(lambda x, y: (x, y))
+        .repeat(10)
+        .batch(32))
+    optimizer = tf.keras.optimizers.Adam()
+
+    @tf.function
+    def train():
+        for x, y in dataset:
+            with tf.GradientTape() as tape:
+                out = layer(x)
+                loss = tf.reduce_mean(
+                    tf.nn.sparse_softmax_cross_entropy_with_logits(
+                        logits=out, labels=y))
+            layer_variables = layer.trainable_variables
+            gradients = tape.gradient(loss, layer_variables)
+            optimizer.apply_gradients(zip(gradients, layer_variables))
+
+    train()
+
+
+baz = SomeClass.something.some_call()
+some_call()
+object = SomeClass().something()
+baz2 = [1, 5].some_call()
+baz3: tf.Variable = tf.Variable(3)
+@tf.function
+def qualifier_test(): # Noncompliant
+    return baz, some_call(), object, baz2, baz3
