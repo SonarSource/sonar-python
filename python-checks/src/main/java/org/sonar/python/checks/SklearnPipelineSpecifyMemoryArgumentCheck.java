1f742eef9b67fb616ede3e62e17948d46a066acd
Ghislain Piot <ghislain.piot@sonarsource.com>
2024-04-25 12:23:30 +0200

    SONARPY-1768 Rule S6969 : "memory" parameter should be specified for Scikit-Learn Pipeline

---
+++ python-checks/src/main/java/org/sonar/python/checks/SklearnPipelineSpecifyMemoryArgumentCheck.java
@@ -0,0 +1,101 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.checks;
+
+import java.util.Optional;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.tree.AssignmentStatement;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.QualifiedExpression;
+import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.semantic.SymbolUtils;
+import org.sonar.python.tree.TreeUtils;
+
+@Rule(key = "S6969")
+public class SklearnPipelineSpecifyMemoryArgumentCheck extends PythonSubscriptionCheck {
+
+  public static final String MESSAGE = "Specify a memory argument for the pipeline.";
+
+  @Override
+  public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Tree.Kind.CALL_EXPR, SklearnPipelineSpecifyMemoryArgumentCheck::checkName);
+  }
+
+  private static void checkName(SubscriptionContext subscriptionContext) {
+    CallExpression callExpression = (CallExpression) subscriptionContext.syntaxNode();
+    if (isPipelineCreation(callExpression)) {
+      var memoryArgument = TreeUtils.argumentByKeyword("memory", callExpression.arguments());
+
+      if (memoryArgument != null) {
+        return;
+      }
+      if (getAssignedName(callExpression).map(SklearnPipelineSpecifyMemoryArgumentCheck::isUsedInAnotherPipeline).orElse(false)) {
+        return;
+      }
+      subscriptionContext.addIssue(callExpression.callee(), MESSAGE);
+    }
+  }
+
+  private static Optional<Name> getAssignedName(Expression expression) {
+    if (expression.is(Tree.Kind.NAME)) {
+      return Optional.of((Name) expression);
+    }
+    if (expression.is(Tree.Kind.QUALIFIED_EXPR)) {
+      return Optional.ofNullable(((QualifiedExpression) expression).name());
+    }
+    var assignment = (AssignmentStatement) TreeUtils.firstAncestorOfKind(expression, Tree.Kind.ASSIGNMENT_STMT);
+    if (assignment == null) {
+      return Optional.empty();
+    }
+    var expressions = SymbolUtils.assignmentsLhs(assignment);
+    if (expressions.size() != 1) {
+      return Optional.empty();
+    }
+    return getAssignedName(expressions.get(0));
+  }
+
+  private static boolean isPipelineCreation(CallExpression callExpression) {
+    return Optional.ofNullable(callExpression.calleeSymbol()).map(Symbol::fullyQualifiedName)
+      .map(fqn -> "sklearn.pipeline.Pipeline".equals(fqn) || "sklearn.pipeline.make_pipeline".equals(fqn))
+      .orElse(false);
+  }
+
+  private static boolean isUsedInAnotherPipeline(Name name) {
+    Symbol symbol = name.symbol();
+    return symbol != null && symbol.usages().stream().filter(usage -> !usage.isBindingUsage()).anyMatch(u -> {
+      Tree tree = u.tree();
+      CallExpression callExpression = (CallExpression) TreeUtils.firstAncestorOfKind(tree, Tree.Kind.CALL_EXPR);
+      while (callExpression != null) {
+        Optional<String> fullyQualifiedName = Optional.ofNullable(callExpression.calleeSymbol()).map(Symbol::fullyQualifiedName);
+        if (fullyQualifiedName.isPresent() && isPipelineCreation(callExpression)) {
+          return true;
+        }
+        callExpression = (CallExpression) TreeUtils.firstAncestorOfKind(callExpression, Tree.Kind.CALL_EXPR);
+      }
+      return false;
+    });
+
+  }
+}
---
+++ python-checks/src/test/resources/checks/sklearn_pipeline_specify_memory_argument.py
@@ -0,0 +1,84 @@
+def compliants():
+    from sklearn.pipeline import Pipeline, make_pipeline
+
+    pipeline1 = Pipeline([
+        ('scaler', StandardScaler()),
+        ('classifier', LinearDiscriminantAnalysis())
+    ], memory=None)
+
+    pipeline2 = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis(), memory=f"some_cache_{var}")
+
+    pipeline3 = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis(), memory=get_cache_string())
+
+    from sklearn.pipeline import make_pipeline as mkpipe
+    pipeline4 = mkpipe(StandardScaler(), LinearDiscriminantAnalysis(), memory=None)
+    pipeline5 = mkpipe(StandardScaler(), LinearDiscriminantAnalysis()) # Noncompliant
+    mkpipe(StandardScaler(), LinearDiscriminantAnalysis()) # Noncompliant
+    _ = mkpipe(StandardScaler(), LinearDiscriminantAnalysis()) # Noncompliant
+
+
+def non_compliants():
+    from sklearn.pipeline import Pipeline, make_pipeline
+
+    pipeline1 = Pipeline([ # Noncompliant
+               #^^^^^^^^
+        ('scaler', StandardScaler()),
+        ('classifier', LinearDiscriminantAnalysis())
+    ])
+
+
+
+    pipeline2 = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis()) # Noncompliant
+               #^^^^^^^^^^^^^
+
+
+    pipe1 = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis()) # Compliant
+    pipe2 = make_pipeline(RobustScaler(), KNeighborsClassifier()) # Compliant
+
+    pipe = Pipeline([ # Noncompliant
+          #^^^^^^^^
+      ("feature_engineering", PolynomialFeatures(2)),
+      ("clf", VotingClassifier([pipe1, pipe2]))
+    ])
+    Pipeline([ # Noncompliant
+   #^^^^^^^^
+          ("feature_engineering", PolynomialFeatures(2)),
+          ("clf", VotingClassifier([pipe1, pipe2]))
+        ])
+
+    a, b = Pipeline([ # Noncompliant
+          #^^^^^^^^
+                 ("feature_engineering", PolynomialFeatures(2)),
+                 ("clf", VotingClassifier([pipe1, pipe2]))
+               ]), True
+
+    something.variable = Pipeline([ # Noncompliant
+                        #^^^^^^^^
+             ("feature_engineering", PolynomialFeatures(2)),
+             ("clf", VotingClassifier([pipe1, pipe2]))
+           ])
+
+    (e, f), b = True, True,  Pipeline([ # Noncompliant
+                 ("feature_engineering", PolynomialFeatures(2)),
+                 ("clf", VotingClassifier([pipe1, pipe2]))
+               ])
+
+def other():
+    from sklearn.pipeline import Pipeline, make_pipeline
+
+    pipeline1 = Pipeline([ # Noncompliant
+        ('scaler', StandardScaler()),
+        ('classifier', LinearDiscriminantAnalysis())
+    ])
+
+    if another_call(some_call(pipeline1)):
+        ...
+
+    pipeline2 = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis()) # Noncompliant
+    if pipeline2 == None:
+        ...
+
+    pipeline3 = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis()) # Noncompliant
+    def a_call(): ...
+    def b_call(): ...
+    a_call(b_call(pipeline3))
