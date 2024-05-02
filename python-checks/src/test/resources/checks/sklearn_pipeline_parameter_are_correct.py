df09afe4ea5f9b52b32d80d5cd473b9b406665f5
Ghislain Piot <ghislain.piot@sonarsource.com>
2024-05-02 15:07:59 +0200

    baby step 1

--- python-checks/src/test/resources/checks/sklearn_pipeline_parameter_are_correct.py
+++ python-checks/src/test/resources/checks/sklearn_pipeline_parameter_are_correct.py
@@ -1,7 +1,22 @@
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 
-pipe = Pipeline(steps=[("clf", SVC())])
+def aa():
+    pipe = Pipeline(steps=[("clf", SVC())])
 
-pipe.set_params(clf__F=10) # Noncompliant
-#               ^^^^^^
+    pipe.set_params(clf__F=10, clf__C=45) # Noncompliant
+    #               ^^^^^^
+
+def incorrect_pipelines():
+    p1 = Pipeline(steps=[("clf", SVC(), "a")])
+    p1.set_params(clf__C=45)
+
+def ignored_pipelines():
+    p1 = Pipeline(steps=[something(), SVC()])
+    p1.set_params(clf__C=45)
+
+    p2 = Pipeline(steps=["classif", FantasyClassifier()])
+    p2.set_params(classif__C=45)
+
+    p3 = Pipeline(steps=["classif", FantasyClassifier()])
+    p3.set_params(C=45)
