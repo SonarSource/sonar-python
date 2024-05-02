87620e59d5733c83ffdddcdc27b085606b18b28d
Ghislain Piot <ghislain.piot@sonarsource.com>
2024-05-02 16:01:01 +0200

    small fix

--- python-checks/src/test/java/org/sonar/python/checks/SklearnPipelineParameterAreCorrectCheckTest.java
+++ python-checks/src/test/java/org/sonar/python/checks/SklearnPipelineParameterAreCorrectCheckTest.java
@@ -22,11 +22,9 @@ package org.sonar.python.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.python.checks.utils.PythonCheckVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SklearnPipelineParameterAreCorrectCheckTest {
   @Test
-  void test(){
+  void test() {
     PythonCheckVerifier.verify("src/test/resources/checks/sklearn_pipeline_parameter_are_correct.py", new SklearnPipelineParameterAreCorrectCheck());
   }
 
