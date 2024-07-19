974416a1be6a7c288e1c7232d2fc7e7c8558d194
David Kunzmann <david.kunzmann@sonarsource.com>
2024-07-19 11:26:24 +0200

    Update test module file system

--- sonar-python-plugin/src/test/java/org/sonar/plugins/python/indexer/TestModuleFileSystem.java
+++ sonar-python-plugin/src/test/java/org/sonar/plugins/python/indexer/TestModuleFileSystem.java
@@ -22,27 +22,28 @@ package org.sonar.plugins.python.indexer;
 import java.util.List;
 import java.util.stream.Stream;
 import org.sonar.api.batch.fs.InputFile;
+import org.sonar.plugins.python.PythonInputFile;
 import org.sonarsource.sonarlint.plugin.api.module.file.ModuleFileSystem;
 
 public class TestModuleFileSystem implements ModuleFileSystem {
 
-  private final List<InputFile> inputFiles;
+  private final List<PythonInputFile> inputFiles;
 
-  public TestModuleFileSystem(List<InputFile> inputFiles) {
+  public TestModuleFileSystem(List<PythonInputFile> inputFiles) {
     this.inputFiles = inputFiles;
   }
 
   @Override
   public Stream<InputFile> files(String s, InputFile.Type type) {
-    return inputFiles.stream();
+    return inputFiles.stream().map(PythonInputFile::originalFile);
   }
 
   @Override
   public Stream<InputFile> files() {
-    return inputFiles.stream();
+    return inputFiles.stream().map(PythonInputFile::originalFile);
   }
 
-  public void addFile(InputFile inputFile) {
+  public void addFile(PythonInputFile inputFile) {
     inputFiles.add(inputFile);
   }
 }
