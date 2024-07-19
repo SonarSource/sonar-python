0c70072ba8bf346913e94e5de60dd0144e7d96f2
David Kunzmann <david.kunzmann@sonarsource.com>
2024-07-19 17:00:22 +0200

    SONARPY-1986: The IPythonSensor should register to .ipynb files

---
+++ its/plugin/it-python-plugin-test/src/test/java/com/sonar/python/it/plugin/NotebookPluginTest.java
@@ -0,0 +1,59 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2012-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.sonar.python.it.plugin;
+
+import com.sonar.orchestrator.build.SonarScanner;
+import com.sonar.orchestrator.junit5.OrchestratorExtension;
+import java.io.File;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.sonarqube.ws.Issues;
+
+import static com.sonar.python.it.plugin.TestsUtils.issues;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NotebookPluginTest {
+
+  private static final String PROJECT_KEY = "ipynb_json_project";
+
+  @RegisterExtension
+  public static final OrchestratorExtension ORCHESTRATOR = TestsUtils.ORCHESTRATOR;
+
+  @BeforeAll
+  static void startServer() {
+    ORCHESTRATOR.getServer().provisionProject(PROJECT_KEY, PROJECT_KEY);
+    SonarScanner build = SonarScanner.create()
+      .setProjectDir(new File("projects", PROJECT_KEY))
+      .setProjectKey(PROJECT_KEY)
+      .setProjectName(PROJECT_KEY)
+      .setProjectVersion("1.0-SNAPSHOT")
+      .setSourceDirs(".");
+    ORCHESTRATOR.executeBuild(build);
+  }
+
+  @Test
+  void test() {
+    List<Issues.Issue> issues = issues(PROJECT_KEY);
+    assertThat(issues).isEmpty();
+  }
+}
+
