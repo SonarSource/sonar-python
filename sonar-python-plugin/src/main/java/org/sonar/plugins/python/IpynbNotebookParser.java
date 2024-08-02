19084359ccd0f791738a3b182618b8dd9cfd1b4d
Ghislain Piot <ghislain.piot@sonarsource.com>
2024-08-02 09:37:04 +0200

    SONARPY-2018 Only analyze notebooks with a Python kernel (#1893)

--- sonar-python-plugin/src/main/java/org/sonar/plugins/python/IpynbNotebookParser.java
+++ sonar-python-plugin/src/main/java/org/sonar/plugins/python/IpynbNotebookParser.java
@@ -27,15 +27,19 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import org.sonar.python.IPythonLocation;
 
 public class IpynbNotebookParser {
 
   public static final String SONAR_PYTHON_NOTEBOOK_CELL_DELIMITER = "#SONAR_PYTHON_NOTEBOOK_CELL_DELIMITER";
 
-  public static GeneratedIPythonFile parseNotebook(PythonInputFile inputFile) {
+  private static final Set<String> ACCEPTED_LANGUAGE = Set.of("python", "ipython");
+
+  public static Optional<GeneratedIPythonFile> parseNotebook(PythonInputFile inputFile) {
     try {
-      return new IpynbNotebookParser(inputFile).parseNotebook();
+      return new IpynbNotebookParser(inputFile).parse();
     } catch (IOException e) {
       throw new IllegalStateException("Cannot read " + inputFile, e);
     }
@@ -53,6 +57,31 @@ public class IpynbNotebookParser {
   private int aggregatedSourceLine = 0;
   private int lastPythonLine = 0;
   private boolean isFirstCell = true;
+
+  public Optional<GeneratedIPythonFile> parse() throws IOException {
+    // If the language is not present, we assume it is a Python notebook
+    var isPythonNotebook = parseLanguage().map(ACCEPTED_LANGUAGE::contains).orElse(true);
+
+    return Boolean.TRUE.equals(isPythonNotebook) ? Optional.of(parseNotebook()) : Optional.empty();
+  }
+
+  public Optional<String> parseLanguage() throws IOException {
+    String content = inputFile.wrappedFile().contents();
+    JsonFactory factory = new JsonFactory();
+    try (JsonParser jParser = factory.createParser(content)) {
+      while (!jParser.isClosed()) {
+        JsonToken jsonToken = jParser.nextToken();
+        if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+          String fieldName = jParser.currentName();
+          if ("language".equals(fieldName)) {
+            jParser.nextToken();
+            return Optional.ofNullable(jParser.getValueAsString());
+          }
+        }
+      }
+    }
+    return Optional.empty();
+  }
 
   public GeneratedIPythonFile parseNotebook() throws IOException {
     String content = inputFile.wrappedFile().contents();
