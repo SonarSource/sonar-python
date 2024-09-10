7e39d7095f5976c71941ead189f2bd73bf1457b8
Ghislain Piot <ghislain.piot@sonarsource.com>
2024-09-10 10:56:45 +0200

    Fix test

--- python-frontend/src/test/java/org/sonar/python/types/v2/FunctionTypeTest.java
+++ python-frontend/src/test/java/org/sonar/python/types/v2/FunctionTypeTest.java
@@ -143,10 +143,9 @@ class FunctionTypeTest {
 
   @Test
   void declaredTypes() {
-    // TODO: SONARPY-1776 handle declared return type
     FunctionType functionType = functionType("def fn(p1: int): pass");
     assertThat(functionType.returnType()).isEqualTo(PythonType.UNKNOWN);
-    assertThat(functionType.parameters()).extracting(ParameterV2::declaredType).containsExactly(PythonType.UNKNOWN);
+    assertThat(functionType.parameters()).extracting(ParameterV2::declaredType).extracting(PythonType::unwrappedType).containsExactly(INT_TYPE);
   }
 
   @Test
