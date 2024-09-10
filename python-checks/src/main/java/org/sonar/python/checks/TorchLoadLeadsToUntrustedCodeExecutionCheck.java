320bbb380dabd0c71f7dd1fa74da0ea52319617b
Sebastian  Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2024-09-10 17:54:48 +0200

    SONARPY-1914 implement to not to raise when weights_only is not false

--- python-checks/src/main/java/org/sonar/python/checks/TorchLoadLeadsToUntrustedCodeExecutionCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/TorchLoadLeadsToUntrustedCodeExecutionCheck.java
@@ -19,26 +19,45 @@
  */
 package org.sonar.python.checks;
 
+import java.util.List;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
 import org.sonar.plugins.python.api.symbols.Symbol;
+import org.sonar.plugins.python.api.tree.Argument;
 import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.Name;
+import org.sonar.plugins.python.api.tree.RegularArgument;
 import org.sonar.plugins.python.api.tree.Tree;
+import org.sonar.python.tree.TreeUtils;
 
 @Rule(key = "S6985")
 public class TorchLoadLeadsToUntrustedCodeExecutionCheck extends PythonSubscriptionCheck {
 
   public static final String TORCH_LOAD = "torch.load";
   public static final String MESSAGE = "Replace this call with a safe alternative";
+  public static final String PYTHON_FALSE = "False";
+  public static final String WEIGHTS_ONLY = "weights_only";
 
   @Override
   public void initialize(Context context) {
     context.registerSyntaxNodeConsumer(Tree.Kind.CALL_EXPR, ctx -> {
       CallExpression callExpression = (CallExpression) ctx.syntaxNode();
       Symbol calleeSymbol = callExpression.calleeSymbol();
-      if(calleeSymbol != null && TORCH_LOAD.equals(calleeSymbol.fullyQualifiedName())) {
+      if (calleeSymbol != null && TORCH_LOAD.equals(calleeSymbol.fullyQualifiedName()) && !isWeightsOnlySetToTrue(callExpression.arguments())) {
         ctx.addIssue(callExpression.callee(), MESSAGE);
       }
     });
   }
+
+  private static boolean isWeightsOnlySetToTrue(List<Argument> arguments) {
+    RegularArgument weightsOnlyArg = TreeUtils.argumentByKeyword(WEIGHTS_ONLY, arguments);
+    if (weightsOnlyArg != null) {
+      Expression weightsOnlyArgExpr = weightsOnlyArg.expression();
+      return weightsOnlyArgExpr.is(Tree.Kind.NAME) && !PYTHON_FALSE.equals(((Name) weightsOnlyArgExpr).name());
+    }
+    return false;
+  }
+
+
 }
--- python-checks/src/test/resources/checks/torchLoadLeadsToUntrustedCodeExecution.py
+++ python-checks/src/test/resources/checks/torchLoadLeadsToUntrustedCodeExecution.py
@@ -8,8 +8,13 @@ model = torch.load('model.pth') # Noncompliant {{Replace this call with a safe a
 some_path = ...
 model = load(some_path) # Noncompliant
 
+torch.load(model2, 'model.pth', weights_only=False) #Noncompliant
+
 model2 = ...
 safetensors.torch.load_model(model2, 'model.pth')
 
-# tests if the rule doesn't crash if the callee symbol is null
+torch.load(model2, 'model.pth', weights_only=True)
+torch.load(model2, 'model.pth', weights_only=some_value)
+
+# test if no issue is raised if there is no symbol for the callee
 something[42]()
