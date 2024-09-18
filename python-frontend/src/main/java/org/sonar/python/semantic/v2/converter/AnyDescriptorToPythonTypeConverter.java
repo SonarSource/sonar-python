5621e77b98d372dba4d39b14ab0450eb17a032c5
Maksim Grebeniuk <122789225+maksim-grebeniuk-sonarsource@users.noreply.github.com>
2024-09-18 11:13:12 +0200

    SONARPY-2126 Implement any Descriptor to a PythonType converter infrastructure (#1968)

---
+++ python-frontend/src/main/java/org/sonar/python/semantic/v2/converter/AnyDescriptorToPythonTypeConverter.java
@@ -0,0 +1,52 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.semantic.v2.converter;
+
+import java.util.Map;
+import org.sonar.python.index.Descriptor;
+import org.sonar.python.semantic.v2.LazyTypesContext;
+import org.sonar.python.types.v2.PythonType;
+
+public class AnyDescriptorToPythonTypeConverter {
+
+  private static final DescriptorToPythonTypeConverter UNKNOWN_DESCRIPTOR_CONVERTER = new UnknownDescriptorToPythonTypeConverter();
+  private final Map<Descriptor.Kind, DescriptorToPythonTypeConverter> converters;
+  private final LazyTypesContext lazyTypesContext;
+
+  public AnyDescriptorToPythonTypeConverter(LazyTypesContext lazyTypesContext) {
+    this.lazyTypesContext = lazyTypesContext;
+    converters = Map.of();
+  }
+
+  public PythonType convert(Descriptor from) {
+    var ctx = new ConversionContext(lazyTypesContext, this::convert);
+    return convert(ctx, from);
+  }
+
+  private PythonType convert(ConversionContext ctx, Descriptor from) {
+    return converterFor(from).convert(ctx, from);
+  }
+
+  private DescriptorToPythonTypeConverter converterFor(Descriptor descriptor) {
+    return converters.getOrDefault(descriptor.kind(), UNKNOWN_DESCRIPTOR_CONVERTER);
+  }
+
+
+}
