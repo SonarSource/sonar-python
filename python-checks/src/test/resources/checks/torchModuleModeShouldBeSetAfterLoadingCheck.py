d6c2f434385300d0513e70aba9189d9a42f9fc1d
Sebastian  Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2024-09-24 10:56:06 +0200

    SONARPY-2151 fix tests

--- python-checks/src/test/resources/checks/torchModuleModeShouldBeSetAfterLoadingCheck.py
+++ python-checks/src/test/resources/checks/torchModuleModeShouldBeSetAfterLoadingCheck.py
@@ -1,45 +1,54 @@
-import torch.nn
+
 import torchvision.models as models
 
-def noncompliant():
-    model = models.vgg16()
-    model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant {{Set the module in training or evaluation mode.}}
-   #^^^^^^^^^^^^^^^^^^^^^
-    ...
+def torch_nn_imported():
+    import torch.nn
+    def noncompliant():
+        model = models.vgg16()
+        model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant {{Set the module in training or evaluation mode.}}
+       #^^^^^^^^^^^^^^^^^^^^^
+        ...
 
-class CustomModule(torch.nn.Module):
-    pass
+    class CustomModule(torch.nn.Module):
+        pass
 
-def noncompliant():
-    model = CustomModule()
-    model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant
-    ...
+    def noncompliant():
+        model = CustomModule()
+        model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant
+        ...
 
-def noncompliant():
-    model = models.vgg16()
-    model.train()
-    model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant
+    def noncompliant():
+        model = models.vgg16()
+        model.train()
+        model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant
 
-def noncompliant():
-    model = models.vgg16()
-    model.load_state_dict(torch.load('model_weights.pth'))
-    model.train()
+    def noncompliant():
+        model = models.vgg16()
+        model.load_state_dict(torch.load('model_weights.pth'))
+        model.train()
 
-    model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant
+        model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant
 
-    other_model = model
+        other_model = model
 
-def compliant(model):
-    model.load_state_dict(torch.load('model_weights.pth'))
-
-
-
-def compliant():
-    model3 = models.vgg16()
-    model3.load_state_dict(torch.load('model_weights.pth')) # Ok if model is passed as argument to a function do not raise at all train or eval could be called in such functions
-    foo(model3)
+    def compliant(model):
+        model.load_state_dict(torch.load('model_weights.pth'))
 
 
 
+    def compliant():
+        model3 = models.vgg16()
+        model3.load_state_dict(torch.load('model_weights.pth')) # Ok if model is passed as argument to a function do not raise at all train or eval could be called in such functions
+        foo(model3)
 
 
+def torch_imported():
+    import torch
+
+    class CustomModule(torch.nn.Module):
+        pass
+
+    def noncompliant():
+        model = CustomModule()
+        model.load_state_dict(torch.load('model_weights.pth')) # Noncompliant
+        ...
