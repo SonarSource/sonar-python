88296eb4eb717e6b5ed86fcf91519f2e84945004
Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>
2024-09-25 14:16:42 +0200

    SONARPY-2157 Implement OverloadedFunctionSymbolToDescriptorConverter

---
+++ python-frontend/src/test/java/org/sonar/python/semantic/v2/typeshed/OverloadedFunctionSymbolToDescriptorConverterTest.java
@@ -0,0 +1,91 @@
+/*
+ * SonarQube Python Plugin
+ * Copyright (C) 2011-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.python.semantic.v2.typeshed;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.sonar.python.index.Descriptor;
+import org.sonar.python.index.FunctionDescriptor;
+import org.sonar.python.types.protobuf.SymbolsProtos;
+
+class OverloadedFunctionSymbolToDescriptorConverterTest {
+
+
+  @Test
+  void test() {
+    var symbol = SymbolsProtos.OverloadedFunctionSymbol.newBuilder()
+      .setName("something")
+      .setFullname("module.something")
+      .addDefinitions(SymbolsProtos.FunctionSymbol.newBuilder()
+        .setName("f1")
+        .setIsStatic(true)
+        .build())
+      .addDefinitions(SymbolsProtos.FunctionSymbol.newBuilder()
+        .setName("f2")
+        .build())
+      .build();
+    var converter = new OverloadedFunctionSymbolToDescriptorConverter(new FunctionSymbolToDescriptorConverter());
+
+    var descriptor = converter.convert(symbol, true);
+    Assertions.assertThat(descriptor.name()).isEqualTo("something");
+    Assertions.assertThat(descriptor.fullyQualifiedName()).isEqualTo("module.something");
+    Assertions.assertThat(descriptor.alternatives()).hasSize(2);
+
+    var candidatesByName = descriptor.alternatives()
+      .stream()
+      .collect(Collectors.toMap(Descriptor::name, Function.identity()));
+
+    var f1 = (FunctionDescriptor) candidatesByName.get("f1");
+    var f2 = (FunctionDescriptor) candidatesByName.get("f2");
+    Assertions.assertThat(f1.isInstanceMethod()).isFalse();
+    Assertions.assertThat(f2.isInstanceMethod()).isTrue();
+  }
+
+  @Test
+  void builtinTest() {
+    var symbol = SymbolsProtos.OverloadedFunctionSymbol.newBuilder()
+      .setName("int")
+      .setFullname("builtins.int")
+      .addDefinitions(SymbolsProtos.FunctionSymbol.newBuilder().build())
+      .addDefinitions(SymbolsProtos.FunctionSymbol.newBuilder().build())
+      .build();
+    var converter = new OverloadedFunctionSymbolToDescriptorConverter(new FunctionSymbolToDescriptorConverter());
+
+    var descriptor = converter.convert(symbol);
+    Assertions.assertThat(descriptor.name()).isEqualTo("int");
+    Assertions.assertThat(descriptor.fullyQualifiedName()).isEqualTo("int");
+    Assertions.assertThat(descriptor.alternatives()).hasSize(2);
+  }
+
+  @Test
+  void singleCandidateExceptionTest() {
+    var symbol = SymbolsProtos.OverloadedFunctionSymbol.newBuilder()
+      .addDefinitions(SymbolsProtos.FunctionSymbol.newBuilder().build())
+      .build();
+    var converter = new OverloadedFunctionSymbolToDescriptorConverter(new FunctionSymbolToDescriptorConverter());
+
+    Assertions.assertThatThrownBy(() -> converter.convert(symbol))
+      .isInstanceOf(IllegalStateException.class)
+      .hasMessage("Overloaded function symbols should have at least two definitions.");
+  }
+
+}
