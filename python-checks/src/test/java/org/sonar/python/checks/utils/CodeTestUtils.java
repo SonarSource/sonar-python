acd7438666d160551a7a39f3b156a2925a4934e7
David Kunzmann <david.kunzmann@sonarsource.com>
2024-09-26 15:39:14 +0200

    SONARPY-2164: Update sonar-plugin-api to version 10.11.0.2468 (#2010)

--- python-checks/src/test/java/org/sonar/python/checks/utils/CodeTestUtils.java
+++ python-checks/src/test/java/org/sonar/python/checks/utils/CodeTestUtils.java
@@ -19,11 +19,9 @@
  */
 package org.sonar.python.checks.utils;
 
-import org.sonar.api.internal.apachecommons.lang.StringUtils;
-
 public class CodeTestUtils {
 
   public static String code(String... lines) {
-    return StringUtils.join(lines, '\n');
+    return String.join("\n", lines);
   }
 }
