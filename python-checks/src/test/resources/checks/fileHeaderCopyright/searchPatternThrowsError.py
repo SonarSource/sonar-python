e70142217db3e2c8fbbac936909d014d339c9bbb
Sebastian  Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2024-10-08 11:50:58 +0200

    SONARPY-2084 update comment in test file

--- python-checks/src/test/resources/checks/fileHeaderCopyright/searchPatternThrowsError.py
+++ python-checks/src/test/resources/checks/fileHeaderCopyright/searchPatternThrowsError.py
@@ -1,6 +1,6 @@
 # Copyright 2020
 # Another comment
 # All rights reserved.
-# Noncompliant@-4 {{}}
+# This file is used to check that the search pattern is correctly used to find the header of the file.
 def foo():
     pass
