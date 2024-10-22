1067b185eb4c8a14a2e9635ed0a005d79ff1eec8
Ghislain Piot <ghislain.piot@sonarsource.com>
2024-10-22 07:36:29 +0000

    SONARPY-2216 Type of aliased ImportFrom is not propagated (#2079)
    Co-authored-by: Sebastian Zumbrunn <sebastian.zumbrunn@sonarsource.com>

--- python-frontend/src/test/java/org/sonar/python/types/v2/UnresolvedImportTypeTest.java
+++ python-frontend/src/test/java/org/sonar/python/types/v2/UnresolvedImportTypeTest.java
@@ -89,7 +89,6 @@ class UnresolvedImportTypeTest {
   }
 
   @Test
-  @Disabled("SONARPY-2216 Type of alias from import is not propagated")
   void import_from_as() {
     FileInput fileInput = inferTypesWithNoResolution("""
       from something import a_func as f
@@ -111,6 +110,85 @@ class UnresolvedImportTypeTest {
       """);
     var xType = ((ExpressionStatement) fileInput.statements().statements().get(2)).expressions().get(0).typeV2();
     assertThat(xType).isInstanceOf(UnknownType.class);
+  }
+
+  @Test
+  void all_imported_names_resolved() {
+    FileInput fileInput = inferTypesWithNoResolution("""
+      from a import b, c as cc
+      a
+      b
+      c
+      cc
+      """);
+    var aType = (((ExpressionStatement) fileInput.statements().statements().get(1)).expressions().get(0).typeV2());
+    var bType = ((UnresolvedImportType) ((ExpressionStatement) fileInput.statements().statements().get(2)).expressions().get(0).typeV2());
+    var cType = (((ExpressionStatement) fileInput.statements().statements().get(3)).expressions().get(0).typeV2());
+    var ccType = ((UnresolvedImportType) ((ExpressionStatement) fileInput.statements().statements().get(4)).expressions().get(0).typeV2());
+
+    assertThat(aType).isInstanceOf(UnknownType.class);
+    assertThat(bType.importPath()).isEqualTo("a.b");
+    assertThat(cType).isInstanceOf(UnknownType.class);
+    assertThat(ccType.importPath()).isEqualTo("a.c");
+  }
+
+  @Test
+  void all_imported_name_resolved_2() {
+    FileInput fileInput = inferTypesWithNoResolution("""
+      import a.b as c, d.e
+      a
+      b
+      c
+      d
+      e
+      """);
+    var aType = (((ExpressionStatement) fileInput.statements().statements().get(1)).expressions().get(0).typeV2());
+    var bType = (((ExpressionStatement) fileInput.statements().statements().get(2)).expressions().get(0).typeV2());
+    var cType = ((UnresolvedImportType) ((ExpressionStatement) fileInput.statements().statements().get(3)).expressions().get(0).typeV2());
+    var dType = (((ExpressionStatement) fileInput.statements().statements().get(4)).expressions().get(0).typeV2());
+    var eType = (((ExpressionStatement) fileInput.statements().statements().get(5)).expressions().get(0).typeV2());
+
+    assertThat(aType).isInstanceOf(UnknownType.class);
+    assertThat(bType).isInstanceOf(UnknownType.class);
+    assertThat(cType.importPath()).isEqualTo("a.b");
+    assertThat(dType).isInstanceOfSatisfying(UnresolvedImportType.class, t -> assertThat(t.importPath()).isEqualTo("d"));
+    assertThat(eType).isInstanceOf(UnknownType.class);
+  }
+
+  @Test
+  void import_try_except() {
+    FileInput fileInput = parseAndInferTypes("""
+      i = 3
+      try:
+        import a as i
+      except ImportError:
+        import b as i
+      i
+      """);
+    var iType = (UnionType) (((ExpressionStatement) fileInput.statements().statements().get(2)).expressions().get(0).typeV2());
+    assertThat(iType.candidates()).containsExactlyInAnyOrder(
+      new UnresolvedImportType("a"),
+      new UnresolvedImportType("b"),
+      new ObjectType(new SimpleTypeWrapper(TypesTestUtils.INT_TYPE))
+    );
+  }
+
+  @Test
+  void import_from_try_except() {
+    FileInput fileInput = parseAndInferTypes("""
+      i = 3
+      try:
+        from a import aa as i
+      except ImportError:
+        from b import bb as i
+      i
+      """);
+    var iType = (UnionType) (((ExpressionStatement) fileInput.statements().statements().get(2)).expressions().get(0).typeV2());
+    assertThat(iType.candidates()).containsExactlyInAnyOrder(
+      new UnresolvedImportType("a.aa"),
+      new UnresolvedImportType("b.bb"),
+      new ObjectType(new SimpleTypeWrapper(TypesTestUtils.INT_TYPE))
+    );
   }
 
   private static FileInput inferTypesWithNoResolution(String lines) {
