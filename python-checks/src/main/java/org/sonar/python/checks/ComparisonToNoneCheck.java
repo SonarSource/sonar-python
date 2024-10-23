62d882159210b81f2c1d878b7c758dd416591079
Sebastian  Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2024-10-23 11:23:05 +0200

    SONARPY-2202 Migrate S5727 ComparisonToNoneCheck to the new type model

--- python-checks/src/main/java/org/sonar/python/checks/ComparisonToNoneCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/ComparisonToNoneCheck.java
@@ -27,55 +27,113 @@ import org.sonar.plugins.python.api.tree.IsExpression;
 import org.sonar.plugins.python.api.tree.Tree;
 import org.sonar.plugins.python.api.tree.Tree.Kind;
 import org.sonar.plugins.python.api.types.BuiltinTypes;
-import org.sonar.plugins.python.api.types.InferredType;
-
-import static org.sonar.python.checks.utils.CheckUtils.isNone;
+import org.sonar.python.types.v2.FunctionType;
+import org.sonar.python.types.v2.PythonType;
+import org.sonar.python.types.v2.TriBool;
+import org.sonar.python.types.v2.TypeCheckBuilder;
+import org.sonar.python.types.v2.TypeChecker;
+import org.sonar.python.types.v2.TypeUtils;
 
 @Rule(key = "S5727")
 public class ComparisonToNoneCheck extends PythonSubscriptionCheck {
+  private TypeCheckBuilder canBeNoneTypeCheck;
+  private TypeCheckBuilder isNoneTypeCheck;
+  private TypeCheckBuilder isObjectTypeCheck;
+  private TypeCheckBuilder isExactTypeSource;
+  private TypeChecker typeChecker;
 
   @Override
   public void initialize(Context context) {
+    context.registerSyntaxNodeConsumer(Kind.FILE_INPUT, ctx -> {
+      typeChecker = ctx.typeChecker();
+      canBeNoneTypeCheck = ctx.typeChecker().typeCheckBuilder().isExactTypeSource().canBeBuiltinWithName(BuiltinTypes.NONE_TYPE);
+      isNoneTypeCheck = ctx.typeChecker().typeCheckBuilder().isExactTypeSource().isBuiltinWithName(BuiltinTypes.NONE_TYPE);
+      isObjectTypeCheck = ctx.typeChecker().typeCheckBuilder().isBuiltinWithName(BuiltinTypes.OBJECT_TYPE);
+      isExactTypeSource = ctx.typeChecker().typeCheckBuilder().isExactTypeSource();
+    });
+
     context.registerSyntaxNodeConsumer(Kind.IS, ctx -> checkIdentityComparison(ctx, (IsExpression) ctx.syntaxNode()));
     context.registerSyntaxNodeConsumer(Kind.COMPARISON, ctx -> checkEqualityComparison(ctx, (BinaryExpression) ctx.syntaxNode()));
   }
 
-  private static void checkEqualityComparison(SubscriptionContext ctx, BinaryExpression comparison) {
+  private void checkEqualityComparison(SubscriptionContext ctx, BinaryExpression comparison) {
     String operator = comparison.operator().value();
     if (!"==".equals(operator) && !"!=".equals(operator)) {
       return;
     }
-    InferredType left = comparison.leftOperand().type();
-    InferredType right = comparison.rightOperand().type();
-    if (isNone(left) && isNone(right)) {
+    PythonType left = comparison.leftOperand().typeV2();
+    PythonType right = comparison.rightOperand().typeV2();
+    if (isTypeHint(left) || isTypeHint(right)) {
+      return;
+    }
+    if (isStubProbablyWrong(left) || isStubProbablyWrong(right)) {
+      return;
+    }
+    if (isPropertyDecoratedFunction(left) || isPropertyDecoratedFunction(right)) {
+      return;
+    }
+    if (isNoneCheckAlwaysTrue(left, right)) {
       addIssue(ctx, comparison, operator + " comparison", "==".equals(operator));
-    } else if ((isNone(left) && cannotBeNone(right)) || (cannotBeNone(left) && isNone(right))) {
+    } else if (isNoneCheckImpossible(left, right)) {
       addIssue(ctx, comparison, operator + " comparison", "!=".equals(operator));
     }
   }
 
-  private static void checkIdentityComparison(SubscriptionContext ctx, IsExpression comparison) {
-    InferredType left = comparison.leftOperand().type();
-    InferredType right = comparison.rightOperand().type();
-    // `isObject` Removes FP when the return type of a function is an object
-    if (!left.isIdentityComparableWith(right) && (isNone(left) || isNone(right)) && !(isObject(left) || isObject(right))) {
+  private void checkIdentityComparison(SubscriptionContext ctx, IsExpression comparison) {
+    PythonType left = comparison.leftOperand().typeV2();
+    PythonType right = comparison.rightOperand().typeV2();
+    if (isStubProbablyWrong(left) || isStubProbablyWrong(right)) {
+      return;
+    }
+    if (isTypeHint(left) || isTypeHint(right)) {
+      return;
+    }
+    if (isPropertyDecoratedFunction(left) || isPropertyDecoratedFunction(right)) {
+      return;
+    }
+    if (isNotIdentityComparableWith(left, right) && isNoneCheckImpossible(left, right)) {
       addIssue(ctx, comparison, "identity check", comparison.notToken() != null);
-    } else if (isNone(left) && isNone(right)) {
+    } else if (isNoneCheckAlwaysTrue(left, right)) {
       addIssue(ctx, comparison, "identity check", comparison.notToken() == null);
     }
+  }
+
+  private boolean isNoneCheckImpossible(PythonType left, PythonType right) {
+    return (isNone(left) && cannotBeNone(right)) || (cannotBeNone(left) && isNone(right));
+  }
+
+  private boolean isNoneCheckAlwaysTrue(PythonType left, PythonType right) {
+    return isNone(left) && isNone(right);
+  }
+
+  private boolean cannotBeNone(PythonType type) {
+    return canBeNoneTypeCheck.check(TypeUtils.unwrapType(type)) == TriBool.FALSE;
+  }
+
+  private boolean isNone(PythonType type) {
+    return isNoneTypeCheck.check(TypeUtils.unwrapType(type)) == TriBool.TRUE;
+  }
+
+  private boolean isNotIdentityComparableWith(PythonType left, PythonType right) {
+    return typeChecker.typeCheckBuilder().isIdentityComparableWith(left).check(right) == TriBool.FALSE;
+  }
+
+  private boolean isStubProbablyWrong(PythonType type) {
+    // we assume that if a stub returns an object that its type is probably not correct and the author probably meant writing `Any`
+    return isObjectTypeCheck.check(type) == TriBool.TRUE;
+  }
+
+  private boolean isTypeHint(PythonType type) {
+    return isExactTypeSource.check(type) == TriBool.FALSE;
+  }
+
+  private static boolean isPropertyDecoratedFunction(PythonType type) {
+    //TODO SONAR-1772 actually detect decorated functions (or remove check entirely if type is correctly resolved)
+    return type instanceof FunctionType;
   }
 
   private static void addIssue(SubscriptionContext ctx, Tree tree, String comparisonKind, boolean result) {
     String resultAsString = result ? "True" : "False";
     ctx.addIssue(tree, String.format("Remove this %s; it will always be %s.", comparisonKind, resultAsString));
   }
-
-  private static boolean cannotBeNone(InferredType type) {
-    return !type.canBeOrExtend(BuiltinTypes.NONE_TYPE) && !isObject(type);
-  }
-
-  private static boolean isObject(InferredType type) {
-    return type.canOnlyBe(BuiltinTypes.OBJECT_TYPE);
-  }
-
 }
--- python-checks/src/test/resources/checks/comparisonToNoneCheck.py
+++ python-checks/src/test/resources/checks/comparisonToNoneCheck.py
@@ -50,3 +50,154 @@ def equality_check(param):
     if obj == None: pass # FN
     if None == obj: pass # FN
 
+def both_can_be_none(cond1, cond2):
+    if cond1:
+        a = 42
+    else:
+        a = None
+
+    if cond2:
+        b = True
+    else:
+        b = None
+
+    if a == b:
+        pass
+
+    if a is b:
+        pass
+
+def type_hints_are_skiped(a: int):
+    if a is None: pass
+    if None is a: pass
+    if a == None: pass
+    if None == a: pass
+
+def next_in_loop(list_param):
+    while True:
+        p = next(list_param)
+        if p is None: # Compliant
+            break
+
+def assert_is_none():
+    import re
+    _re_prop_name = re.compile(r"^[a-zA-Z][a-zA-Z0-9_]*$")
+    assert _re_prop_name.match("_test") is None # Compliant
+
+def no_issue_on_declared_type():
+    class SomeClass:
+        test = 3
+
+    def test_method(a0: SomeClass) -> None:
+        if a0 is None:
+            return
+        if a0.test is None:
+            return
+
+def getattr(self):
+    func = getattr(self, "eventReceived_", None)
+    if func == None: # Compliant
+        pass
+
+# TODO: SONARPY-2236 because @property are not handled, the following code will raise FPs
+def class_property(arr):
+    class A:
+        @property
+        def always_none(self):
+            return None
+
+        @property
+        def never_none(self):
+            return True
+
+        @property
+        def maybe_none(self, a):
+            if a:
+                return True
+            return None
+    a = A()
+
+    if a.always_none is None:
+        pass
+    if a.never_none is None:
+        pass
+    if a.maybe_none is None:
+        pass
+
+    if None is a.always_none:
+        pass
+    if None is a.never_none:
+        pass
+    if None is a.maybe_none:
+        pass
+
+    if a.always_none == None:
+        pass
+    if a.never_none == None:
+        pass
+    if a.maybe_none == None:
+        pass
+    if None == a.always_none:
+        pass
+    if None == a.never_none:
+        pass
+    if None == a.maybe_none:
+        pass
+
+    v = memoryview(arr)
+    if v.shape is None:
+        pass
+
+    @unittest.skipIf(arr is None)
+    def some_func():
+        return None
+
+def reference_to_unkown_variable():
+    if input is None:
+        pass
+
+    if some_unkown_variable == None:
+        pass
+
+def checks_with_classes(arr):
+    from not_found_module import NotFoundClass
+    class A(): pass
+    if A() is None: #Noncompliant
+        pass
+    if A() == None: #Noncompliant
+        pass
+
+    if memoryview(arr) is None: #Noncompliant
+        pass
+
+    if memoryview(arr) == None: #Noncompliant
+        pass
+
+    if NotFoundClass() is None:
+        pass
+
+    @some_decorator()
+    class DecoratedClass:
+        pass
+
+    decorated_class = DecoratedClass()
+    if decorated_class is None: #Noncompliant
+        pass
+
+    if decorated_class == None: #Noncompliant
+        pass
+
+def use_after_class_declaration():
+    class A: pass
+    def method():
+        a = A()
+        if a is None: #Noncompliant
+            pass
+
+def if_in_class():
+    class A: pass
+    class B:
+        if A() is None: #Noncompliant
+            some_field = "test"
+        if A() == None: #Noncompliant
+            some_other_field = "test"
