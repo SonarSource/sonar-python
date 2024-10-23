444042950d3d2677c52affcefa999df275d1a82a
Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>
2024-10-23 11:51:58 +0200

    SONARPY-2219 get rid of TypeInferenceV2.typesBySymbol field

--- python-frontend/src/main/java/org/sonar/python/semantic/v2/TypeInferenceV2.java
+++ python-frontend/src/main/java/org/sonar/python/semantic/v2/TypeInferenceV2.java
@@ -21,7 +21,6 @@ package org.sonar.python.semantic.v2;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -58,11 +57,11 @@ public class TypeInferenceV2 {
     this.pythonFile = pythonFile;
   }
 
-  public void inferTypes(FileInput fileInput) {
+  public Map<SymbolV2, Set<PythonType>> inferTypes(FileInput fileInput) {
     TrivialTypeInferenceVisitor trivialTypeInferenceVisitor = new TrivialTypeInferenceVisitor(projectLevelTypeTable, pythonFile);
     fileInput.accept(trivialTypeInferenceVisitor);
 
-    inferTypesAndMemberAccessSymbols(fileInput);
+    var typesBySymbol = inferTypesAndMemberAccessSymbols(fileInput);
 
     fileInput.accept(new BaseTreeVisitor() {
       @Override
@@ -71,20 +70,17 @@ public class TypeInferenceV2 {
         inferTypesAndMemberAccessSymbols(funcDef);
       }
     });
-  }
-
-  public Map<SymbolV2, Set<PythonType>> getTypesBySymbol() {
     return typesBySymbol;
   }
 
-  private void inferTypesAndMemberAccessSymbols(FileInput fileInput) {
+  private Map<SymbolV2, Set<PythonType>> inferTypesAndMemberAccessSymbols(FileInput fileInput) {
     StatementList statements = fileInput.statements();
     if (statements == null) {
-      return;
+      return Map.of();
     }
     var moduleSymbols = symbolTable.getSymbolsByRootTree(fileInput);
 
-    typesBySymbol = inferTypesAndMemberAccessSymbols(
+    return inferTypesAndMemberAccessSymbols(
       fileInput,
       statements,
       moduleSymbols,
--- python-frontend/src/test/java/org/sonar/python/semantic/v2/TypeInferenceV2Test.java
+++ python-frontend/src/test/java/org/sonar/python/semantic/v2/TypeInferenceV2Test.java
@@ -2670,8 +2670,7 @@ public class TypeInferenceV2Test {
     FileInput root = parse(lines);
     var symbolTable = new SymbolTableBuilderV2(root).build();
     var typeInferenceV2 = new TypeInferenceV2(PROJECT_LEVEL_TYPE_TABLE, pythonFile, symbolTable);
-    typeInferenceV2.inferTypes(root);
-    return typeInferenceV2.getTypesBySymbol();
+    return typeInferenceV2.inferTypes(root);
   }
 
   private static FileInput inferTypes(String lines) {
