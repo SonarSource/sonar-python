049c359eb21025eff6ea3495c6e04b074de3aea8
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2024-10-30 10:25:49 +0100

    SONARPY-2276 Add expectations on S930 to avoid FPs on bound methods

--- python-checks/src/test/resources/checks/argumentNumberWithImport.py
+++ python-checks/src/test/resources/checks/argumentNumberWithImport.py
@@ -1,5 +1,7 @@
 from argumentNumberImported import fn, A
 import argumentNumberImported
+from argumentNumberImported import reassigned_unbound_meth as aliased_unbound
+from argumentNumberImported import reassigned_bound_meth as aliased_bound
 
 fn(1, 2) # Noncompliant
 fn(1) # OK
@@ -12,3 +14,26 @@ def stub_functions():
     a.meth(42)
     a.meth(42, 43) # Noncompliant
     argumentNumberImported.fn(1)
+
+
+def calling_reassigned_imported_valid_usages():
+    # SONARPY-2285: Bound and unbound methods are currently not exported
+    argumentNumberImported.reassigned_unbound_meth(1, 2)
+    argumentNumberImported.reassigned_bound_meth(1)
+
+    aliased_unbound(1, 2)
+    aliased_bound(1)
+
+def calling_reassigned_imported_noncompliant():
+    # SONARPY-2285: Bound and unbound methods are currently not exported
+    argumentNumberImported.reassigned_unbound_meth() # FN SONARPY-2285
+    argumentNumberImported.reassigned_unbound_meth(42, 43) # FN SONARPY-2285
+    argumentNumberImported.reassigned_bound_meth() # FN SONARPY-2285
+    argumentNumberImported.reassigned_bound_meth(42, 43) # FN SONARPY-2285
+
+    aliased_unbound() # FN SONARPY-2285
+    aliased_unbound(1, 2, 3) # FN SONARPY-2285
+
+    aliased_bound() # FN SONARPY-2285
+    aliased_bound(42, 43) # FN SONARPY-2285
+
