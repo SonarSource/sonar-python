db907a13a62503812a7689a05ca40ae704493b73
Thomas Serre <118730793+thomas-serre-sonarsource@users.noreply.github.com>
2024-11-06 09:03:44 +0100

    SONARPY-2255 add tests for S5756 on aliased classes (#2129)
    Co-authored-by: Guillaume Dequenne <guillaume.dequenne@sonarsource.com>

--- python-checks/src/test/resources/checks/nonCallableCalledImporter.py
+++ python-checks/src/test/resources/checks/nonCallableCalledImporter.py
@@ -1,10 +1,12 @@
 from nonCallableCalledImported import MyNonCallableClass, MyCallableClass
-
+import nonCallableCalledImported
 
 imported_non_callable = MyNonCallableClass()
+imported_non_callable_alias = nonCallableCalledImported.MyNonCallableClassAlias()
 imported_callable = MyCallableClass()
 
 imported_non_callable()  # Noncompliant
+imported_non_callable_alias()  # FN : this is non callable
 imported_callable()
 
 
