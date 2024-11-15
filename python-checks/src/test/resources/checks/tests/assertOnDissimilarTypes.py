05d7b5ca8c55da16c4718afdc9fb11e603842822
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2024-11-15 10:10:14 +0100

    SONARPY-2328 Document FP on S5845 when comparing enum objects with types (#2154)
    Co-authored-by: Thomas Serre <118730793+thomas-serre-sonarsource@users.noreply.github.com>

--- python-checks/src/test/resources/checks/tests/assertOnDissimilarTypes.py
+++ python-checks/src/test/resources/checks/tests/assertOnDissimilarTypes.py
@@ -182,3 +182,25 @@ class AmbiguousSymbolsNoType(unittest.TestCase):
                     pass
 
             self.assertEqual(ClassWithMultipleDefinitions(1), 1)  # OK, ambiguous type
+
+
+class MyClass:
+    ...
+
+class ComparingTypeAndClass(unittest.TestCase):
+  def test_type_of_instance_and_class_object(self):
+    inst = MyClass()
+    self.assertIs(type(inst), MyClass)
+
+
+from enum import Enum
+
+class Flag(Enum):
+    ...
+
+class ComparingTypeAndEnum(unittest.TestCase):
+    def test_type_of_enum_and_enum_class(self):
+        FlagType = Flag(...)
+        flag = FlagType(...)
+        # FP SONARPY-2332: FlagType is a proper "type" object as an instance of "Flag", due to it being an Enum
+        self.assertIs(type(flag), FlagType)  # Noncompliant
