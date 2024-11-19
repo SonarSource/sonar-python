1df1fc54ad9caa9c52125e758ad6cad67d1b0db8
Thomas Serre <118730793+thomas-serre-sonarsource@users.noreply.github.com>
2024-11-19 17:25:41 +0100

    SONARPY-2345 Document new S1172 FN when super class is ambiguous (#2164)

--- python-checks/src/test/resources/checks/unusedFunctionParameter/unusedFunctionParameterImport.py
+++ python-checks/src/test/resources/checks/unusedFunctionParameter/unusedFunctionParameterImport.py
@@ -1,7 +1,8 @@
-from unusedFunctionParameterImported import ImportedParent
+from unusedFunctionParameterImported import ImportedParent, ParentWithDuplicatedParent
+
 class ChildFromImported(ImportedParent):
 
-    # SONARPY-2327 `method_defined_in_child_class_only` is not considered a member of ImportedParent class, thus S1172 is raised
+    # SONARPY-1829 `method_defined_in_child_class_only` is not considered a member of ImportedParent class, thus S1172 is raised
     def method_defined_in_child_class_only(self, a): # Noncompliant
         #                                        ^
         return compute()
@@ -9,3 +10,9 @@ class ChildFromImported(ImportedParent):
     def method_defined_in_child_class_only_and_not_used(self, a): # Noncompliant
         #                                                     ^
         return compute()
+
+class ChildFromDuplicated(ParentWithDuplicatedParent):
+
+    def do_something(self, a): # FN SONARPY-1829 ChildFromDuplicated has an unresolved type hierarchy, because of the duplicated parent classes
+        return compute()
+
