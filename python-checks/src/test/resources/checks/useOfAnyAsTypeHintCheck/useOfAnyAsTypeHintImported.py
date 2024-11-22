478bd09aea6e0284f7e8d12abd050e5fa9fcae73
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2024-11-22 09:38:56 +0100

    Document no issue on S6542 when inheriting from annotated class member

--- python-checks/src/test/resources/checks/useOfAnyAsTypeHintCheck/useOfAnyAsTypeHintImported.py
+++ python-checks/src/test/resources/checks/useOfAnyAsTypeHintCheck/useOfAnyAsTypeHintImported.py
@@ -3,3 +3,7 @@ from abc import ABCMeta
 class ImportedParentWithoutMetaClass: ...
 
 class ImportedParentWithMetaClass(metaclass=ABCMeta): ...
+
+from typing import Callable
+class MyClassWithAnnotatedMember:
+    my_member: Callable[[Any],Any] # No issue on nested values of "Callable"
