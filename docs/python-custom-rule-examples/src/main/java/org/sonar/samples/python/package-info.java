2150d0261883f0662f5fb8e529989d77cf7cd3b3
Thomas Serre <118730793+thomas-serre-sonarsource@users.noreply.github.com>
2024-11-27 14:50:00 +0100

    SONARPY-2371 Move the custom rule examples to sonar-python repository (#2183)
    Co-authored-by: Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>

---
+++ docs/python-custom-rule-examples/src/main/java/org/sonar/samples/python/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2011-2024 SonarSource SA - mailto:info AT sonarsource DOT com
+ * This code is released under [MIT No Attribution](https://opensource.org/licenses/MIT-0) license.
+ */
+@ParametersAreNonnullByDefault
+package org.sonar.samples.python;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
