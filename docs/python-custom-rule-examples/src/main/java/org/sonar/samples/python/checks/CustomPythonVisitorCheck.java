19ab00021d468ce7860e5359205e6913e251d7f4
Thomas Serre <118730793+thomas-serre-sonarsource@users.noreply.github.com>
2024-11-27 14:50:00 +0100

    SONARPY-2371 Move the custom rule examples to sonar-python repository (#2183)
    Co-authored-by: Maksim Grebeniuk <maksim.grebeniuk@sonarsource.com>

---
+++ docs/python-custom-rule-examples/src/main/java/org/sonar/samples/python/checks/CustomPythonVisitorCheck.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2011-2024 SonarSource SA - mailto:info AT sonarsource DOT com
+ * This code is released under [MIT No Attribution](https://opensource.org/licenses/MIT-0) license.
+ */
+package org.sonar.samples.python.checks;
+
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonVisitorCheck;
+import org.sonar.plugins.python.api.tree.FunctionDef;
+
+@Rule(
+  key = CustomPythonVisitorCheck.RULE_KEY,
+  priority = Priority.MINOR,
+  name = "Python visitor check",
+  description = "desc")
+public class CustomPythonVisitorCheck extends PythonVisitorCheck {
+
+  public static final String RULE_KEY = "visitor";
+
+  @Override
+  public void visitFunctionDef(FunctionDef pyFunctionDefTree) {
+    addIssue(pyFunctionDefTree.name(), "Function def.");
+    super.visitFunctionDef(pyFunctionDefTree);
+  }
+
+}
---
+++ docs/python-custom-rule-examples/src/test/java/org/sonar/samples/python/CustomPythonRuleRepositoryTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2011-2024 SonarSource SA - mailto:info AT sonarsource DOT com
+ * This code is released under [MIT No Attribution](https://opensource.org/licenses/MIT-0) license.
+ */
+package org.sonar.samples.python;
+
+import java.io.IOException;
+import java.net.URL;
+import org.junit.Test;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CustomPythonRuleRepositoryTest {
+
+  @Test
+  public void test_rule_repository() {
+    CustomPythonRuleRepository customPythonRuleRepository = new CustomPythonRuleRepository();
+    RulesDefinition.Context context = new RulesDefinition.Context();
+    customPythonRuleRepository.define(context);
+    assertThat(customPythonRuleRepository.repositoryKey()).isEqualTo("python-custom-rules");
+    assertThat(context.repositories()).hasSize(1).extracting("key").containsExactly(customPythonRuleRepository.repositoryKey());
+    assertThat(context.repositories().get(0).rules()).hasSize(2);
+    assertThat(customPythonRuleRepository.checkClasses()).hasSize(2);
+  }
+
+  @Test
+  public void test_unfound_resource() {
+    CustomPythonRuleRepository repository =  new CustomPythonRuleRepository();
+    assertThatThrownBy(() -> repository.loadResource("/unknown"))
+      .isInstanceOf(IllegalStateException.class)
+      .hasMessage("Resource not found: /unknown");
+  }
+
+  @Test
+  public void test_read_exception_resource() throws IOException {
+    URL urlMock = Mockito.mock(URL.class);
+    Mockito.when(urlMock.openStream()).thenThrow(IOException.class);
+    Mockito.when(urlMock.toString()).thenReturn("MyURL");
+    assertThatThrownBy(() -> CustomPythonRuleRepository.readResource(urlMock))
+      .isInstanceOf(IllegalStateException.class)
+      .hasMessage("Failed to read resource: MyURL");
+  }
+}
