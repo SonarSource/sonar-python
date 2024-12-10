c6e4d89b34b0b023952d5925d73d78b170b1280b
Sebastian Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2024-12-10 17:01:04 +0100

    SONARPY-2431 parallelize the test execution
    test

--- its/ruling/src/test/java/org/sonar/python/it/PythonRulingTest.java
+++ its/ruling/src/test/java/org/sonar/python/it/PythonRulingTest.java
@@ -17,6 +17,9 @@
 package org.sonar.python.it;
 
 import com.sonar.orchestrator.build.SonarScanner;
+import com.sonar.orchestrator.version.Version;
+import com.sonar.orchestrator.build.SonarScannerInstaller;
+import com.sonar.orchestrator.config.Configuration;
 import com.sonar.orchestrator.junit5.OrchestratorExtension;
 import com.sonar.orchestrator.locator.FileLocation;
 import java.io.File;
@@ -26,6 +29,8 @@ import java.util.Collections;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.sonarsource.analyzer.commons.ProfileGenerator;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -33,10 +38,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonar.python.it.RulingHelper.getOrchestrator;
 
 // Ruling test for bug rules, to ensure they are properly tested without slowing down the CI
+@Execution(ExecutionMode.CONCURRENT)
 class PythonRulingTest {
+
 
   @RegisterExtension
   public static final OrchestratorExtension ORCHESTRATOR = getOrchestrator();
+  public static final Configuration CONFIGURATION = ORCHESTRATOR.getConfiguration();
 
   private static final String PROFILE_NAME = "rules";
 
@@ -50,6 +58,11 @@ class PythonRulingTest {
     ORCHESTRATOR.getServer().restoreProfile(FileLocation.of(profileFile));
     File iPythonProfileFile = ProfileGenerator.generateProfile(serverUrl, "ipynb", "ipython", parameters, Collections.emptySet());
     ORCHESTRATOR.getServer().restoreProfile(FileLocation.of(iPythonProfileFile));
+  }
+
+  @BeforeAll
+  static void install_sonar_scanner() {
+    new SonarScannerInstaller(CONFIGURATION.locators()).install(Version.create(SonarScanner.DEFAULT_SCANNER_VERSION), CONFIGURATION.fileSystem().workspace());
   }
 
   @Test
