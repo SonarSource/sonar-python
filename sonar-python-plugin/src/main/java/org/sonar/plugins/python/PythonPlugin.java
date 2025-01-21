a94a7248f8e7de77c9dd577c7a015e92061653d5
Ghislain Piot <ghislain.piot@sonarsource.com>
2025-01-21 11:24:32 +0100

    SONARPY-2506 Move extension registration to PythonExtensions

--- sonar-python-plugin/src/main/java/org/sonar/plugins/python/PythonPlugin.java
+++ sonar-python-plugin/src/main/java/org/sonar/plugins/python/PythonPlugin.java
@@ -17,81 +17,13 @@
 package org.sonar.plugins.python;
 
 import org.sonar.api.Plugin;
-import org.sonar.api.SonarProduct;
-import org.sonar.api.SonarRuntime;
-import org.sonar.api.config.PropertyDefinition;
-import org.sonar.api.resources.Qualifiers;
-import org.sonar.plugins.python.warnings.AnalysisWarningsWrapper;
-
-import static org.sonar.plugins.python.api.PythonVersionUtils.PYTHON_VERSION_KEY;
 
 public class PythonPlugin implements Plugin {
 
 
   @Override
   public void define(Context context) {
-
-    context.addExtensions(
-      PropertyDefinition.builder(PythonExtensions.PYTHON_FILE_SUFFIXES_KEY)
-        .index(10)
-        .name("File Suffixes")
-        .description("List of suffixes of Python files to analyze.")
-        .multiValues(true)
-        .category(PythonExtensions.PYTHON_CATEGORY)
-        .subCategory(PythonExtensions.GENERAL)
-        .onQualifiers(Qualifiers.PROJECT)
-        .defaultValue("py")
-        .build(),
-
-      PropertyDefinition.builder(PythonExtensions.IPYNB_FILE_SUFFIXES_KEY)
-        .index(11)
-        .name("IPython File Suffixes")
-        .description("List of suffixes of IPython Notebooks files to analyze.")
-        .multiValues(true)
-        .category(PythonExtensions.PYTHON_CATEGORY)
-        .subCategory(PythonExtensions.GENERAL)
-        .onQualifiers(Qualifiers.PROJECT)
-        .defaultValue("ipynb")
-        .build(),
-
-      PropertyDefinition.builder(PYTHON_VERSION_KEY)
-      .index(12)
-      .name("Python versions")
-      .description("Comma-separated list of Python versions this project is compatible with.")
-      .multiValues(true)
-        .category(PythonExtensions.PYTHON_CATEGORY)
-        .subCategory(PythonExtensions.GENERAL)
-      .onQualifiers(Qualifiers.PROJECT)
-      .build(),
-
-      Python.class,
-
-      PythonProfile.class,
-
-      PythonSensor.class,
-      PythonRuleRepository.class,
-      AnalysisWarningsWrapper.class,
-
-      IPynb.class,
-      IPynbProfile.class,
-      IPynbSensor.class,
-      IPynbRuleRepository.class);
-
-    SonarRuntime sonarRuntime = context.getRuntime();
-    if (sonarRuntime.getProduct() != SonarProduct.SONARLINT) {
-      PythonExtensions.addCoberturaExtensions(context);
-      PythonExtensions.addXUnitExtensions(context);
-      PythonExtensions.addPylintExtensions(context);
-      PythonExtensions.addBanditExtensions(context);
-      PythonExtensions.addFlake8Extensions(context);
-      PythonExtensions.addMypyExtensions(context);
-      PythonExtensions.addRuffExtensions(context);
-    }
-
-    if (sonarRuntime.getProduct() == SonarProduct.SONARLINT) {
-      PythonExtensions.SonarLintPluginAPIManager sonarLintPluginAPIManager = new PythonExtensions.SonarLintPluginAPIManager();
-      sonarLintPluginAPIManager.addSonarlintPythonIndexer(context, new PythonExtensions.SonarLintPluginAPIVersion());
-    }
+    PythonExtensions.addCommonExtensions(context);
   }
 
 }
