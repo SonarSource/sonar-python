e3a151e3f7cb6eb8fdabd547eacd45f45b1a12ec
Sebastian Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2025-01-24 14:24:49 +0100

    SONARPY-2539 Fix license check in the enterprise-plugin (#11)
    GitOrigin-RevId: 82f55d872594eaa348135faed8d0ab3adeeed2c3

--- check-license-compliance.sh
+++ check-license-compliance.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 mvn org.codehaus.mojo:license-maven-plugin:aggregate-add-third-party \
   -Dlicense.overrideUrl=file://$(pwd)/override-dep-licenses.properties \
+  -P-private \
   "$@"
