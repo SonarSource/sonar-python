4bd2d74f3e2b2934ae81146ddef1526fdec20f9d
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2025-04-17 09:30:39 +0200

    Fix quality gate issues
    GitOrigin-RevId: 47261e8c380103c4c7e98a38d346624375369995

--- python-commons/src/main/java/org/sonar/plugins/python/PythonSensor.java
+++ python-commons/src/main/java/org/sonar/plugins/python/PythonSensor.java
@@ -21,9 +21,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
-import javax.annotation.Nullable;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.SonarProduct;
