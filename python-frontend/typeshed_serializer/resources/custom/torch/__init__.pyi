a81cfec726749d14c705d4bbf1b301665e25f72e
David Kunzmann <david.kunzmann@sonarsource.com>
2025-08-29 10:35:06 +0200

    SONARPY-3286: S7675 Fix implementation to use types (#475)
    GitOrigin-RevId: 333d946d23adffc51d17118d73aa858b3f2edd27

--- python-frontend/typeshed_serializer/resources/custom/torch/__init__.pyi
+++ python-frontend/typeshed_serializer/resources/custom/torch/__init__.pyi
@@ -1,1 +1,115 @@
+from SonarPythonAnalyzerFakeStub import CustomStubBase
 import torch.nn as nn
+
+from typing import Any, Optional, Sequence, Union
+
+class dtype(CustomStubBase): ...
+class layout(CustomStubBase): ...
+class device(CustomStubBase):
+    def __init__(
+        self,
+        type: str,
+        index: Optional[int] = ...,
+    ) -> None: ...
+
+class Tensor(CustomStubBase):
+    def new_tensor(
+        self,
+        data: Any,
+        dtype: Optional[dtype] = ...,
+        device: Optional[Union[str, device]] = ...,
+        requires_grad: Optional[bool] = ...,
+        pin_memory: bool = ...,
+    ) -> "Tensor": ...
+# --- Common Tensor Creation Functions ---
+
+def tensor(
+    data: Any,
+    dtype: Optional[dtype] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+
+def empty(
+    size: Union[int, Sequence[int]],
+    *,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+def zeros(
+    size: Union[int, Sequence[int]],
+    *,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+def ones(
+    size: Union[int, Sequence[int]],
+    *,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+def rand(
+    *size: int,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+def randn(
+    *size: int,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+def arange(
+    start: Union[int, float],
+    end: Optional[Union[int, float]] = ...,
+    step: Union[int, float] = ...,
+    *,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+def full(
+    size: Union[int, Sequence[int]],
+    fill_value: Union[int, float],
+    *,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
+
+def eye(
+    n: int,
+    m: Optional[int] = ...,
+    *,
+    dtype: Optional[dtype] = ...,
+    layout: Optional[layout] = ...,
+    device: Optional[Union[str, device]] = ...,
+    requires_grad: bool = ...,
+    pin_memory: bool = ...,
+) -> Tensor: ...
