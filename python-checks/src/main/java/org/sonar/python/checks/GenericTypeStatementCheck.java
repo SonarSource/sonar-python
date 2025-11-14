82324bd30318324e67142cc70999be85d58e4dc2
Samir M <samir.marini@sonarsource.com>
2025-11-14 14:20:42 +0100

    SC-35440 scripted updated of SonarSource SA to SonarSource Sàrl

--- python-checks/src/main/java/org/sonar/python/checks/GenericTypeStatementCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/GenericTypeStatementCheck.java
@@ -4,7 +4,7 @@
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,7 @@
  */
 /*
  * SonarQube Python Plugin
- * Copyright (C) 2011-2023 SonarSource SA
+ * Copyright (C) 2011-2023 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
