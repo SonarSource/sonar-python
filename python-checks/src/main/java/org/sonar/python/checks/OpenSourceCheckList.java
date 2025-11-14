7a9f518ae0e83b5c3d9dc26a401c035a31c7a6df
David Kunzmann <david.kunzmann@sonarsource.com>
2025-11-14 17:28:58 +0100

    Revert "SONARPY-3394 S7932 Return, break, or continue statements should not exit finally blocks (#601)" (#633)
    GitOrigin-RevId: 9b6b93ca58d7776e58d7311ba9e73eb0410ae852

--- python-checks/src/main/java/org/sonar/python/checks/OpenSourceCheckList.java
+++ python-checks/src/main/java/org/sonar/python/checks/OpenSourceCheckList.java
@@ -224,7 +224,6 @@ public class OpenSourceCheckList {
       FileComplexityCheck.class,
       FilePermissionsCheck.class,
       FileHeaderCopyrightCheck.class,
-      FinallyBlockExitStatementCheck.class,
       FixmeCommentCheck.class,
       FlaskHardCodedJWTSecretKeyCheck.class,
       FlaskHardCodedSecretKeyCheck.class,
