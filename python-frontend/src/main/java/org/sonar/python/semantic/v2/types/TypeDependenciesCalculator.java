8842f601e2101ff647d3c0a0b55d0f170f15b861
Sebastian Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2026-01-19 14:27:57 +0100

    SONARPY-3638 Move all cases into type dependencies calculator (#762)
    GitOrigin-RevId: 74f6680c88f3b792dcd832b9bdafc4cc79d428fa

--- python-frontend/src/main/java/org/sonar/python/semantic/v2/types/TypeDependenciesCalculator.java
+++ python-frontend/src/main/java/org/sonar/python/semantic/v2/types/TypeDependenciesCalculator.java
@@ -16,28 +16,61 @@
  */
 package org.sonar.python.semantic.v2.types;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.sonar.plugins.python.api.tree.AssignmentExpression;
 import org.sonar.plugins.python.api.tree.AwaitExpression;
 import org.sonar.plugins.python.api.tree.BinaryExpression;
+import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.ComprehensionExpression;
+import org.sonar.plugins.python.api.tree.ConditionalExpression;
+import org.sonar.plugins.python.api.tree.DictCompExpression;
 import org.sonar.plugins.python.api.tree.Expression;
+import org.sonar.plugins.python.api.tree.ExpressionList;
+import org.sonar.plugins.python.api.tree.ParenthesizedExpression;
 import org.sonar.plugins.python.api.tree.QualifiedExpression;
-import org.sonar.python.types.HasTypeDependencies;
+import org.sonar.plugins.python.api.tree.SliceExpression;
+import org.sonar.plugins.python.api.tree.SubscriptionExpression;
+import org.sonar.plugins.python.api.tree.Tuple;
+import org.sonar.plugins.python.api.tree.UnaryExpression;
+import org.sonar.plugins.python.api.tree.UnpackingExpression;
+import org.sonar.plugins.python.api.tree.YieldExpression;
 
 public class TypeDependenciesCalculator {
+
   public List<Expression> getTypeDependencies(Expression expression) {
-    if (expression instanceof BinaryExpression binaryExpression) {
-      return List.of(binaryExpression.leftOperand(), binaryExpression.rightOperand());
-    } else if (expression instanceof QualifiedExpression qualifiedExpression) {
-      return List.of(qualifiedExpression.qualifier());
-    } else if (expression instanceof AwaitExpression awaitExpression) {
-      return List.of(awaitExpression.expression());
-    } else if (expression instanceof HasTypeDependencies hasTypeDependencies) {
-      // SONARPY-2417 Once we get rid of v1 type inference -
-      // we won’t need a HasTypeDependencies interface implemented by a tree model classes.
-      // The implementation of the logic is still needed for v2 type inference
-      // but to keep tree model decoupled from the type inference and type model -
-      // the logic should be moved here as it is done for BinaryExpression
-      return hasTypeDependencies.typeDependencies();
+    if (expression instanceof BinaryExpression e) {
+      return List.of(e.leftOperand(), e.rightOperand());
+    } else if (expression instanceof QualifiedExpression e) {
+      return List.of(e.qualifier());
+    } else if (expression instanceof AwaitExpression e) {
+      return List.of(e.expression());
+    } else if (expression instanceof AssignmentExpression e) {
+      return List.of(e.expression());
+    } else if (expression instanceof CallExpression e) {
+      return List.of(e.callee());
+    } else if (expression instanceof ConditionalExpression e) {
+      return List.of(e.trueExpression(), e.falseExpression());
+    } else if (expression instanceof ParenthesizedExpression e) {
+      return List.of(e.expression());
+    } else if (expression instanceof SliceExpression e) {
+      return List.of(e.object());
+    } else if (expression instanceof SubscriptionExpression e) {
+      return List.of(e.object());
+    } else if (expression instanceof UnaryExpression e) {
+      return List.of(e.expression());
+    } else if (expression instanceof UnpackingExpression e) {
+      return List.of(e.expression());
+    } else if (expression instanceof ExpressionList e) {
+      return new ArrayList<>(e.expressions());
+    } else if (expression instanceof ComprehensionExpression e) {
+      return List.of(e.resultExpression());
+    } else if (expression instanceof DictCompExpression e) {
+      return List.of(e.keyExpression(), e.valueExpression());
+    } else if (expression instanceof Tuple e) {
+      return new ArrayList<>(e.elements());
+    } else if (expression instanceof YieldExpression e) {
+      return new ArrayList<>(e.expressions());
     }
     return List.of();
   }
--- python-frontend/src/test/java/org/sonar/python/semantic/v2/types/TypeDependenciesCalculatorTest.java
+++ python-frontend/src/test/java/org/sonar/python/semantic/v2/types/TypeDependenciesCalculatorTest.java
@@ -17,44 +17,160 @@
 package org.sonar.python.semantic.v2.types;
 
 import org.junit.jupiter.api.Test;
+import org.sonar.plugins.python.api.tree.AssignmentExpression;
 import org.sonar.plugins.python.api.tree.AwaitExpression;
 import org.sonar.plugins.python.api.tree.BinaryExpression;
 import org.sonar.plugins.python.api.tree.CallExpression;
+import org.sonar.plugins.python.api.tree.ComprehensionExpression;
+import org.sonar.plugins.python.api.tree.ConditionalExpression;
+import org.sonar.plugins.python.api.tree.DictCompExpression;
+import org.sonar.plugins.python.api.tree.ListLiteral;
+import org.sonar.plugins.python.api.tree.ParenthesizedExpression;
 import org.sonar.plugins.python.api.tree.QualifiedExpression;
+import org.sonar.plugins.python.api.tree.SliceExpression;
+import org.sonar.plugins.python.api.tree.SubscriptionExpression;
+import org.sonar.plugins.python.api.tree.Tuple;
+import org.sonar.plugins.python.api.tree.UnaryExpression;
+import org.sonar.plugins.python.api.tree.UnpackingExpression;
+import org.sonar.plugins.python.api.tree.YieldStatement;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonar.python.PythonTestUtils.lastExpression;
+import static org.sonar.python.PythonTestUtils.lastStatement;
 
 class TypeDependenciesCalculatorTest {
 
+  private final TypeDependenciesCalculator calculator = new TypeDependenciesCalculator();
+
   @Test
-  void getTypeDependenciesTest() {
-    var calculator = new TypeDependenciesCalculator();
-    var binaryExpression = (BinaryExpression) lastExpression("a or b");
-    var dependencies = calculator.getTypeDependencies(binaryExpression);
-    assertThat(dependencies).containsExactly(binaryExpression.leftOperand(), binaryExpression.rightOperand());
+  void binaryExpression() {
+    var expr = (BinaryExpression) lastExpression("a or b");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.leftOperand(), expr.rightOperand());
 
-
-    binaryExpression = (BinaryExpression) lastExpression("a + b");
-    dependencies = calculator.getTypeDependencies(binaryExpression);
-    assertThat(dependencies).containsExactly(binaryExpression.leftOperand(), binaryExpression.rightOperand());
-
-    var callExpression = (CallExpression) lastExpression("a()");
-    dependencies = calculator.getTypeDependencies(callExpression);
-    assertThat(dependencies).containsExactly(callExpression.callee());
-
-    var expression = lastExpression("a");
-    dependencies = calculator.getTypeDependencies(expression);
-    assertThat(dependencies).isEmpty();
-
-    var qualifiedExpression = (QualifiedExpression) lastExpression("a.b");
-    dependencies = calculator.getTypeDependencies(qualifiedExpression);
-    assertThat(dependencies).containsExactly(qualifiedExpression.qualifier());
-
-    var awaitExpression = (AwaitExpression) lastExpression("await a");
-    dependencies = calculator.getTypeDependencies(awaitExpression);
-    assertThat(dependencies).containsExactly(awaitExpression.expression());
+    expr = (BinaryExpression) lastExpression("a + b");
+    dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.leftOperand(), expr.rightOperand());
   }
 
+  @Test
+  void qualifiedExpression() {
+    var expr = (QualifiedExpression) lastExpression("a.b");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.qualifier());
+  }
+
+  @Test
+  void awaitExpression() {
+    var expr = (AwaitExpression) lastExpression("await a");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.expression());
+  }
+
+  @Test
+  void assignmentExpression() {
+    var paren = (ParenthesizedExpression) lastExpression("(x := 1)");
+    var expr = (AssignmentExpression) paren.expression();
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.expression());
+  }
+
+  @Test
+  void callExpression() {
+    var expr = (CallExpression) lastExpression("a()");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.callee());
+  }
+
+  @Test
+  void conditionalExpression() {
+    var expr = (ConditionalExpression) lastExpression("a if cond else b");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.trueExpression(), expr.falseExpression());
+  }
+
+  @Test
+  void parenthesizedExpression() {
+    var expr = (ParenthesizedExpression) lastExpression("(a)");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.expression());
+  }
+
+  @Test
+  void sliceExpression() {
+    var expr = (SliceExpression) lastExpression("a[1:2]");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.object());
+  }
+
+  @Test
+  void subscriptionExpression() {
+    var expr = (SubscriptionExpression) lastExpression("a[0]");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.object());
+  }
+
+  @Test
+  void unaryExpression() {
+    var expr = (UnaryExpression) lastExpression("-a");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.expression());
+
+    expr = (UnaryExpression) lastExpression("not a");
+    dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.expression());
+  }
+
+  @Test
+  void expressionList() {
+    var subscription = (SubscriptionExpression) lastExpression("a[1, 2]");
+    var exprList = subscription.subscripts();
+    var dependencies = calculator.getTypeDependencies(exprList);
+    assertThat(dependencies).containsExactlyElementsOf(exprList.expressions());
+  }
+
+  @Test
+  void comprehensionExpression() {
+    var expr = (ComprehensionExpression) lastExpression("[x for x in a]");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.resultExpression());
+  }
+
+  @Test
+  void dictCompExpression() {
+    var expr = (DictCompExpression) lastExpression("{k: v for k, v in items}");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.keyExpression(), expr.valueExpression());
+  }
+
+  @Test
+  void tuple() {
+    var expr = (Tuple) lastExpression("(a, b)");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactlyElementsOf(expr.elements());
+  }
+
+  @Test
+  void unpackingExpression() {
+    var list = (ListLiteral) lastExpression("[*a]");
+    var expr = (UnpackingExpression) list.elements().expressions().get(0);
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactly(expr.expression());
+  }
+
+  @Test
+  void yieldExpression() {
+    var yieldStmt = (YieldStatement) lastStatement("yield a");
+    var expr = yieldStmt.yieldExpression();
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).containsExactlyElementsOf(expr.expressions());
+  }
+
+  @Test
+  void unknownExpression_returnsEmpty() {
+    var expr = lastExpression("a");
+    var dependencies = calculator.getTypeDependencies(expr);
+    assertThat(dependencies).isEmpty();
+  }
 
 }
