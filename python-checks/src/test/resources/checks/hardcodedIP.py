e1d5a775e4c9e81523cea3f88bf61ad719a67087
Erwan Le Forestier <erwan.leforestier@sonarsource.com>
2026-08-18 16:48:13 +0200

    SONARPY-4539 Ignore version IP values

--- python-checks/src/test/resources/checks/hardcodedIP.py
+++ python-checks/src/test/resources/checks/hardcodedIP.py
@@ -22,6 +22,13 @@ notAnIp5 = "0.256.0.0"
 fileName = "v0.0.1.200__do_something.sql" # Compliant - suffixed and prefixed
 version = "1.0.0.0-1" # Compliant - suffixed
 
+__version__ = "26.8.0.1" # Compliant
+__version__ = ("26.8.0.1") # Compliant
+__version__: str = "26.8.0.1" # Compliant
+version = "26.8.0.1" # Noncompliant
+VERSION = "26.8.0.1" # Noncompliant
+__version__ = version = "26.8.0.1" # Noncompliant
+
 "1080:0:0:0:8:800:200C:417A" # Noncompliant {{Make sure using this hardcoded IP address "1080:0:0:0:8:800:200C:417A" is safe here.}}
 "[1080::8:800:200C:417A]" # Noncompliant
 "::800:200C:417A" # Noncompliant
