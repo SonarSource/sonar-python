51e5557a3370e68ce4be0a3958bc214bbed60921
Erwan Le Forestier <erwan.leforestier@sonarsource.com>
2026-08-18 17:04:23 +0200

    SONARPY-4539 Reuse parenthesis normalization

--- python-checks/src/main/java/org/sonar/python/checks/HardcodedIPCheck.java
+++ python-checks/src/main/java/org/sonar/python/checks/HardcodedIPCheck.java
@@ -132,10 +132,7 @@ public class HardcodedIPCheck extends PythonSubscriptionCheck {
    * @return whether the expression is named {@code __version__}
    */
   private static boolean isVersionName(Expression expression) {
-    while (expression instanceof ParenthesizedExpression parenthesizedExpression) {
-      expression = parenthesizedExpression.expression();
-    }
-    return expression instanceof Name name && "__version__".equals(name.name());
+    return Expressions.removeParentheses(expression) instanceof Name name && "__version__".equals(name.name());
   }
 
   private static boolean isMultilineString(StringLiteral pyStringLiteralTree) {
